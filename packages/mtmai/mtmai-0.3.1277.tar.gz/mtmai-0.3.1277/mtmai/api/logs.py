49c69b3e77d984a0ff0e727d9ed2dbc6cb12d87a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-29 15:03:30 +0000

    Add package mtmai/0.3.1277/mtmai-0.3.1277.tar.gz

---
+++ packages/mtmai/mtmai-0.3.1277.tar.gz/mtmai-0.3.1277/mtmai/api/logs.py
@@ -0,0 +1,19 @@
+from fastapi import APIRouter, Depends, HTTPException
+from sqlmodel import Session
+
+from mtmai.core.db import get_session
+from mtmai.core.logging import get_logger
+from mtmai.crud.curd_logs import list_log_items
+from mtmai.models.logitems import LogItemListRequest, LogItemListResponse
+
+router = APIRouter()
+logger = get_logger()
+
+
+@router.post("/logs", response_model=LogItemListResponse)
+async def log_list(*, db: Session = Depends(get_session), req: LogItemListRequest):
+    resource_id = req.resource_id
+    if not resource_id:
+        raise HTTPException(status_code=400, detail="resource_id is required")
+    logitems = await list_log_items(req.app, req.resource_id)
+    return logitems
