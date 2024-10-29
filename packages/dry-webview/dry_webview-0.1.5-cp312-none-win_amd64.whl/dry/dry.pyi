1fa2aef2d3395b5ba1fc5bfc3aa24cbe5069d5e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-29 14:04:02 +0000

    Add package dry-webview/0.1.5/dry_webview-0.1.5-cp312-none-win_amd64.whl

---
+++ packages/dry-webview/dry_webview-0.1.5-cp312-none-win_amd64.whl/dry/dry.pyi
@@ -0,0 +1,12 @@
+from typing import Any, Callable, Mapping
+
+def run(
+    title: str,
+    min_size: tuple[int, int],
+    size: tuple[int, int],
+    icon_path: str | None = None,
+    html: str | None = None,
+    url: str | None = None,
+    api: Mapping[str, Callable[..., Any]] | None = None,
+    dev_tools: bool | None = None,
+) -> None: ...
