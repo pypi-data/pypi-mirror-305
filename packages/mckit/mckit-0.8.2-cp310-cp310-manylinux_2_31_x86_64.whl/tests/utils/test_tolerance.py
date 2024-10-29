24d6c3d0d47feb1445e4b7aa4ec635a71370ee06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-29 03:09:12 +0000

    Add package mckit/0.8.2/mckit-0.8.2-cp310-cp310-manylinux_2_31_x86_64.whl

---
+++ packages/mckit/mckit-0.8.2-cp310-cp310-manylinux_2_31_x86_64.whl/tests/utils/test_tolerance.py
@@ -0,0 +1,38 @@
+from __future__ import annotations
+
+import numpy as np
+import pytest
+
+from mckit.utils.tolerance import DEFAULT_TOLERANCE_ESTIMATOR
+
+SOME_ARRAY = np.arange(3, dtype=float)
+
+
+@pytest.mark.parametrize(
+    "a, b, expected, msg",
+    [
+        (1.0, 1.0 + 1.0e-12, False, "#1 1.0e-12 is to be distinguished"),
+        (1.0, 1.0 + 1.0e-13, True, "#2 1.0e-13 is not to be distinguished"),
+        (
+            SOME_ARRAY,
+            SOME_ARRAY + 1.0e-11,
+            False,
+            "#3 on ndarray 1.0e-11 is to be distinguished",
+        ),
+        (
+            SOME_ARRAY,
+            SOME_ARRAY + 1.0e-13,
+            True,
+            "#4 on ndarray 1.0e-12 is not to be distinguished",
+        ),
+        ((1.0, SOME_ARRAY), (1.0, SOME_ARRAY), True, "#5 iterables"),
+        (1, 1, True, "#6 Integers"),
+    ],
+)
+def test_tolerance_estimator(a, b, expected, msg):
+    actual = DEFAULT_TOLERANCE_ESTIMATOR(a, b)
+    assert actual == expected, msg
+
+
+if __name__ == "__main__":
+    pytest.main()
