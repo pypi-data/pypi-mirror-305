84f8fceacd7301bf9b79224af296100d4ebb5d30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-29 16:05:03 +0000

    Add package osrs/0.0.12/osrs-0.0.12.tar.gz

---
+++ packages/osrs/osrs-0.0.12.tar.gz/osrs-0.0.12/osrs/async_api/wiki/__init__.py
@@ -0,0 +1,9 @@
+from osrs.async_api.wiki.prices import (
+    AveragePrices,
+    ItemMapping,
+    LatestPrices,
+    TimeSeries,
+    WikiPrices,
+)
+
+__all__ = ["WikiPrices", "AveragePrices", "LatestPrices", "TimeSeries", "ItemMapping"]
