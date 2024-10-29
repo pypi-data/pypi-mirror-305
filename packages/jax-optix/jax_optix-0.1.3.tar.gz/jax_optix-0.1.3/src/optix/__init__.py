604e7f7e6ed0a0f048e43e3810e4068e56e758ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-29 03:09:12 +0000

    Add package jax-optix/0.1.3/jax_optix-0.1.3.tar.gz

---
+++ packages/jax-optix/jax_optix-0.1.3.tar.gz/jax_optix-0.1.3/src/optix/__init__.py
@@ -0,0 +1,153 @@
+from __future__ import annotations
+
+from collections.abc import Callable
+from dataclasses import dataclass
+from typing import Generic, Protocol, TypeVar, runtime_checkable
+
+import equinox as eqx
+
+
+__all__ = ["Lens", "FreeLens", "BoundLens", "Focused", "focus"]
+
+
+T = TypeVar("T")
+S = TypeVar("S")
+U = TypeVar("U")
+
+
+@runtime_checkable
+class Lens(Protocol[T, S]):
+    where: Callable[[T], S]
+
+    def get(self) -> S:
+        ...
+    
+    def set(self, val: S) -> T:
+        ...
+    
+    def apply(self, update: Callable[[S], S]) -> T:
+        ...
+
+
+class FreeLens(eqx.Module, Generic[T, S]):
+    """ A lens that focuses on a value in an object.
+
+    Args:
+        where: A function that retrieves the focused value from the object.
+    """
+
+    where: Callable[[T], S]
+
+    def get(self, obj: T) -> S:
+        """ Get the value of the focus in the object.
+
+        Args:
+            obj: The object to query.
+        
+        Returns:
+            The focused value.
+        """
+            
+        return self.where(obj)
+    
+    def set(self, obj: T, val: S) -> T:
+        """ Set the value of the focus in the object.
+
+        Args:
+            obj: The object to modify.
+            val: The new value to set.
+
+        Returns:
+            A modified copy of the object.
+        """
+        return eqx.tree_at(self.where, obj, val)
+    
+    def apply(self, obj: T, update: Callable[[S], S]) -> T:
+        """ Apply a function to the focused value in the object.
+
+        Args:
+            update: The object to modify.
+            new: The function to apply to the focused value.
+            
+        Returns:
+            A modified copy of the object.
+        """
+        return eqx.tree_at(self.where, obj, replace=update(self.get(obj)))
+    
+    def bind(self, obj: T) -> BoundLens[T, S]:
+        """ Bind the lens to an object.
+
+        Args:
+            obj: The object to bind to.
+
+        Returns:
+            A bound lens.
+        """
+        return BoundLens(obj, self.where)
+
+
+class BoundLens(eqx.Module, Generic[T, S]):
+    """ A lens that focuses on a value in a bound object.
+
+    Args:
+        obj: The object to focus on. 
+        where: A function that retrieves the focused value from the object.
+    """
+
+    obj: T
+    where: Callable[[T], S]
+
+    def get(self) -> S:
+        """ Get the value of the focus in the object.
+
+        Returns:
+            The focused value.
+        """
+        return self.where(self.obj)
+    
+    def set(self, val: S) -> T:
+        """ Set the value of the focus in the object.
+
+        Args:
+            val: The new value to set.
+
+        Returns:
+            A modified copy of the object.
+        """
+        return eqx.tree_at(self.where, self.obj, replace=val)
+    
+    def apply(self, update: Callable[[S], S]) -> T:
+        """ Apply a function to the focused value in the object.
+
+        Args:
+            update: The function to apply to the focused value.
+
+        Returns:
+            A modified copy of the object.
+        """
+        return eqx.tree_at(self.where, self.obj, replace=update(self.get()))
+    
+
+class Focused(eqx.Module, Generic[T]):
+    """ An object that can be focused on.
+
+    Args:
+        obj: The object to focus on.
+    """
+    obj: T
+
+    def at(self, where: Callable[[T], S]) -> BoundLens[T, S]:
+        """ Focus on a value in the object.
+
+        Args:
+            where: A function that retrieves the focused value from the object.
+
+        Returns:
+            A bound lens.
+        """
+        return BoundLens(self.obj, where)
+
+
+def focus(obj: T) -> Focused[T]:
+    """ Focus on an object. """
+    return Focused(obj)
---
+++ packages/jax-optix/jax_optix-0.1.3.tar.gz/jax_optix-0.1.3/tests/test_optix.py
@@ -0,0 +1,44 @@
+import re
+
+import pytest
+
+import equinox as eqx
+import jax
+import jax.numpy as jnp
+
+from optix import focus
+
+class Foo(eqx.Module):
+    a: jax.Array
+    b: str = eqx.field(static=True)
+
+
+class Bar(eqx.Module):
+    x: jax.Array
+    foo: Foo
+
+def test_correct_hlo():
+
+    @jax.jit
+    def test_verbose(bar: Bar):
+        return bar.x, jnp.cos(bar.foo.a)
+
+
+    @jax.jit
+    def test_lens(bar: Bar):
+        return focus(bar).at(lambda x: x.foo.a).apply(jnp.cos)
+    
+    bar = Bar(x=jnp.array([1.0, 2.0, 3.0]), foo=Foo(a=jnp.array([1.0, 2.0, 3.0]), b='hello'))
+
+    hlo_verbose = test_verbose.lower(bar).compile().as_text()
+    hlo_lens = test_lens.lower(bar).compile().as_text()
+
+    # remove module name
+    hlo_verbose = re.sub(r"HloModule .*?, ", "", hlo_verbose)
+    hlo_lens = re.sub(r"HloModule .*?, ", "", hlo_lens)
+    
+    # remove metadata
+    hlo_verbose = re.sub(r"metadata=\{.*\}", "", hlo_verbose)
+    hlo_lens = re.sub(r"metadata=\{.*\}", "", hlo_lens)
+
+    assert hlo_verbose == hlo_lens
