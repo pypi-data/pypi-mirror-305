a996bd8ca8013a4258a3a1b517741c4e0ca93435
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-29 17:04:18 +0000

    Add package agi-med-utils/0.3.0/agi_med_utils-0.3.0-py3-none-any.whl

---
+++ packages/agi-med-utils/agi_med_utils-0.3.0-py3-none-any.whl/agi_med_utils/dig_ass/xml_parser.py
@@ -0,0 +1,15 @@
+import re
+
+
+def make_xml(body: str, tag: str) -> str:
+    return f"<{tag}>{body}</{tag}>"
+
+
+def remove_xml(text: str) -> str:
+    re_xml = re.compile(r"<(.*?)>")
+    return re.sub(re_xml, "", text).replace("  ", " ").strip()
+
+
+def get_tag_list(response: str, tag: str):
+    tag_content = re.findall(f"<{tag}>(.*?)</{tag}>", response, re.DOTALL)
+    return [content.lower().strip() for content in tag_content]
