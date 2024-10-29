56e42f3c60d0baa36ae584163754aca5513a04cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-29 15:04:36 +0000

    Add package larch6/6.0.30/larch6-6.0.30-py3-none-any.whl

---
+++ packages/larch6/larch6-6.0.30-py3-none-any.whl/larch/__init__.py
@@ -0,0 +1,55 @@
+from __future__ import annotations
+
+import os
+
+from xmle import NumberedCaption, Reporter
+from xmle import load_metadata as read_metadata
+
+from . import examples
+from ._version import version as _install_version
+from .dataset import DataArray, Dataset, DataTree
+from .examples import example, example_file
+from .model import mixtures
+from .model.basemodel import BaseModel
+from .model.jaxmodel import Model
+from .model.latent_class import LatentClass, MixedLatentClass
+from .model.model_group import ModelGroup
+from .model.param_core import ParameterBucket
+from .model.saving import load_model
+from .model.tree import NestingTree
+from .shorts import PX, P, X
+from .util import figures
+
+# Get decorated version when in development
+try:
+    top_package = __import__(__name__.split(".")[0])
+    import setuptools_scm
+
+    __version__ = setuptools_scm.get_version(os.path.dirname(top_package.__path__[0]))
+except Exception:
+    __version__ = _install_version
+
+__all__ = [
+    "NumberedCaption",
+    "Reporter",
+    "read_metadata",
+    "examples",
+    "example",
+    "example_file",
+    "DataArray",
+    "Dataset",
+    "DataTree",
+    "mixtures",
+    "BaseModel",
+    "Model",
+    "ModelGroup",
+    "LatentClass",
+    "MixedLatentClass",
+    "ParameterBucket",
+    "load_model",
+    "NestingTree",
+    "PX",
+    "P",
+    "X",
+    "figures",
+]
---
+++ packages/larch6/larch6-6.0.30-py3-none-any.whl/sharrow/example_data.py
@@ -0,0 +1,107 @@
+import os
+
+import numpy as np
+import pandas as pd
+
+
+def get_skims_filename() -> str:
+    """Return the path to the example skims file."""
+    return os.path.join(os.path.dirname(__file__), "example_data", "skims.omx")
+
+
+def get_skims():
+    import openmatrix
+
+    from . import dataset
+
+    zfilename = os.path.join(os.path.dirname(__file__), "example_data", "skims.zarr")
+    if os.path.exists(zfilename):
+        skims = dataset.from_zarr(zfilename, consolidated=False)
+    else:
+        filename = os.path.join(os.path.dirname(__file__), "example_data", "skims.omx")
+        with openmatrix.open_file(filename) as f:
+            skims = dataset.from_omx_3d(
+                f,
+                index_names=("otaz", "dtaz", "time_period"),
+                indexes=None,
+                time_periods=["EA", "AM", "MD", "PM", "EV"],
+                time_period_sep="__",
+                max_float_precision=32,
+            ).compute()
+        skims.to_zarr(zfilename)
+    return skims
+
+
+def get_households():
+    filename = os.path.join(
+        os.path.dirname(__file__), "example_data", "households.csv.gz"
+    )
+    return pd.read_csv(filename, index_col="HHID")
+
+
+def get_persons():
+    filename = os.path.join(os.path.dirname(__file__), "example_data", "persons.csv.gz")
+    return pd.read_csv(filename, index_col="PERID")
+
+
+def get_land_use():
+    filename = os.path.join(
+        os.path.dirname(__file__), "example_data", "land_use.csv.gz"
+    )
+    return pd.read_csv(filename, index_col="TAZ")
+
+
+def get_maz_to_taz():
+    filename = os.path.join(os.path.dirname(__file__), "example_data", "maz_to_taz.csv")
+    return pd.read_csv(filename, index_col="MAZ")
+
+
+def get_maz_to_maz_walk():
+    filename = os.path.join(
+        os.path.dirname(__file__), "example_data", "maz_to_maz_walk.csv"
+    )
+    return pd.read_csv(filename)
+
+
+def get_data():
+    result = {
+        "hhs": get_households(),
+        "persons": get_persons(),
+        "land_use": get_land_use(),
+        "skims": get_skims(),
+        "maz_taz": get_maz_to_taz(),
+        "maz_maz_walk": get_maz_to_maz_walk(),
+    }
+    try:
+        from addicty import Dict
+    except ImportError:
+        pass
+    else:
+        result = Dict(result)
+        result.freeze()
+    return result
+
+
+def get_tour_mode_choice_spec(purpose="work"):
+    filename = os.path.join(
+        os.path.dirname(__file__), "example_data", "tour_mode_choice_spec.csv"
+    )
+    coeffs_filename = os.path.join(
+        os.path.dirname(__file__), "example_data", "tour_mode_choice_coefs.csv"
+    )
+    coeffs_template_filename = os.path.join(
+        os.path.dirname(__file__), "example_data", "tour_mode_choice_coef_template.csv"
+    )
+    spec = pd.read_csv(filename, comment="#")
+    coefs = pd.read_csv(coeffs_filename, index_col="coefficient_name", comment="#")
+    template = pd.read_csv(
+        coeffs_template_filename, index_col="coefficient_name", comment="#"
+    )
+    spec_numeric = (
+        spec.iloc[:, 3:]
+        .applymap(lambda i: template[purpose].get(i, i))
+        .applymap(lambda i: coefs.value.get(i, i))
+        .astype(np.float32)
+        .fillna(0)
+    )
+    return pd.concat([spec.iloc[:, :3], spec_numeric], axis=1)
