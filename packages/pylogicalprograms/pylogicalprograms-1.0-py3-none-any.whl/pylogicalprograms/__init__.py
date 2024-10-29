9c9732706797db80f915b68180defae565ae32bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-29 16:05:03 +0000

    Add package pylogicalprograms/1.0/pylogicalprograms-1.0-py3-none-any.whl

---
+++ packages/pylogicalprograms/pylogicalprograms-1.0-py3-none-any.whl/pylogicalprograms/__init__.py
@@ -0,0 +1,47 @@
+"""
+Importing logical programs modules.
+Official License By rdinesh808
+mail Id: rdinesh808@gmail.com0
+"""
+
+from .numberdigitcount import *
+from .factorial import *
+from .multiplicationtable import *
+from .integertoroman import *
+from .numbertoword import number_to_words
+from .binarysearch import binarySearch
+from .linearsearch import linearSearch
+from .swaptwonumbers import *
+from .zigzagconvertion import zigzagconvertion
+from .missingnumber import missingNumber
+from .stringreverseandpalindrome import *
+from .fibonacciseries import *
+from .armstrongnumber import armstrongNumber
+from .romantointeger import romanToInteger
+from .permutations import *
+from .spiralmatrix import *
+from .numberofisland import numIslands
+from .parentheses import generateParenthesis
+from .reverseinteger import *
+from .permutation import permutation
+from .testjustification import justify_text
+from .shift.leftShift import left_shift
+from .shift.rightShift import right_shift
+from .patterns.Patterns import *
+
+
+__author__ = 'rdinesh808'
+__version__ = '1.0'
+__mailid__ = 'rdinesh808@gmail.com'
+
+# methods names
+__all__ = ['numberDigitCount', 'NumberDigitCountRecursion', 'factorial', 'multiplicationTableWithRange','multiplicationSpecificTable', 
+           'integerToRoman', 'number_to_words', 'binarySearch', 'swapTwoNumWithTemp', 'swapTwoNumWithoutTemp', 'zigzagconvertion', 
+           'missingNumber', 'linearSearch', 'stringReverse1', 'stringReverse2', 'stringPalindrome', 'numberPalindrome', 'fibonacciseries', 
+           'fibonacciSeriesWithRecurtion', 'armstrongNumber', 'romanToInteger', 'permutationsInt', 'permutationsIntWithLen', 'permutationsStr', 
+           'permutationsStrWithLen', 'spiralMatrixWithList', 'spiralMatrixWithInt', 'numIslands', 'generateParenthesis', 'reverseInteger',
+           'reverseIntegerWithSigned', 'permutation', 'justify_text', 'left_shift', 'right_shift', 'Square_Pattern', 'Hollow_Square_Pattern', 
+           'Square_With_Cross_Pattern', 'Left_Triangle_Star_Pattern', 'Left_Downward_Triangle_Pattern', 'Right_Triangle_Star_Pattern', 
+           'Right_Downward_Triangle_Pattern', 'Hollow_Triangle_Star_Pattern', 'Pyramid_Pattern', 'Reverse_Pyramid_Pattern', 'Right_Pascal_Star_Pattern', 
+           'Left_Pascal_Star_Pattern', 'Hollow_Pyramid_Pattern', 'Plus_Pattern', 'Cross_Pattern', 'Heart_Pattern', 'Diamond_Star_Pattern', 'Hourglass_Star_Pattern',
+           'Hollow_Diamond_Star_Pattern']
