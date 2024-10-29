d2aa89f45a17b490470e5bab6b10497d3067d8a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-29 17:04:16 +0000

    Add package logfire/1.3.2/logfire-1.3.2.tar.gz

---
+++ packages/logfire/logfire-1.3.2.tar.gz/logfire-1.3.2/tests/auto_trace_samples/foo.py
@@ -0,0 +1,24 @@
+from typing import Iterator
+
+import logfire
+
+
+async def bar():
+    lst = [x async for x in async_gen()]
+    return lst[10]
+
+
+def gen() -> Iterator[int]:
+    yield from range(3)
+
+
+# @instrument overrides auto-tracing
+@logfire.instrument('Calling async_gen via @instrument')
+async def async_gen():
+    def inner():
+        return 1
+
+    inner()  # This is not traced
+
+    for x in gen():  # pragma: no branch
+        yield x
