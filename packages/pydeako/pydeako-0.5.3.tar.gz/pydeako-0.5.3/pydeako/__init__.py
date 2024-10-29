39c37ce75e12a204a7361a21c5aa3230d6cc86bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-29 06:03:19 +0000

    Add package pydeako/0.5.3/pydeako-0.5.3.tar.gz

---
+++ packages/pydeako/pydeako-0.5.3.tar.gz/pydeako-0.5.3/pydeako/__init__.py
@@ -0,0 +1,8 @@
+"""
+pydeako module provides the following:
+ - models to interact with Deako devices locally with a socket connection
+ - implementation of a Deako socket client
+ - mdns discovery client
+"""
+from . import discover
+from . import models
