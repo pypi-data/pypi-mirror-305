498d302433bd934f4b2ad38ce6cdab37b2655190
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-29 17:05:50 +0000

    Add package market-data-assembler/0.4.16/market_data_assembler-0.4.16.tar.gz

---
+++ packages/market-data-assembler/market_data_assembler-0.4.16.tar.gz/market_data_assembler-0.4.16/loader/ohlc_loader_interface.py
@@ -0,0 +1,17 @@
+from abc import ABC, abstractmethod
+from datetime import datetime
+from typing import List, Dict, Any
+
+
+class IOhlcLoader(ABC):
+
+    @abstractmethod
+    def __init__(self, selected_date: datetime, instrument: str):
+        pass
+
+    def is_exist(self) -> bool:
+        pass
+
+    @abstractmethod
+    def get_ohlc_series(self) -> List[Dict[str, Any]]:
+        pass
