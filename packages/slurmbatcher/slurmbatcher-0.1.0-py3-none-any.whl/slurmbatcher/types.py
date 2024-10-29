103dd0e46c62c14ea5c2fa8d0ed5eb0ef238a4b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-29 04:03:04 +0000

    Add package slurmbatcher/0.1.0/slurmbatcher-0.1.0-py3-none-any.whl

---
+++ packages/slurmbatcher/slurmbatcher-0.1.0-py3-none-any.whl/slurmbatcher/types.py
@@ -0,0 +1,23 @@
+from typing import Annotated
+
+from pydantic import BaseModel, BeforeValidator
+
+from slurmbatcher.utils import to_string_list
+
+String = Annotated[str, BeforeValidator(str)]
+StringList = Annotated[list[str], BeforeValidator(to_string_list)]
+
+
+class Matrix(BaseModel):
+    jobs: dict[str, StringList] = {}
+    parameters: dict[str, StringList]
+
+
+class SBatchSettings(BaseModel):
+    parameters: dict[str, String] = {}
+
+
+class Configuration(BaseModel):
+    command_template: str
+    sbatch: SBatchSettings
+    matrix: Matrix
