67ac979842d515d8bf43e4e77278f371e18002de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-29 17:04:55 +0000

    Add package django-pycorpkit/1.0.0/django_pycorpkit-1.0.0-py2.py3-none-any.whl

---
+++ packages/django-pycorpkit/django_pycorpkit-1.0.0-py2.py3-none-any.whl/pycorpkit/accountx/models/invitation.py
@@ -0,0 +1,14 @@
+import logging
+
+from django.db import models
+
+from pycorpkit.common.models.abstract import AbstractBase
+
+LOGGER = logging.getLogger(__name__)
+
+
+class Invitation(AbstractBase):
+    email = models.EmailField()
+    accepted_invitation = models.BooleanField(default=False)
+    accepted_time = models.DateTimeField(null=True, blank=True)
+    invitation_sent = models.BooleanField(default=False)
