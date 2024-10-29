f3659899311b24246026e2e0cbcfcd93312bb1c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-29 17:05:25 +0000

    Add package ptan/0.8.1/ptan-0.8.1.tar.gz

---
+++ packages/ptan/ptan-0.8.1.tar.gz/ptan-0.8.1/setup.py
@@ -0,0 +1,19 @@
+"""
+PTAN stands for PyTorch AgentNet -- reimplementation of AgentNet library for pytorch
+"""
+import pathlib
+import setuptools
+
+requirements = pathlib.Path("requirements.txt").read_text().splitlines()
+
+
+setuptools.setup(
+    name="ptan",
+    author="Max Lapan",
+    author_email="max.lapan@gmail.com",
+    license='GPL-v3',
+    description="PyTorch reinforcement learning framework",
+    version="0.8.1",
+    packages=setuptools.find_packages(),
+    install_requires=requirements,
+)
