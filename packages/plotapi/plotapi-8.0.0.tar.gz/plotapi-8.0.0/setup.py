366371a7946e2fd4c69168cf228fa137ce6d9324
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-29 14:03:53 +0000

    Add package plotapi/8.0.0/plotapi-8.0.0.tar.gz

---
+++ packages/plotapi/plotapi-8.0.0.tar.gz/plotapi-8.0.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="plotapi",
+    version="8.0.0",
+    description="Engaging visualisations, made easy.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://plotapi.com",
+    author="Dr. Shahin Rostami",
+    author_email="hello@shahinrostami.com",
+    license="MIT",
+    packages=["plotapi"],
+    install_requires=["requests >=2.20.0, <3.0.0"],
+    zip_safe=False,
+)
