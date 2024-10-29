421366cda1eb997de692691f752db723056288f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-29 17:03:50 +0000

    Add package btdx/0.0.3/btdx-0.0.3.tar.gz

---
+++ packages/btdx/btdx-0.0.3.tar.gz/btdx-0.0.3/src/btdx/__init__.py
@@ -0,0 +1,37 @@
+
+"""
+
+To use the `btdx`, you need to create an instance of `DX` first. Then use either `post` method to ingest data or `get` method to retrieve data
+
+1. **Initialization** To create an instance of DX, provide your API key, feed ID, and optionally a version number..
+   
+   ```python
+   from btdx import DX  # Make sure to import the DX class from the module
+   api_key = "your_api_key"     # Your API key
+   feed_id = "your_feed_id"     # The feed ID you want to interact with
+   version = 1                  # Optional: Version number (default is 1)
+   response = DX(api_key, feed_id, version)
+   ```
+
+2. **Posting Data** You can post data to a specific stream by calling the post method. You need to provide the stream ID and the data you want to post.
+
+    ```python
+    stream_id = "100"           # The ID of the stream you want to post data to
+    data =  "AQI: 2"         # The value you want to post
+    response.post(stream_id=stream_id, data=data)
+    ```
+
+3. **Getting Data** To retrieve data from a specific stream, use the get method. You can specify the stream ID and whether you want to display the data. You can also pass optional parameters such as agregate.
+
+    ```python
+    stream_id = "100"           # The ID of the stream you want to get data from
+    # Retrieve and display data
+    response.get(stream_id=stream_id, display=True)
+    # Retrieve last 100 values between a date range and display data, if no range is provided last 100 values from current time will be obtained.
+    start="YYYY-MM-DD"
+    end="YYYY-MM-DD"
+    response.get(stream_id=stream_id, start=start, end=end, display=True, agregate=True)
+    ``` 
+"""
+
+from .DX import DX
