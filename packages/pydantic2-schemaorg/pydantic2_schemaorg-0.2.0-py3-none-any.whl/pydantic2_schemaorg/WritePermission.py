1a7e3c05baa6885b387fc10404b38f8d65cea5ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-29 15:04:37 +0000

    Add package pydantic2-schemaorg/0.2.0/pydantic2_schemaorg-0.2.0-py3-none-any.whl

---
+++ packages/pydantic2-schemaorg/pydantic2_schemaorg-0.2.0-py3-none-any.whl/pydantic2_schemaorg/WritePermission.py
@@ -0,0 +1,15 @@
+from __future__ import annotations
+
+
+from pydantic.v1 import Field
+from pydantic2_schemaorg.DigitalDocumentPermissionType import DigitalDocumentPermissionType
+
+
+class WritePermission(DigitalDocumentPermissionType):
+    """Permission to write or edit the document.
+
+    See: https://schema.org/WritePermission
+    Model depth: 5
+    """
+    type_: str = Field(default="WritePermission", alias='@type', const=True)
+    
