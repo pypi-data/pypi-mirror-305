fd32a05c1e9d3037b184894b661438114ee6d86a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-29 04:03:26 +0000

    Add package stskr-2/0.0/stskr_2-0.0.tar.gz

---
+++ packages/stskr-2/stskr_2-0.0.tar.gz/stskr_2-0.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='stskr_2',
+      version='0.0',
+      description='stskr_2',
+      packages=['stskr_2'],
+      author_email='ilapronin0700@gmail.com',
+      zip_safe=False)
