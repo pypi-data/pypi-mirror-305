0ded64d7f425530a4536e9c8a47ccc8134a8ac5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-29 16:04:06 +0000

    Add package marqo/3.9.0/marqo-3.9.0.tar.gz

---
+++ packages/marqo/marqo-3.9.0.tar.gz/marqo-3.9.0/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+# from src.marqo.version import __version__
+
+with open("README.md", "r", encoding="utf8") as fh:
+    long_description = fh.read()
+
+setup(
+    install_requires=[
+        # client:
+        "requests",
+        "urllib3<2.0.0, >=1.26.0",
+        "pydantic>=2.0.0",
+        "typing-extensions>=4.5.0",
+        "packaging"
+    ],
+    tests_require=[
+        "pytest",
+        "tox"
+    ],
+    name="marqo",
+    version="3.9.0",
+    author="marqo org",
+    author_email="org@marqo.io",
+    description="Tensor search for humans",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(where="src", exclude=("tests*",)),
+    keywords="search python marqo opensearch tensor neural semantic vector embedding",
+    platform="any",
+    classifiers=[
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    include_package_data=True,
+    python_requires=">=3",
+    package_dir={"": "src"},
+)
