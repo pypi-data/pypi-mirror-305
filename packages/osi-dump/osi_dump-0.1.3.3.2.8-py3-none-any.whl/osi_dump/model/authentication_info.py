9eb31bc2c1db1ac7a8e1a53f0ea5de170ca8121f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-29 16:04:56 +0000

    Add package osi-dump/0.1.3.3.2.8/osi_dump-0.1.3.3.2.8-py3-none-any.whl

---
+++ packages/osi-dump/osi_dump-0.1.3.3.2.8-py3-none-any.whl/osi_dump/model/authentication_info.py
@@ -0,0 +1,12 @@
+from pydantic import BaseModel, ConfigDict
+
+
+class AuthenticationInfo(BaseModel):
+    model_config = ConfigDict(strict=True)
+
+    auth_url: str
+    project_name: str
+    username: str
+    password: str
+    user_domain_name: str
+    project_domain_name: str
