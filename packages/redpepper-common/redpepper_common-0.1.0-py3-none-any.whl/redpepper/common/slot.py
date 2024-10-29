d77402c9bb9ae43a65e1f76fd05cae10cff2d7d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-29 05:04:19 +0000

    Add package redpepper-common/0.1.0/redpepper_common-0.1.0-py3-none-any.whl

---
+++ packages/redpepper-common/redpepper_common-0.1.0-py3-none-any.whl/redpepper/common/slot.py
@@ -0,0 +1,23 @@
+from typing import Any
+
+import trio
+
+
+class Slot:
+    def __init__(self):
+        self._value: Any = None
+        self._event = trio.Event()
+
+    async def set(self, value: Any) -> None:
+        self._value = value
+        self._event.set()
+
+    async def get(self, timeout: float | None = None) -> Any:
+        if self._event.is_set():
+            return self._value
+        if timeout is None:
+            await self._event.wait()
+        else:
+            with trio.fail_after(timeout):
+                await self._event.wait()
+        return self._value
