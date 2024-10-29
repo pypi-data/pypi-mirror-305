098a54a5fabf02494cbfa531e31c9460910b7891
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-29 05:03:55 +0000

    Add package duckyai/1.0.7/duckyai-1.0.7.tar.gz

---
+++ packages/duckyai/duckyai-1.0.7.tar.gz/duckyai-1.0.7/duckyai/api/__init__.py
@@ -0,0 +1,9 @@
+# flake8: noqa
+
+# import apis into api package
+from duckyai.api.ducky_api import DuckyApi
+from duckyai.api.eval_api import EvalApi
+from duckyai.api.gen_api import GenApi
+from duckyai.api.ingest_api import IngestApi
+from duckyai.api.webapp_api import WebappApi
+
