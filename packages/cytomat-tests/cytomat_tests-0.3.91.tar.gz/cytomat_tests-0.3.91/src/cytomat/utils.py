272a35c788e3732b04ef59cacb11bc6f4f4a1a83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-29 05:03:08 +0000

    Add package cytomat-tests/0.3.91/cytomat_tests-0.3.91.tar.gz

---
+++ packages/cytomat-tests/cytomat_tests-0.3.91.tar.gz/cytomat_tests-0.3.91/src/cytomat/utils.py
@@ -0,0 +1,84 @@
+from contextlib import contextmanager
+from enum import IntEnum
+from threading import Lock
+from typing import Dict, Iterator, Tuple, Type, TypeVar
+from cytomat.parameters import Parameters
+GenericIntEnum = TypeVar("GenericIntEnum", bound=IntEnum)
+
+@contextmanager
+def lock_threading_lock(lock: Lock, *, timeout: float) -> Iterator[None]:
+    """Context manager for using a `threading.lock` with a timeout"""
+    if not timeout > 0:
+        raise ValueError("Timeout must be positive")
+
+    if not lock.acquire(timeout=timeout):
+        raise TimeoutError(f"Could not acquire lock after {timeout} seconds")
+
+    try:
+        yield
+    finally:
+        lock.release()
+
+
+def enum_to_dict(enum: Type[GenericIntEnum]) -> Dict[int, GenericIntEnum]:
+    """Converts an enum class to a Dict[value, name]"""
+    ret = {}
+    for attr in dir(enum):
+        item = getattr(enum, attr)
+        if not isinstance(item, enum):
+            continue
+        ret[item.value] = item
+    return ret
+
+
+def int_to_bits(num: int, n_bits: int) -> Tuple[bool, ...]:
+    """
+    Convert an integer to its bit representation.
+
+    Examples
+    --------
+
+    >>> int_to_bits(3, 2)
+    (True, True)
+    >>> int_to_bits(0xF1, 8)
+    (True, True, True, True, False, False, False, True)
+    """
+    max_num_representable_by_n_bits = 2**n_bits - 1
+    if num > max_num_representable_by_n_bits:
+        raise ValueError(f"{n_bits} can only represent numbers <= {max_num_representable_by_n_bits}, got {num}")
+    return tuple(bool(num & (2 ** (n_bits - i - 1))) for i in range(n_bits))
+
+class ConvertSteps:
+    parameters = Parameters()
+
+    @classmethod
+    def mm_to_steps_x(cls, mm: float)-> int:
+        return round(cls.parameters.steps_per_mm_x * mm)
+
+    @classmethod
+    def steps_to_mm_x(cls, steps: int)-> float:
+        return round(1/(cls.parameters.steps_per_mm_x / steps), 4)
+    
+    @classmethod
+    def mm_to_steps_h(cls, mm: float)-> int:
+        return round(cls.parameters.steps_per_mm_h * mm)
+
+    @classmethod
+    def steps_to_mm_h(cls, steps: int)-> float:
+        return round(1/(cls.parameters.steps_per_mm_h / steps),4)
+    
+    @classmethod
+    def mm_to_steps_shovel(cls, mm: float)-> int:
+        return round(cls.parameters.steps_per_mm_shovel * mm)
+    
+    @classmethod
+    def steps_to_mm_shovel(cls, steps: int)-> float:
+        return round(1/(cls.parameters.steps_per_mm_shovel / steps),4)
+    
+    @classmethod
+    def deg_to_steps_turn(cls, deg: float)-> int:
+        return round(cls.parameters.steps_per_deg_turn * deg)
+    
+    @classmethod
+    def steps_to_deg_turn(cls, steps: int)-> float:
+        return round(1/(cls.parameters.steps_per_deg_turn / steps),4)
