dccec24e0ece877d03f1ece13b9f932aba5d3515
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-29 03:09:29 +0000

    Add package business-calculate/0.1.0/business_calculate-0.1.0.tar.gz

---
+++ packages/business-calculate/business_calculate-0.1.0.tar.gz/business_calculate-0.1.0/business/__init__.py
@@ -0,0 +1,4 @@
+from .calculator import calculate_profit, calculate_roi
+
+__all__ = ["calculate_profit",
+           "calculate_roi"]
---
+++ packages/business-calculate/business_calculate-0.1.0.tar.gz/business_calculate-0.1.0/business/calculator.py
@@ -0,0 +1,6 @@
+def calculate_profit(revenue, costs):
+    print(f"Чистая прибыль: {revenue - costs}")
+
+
+def calculate_roi(revenue, costs):
+    print(f"ROI: {revenue / costs * 100}%")
