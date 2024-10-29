75e0ab150fb62319490dc587c71326b39fb0c187
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-29 03:09:19 +0000

    Add package freva-deployment/2410.0.3/freva_deployment-2410.0.3-py3-none-any.whl

---
+++ packages/freva-deployment/freva_deployment-2410.0.3-py3-none-any.whl/freva_deployment-2410.0.3.data/data/share/freva/deployment/scripts/create_cron.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+cmd=$(/tmp/docker-or-podman -p exec $1 bash /usr/local/bin/daily_backup)
+cmd="$cmd 1> /dev/null"
+# Function to check if the cron job already exists in the crontab
+cron_job_exists() {
+    local cmd="$1"
+    crontab -l 2>/dev/null | grep -F "$cmd" >/dev/null
+}
+if [ -w /etc/cron.daily ]; then
+    # If /etc/cron.daily is writable, create the cron job there
+    cron_dir="/etc/cron.daily"
+    cron_file="$cron_dir/backup_$1"
+else
+    # If /etc/cron.daily is not writable, add the cron job to the user's crontab
+    cron_dir=""
+    cron_file="user"
+fi
+
+
+if [ "$cron_dir" ]; then
+    mkdir -p "$cron_dir"
+    echo "#!/bin/sh" > "$cron_file"
+    echo "# Run daily backup for $1" >> "$cron_file"
+    if [ "$2" ]; then
+        echo "MAILTO=$2" >> "$cron_file"
+    fi
+    echo "$cmd" >> "$cron_file"
+    chmod +x "$cron_file"
+else
+    if [ "$(which crontab 2> /dev/null)" ];then
+        if ! cron_job_exists "$cmd"; then
+            echo "0 0 * * * $cmd" >> "$HOME/.crontab"
+            crontab "$HOME/.crontab"
+        fi
+    fi
+fi
---
+++ packages/freva-deployment/freva_deployment-2410.0.3-py3-none-any.whl/freva_deployment-2410.0.3.data/data/share/freva/deployment/scripts/create_systemd.py
@@ -0,0 +1,157 @@
+#!/usr/bin/env python3
+import argparse
+import os
+import shlex
+import subprocess
+import sys
+from pathlib import Path
+from typing import Dict, List, Tuple
+
+SYSTEMD_TMPL = dict(
+    Unit=dict(
+        Description="Start/Stop freva services containers",
+        After="network-online.target",
+    ),
+    Service=dict(
+        TimeoutStartSec="35s",
+        TimeoutStopSec="35s",
+        ExecStartPre='/bin/sh -c "{delete_command}"',
+        ExecStart='/bin/sh -c "{container_cmd} {container_args}"',
+        ExecStop='/bin/sh -c "{delete_command}"',
+        Restart="no",
+    ),
+    Install=dict(WantedBy="default.target"),
+)
+
+
+def parse_args() -> Tuple[str, str, List[str], bool]:
+    """Parse the commandline arguments."""
+
+    app = argparse.ArgumentParser(
+        prog=sys.argv[0], description="Create a new systemd unit."
+    )
+    app.add_argument("name", type=str, help="Set the container name")
+    app.add_argument(
+        "--enable",
+        action="store_true",
+        default=False,
+        help="Enable this unit.",
+    )
+    app.add_argument("--requires", type=str, nargs="+", default=[])
+    app.add_argument(
+        "--gracious",
+        action="store_true",
+        default=False,
+        help="Do not restart but only start the unit",
+    )
+    args, other = app.parse_known_args()
+    enable = args.enable
+    if os.environ.get("DEBUG", "false").lower() == "true":
+        enable = False
+    return " ".join(other), args.name, args.requires, enable, args.gracious
+
+
+def _parse_dict(tmp_dict: Dict[str, Dict[str, str]]) -> str:
+    systemd_unit = ""
+    for section, keys in tmp_dict.items():
+        systemd_unit += "[{}]\n".format(section)
+        for key, values in keys.items():
+            systemd_unit += "{}={}\n".format(key, values)
+    return systemd_unit
+
+
+def load_unit(
+    unit: str, content: str, enable: bool = True, gracious: bool = False
+) -> None:
+    """Load a given systemd unit."""
+    files = (
+        "/etc/systemd/system/{}.service".format(unit),
+        "~/.local/share/systemd/user/{}.service".format(unit),
+    )
+    flags = ("", "--user")
+    for file, flag in zip(files, flags):
+        out_file = Path(file).expanduser()
+        try:
+            out_file.parent.mkdir(exist_ok=True, parents=True)
+            with out_file.open(mode="w", encoding="utf-8") as f_obj:
+                f_obj.write(content)
+        except PermissionError:
+            continue
+        cmd = ["systemctl"]
+        if flag:
+            cmd += [flag]
+
+        subprocess.run(cmd + ["daemon-reload"], check=True)
+        if enable:
+            subprocess.run(cmd + ["enable", unit], check=True)
+        if gracious:
+            subprocess.run(cmd + ["start", unit], check=True)
+        else:
+            subprocess.run(cmd + ["restart", unit], check=True)
+        return
+
+
+def get_container_cmd(args: str) -> Tuple[str, str]:
+    """Get the correct container command for the system."""
+    path = os.environ.get("PATH", "") + ":" + "/usr/local/bin"
+    env = os.environ.copy()
+    env["PATH"] = path
+    cmd = ["/tmp/docker-or-podman", "--print-only"] + shlex.split(args)
+    res = subprocess.run(
+        cmd,
+        check=True,
+        stdout=subprocess.PIPE,
+        env=env,
+    )
+    out = res.stdout.decode().split()
+    if out:
+        return out[0], " ".join(out[1:])
+    return "", ""
+
+
+def create_unit(
+    args: str, unit: str, requires: List[str], enable: bool, gracious: bool
+) -> None:
+    """Create the systemd unit."""
+    container_cmd, container_args = get_container_cmd(args)
+    cmd = args.split()
+    if "compose" in cmd and "up" in cmd:
+        new_cmd = []
+        for word in cmd:
+            if word == "up":
+                new_cmd.append("down")
+            elif word not in ("-d", "--detach"):
+                new_cmd.append(word)
+        _, delete_command = get_container_cmd(" ".join(new_cmd))
+    else:
+        _, delete_command = get_container_cmd("rm -f {}".format(unit))
+    if delete_command:
+        delete_command = "{} {}".format(container_cmd, delete_command)
+    else:
+        delete_command = ""
+    if "docker" in container_cmd:
+        SYSTEMD_TMPL["Unit"]["Requires"] = "docker.service"
+        SYSTEMD_TMPL["Unit"]["After"] += " docker.service"
+    # else:
+    #    SYSTEMD_TMPL["Service"]["Environment"] = "PODMAN_USERNS=keep-id"
+    for key in ("ExecStart",):
+        SYSTEMD_TMPL["Service"][key] = SYSTEMD_TMPL["Service"][key].format(
+            container_cmd=container_cmd,
+            container_args=container_args,
+            unit=unit,
+        )
+    for key in ("ExecStartPre", "ExecStop"):
+        SYSTEMD_TMPL["Service"][key] = SYSTEMD_TMPL["Service"][key].format(
+            delete_command=delete_command
+        )
+    for service in requires:
+        for key in ("After", "Requires"):
+            try:
+                SYSTEMD_TMPL["Unit"][key] += " {}.service".format(service)
+            except KeyError:
+                SYSTEMD_TMPL["Unit"][key] = " {}.service".format(service)
+    load_unit(unit, _parse_dict(SYSTEMD_TMPL), enable, gracious=gracious)
+
+
+if __name__ == "__main__":
+    create_unit(*parse_args())
