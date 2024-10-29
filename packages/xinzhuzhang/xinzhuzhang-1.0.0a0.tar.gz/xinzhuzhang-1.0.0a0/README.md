4f4d954591d293358c44909fac44d407a234b9d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-29 16:04:37 +0000

    Add package xinzhuzhang/1.0.0a0/xinzhuzhang-1.0.0a0.tar.gz

---
+++ packages/xinzhuzhang/xinzhuzhang-1.0.0a0.tar.gz/xinzhuzhang-1.0.0a0/README.md
@@ -0,0 +1,1 @@
+I LOVe xinzhuzhang
---
+++ packages/xinzhuzhang/xinzhuzhang-1.0.0a0.tar.gz/xinzhuzhang-1.0.0a0/xinzhuzhang/app.py
@@ -0,0 +1,11 @@
+# I LOVE YOUUU YOU SO MUCH
+# Wo ai ni 
+def love(x):
+    if x == "LUKA IS ON TIME TODAY":
+        return "KAITLYN IS HAPPY AGAIN"
+    else:
+        return "KAITLYN IS DISAPPOITNTED AND SAD"
+    
+
+
+
