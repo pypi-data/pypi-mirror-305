99bb5e39228fa09b89d559ce96d590dd851bfb97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-29 14:03:38 +0000

    Add package ekogram/0.5.5/ekogram-0.5.5-py3-none-any.whl

---
+++ packages/ekogram/ekogram-0.5.5-py3-none-any.whl/ekogram/__init__.py
@@ -0,0 +1,3 @@
+from .ekogram import Bot, Markup, InputFile, InputMediaAudio, InputMediaDocument, InputMediaVideo, InputMediaAnimation, InputMediaPhoto, MessageEntity
+
+__all__ = ['Bot', 'Markup', 'InputFile', 'InputMediaAnimation', 'InputMediaPhoto', 'InputMediaVideo', 'InputMediaAudio', 'InputMediaDocument', 'MessageEntity']
