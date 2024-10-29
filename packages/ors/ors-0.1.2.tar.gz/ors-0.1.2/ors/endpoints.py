d3080ec21b651dfe148f57c72e3ae1723af7ce10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-29 04:02:57 +0000

    Add package ors/0.1.2/ors-0.1.2.tar.gz

---
+++ packages/ors/ors-0.1.2.tar.gz/ors-0.1.2/ors/endpoints.py
@@ -0,0 +1,67 @@
+from enum import StrEnum
+import json
+from collections.abc import Iterable
+from typing import Any, Callable
+from ors.types import Response, HTTPClient, Context, GeoJSON
+
+
+from urllib.parse import urljoin
+
+
+class Endpoints(StrEnum):
+    ISOCHRONES = "/v2/isochrones"
+
+
+def _prepare_isoch_url(base: str, endpoint: str, profile: str) -> str:
+    ep = endpoint + "/" + profile
+    return urljoin(base=base, url=ep)
+
+
+def _parse_isoch_response(resp: str) -> Response:
+    return json.loads(resp)
+
+
+def _prepare_isoch_body(body: dict[str, Any]) -> str:
+    return json.dumps({k: v for k, v in body.items() if v is not None})
+
+
+def isochrones(
+    locations: Iterable[tuple[float, float]],
+    range_: Iterable[int],
+    attributes: Iterable[str] | None = None,
+    id_: str | None = None,
+    intersections: bool | None = None,
+    interval: int | None = None,
+    location_type: str | None = None,
+    options: dict[Any, Any] | None = None,
+    range_type: str | None = None,
+    smoothing: int | None = None,
+    area_units: str | None = None,
+    units: str | None = None,
+) -> Callable[[HTTPClient, Context], GeoJSON]:
+    body = {
+        "locations": locations,
+        "range": range_,
+        "attributes": attributes,
+        "id": id_,
+        "intersections": intersections,
+        "interval": interval,
+        "location_type": location_type,
+        "options": options,
+        "range_type": range_type,
+        "smoothing": smoothing,
+        "area_units": area_units,
+        "units": units,
+    }
+
+    def call(http: HTTPClient, ctx: Context) -> GeoJSON:
+        url = _prepare_isoch_url(ctx.base_url, Endpoints.ISOCHRONES, ctx.profile)
+        resp = http.request(
+            "POST",
+            url,
+            body=_prepare_isoch_body(body),
+            headers=ctx.headers,
+        )
+        return _parse_isoch_response(resp.data)
+
+    return call
