1e41aacae487908aa800d88ef10ab88a88bddc05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-29 14:03:57 +0000

    Add package bobberThing/0.1.2/bobberthing-0.1.2.tar.gz

---
+++ packages/bobberThing/bobberthing-0.1.2.tar.gz/bobberthing-0.1.2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='bobberThing',
+    version='0.1.2',
+    packages=find_packages(),
+    description='bobber mcbob',
+    author='sebass',
+    author_email='sebass@sebass.fr.skibidi.com',
+)
