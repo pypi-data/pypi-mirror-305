4e9f5e1d6cb459c2a83b2cc4b7965ee160e91fd1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-29 14:03:41 +0000

    Add package printextent/0.1.10/printextent-0.1.10.tar.gz

---
+++ packages/printextent/printextent-0.1.10.tar.gz/printextent-0.1.10/printextent/__init__.py
@@ -0,0 +1,3 @@
+from .colorPrint import *
+from .typewritePrint import *; from .reversePrint import *; from .asciiPrint import *
+from .informUserPrint import *; from .warnUserPrint import *; from .errorUserPrint import *
