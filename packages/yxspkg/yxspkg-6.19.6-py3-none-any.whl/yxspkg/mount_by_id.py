e89a28f7703dfd0ee74d442b96f50a1932734cb4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-29 03:09:15 +0000

    Add package yxspkg/6.19.6/yxspkg-6.19.6-py3-none-any.whl

---
+++ packages/yxspkg/yxspkg-6.19.6-py3-none-any.whl/yxspkg/mount_by_id.py
@@ -0,0 +1,24 @@
+import subprocess
+import click
+
+@click.command()
+@click.argument('args',nargs=-1)
+def main(args):
+    assert len(args)>=2
+    cmd = f'blkid | grep {args[0]}'
+    out = subprocess.getoutput(cmd).split('\n')
+    print(out)
+    if len(out)>1:
+        raise Exception("Too mandy disks are pointed")
+    if len(out) == 1:
+        out = out[0].strip().split()[0][:-1]
+        cmd = f'mount {out} {args[1]}'
+        print(cmd)
+        stat = subprocess.call(cmd,shell=True)
+        if stat != 0:
+            print('mount failed')
+    else:
+        print('no disk')
+
+if __name__=='__main__':
+    main()
---
+++ packages/yxspkg/yxspkg-6.19.6-py3-none-any.whl/yxspkg/sci_plot.py
@@ -0,0 +1,7 @@
+ 
+from matplotlib import pyplot as plt
+import seaborn as sns
+plt.style.use(['science', 'ieee', 'no-latex'])
+colors = ['black','red','blue','green','brown','orange','violet','pink','light blue','teal','yellow','magenta','dark green','dark red','dark orange']
+palette = sns.xkcd_palette(colors)
+sns.set_palette(palette)
