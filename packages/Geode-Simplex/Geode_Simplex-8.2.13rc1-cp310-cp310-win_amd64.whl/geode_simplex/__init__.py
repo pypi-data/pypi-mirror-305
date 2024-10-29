f298aa31482d985f1cf55153e25b119f04e183af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-29 16:04:57 +0000

    Add package Geode-Simplex/8.2.13rc1/Geode_Simplex-8.2.13rc1-cp310-cp310-win_amd64.whl

---
+++ packages/Geode-Simplex/Geode_Simplex-8.2.13rc1-cp310-cp310-win_amd64.whl/geode_simplex/__init__.py
@@ -0,0 +1,8 @@
+## Copyright (c) 2019 - 2024 Geode-solutions
+
+import os, pathlib
+os.add_dll_directory(pathlib.Path(__file__).parent.resolve().joinpath('bin'))
+
+from .metric import *
+from .section import *
+from .brep import *
