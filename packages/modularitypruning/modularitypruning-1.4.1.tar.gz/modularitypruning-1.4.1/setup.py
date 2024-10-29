1be951db64eaf6b42fc9eb06b126e6d5f5fb70f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-29 15:04:45 +0000

    Add package modularitypruning/1.4.1/modularitypruning-1.4.1.tar.gz

---
+++ packages/modularitypruning/modularitypruning-1.4.1.tar.gz/modularitypruning-1.4.1/setup.py
@@ -0,0 +1,39 @@
+import os
+
+from setuptools import setup
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+# Get the long description from the README file
+with open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='modularitypruning',
+    version='1.4.1',
+    package_dir={'modularitypruning': 'utilities'},
+    packages=['modularitypruning'],
+    url='https://github.com/ragibson/ModularityPruning',
+    license='',
+    author='Ryan Gibson',
+    author_email='ryan.alex.gibson@gmail.com',
+    description='Pruning tool to identify small subsets of network partitions that are '
+                'significant from the perspective of stochastic block model inference.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Topic :: Scientific/Engineering :: Information Analysis',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3 :: Only',
+        'License :: OSI Approved :: MIT License'
+    ],
+    python_requires='>=3.8, <4',
+    install_requires=['leidenalg', 'matplotlib', "numpy", 'psutil', 'igraph',
+                      "scikit-learn", "scipy>=1.7", 'seaborn', 'tqdm']
+)
