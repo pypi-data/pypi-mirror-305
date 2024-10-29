b607bc8ff90ad34b05b88ac13708134bb5dd2cec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-29 14:03:42 +0000

    Add package malsearch/0.2.0/malsearch-0.2.0-py3-none-any.whl

---
+++ packages/malsearch/malsearch-0.2.0-py3-none-any.whl/malsearch/clients/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: UTF-8 -*-
+from .maldatabase import Maldatabase
+from .malpedia import Malpedia
+from .malshare import MalShare
+from .malwarebazaar import MalwareBazaar
+from .triage import Triage
+from .virusshare import VirusShare
+from .virustotal import VirusTotal
+
+
+__all__ = ["Maldatabase", "Malpedia", "MalShare", "MalwareBazaar", "Triage", "VirusShare", "VirusTotal"]
+
