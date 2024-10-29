e0160326485a17c3a9bdfda72b73ae7d6218d938
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-29 05:03:04 +0000

    Add package python-fastflow/0.3.0/python_fastflow-0.3.0-py3-none-any.whl

---
+++ packages/python-fastflow/python_fastflow-0.3.0-py3-none-any.whl/fastflow/tasks/stdlib.py
@@ -0,0 +1,11 @@
+from typing import Optional
+
+from fastflow.engine import TaskImpl, TaskInputList, TaskOutput, TaskResult
+
+
+class Echo(TaskImpl):
+    args: TaskInputList
+    stdout: TaskOutput
+
+    async def complete(self, **_) -> Optional[TaskResult]:
+        return TaskResult(outputs={self.stdout: "".join(self.args)})
