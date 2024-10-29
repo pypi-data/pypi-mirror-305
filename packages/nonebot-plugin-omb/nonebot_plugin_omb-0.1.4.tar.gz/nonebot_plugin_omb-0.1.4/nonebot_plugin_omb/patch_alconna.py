c5b530777962c3f658dd47bb91751af3ed921a56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-29 17:04:24 +0000

    Add package nonebot-plugin-omb/0.1.4/nonebot_plugin_omb-0.1.4.tar.gz

---
+++ packages/nonebot-plugin-omb/nonebot_plugin_omb-0.1.4.tar.gz/nonebot_plugin_omb-0.1.4/nonebot_plugin_omb/patch_alconna.py
@@ -0,0 +1,40 @@
+from typing import Union
+
+from nonebot import Bot
+from arclet.alconna import Alconna
+from nonebot.typing import T_PermissionChecker
+from nonebot.permission import SUPERUSER
+import nonebot_plugin_alconna
+from nonebot.internal.adapter import Event
+from nonebot.internal.permission import Permission
+from nonebot_plugin_alconna.extension import Extension, ExtensionExecutor
+
+from nonebot_plugin_omb.util import SuperUserObj, patch
+
+_raw_on_alconna = nonebot_plugin_alconna.on_alconna
+
+
+@patch(nonebot_plugin_alconna, name="on_alconna")
+def patch_on_alconna(*args, **kwargs):
+    if (permission := kwargs.get("permission")) is None:
+        kwargs["permission"] = SUPERUSER
+    else:
+        permission: Union[Permission, T_PermissionChecker]
+        kwargs["permission"] = permission | SUPERUSER
+    return _raw_on_alconna(*args, **kwargs)
+
+
+class MyExtension(Extension):
+    @property
+    def id(self) -> str:
+        return "OmbExtension"
+
+    @property
+    def priority(self) -> int:
+        return 0
+
+    async def permission_check(self, bot: Bot, event: Event, command: Alconna) -> bool:
+        return SuperUserObj(bot, event)
+
+
+ExtensionExecutor.globals.append(MyExtension)
