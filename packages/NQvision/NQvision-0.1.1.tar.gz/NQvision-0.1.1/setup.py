71453e69af29af6d63a31efa9a14112c434f40e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-29 04:03:26 +0000

    Add package NQvision/0.1.1/NQvision-0.1.1.tar.gz

---
+++ packages/NQvision/NQvision-0.1.1.tar.gz/NQvision-0.1.1/setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup, find_packages
+
+
+print("Starting setup.py execution...")
+
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="NQvision",
+    version="0.1.1",
+    author="Neuron Q",
+    author_email="debbichi.raki@neuronq.io",
+    description="A library to simplify the development of AI-driven object detection and monitoring solutions.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://neuronq.io",
+    packages=find_packages(exclude=["tests", "examples"]),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+    python_requires=">=3.7",
+    install_requires=[
+        "numpy",
+        "opencv-python",
+        "Pillow",
+        "deep_sort_realtime",
+        "torch",
+        "torchvision",
+        "vidgear",
+        "numba",
+    ],
+    license="MIT",
+    keywords="computer vision, object detection, tracking, CUDA, enterprise",
+)
