4919ccab031dd1c23d0f413f19b5f70fefef4915
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-29 05:03:03 +0000

    Add package optima-0.1/0.1.4/optima_0_1-0.1.4.tar.gz

---
+++ packages/optima-0.1/optima_0_1-0.1.4.tar.gz/optima_0_1-0.1.4/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="optima-0.1",
+    version="0.1.4",
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    include_package_data=True,
+    install_requires=[
+        "click>=8.1.0",
+        "python-dotenv>=0.19.0",
+        "requests>=2.32.0",
+        "setuptools>=74.1.0",
+    ],
+    entry_points={
+        "console_scripts": [
+            "op=optima_0_1.cli:cli",
+        ],
+    },
+    author="Ashley Wang",
+    author_email="xiaohuyaw@gmail.com",
+    description="OPTIMA CLI",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.11",
+)
