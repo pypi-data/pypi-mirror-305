a6726eeb46ca3e7bacabe211d710184791815f84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-29 03:09:45 +0000

    Add package structifyai/1.45.0/structifyai-1.45.0-py3-none-any.whl

---
+++ packages/structifyai/structifyai-1.45.0-py3-none-any.whl/structify/types/job_list_response.py
@@ -0,0 +1,22 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from typing import Optional
+from datetime import datetime
+from typing_extensions import Literal
+
+from .._models import BaseModel
+
+__all__ = ["JobListResponse"]
+
+
+class JobListResponse(BaseModel):
+    id: str
+
+    creation_time: datetime
+
+    status: Literal["Queued", "Running", "Completed", "Failed"]
+
+    message: Optional[str] = None
+
+    run_started_time: Optional[datetime] = None
+    """What time did the job start running?"""
