2aabe5b8505d39376b1bd89cd2965ed099d6dd57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-29 15:04:44 +0000

    Add package django5-aliyun-oss/1.1.2/django5_aliyun_oss-1.1.2-py3-none-any.whl

---
+++ packages/django5-aliyun-oss/django5_aliyun_oss-1.1.2-py3-none-any.whl/django5_aliyun_oss/apps.py
@@ -0,0 +1,6 @@
+# oss_storage/apps.py
+from django.apps import AppConfig
+
+class OssStorageConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'django5_aliyun_oss'
