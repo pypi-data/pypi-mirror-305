1b9eb306f52160674a10a72a197fed12b5b49550
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-29 04:03:13 +0000

    Add package nc-to-gpkg/0.0.2/nc_to_gpkg-0.0.2.tar.gz

---
+++ packages/nc-to-gpkg/nc_to_gpkg-0.0.2.tar.gz/nc_to_gpkg-0.0.2/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="nc_to_gpkg",
+    version="0.0.2",
+    description="A package for processing NetCDF files and merging with shapefiles",
+    author="Isaac Barnhart",
+    author_email="isaac.barnhart@corteva.com",
+    packages=find_packages(),
+    install_requires=['pandas', 'geopandas', 'xarray', 'shapely', 'tqdm'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
+    python_requires=">=3.6",
+)
