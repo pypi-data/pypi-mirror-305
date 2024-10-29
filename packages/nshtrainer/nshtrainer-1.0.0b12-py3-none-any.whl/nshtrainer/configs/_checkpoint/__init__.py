cf6798d070e08cde6b3cc3cbb7c5c404c22abf81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-29 14:03:35 +0000

    Add package nshtrainer/1.0.0b12/nshtrainer-1.0.0b12-py3-none-any.whl

---
+++ packages/nshtrainer/nshtrainer-1.0.0b12-py3-none-any.whl/nshtrainer/configs/_checkpoint/__init__.py
@@ -0,0 +1,70 @@
+from __future__ import annotations
+
+__codegen__ = True
+
+from typing import TYPE_CHECKING
+
+# Config/alias imports
+
+if TYPE_CHECKING:
+    from nshtrainer._checkpoint.loader import (
+        BestCheckpointStrategyConfig as BestCheckpointStrategyConfig,
+    )
+    from nshtrainer._checkpoint.loader import (
+        CheckpointLoadingConfig as CheckpointLoadingConfig,
+    )
+    from nshtrainer._checkpoint.loader import (
+        CheckpointLoadingStrategyConfig as CheckpointLoadingStrategyConfig,
+    )
+    from nshtrainer._checkpoint.loader import CheckpointMetadata as CheckpointMetadata
+    from nshtrainer._checkpoint.loader import (
+        LastCheckpointStrategyConfig as LastCheckpointStrategyConfig,
+    )
+    from nshtrainer._checkpoint.loader import MetricConfig as MetricConfig
+    from nshtrainer._checkpoint.loader import (
+        UserProvidedPathCheckpointStrategyConfig as UserProvidedPathCheckpointStrategyConfig,
+    )
+    from nshtrainer._checkpoint.metadata import EnvironmentConfig as EnvironmentConfig
+else:
+
+    def __getattr__(name):
+        import importlib
+
+        if name in globals():
+            return globals()[name]
+        if name == "BestCheckpointStrategyConfig":
+            return importlib.import_module(
+                "nshtrainer._checkpoint.loader"
+            ).BestCheckpointStrategyConfig
+        if name == "CheckpointLoadingConfig":
+            return importlib.import_module(
+                "nshtrainer._checkpoint.loader"
+            ).CheckpointLoadingConfig
+        if name == "CheckpointMetadata":
+            return importlib.import_module(
+                "nshtrainer._checkpoint.loader"
+            ).CheckpointMetadata
+        if name == "EnvironmentConfig":
+            return importlib.import_module(
+                "nshtrainer._checkpoint.metadata"
+            ).EnvironmentConfig
+        if name == "LastCheckpointStrategyConfig":
+            return importlib.import_module(
+                "nshtrainer._checkpoint.loader"
+            ).LastCheckpointStrategyConfig
+        if name == "MetricConfig":
+            return importlib.import_module("nshtrainer._checkpoint.loader").MetricConfig
+        if name == "UserProvidedPathCheckpointStrategyConfig":
+            return importlib.import_module(
+                "nshtrainer._checkpoint.loader"
+            ).UserProvidedPathCheckpointStrategyConfig
+        if name == "CheckpointLoadingStrategyConfig":
+            return importlib.import_module(
+                "nshtrainer._checkpoint.loader"
+            ).CheckpointLoadingStrategyConfig
+        raise AttributeError(f"module '{__name__}' has no attribute '{name}'")
+
+
+# Submodule exports
+from . import loader as loader
+from . import metadata as metadata
