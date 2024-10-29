2feaec937d8b7ebb31106feb4ba6a54ecba2f0c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-29 04:03:01 +0000

    Add package pixegami-hello-new/0.3/pixegami_hello_new-0.3-py3-none-any.whl

---
+++ packages/pixegami-hello-new/pixegami_hello_new-0.3-py3-none-any.whl/pixegami_hello/__init__.py
@@ -0,0 +1,3 @@
+from .main import hello
+# First,__init__.py tells Python that the whole directory here should be treated as a package
+# Second, it controls what's actually available when that package is imported
