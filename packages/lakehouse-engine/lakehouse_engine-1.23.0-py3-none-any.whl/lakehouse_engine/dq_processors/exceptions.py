a851b6d1888d3f95673d9aa689620a38f7e2dc89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-29 03:09:42 +0000

    Add package lakehouse-engine/1.23.0/lakehouse_engine-1.23.0-py3-none-any.whl

---
+++ packages/lakehouse-engine/lakehouse_engine-1.23.0-py3-none-any.whl/lakehouse_engine/dq_processors/exceptions.py
@@ -0,0 +1,19 @@
+"""Package defining all the DQ custom exceptions."""
+
+
+class DQValidationsFailedException(Exception):
+    """Exception for when the data quality validations fail."""
+
+    pass
+
+
+class DQCheckpointsResultsException(Exception):
+    """Exception for when the checkpoint results parsing fail."""
+
+    pass
+
+
+class DQSpecMalformedException(Exception):
+    """Exception for when the DQSpec is malformed."""
+
+    pass
