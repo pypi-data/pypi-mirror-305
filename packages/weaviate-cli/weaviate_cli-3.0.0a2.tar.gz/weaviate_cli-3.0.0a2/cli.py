e48dcbc725b17024a87285c7268192c453161ea3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-29 17:05:06 +0000

    Add package weaviate-cli/3.0.0a2/weaviate_cli-3.0.0a2.tar.gz

---
+++ packages/weaviate-cli/weaviate_cli-3.0.0a2.tar.gz/weaviate_cli-3.0.0a2/cli.py
@@ -0,0 +1,28 @@
+import click
+from weaviate_cli.managers.config_manager import ConfigManager
+from weaviate_cli.commands.create import create
+from weaviate_cli.commands.delete import delete
+from weaviate_cli.commands.get import get
+from weaviate_cli.commands.update import update
+from weaviate_cli.commands.query import query
+from weaviate_cli.commands.restore import restore
+
+
+@click.group()
+@click.pass_context
+@click.option('--config-file', required=False, default=None, type=str, is_flag=False,
+              help="If specified cli uses the config specified with this path.")
+def main(ctx: click.Context, config_file):
+    ctx.obj = {
+        "config": ConfigManager(config_file)
+    }
+
+
+main.add_command(create)
+main.add_command(delete)
+main.add_command(get)
+main.add_command(update)
+main.add_command(restore)
+main.add_command(query)
+if __name__ == "__main__":
+    main()
