c5af4b388120bcf3d04b84d962f48afe1c95b53d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-29 05:03:04 +0000

    Add package brigid/0.9.0/brigid-0.9.0-py3-none-any.whl

---
+++ packages/brigid/brigid-0.9.0-py3-none-any.whl/brigid/theme/default_translations.py
@@ -0,0 +1,30 @@
+translations = {
+    "ru": {
+        "read_more": "Далее",
+        "no_posts_found": "Ничего не найдено.",
+        "pagination_first_page": "к новым",
+        "pagination_last_page": "к старым",
+        "tags_filter_header": "Нашли постов:",
+        "article_similar_header": "Похожее",
+        "last_posts_header": "Последние посты",
+        "similarity_score_tooltip": "Уровень похожести",
+        "license": "лицензия",
+        "content_repository": "исходники постов",
+        "site_is_created_with": "сайт создан с помощью",
+        "icons_attribution": 'сайт использует <a href="https://github.com/tabler/tabler-icons" target="_blank">Tabler Icons</a>, лицензированные под <a href="https://github.com/tabler/tabler-icons/blob/main/LICENSE" target="_blank">лицензией MIT</a>',  # noqa
+    },
+    "en": {
+        "read_more": "Read more",
+        "no_posts_found": "Nothing found.",
+        "pagination_first_page": "to new",
+        "pagination_last_page": "to old",
+        "tags_filter_header": "Found posts:",
+        "article_similar_header": "Similar",
+        "last_posts_header": "Last posts",
+        "similarity_score_tooltip": "Similarity score",
+        "license": "license",
+        "content_repository": "content repository",
+        "site_is_created_with": "site is created with",
+        "icons_attribution": 'site is using <a href="https://github.com/tabler/tabler-icons" target="_blank">Tabler Icons</a>, licensed under the <a href="https://github.com/tabler/tabler-icons/blob/main/LICENSE" target="_blank">MIT License</a>',  # noqa
+    },
+}
