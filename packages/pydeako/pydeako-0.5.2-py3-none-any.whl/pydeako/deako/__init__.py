b835aab7c8c5cbb54b22f3f6d8e908f52f6d6335
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-29 06:03:14 +0000

    Add package pydeako/0.5.2/pydeako-0.5.2-py3-none-any.whl

---
+++ packages/pydeako/pydeako-0.5.2-py3-none-any.whl/pydeako/deako/__init__.py
@@ -0,0 +1,3 @@
+"""Module for controlling deako devices locally."""
+
+from ._deako import Deako, FindDevicesTimeout, DeviceListTimeout
---
+++ packages/pydeako/pydeako-0.5.2-py3-none-any.whl/pydeako/models/__init__.py
@@ -0,0 +1,11 @@
+"""
+This module provides the types and functions to generate requests
+to deako devices. Source reference:
+https://github.com/DeakoLights/local-integrations/blob/master/API.md
+"""
+from ._const import RequestType, ResponseType
+from ._request import (
+    device_list_request,
+    device_ping_request,
+    state_change_request,
+)
