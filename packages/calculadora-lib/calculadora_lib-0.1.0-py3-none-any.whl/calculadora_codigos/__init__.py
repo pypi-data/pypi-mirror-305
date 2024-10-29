1dbdf162086c041a9825dd1aa2a149db633d1f9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-29 05:04:08 +0000

    Add package calculadora-lib/0.1.0/calculadora_lib-0.1.0-py3-none-any.whl

---
+++ packages/calculadora-lib/calculadora_lib-0.1.0-py3-none-any.whl/calculadora_codigos/__init__.py
@@ -0,0 +1,3 @@
+from .soma import *
+
+
---
+++ packages/calculadora-lib/calculadora_lib-0.1.0-py3-none-any.whl/calculadora_codigos/soma.py
@@ -0,0 +1,14 @@
+def Soma(x: int, y: int) -> int:
+        
+    ''' 
+    FAZ UMA SOMA
+
+    Args:
+    x(int): O primeiro número
+    y(int): O segundo número
+
+    Returns:
+        int: A soma dos dois números
+
+    '''
+    return x + y
