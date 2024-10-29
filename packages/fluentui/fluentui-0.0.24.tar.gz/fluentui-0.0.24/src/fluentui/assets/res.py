72ee3164b39fc1a06b9a51ed7fff41e4b8ad124c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-29 04:03:15 +0000

    Add package fluentui/0.0.24/fluentui-0.0.24.tar.gz

---
+++ packages/fluentui/fluentui-0.0.24.tar.gz/fluentui-0.0.24/src/fluentui/assets/res.py
@@ -0,0 +1,60 @@
+# Resource object code (Python 3)
+# Created by: object code
+# Created by: The Resource Compiler for Qt version 6.8.0
+# WARNING! All changes made in this file will be lost!
+
+from PySide6 import QtCore
+
+qt_resource_data = b"\
+\x00\x00\x01D\
+<\
+svg fill=\x22curren\
+tColor\x22\x0d\x0a     ar\
+ia-hidden=\x22true\x22\
+\x0d\x0a     width=\x2220\
+\x22\x0d\x0a     height=\x22\
+20\x22\x0d\x0a     viewBo\
+x=\x220 0 20 20\x22\x0d\x0a \
+    xmlns=\x22http:\
+//www.w3.org/200\
+0/svg\x22\x0d\x0a>\x0d\x0a    <\
+path d=\x22M15.85 7\
+.65c.2.2.2.5 0 .\
+7l-5.46 5.49a.55\
+.55 0 0 1-.78 0L\
+4.15 8.35a.5.5 0\
+ 1 1 .7-.7L10 12\
+.8l5.15-5.16c.2-\
+.2.5-.2.7 0Z\x22\x0d\x0a \
+         fill=\x22#\
+616161\x22/>\x0d\x0a</svg\
+>\x0d\x0a\
+"
+
+qt_resource_name = b"\
+\x00\x05\
+\x00o\xa6S\
+\x00i\
+\x00c\x00o\x00n\x00s\
+\x00\x10\
+\x0e\x17*\x87\
+\x00c\
+\x00h\x00e\x00v\x00r\x00o\x00n\x00_\x00d\x00o\x00w\x00n\x00.\x00s\x00v\x00g\
+"
+
+qt_resource_struct = b"\
+\x00\x00\x00\x00\x00\x02\x00\x00\x00\x01\x00\x00\x00\x01\
+\x00\x00\x00\x00\x00\x00\x00\x00\
+\x00\x00\x00\x00\x00\x02\x00\x00\x00\x01\x00\x00\x00\x02\
+\x00\x00\x00\x00\x00\x00\x00\x00\
+\x00\x00\x00\x10\x00\x00\x00\x00\x00\x01\x00\x00\x00\x00\
+\x00\x00\x01\x92\xcc\xed7\xfd\
+"
+
+def qInitResources():
+    QtCore.qRegisterResourceData(0x03, qt_resource_struct, qt_resource_name, qt_resource_data)
+
+def qCleanupResources():
+    QtCore.qUnregisterResourceData(0x03, qt_resource_struct, qt_resource_name, qt_resource_data)
+
+qInitResources()
---
+++ packages/fluentui/fluentui-0.0.24.tar.gz/fluentui-0.0.24/src/fluentui/widgets/button.py
@@ -0,0 +1,88 @@
+from typing import Callable
+
+from PySide6.QtCore import QSize
+from PySide6.QtGui import QIcon, QPixmap
+from PySide6.QtWidgets import QPushButton, QCheckBox
+
+from .style import Qss
+from .widget import WidgetMix
+
+
+class AbsButtonMix(WidgetMix):
+    def __init__(self,
+                 text: str = '', *,
+                 icon: QIcon | str = None,
+                 icon_size: int | tuple[int, int] = None,
+                 checked=False,
+                 checkable: bool = None,
+                 auto_exclusive: bool = None,
+                 on_toggled: Callable[[bool], None] = None,
+                 **kwargs
+                 ):
+        super().__init__(**kwargs)
+        if on_toggled: self.toggled.connect(on_toggled)
+        if auto_exclusive is not None: self.setAutoExclusive(auto_exclusive)
+        if checkable is not None: self.setCheckable(checkable)
+
+        self.setText(text)
+        self.setChecked(checked)
+
+        if icon_size is not None:
+            if isinstance(icon_size, int):
+                icon_size = (icon_size, icon_size)
+            self.setIconSize(*icon_size)
+        if icon: self.setIcon(icon)
+
+    def setIcon(self, icon: QIcon | QPixmap | str) -> None:
+        icon = QIcon(icon) if isinstance(icon, str) else icon
+        super().setIcon(icon)
+
+    def setIconSize(self, w: int, h: int) -> None:
+        super().setIconSize(QSize(w, h))
+
+
+class Button(AbsButtonMix, QPushButton):
+    def __init__(self, text='', *,
+                 default: bool = None,
+                 auto_default: bool = None,
+                 appear='',
+                 **kwargs
+                 ):
+        super().__init__(text, **kwargs)
+        self.on_key_enter_pressed.connect(self.on_clicked.emit)
+        if default is not None: self.setDefault(default)
+        if auto_default is not None: self.setAutoDefault(auto_default)
+
+        if appear == '':
+            self.setStyleSheet(Qss({
+                'background-color': '#fff',
+                'border': '1 solid #d1d1d1',
+                'padding': '6 12',
+                'border-radius': '4',
+                ':hover': {
+                    'background-color': '#f5f5f5',
+                    'border': '1 solid #c7c7c7',
+                },
+                ':pressed': {
+                    'background-color': '#e0e0e0',
+                    'border': '1 solid #b3b3b3',
+                },
+            }).merge(self.qss))
+        elif appear == 'subtle':
+            self.setStyleSheet(Qss({
+                'background-color': ' transparent',
+                'border': '1 solid transparent',
+                'padding': '6 12',
+                'border-radius': '4',
+                ':hover': {'background-color': '#f5f5f5'},
+                ':pressed': {'background-color': '#e0e0e0'},
+            }).merge(self.qss))
+
+
+class SubtleButton(Button):
+    def __init__(self, text='', **kwargs):
+        super().__init__(text, appear='subtle', **kwargs)
+
+
+class CheckBox(AbsButtonMix, QCheckBox):
+    ...
