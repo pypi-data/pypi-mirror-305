8d122c93e76d9edc9c51e19822f63377bbf49a88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-29 04:02:55 +0000

    Add package xaux/0.2.1/xaux-0.2.1.tar.gz

---
+++ packages/xaux/xaux-0.2.1.tar.gz/xaux-0.2.1/xaux/tools/__init__.py
@@ -0,0 +1,7 @@
+# copyright ############################### #
+# This file is part of the Xaux Package.    #
+# Copyright (c) CERN, 2024.                 #
+# ######################################### #
+
+from .general_tools import *
+from .release_tools import dev_release, dev_make_release_branch, dev_rename_release_branch
