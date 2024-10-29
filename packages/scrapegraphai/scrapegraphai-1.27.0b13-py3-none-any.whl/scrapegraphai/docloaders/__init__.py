4ab4feba3c04c80394bf47eb2143c1ab7d0ecec3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-29 16:05:05 +0000

    Add package scrapegraphai/1.27.0b13/scrapegraphai-1.27.0b13-py3-none-any.whl

---
+++ packages/scrapegraphai/scrapegraphai-1.27.0b13-py3-none-any.whl/scrapegraphai/docloaders/__init__.py
@@ -0,0 +1,5 @@
+"""__init__.py file for docloaders folder"""
+
+from .chromium import ChromiumLoader
+from .browser_base import browser_base_fetch
+from .scrape_do import scrape_do_fetch
---
+++ packages/scrapegraphai/scrapegraphai-1.27.0b13-py3-none-any.whl/scrapegraphai/models/__init__.py
@@ -0,0 +1,7 @@
+"""
+    __init__.py file for models folder
+"""
+from .openai_itt import OpenAIImageToText
+from .openai_tts import OpenAITextToSpeech
+from .deepseek import DeepSeek
+from .oneapi import OneApi
