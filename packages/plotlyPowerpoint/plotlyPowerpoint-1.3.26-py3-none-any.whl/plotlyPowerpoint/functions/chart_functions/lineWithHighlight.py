b4840cbf5cd54504e3002cd3d562868cfe7837d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-29 06:03:30 +0000

    Add package plotlyPowerpoint/1.3.26/plotlyPowerpoint-1.3.26-py3-none-any.whl

---
+++ packages/plotlyPowerpoint/plotlyPowerpoint-1.3.26-py3-none-any.whl/plotlyPowerpoint/functions/chart_functions/lineWithHighlight.py
@@ -0,0 +1,128 @@
+import plotly.express as px
+import plotly.graph_objects as go
+
+def createLineWithHighlightChart(df, chartDefinition, colors):
+
+    #Form figure
+    fig = go.Figure()
+
+    #Iterate through metrics
+    annotations = []
+    for i in range(len(chartDefinition['metrics'])):
+
+        #determine fill type
+        if 'options' in chartDefinition:
+            if 'chart-type' in chartDefinition['options']:
+                if chartDefinition['options']['chart-type'] == 'area':
+                    fillType = 'tozeroy'
+                else:
+                    fillType = 'none'
+            else:
+                fillType = 'none'
+        else:
+            fillType = 'none'
+
+        #add main line
+        fig.add_trace(go.Scatter(x=df[chartDefinition['axis']],
+                                    y=df[chartDefinition['metrics'][i]['name']],
+                                    mode='lines',
+                                    name=chartDefinition['metrics'][i]['prettyName'],
+                                    line = dict(color=colors[i]),
+                                    fill=fillType
+                                )
+                        )        
+
+        #Add the ending marker
+        fig.add_trace(go.Scatter(            
+            x=[df[chartDefinition['axis']][len(df)-1]],
+            y=[df[chartDefinition['metrics'][i]['name']][len(df)-1]],
+            mode='markers',
+            marker=dict(color=colors[i], size=8)            
+        ))
+
+        #determine our text size
+        if 'options' in chartDefinition:
+            if 'ending-label-size' in chartDefinition['options']:
+                textSize = int(chartDefinition['options']['ending-label-size'])
+            else:
+                textSize = 14
+        else:
+            textSize = 14
+
+        #Add an annotation for the text itself   
+        annotations.append(
+            {
+                'xref': 'paper', 'x': 1.003, 'y': df[chartDefinition['metrics'][i]['name']][len(df)-1],
+                'xanchor': 'left', 'yanchor': 'middle', 'text': str(df[chartDefinition['metrics'][i]['prettyColumn']][len(df)-1]),
+                'showarrow': False, 'font': {'size': textSize, 'color': colors[i]}
+            }
+        )
+
+    #add annotations 
+    fig.update_layout(
+        annotations=annotations
+    )   
+
+    #change aesthetics
+    fig.update_layout({
+        'plot_bgcolor': 'rgba(0, 0, 0, 0)',
+        'paper_bgcolor': 'rgba(0, 0, 0, 0)',
+    })
+
+    ### Handle all options
+    if 'options' in chartDefinition:
+
+        ### Grid lines
+        if 'horizontal-grid-lines' in chartDefinition['options']:
+            if chartDefinition['options']['horizontal-grid-lines'] == 'true':
+                fig.update_yaxes(showgrid=True, gridwidth=1, gridcolor='#ebebeb')
+
+        if 'vertical-grid-lines' in chartDefinition['options']:
+            if chartDefinition['options']['vertical-grid-lines'] == 'true':
+                fig.update_xaxes(showgrid=True, gridwidth=1, gridcolor='#ebebeb', title="")
+
+        ### X axis ticks rotation
+        if 'x-axis-ticks-angle' in chartDefinition['options']:
+            fig.update_xaxes(nticks=df[chartDefinition['axis']].nunique(), tickangle=chartDefinition['options']['x-axis-ticks-angle'])
+
+        ### Background color
+        if 'background-color' in chartDefinition['options']:
+            fig.update_layout({
+                'plot_bgcolor': chartDefinition['options']['background-color'],
+                'paper_bgcolor': chartDefinition['options']['background-color']
+            })
+
+        ## Text Size - X Axis
+        if 'x-axis-text-size' in chartDefinition['options']:
+            fig.update_layout( xaxis = dict( tickfont = dict(size=int(chartDefinition['options']['x-axis-text-size']))))
+
+        ## Text Size - Y Axis
+        if 'y-axis-text-size' in chartDefinition['options']:
+            fig.update_layout( yaxis = dict( tickfont = dict(size=int(chartDefinition['options']['y-axis-text-size']))))
+
+
+    #hide legend for now
+    fig.update_layout(showlegend=False)
+
+    #X axis title
+    if 'x-axis-title' in chartDefinition:
+        fig.update_layout(
+            xaxis_title=chartDefinition['x-axis-title']
+        )
+
+    #Y axis title
+    if 'y-axis-title' in chartDefinition:
+        fig.update_layout(
+            yaxis_title=chartDefinition['y-axis-title']
+        )
+
+    #Force y axis to 0
+    fig.update_yaxes(rangemode="tozero")
+
+    #if we want to change the y axis type
+    if 'y-axis-type' in chartDefinition:
+        if chartDefinition['y-axis-type'] == 'percent':
+            fig.layout.yaxis.tickformat = ',.0%'
+
+    #return
+    return fig
