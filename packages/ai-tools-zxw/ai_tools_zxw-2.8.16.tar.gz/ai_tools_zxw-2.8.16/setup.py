3f1733852426032832c4338e904e895880354e5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-29 15:04:13 +0000

    Add package ai-tools-zxw/2.8.16/ai_tools_zxw-2.8.16.tar.gz

---
+++ packages/ai-tools-zxw/ai_tools_zxw-2.8.16.tar.gz/ai_tools_zxw-2.8.16/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ai_tools_zxw",
+    version="2.8.16",
+    packages=find_packages(),
+    install_requires=[
+        'xlwt',
+        'psutil',
+    ],
+    author="xue wei zhang",
+    author_email="",
+    description="薛伟的AI工具集",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/sunshineinwater/",
+    classifiers=[
+        "Programming Language :: Python :: 3.10",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
