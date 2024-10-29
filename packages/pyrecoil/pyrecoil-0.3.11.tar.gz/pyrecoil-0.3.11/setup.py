357fa36d6c61aad4cf2d5150281b233228f394cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-29 15:04:36 +0000

    Add package pyrecoil/0.3.11/pyrecoil-0.3.11.tar.gz

---
+++ packages/pyrecoil/pyrecoil-0.3.11.tar.gz/pyrecoil-0.3.11/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages, Extension
+
+recoil_module = Extension('_recoil', sources=['recoil_interface.c', 'recoil.c'])
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+setup(
+    name="pyrecoil",
+    version="0.3.11",
+    packages=['recoil'],
+    ext_modules=[recoil_module],
+    include_package_data=True,
+
+    author="Matt Westcott",
+    author_email="matt@west.co.tt",
+    description="Python bindings for RECOIL, the Retro Computer Image Library",
+    long_description=readme(),
+    python_requires=">=3.8",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Topic :: Multimedia :: Graphics",
+        "Topic :: Multimedia :: Graphics :: Graphics Conversion",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
+        "License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)",
+    ],
+    url="http://github.com/demozoo/pyrecoil",
+    license="GPLv2+",
+)
