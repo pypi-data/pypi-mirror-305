897dc06b091d7c721f5ee6e2b8719a8799b39f25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-29 17:04:48 +0000

    Add package xrdPlanner/2.1.1/xrdplanner-2.1.1.tar.gz

---
+++ packages/xrdPlanner/xrdplanner-2.1.1.tar.gz/xrdplanner-2.1.1/xrdPlanner/__init__.py
@@ -0,0 +1,6 @@
+__version__ = '2.1.1'
+__author__ = 'Lennard Krause'
+__authors__ = ['Lennard Krause', 'Frederik H. Gjørup', 'Mads R. V. Jørgensen']
+__email__ = 'lkrause@chem.au.dk'
+__date__ = '29.10.2024'
+__year__ = '2024'
