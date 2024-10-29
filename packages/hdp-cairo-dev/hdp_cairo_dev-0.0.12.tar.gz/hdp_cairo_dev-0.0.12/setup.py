1afbc7f12764ec72967f540ab3c812bdc1c7f0cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-29 17:03:49 +0000

    Add package hdp-cairo-dev/0.0.12/hdp_cairo_dev-0.0.12.tar.gz

---
+++ packages/hdp-cairo-dev/hdp_cairo_dev-0.0.12.tar.gz/hdp_cairo_dev-0.0.12/setup.py
@@ -0,0 +1,45 @@
+from setuptools import setup
+from setuptools.command.install import install
+from pathlib import Path
+
+# Read version from version.py
+version = {}
+with open("version.py") as fp:
+    exec(fp.read(), version)
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "package.md").read_text()
+
+
+class PostInstallCommand(install):
+    """Custom post-installation for installation mode."""
+
+    def run(self):
+        install.run(self)
+
+
+# Read the requirements from the requirements.txt file
+with open("tools/make/requirements.txt") as requirements_file:
+    requirements = requirements_file.read().splitlines()
+
+setup(
+    name="hdp-cairo-dev",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    version=version["__version__"],
+    packages=[
+        "tools",
+        "contract_bootloader",
+    ],
+    install_requires=requirements,
+    package_dir={
+        "tools": "tools",
+        "contract_bootloader": "src/contract_bootloader",
+    },
+    zip_safe=False,
+    package_data={
+        "tools": ["**/*.py"],
+        "contract_bootloader": ["*.cairo", "*/*.cairo", "*/*.py", "**/*.py"],
+    },
+    cmdclass={"install": PostInstallCommand},
+)
