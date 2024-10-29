a7f9dc2534c9cfc95effbc625770f503a05c7d10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-29 14:03:41 +0000

    Add package signalflow/0.5.1/signalflow-0.5.1-cp312-cp312-win_amd64.whl

---
+++ packages/signalflow/signalflow-0.5.1-cp312-cp312-win_amd64.whl/signalflow_visualisation/__init__.py
@@ -0,0 +1,14 @@
+"""""" # start delvewheel patch
+def _delvewheel_patch_1_8_3():
+    import os
+    if os.path.isdir(libs_dir := os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, 'signalflow.libs'))):
+        os.add_dll_directory(libs_dir)
+
+
+_delvewheel_patch_1_8_3()
+del _delvewheel_patch_1_8_3
+# end delvewheel patch
+
+from .patch_structure import visualise_patch_structure
+from .node import plot_node_output
+from .buffer import plot_buffer
