b27bb2eb39017b962435e00dd73d3c92beb1d081
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-29 05:03:12 +0000

    Add package optima-cli/1.0.1/optima_cli-1.0.1.tar.gz

---
+++ packages/optima-cli/optima_cli-1.0.1.tar.gz/optima_cli-1.0.1/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="optima-cli",
+    version="1.0.1",
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    include_package_data=True,
+    package_data={
+        'optima': ['unimelb-mf-clients-0.7.8/**/*'],
+    },
+    install_requires=[
+        "click>=8.1.0",
+        "python-dotenv>=0.19.0",
+        "requests>=2.32.0",
+        "setuptools>=74.1.0",
+    ],
+    entry_points={
+        "console_scripts": [
+            "op=optima.cli:cli",
+        ],
+    },
+    author="Ashley Wang",
+    author_email="xiaohuyaw@gmail.com",
+    description="OPTIMA CLI",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Environment :: Console", 
+    ],
+    python_requires=">=3.11",
+)
