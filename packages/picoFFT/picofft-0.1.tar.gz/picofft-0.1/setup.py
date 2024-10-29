d887a5707fe408ac0919c51318785fbcf2b49144
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-29 15:03:28 +0000

    Add package picoFFT/0.1/picofft-0.1.tar.gz

---
+++ packages/picoFFT/picofft-0.1.tar.gz/picofft-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='picoFFT',
+    version='0.1',
+    description='picoFFT: impossible is possible LLM',
+    author='Timur Ishuov',
+    packages=find_packages()
+)
