7dd51b3c2ed51ab44f79694ce61cfcac95ca4151
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-29 14:04:04 +0000

    Add package ratify/0.1.0/ratify-0.1.0.tar.gz

---
+++ packages/ratify/ratify-0.1.0.tar.gz/ratify-0.1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ratify',
+    packages=find_packages(include=['ratify']),
+    version='0.1.0',
+    description='A simple validation library',
+    author='tobiy23@gmail.com',
+    python_requires='>=3.6',
+)
