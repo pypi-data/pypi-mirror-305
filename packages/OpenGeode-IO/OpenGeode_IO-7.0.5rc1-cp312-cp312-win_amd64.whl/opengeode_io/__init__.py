f69b6d20bfdf30b7df57dccbed824f1e711064b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-29 16:04:11 +0000

    Add package OpenGeode-IO/7.0.5rc1/OpenGeode_IO-7.0.5rc1-cp312-cp312-win_amd64.whl

---
+++ packages/OpenGeode-IO/OpenGeode_IO-7.0.5rc1-cp312-cp312-win_amd64.whl/opengeode_io/__init__.py
@@ -0,0 +1,8 @@
+## Copyright (c) 2019 - 2024 Geode-solutions
+
+import os, pathlib
+os.add_dll_directory(pathlib.Path(__file__).parent.resolve().joinpath('bin'))
+
+from .image import *
+from .mesh import *
+from .model import *
