917cce960b67bc0f4e312d601ca654f0098bc414
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-29 16:05:12 +0000

    Add package gpt4all-pypi-part-002/0.0.3/gpt4all-pypi-part_002-0.0.3.tar.gz

---
+++ packages/gpt4all-pypi-part-002/gpt4all-pypi-part_002-0.0.3.tar.gz/gpt4all-pypi-part_002-0.0.3/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+import os
+def package_files(directory):
+	paths = []
+	for (path, directories, filenames) in os.walk(directory):
+		for filename in filenames:
+			paths.append(os.path.relpath(os.path.join(path, filename), directory))
+	return paths
+
+
+binary_files = package_files('src/gpt4all-pypi-part_002/input')
+
+setup(
+	name='gpt4all-pypi-part_002',
+	version='0.0.3',
+	description='Package with binary files and subfolders',
+	packages=find_packages(where='src'),
+	package_dir={'': 'src'},
+	package_data = {
+		'gpt4all-pypi-part_002': binary_files,
+	},
+	include_package_data=True,
+	install_requires=['gpt4all-pypi-part_001==0.0.3', ],
+)
