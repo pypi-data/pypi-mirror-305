7f2c8792c1146edfebfe610a0c12ba572e833c8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-29 05:04:19 +0000

    Add package mypy-boto3-opensearch/1.35.50/mypy_boto3_opensearch-1.35.50-py3-none-any.whl

---
+++ packages/mypy-boto3-opensearch/mypy_boto3_opensearch-1.35.50-py3-none-any.whl/mypy_boto3_opensearch/__init__.py
@@ -0,0 +1,23 @@
+"""
+Main interface for opensearch service.
+
+Usage::
+
+    ```python
+    from boto3.session import Session
+    from mypy_boto3_opensearch import (
+        Client,
+        OpenSearchServiceClient,
+    )
+
+    session = Session()
+    client: OpenSearchServiceClient = session.client("opensearch")
+    ```
+"""
+
+from .client import OpenSearchServiceClient
+
+Client = OpenSearchServiceClient
+
+
+__all__ = ("Client", "OpenSearchServiceClient")
