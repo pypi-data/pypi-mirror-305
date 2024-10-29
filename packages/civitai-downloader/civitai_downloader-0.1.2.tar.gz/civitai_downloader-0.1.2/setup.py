4a56bf884937e4c4c0a0d5972732be2e3cd5c349
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-29 05:03:41 +0000

    Add package civitai-downloader/0.1.2/civitai_downloader-0.1.2.tar.gz

---
+++ packages/civitai-downloader/civitai_downloader-0.1.2.tar.gz/civitai_downloader-0.1.2/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='civitai-downloader',
+    version='0.1.2',
+    packages=find_packages(),
+    install_requires=[],
+    entry_points={
+        'console_scripts': [
+            'civitai-downloader=civitai_downloader.downloader:main',
+        ],
+    },
+    author='Ryouko-Yamanda65777',
+    description='A downloader for CivitAI models',
+    long_description=long_description,  # Include README.md content
+    long_description_content_type='text/markdown',  # Specify markdown format
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
