843ffd044f502b9d63c642f57c9b803fe52c886f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-29 15:04:02 +0000

    Add package spxl/0.0.7/spxl-0.0.7.tar.gz

---
+++ packages/spxl/spxl-0.0.7.tar.gz/spxl-0.0.7/src/spxl/__init__.py
@@ -0,0 +1,26 @@
+#--------------------------------------------------------------------------------
+# 참조 모듈 목록.
+#--------------------------------------------------------------------------------
+from __future__ import annotations
+from typing import Awaitable, Callable, Final, Generic, Iterator, Optional, Sequence, Type, TypeVar, Union, Tuple, Any, List, Dict, Set, cast, overload
+import pkgutil
+
+
+#--------------------------------------------------------------------------------
+# 패키지 안의 클래스 별칭 목록.
+#--------------------------------------------------------------------------------
+from .core import AnonymousObject, UnnamedClass
+from .core import BaseClass, Object
+from .core import BaseConstant, Constant
+from .core import BaseMetaClass, MetaClass, Meta
+from .core import BaseNode, Node
+from .core import Builtins
+# from .deprecated import BaseNode
+from .exception import SingletonException
+from .filesystem import Entry, Directory, Drive, File, Storage
+# from .future import EventNode, Node
+from .manager import BaseRepository, Repository, SharedClass, Singleton
+from .decorator import overridemethod, basemethod
+from .enumflag import EnumFlag, auto
+from .environment import PlatformType, GetPlatformType
+from .path import Path
