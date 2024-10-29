95e0642a31f1d6e2f18fa183dbda13adcffc5d86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-29 04:02:55 +0000

    Add package make87-messages/0.0.35/make87_messages-0.0.35.tar.gz

---
+++ packages/make87-messages/make87_messages-0.0.35.tar.gz/make87_messages-0.0.35/make87_messages/image/compressed/image_png_pb2.pyi
@@ -0,0 +1,14 @@
+from google.protobuf import timestamp_pb2 as _timestamp_pb2
+from google.protobuf import descriptor as _descriptor
+from google.protobuf import message as _message
+from typing import ClassVar as _ClassVar, Mapping as _Mapping, Optional as _Optional, Union as _Union
+
+DESCRIPTOR: _descriptor.FileDescriptor
+
+class ImagePNG(_message.Message):
+    __slots__ = ["data", "timestamp"]
+    DATA_FIELD_NUMBER: _ClassVar[int]
+    TIMESTAMP_FIELD_NUMBER: _ClassVar[int]
+    data: bytes
+    timestamp: _timestamp_pb2.Timestamp
+    def __init__(self, timestamp: _Optional[_Union[_timestamp_pb2.Timestamp, _Mapping]] = ..., data: _Optional[bytes] = ...) -> None: ...
