a12766c2bcf01c4ae9aa11104651ef56d4b51233
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-29 05:03:05 +0000

    Add package pyHaveIBeenPwned/0.1.16/pyHaveIBeenPwned-0.1.16.tar.gz

---
+++ packages/pyHaveIBeenPwned/pyHaveIBeenPwned-0.1.16.tar.gz/pyHaveIBeenPwned-0.1.16/README.md
@@ -0,0 +1,17 @@
+pyHaveIBeenPwned (https://shanewilliamscott.com)
+==
+[![Python package](https://github.com/Hackman238/pyHaveIBeenPwned/actions/workflows/master.yml/badge.svg)](https://github.com/Hackman238/pyHaveIBeenPwned/actions/workflows/master.yml)
+[![Known Vulnerabilities](https://snyk.io/test/github/Hackman238/pyHaveIBeenPwned/badge.svg?targetFile=requirements.txt)](https://snyk.io/test/github/Hackman238/pyHaveIBeenPwned?targetFile=requirements.txt)
+[![Maintainability](https://api.codeclimate.com/v1/badges/1e12401a7a3b46c08549/maintainability)](https://codeclimate.com/github/Hackman238/pyHaveIBeenPwned/maintainability)
+
+## Authors:
+Shane Scott
+
+## About pyHaveIBeenPwned
+Python library to query HaveIBeenPwned.com with handling for CloudFlare anti-bot.
+
+## Installation
+pip3 install pyHaveIBeenPwned
+
+## Credits
+Based on fork from https://github.com/GoVanguard/pyExploitDb by Shane Scott.
---
+++ packages/pyHaveIBeenPwned/pyHaveIBeenPwned-0.1.16.tar.gz/pyHaveIBeenPwned-0.1.16/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pyHaveIBeenPwned",
+    version="0.1.16",
+    author="Shane Scott",
+    author_email="gs@shanewilliamscott.com",
+    description="Library to query HaveIBeenPwned.com with handeling for CloudFlare anti-bot ",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Hackman238/pyHaveIBeenPwned",
+    packages=['pyHaveIBeenPwned'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: POSIX :: Linux',
+        'Topic :: Software Development :: Libraries',
+    ],
+    python_requires='>=3.9',
+)
