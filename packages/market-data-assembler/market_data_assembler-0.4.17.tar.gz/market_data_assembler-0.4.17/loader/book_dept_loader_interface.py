116a966219f6a9e944a3927aa5665e955badd2a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-29 17:05:51 +0000

    Add package market-data-assembler/0.4.17/market_data_assembler-0.4.17.tar.gz

---
+++ packages/market-data-assembler/market_data_assembler-0.4.17.tar.gz/market_data_assembler-0.4.17/loader/book_dept_loader_interface.py
@@ -0,0 +1,14 @@
+from abc import ABC, abstractmethod
+from datetime import datetime
+from typing import List, Dict, Any
+
+
+class IBookDeptLoader(ABC):
+
+    @abstractmethod
+    def __init__(self, selected_date: datetime, instrument: str):
+        pass
+
+    @abstractmethod
+    def get_book_depths(self) -> List[Dict[str, Any]]:
+        pass
---
+++ packages/market-data-assembler/market_data_assembler-0.4.17.tar.gz/market_data_assembler-0.4.17/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='market-data-assembler',
+    version='0.4.17',
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+        'tenacity',
+        'pytz',
+    ],
+    author='Maksym Usanin (usanin.max@gmail.com)',
+    description='Market data provider',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.11",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.11',
+)
