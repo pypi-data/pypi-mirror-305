42a321e8ac94b5d8abc65fea2d32c9f64059e8fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-29 14:03:34 +0000

    Add package SimpleSerial/0.1.8/SimpleSerial-0.1.8.tar.gz

---
+++ packages/SimpleSerial/SimpleSerial-0.1.8.tar.gz/SimpleSerial-0.1.8/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="SimpleSerial",
+    version="0.1.8",
+    packages=find_packages(),
+    install_requires=[
+        "pyserial"
+    ],
+    author="Mekyle Hemani",
+    description="A tool with easy prebuilt function to communicate through PySerial",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/Mekyle-Hemani/SimpleSerial",
+)
