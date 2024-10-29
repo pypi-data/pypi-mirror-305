fda343b88191582ba46da89c540bfa8cabff4419
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-29 05:03:08 +0000

    Add package ors/0.1.4/ors-0.1.4-py3-none-any.whl

---
+++ packages/ors/ors-0.1.4-py3-none-any.whl/ors/client.py
@@ -0,0 +1,50 @@
+from dataclasses import asdict
+from typing import Callable
+from ors.types import Endpoint, HTTPClient, Response, Context, default_headers
+from urllib3 import PoolManager
+
+
+def context(
+    base_url: str = "https://api.openrouteservice.org/v2/",
+    auth: str | None = None,
+    profile: str = "driving-car",
+) -> Context:
+    headers = default_headers()
+    headers["Authorization"] = auth
+    return Context(base_url, headers, profile)
+
+
+def set_context(**kwargs) -> Callable[[Context], Context]:
+    def _(ctx: Context) -> Context:
+        for k, v in kwargs.items():
+            setattr(ctx, k, v)
+        return ctx
+
+    return _
+
+
+def override_context(**kwargs) -> Callable[[Context], Context]:
+    def _(ctx: Context) -> Context:
+        ctx_dict = asdict(ctx)
+        ctx_dict.update(kwargs)
+        return Context(**ctx_dict)
+
+    return _
+
+
+ContextOverride = Callable[[Context], Context]
+
+
+def client(
+    ctx: Context = context(),
+    http: HTTPClient = PoolManager(),
+) -> Callable[[Endpoint, ContextOverride | None], Response]:
+    def _(
+        endpoint: Endpoint,
+        context_override: ContextOverride | None = None,
+    ) -> Response:
+        if context_override:
+            return endpoint(http, context_override(ctx))
+        return endpoint(http, ctx)
+
+    return _
