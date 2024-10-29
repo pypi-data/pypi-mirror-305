49f26e51506439f94bc707718c6ebff943840266
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-29 17:04:41 +0000

    Add package nonebot-plugin-omb/0.1.5/nonebot_plugin_omb-0.1.5-py3-none-any.whl

---
+++ packages/nonebot-plugin-omb/nonebot_plugin_omb-0.1.5-py3-none-any.whl/nonebot_plugin_omb/__init__.py
@@ -0,0 +1,36 @@
+from nonebot import Bot, require
+from nonebot.plugin import PluginMetadata
+from nonebot.message import event_preprocessor
+from nonebot.exception import IgnoredException
+from nonebot.internal.adapter import Event
+
+from .util import SuperUserObj
+
+require("nonebot_plugin_omb.patch_base")
+
+supported_adapters = None
+
+
+@event_preprocessor
+async def only_me_check(bot: Bot, event: Event):
+    if not await SuperUserObj(bot, event):
+        raise IgnoredException("only superuser!")
+
+
+try:
+    require("nonebot_plugin_alconna")
+    from nonebot.plugin import inherit_supported_adapters
+
+    require("nonebot_plugin_omb.patch_alconna")
+    supported_adapters = inherit_supported_adapters("nonebot_plugin_alconna")
+except RuntimeError:
+    pass
+
+__plugin_meta__ = PluginMetadata(
+    name="Ohh My Bot",
+    description="我的Bot我做主~",
+    usage="无",
+    type="library",
+    homepage="https://github.com/eya46/nonebot-plugin-omb",
+    supported_adapters=supported_adapters,
+)
