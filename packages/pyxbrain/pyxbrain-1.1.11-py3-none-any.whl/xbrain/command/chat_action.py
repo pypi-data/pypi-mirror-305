18295d92f7af42d3b526aeb9c28d4c369ef3b255
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-30 15:04:27 +0000

    Add package pyxbrain/1.1.11/pyxbrain-1.1.11-py3-none-any.whl

---
+++ packages/pyxbrain/pyxbrain-1.1.11-py3-none-any.whl/xbrain/command/chat_action.py
@@ -0,0 +1,30 @@
+from xbrain import xbrain_tool
+from pydantic import BaseModel
+import signal
+from xbrain.chat import run
+from xbrain.context import Type
+class XBrainChatAction(BaseModel):
+    """chat with my action"""
+    pass
+
+@xbrain_tool.Tool(model=XBrainChatAction, hit_condition={Type.IS_XBRAIN_PROJECT: True})
+def chat_action():
+    print("Welcome to chat mode!")
+    global running
+    running = True
+    signal.signal(signal.SIGINT, signal_handler)  # Capture Ctrl + C signal
+    while running:
+        try:
+            input_str = input("💬 ")
+        # When exiting using ctrl + c, an EOFError exception is thrown
+        except EOFError:
+            break
+        if input_str == "exit":
+            break
+        res = run([{"role": "user", "content": input_str}], user_prompt=None, chat_model=True)
+        print(res)
+
+def signal_handler(sig, frame):
+    print("\nExiting chat mode, looking forward to seeing you again!")
+    global running
+    running = False
