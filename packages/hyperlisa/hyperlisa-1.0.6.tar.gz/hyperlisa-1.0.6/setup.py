4bd40af36ee0f1d04c72c03dea546d05ed9dd719
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-30 07:04:40 +0000

    Add package hyperlisa/1.0.6/hyperlisa-1.0.6.tar.gz

---
+++ packages/hyperlisa/hyperlisa-1.0.6.tar.gz/hyperlisa-1.0.6/setup.py
@@ -0,0 +1,31 @@
+import os
+import shutil
+from setuptools import setup, find_packages
+
+setup(
+    name="hyperlisa",
+    version="1.0.6",
+    description="A package for combining source code files into one",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Omar Venturi",
+    author_email="omar.venturi@hypertrue.com",
+    url="https://github.com/moonClimber/hyperlisa",  # URL del repository GitHub
+    packages=find_packages(),
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    entry_points={
+        "console_scripts": [
+            "combine-code=lisa._combine_code:main",  # Comando originale
+            "cmb=lisa._combine_code:main",  # Alias breve
+            "lisacmb=lisa._combine_code:main",  # Alias descrittivo
+            "hyperlisacmb=lisa._combine_code:main",  # Alias ancora più descrittivo
+            "hyperlisa-configure=lisa.configure:main",  # Comando per configurazione manuale
+        ],
+    },
+)
