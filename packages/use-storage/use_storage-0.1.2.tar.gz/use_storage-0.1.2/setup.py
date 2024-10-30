962f40f181bffb5130aadf87831d2ddf0caa5233
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-30 15:05:47 +0000

    Add package use-storage/0.1.2/use_storage-0.1.2.tar.gz

---
+++ packages/use-storage/use_storage-0.1.2.tar.gz/use_storage-0.1.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='use_storage',
+    version='0.1.2',
+    description='A package for storing key-value data with backup history.',
+    author='xilin',
+    author_email='infinite357@gmail.com',
+    packages=find_packages(),
+    install_requires=[],
+    python_requires='>=3.6',
+)
