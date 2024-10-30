70f1554b52e04a0a0ec57b302f6364648c0d2bcf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-30 15:05:22 +0000

    Add package file-scan-dd/0.1/file_scan_dd-0.1.tar.gz

---
+++ packages/file-scan-dd/file_scan_dd-0.1.tar.gz/file_scan_dd-0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='file_scan_dd',
+    version='0.1',
+    py_modules=['file_scan_dd'],
+    install_requires=[],
+    description='A simple library to scan files using metadefender',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
