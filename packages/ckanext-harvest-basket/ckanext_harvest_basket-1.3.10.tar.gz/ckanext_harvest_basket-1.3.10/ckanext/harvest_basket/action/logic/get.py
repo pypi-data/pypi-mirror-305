2f621e26f2193eab48c979e1af0fab5da9c83763
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-30 16:04:47 +0000

    Add package ckanext-harvest-basket/1.3.10/ckanext_harvest_basket-1.3.10.tar.gz

---
+++ packages/ckanext-harvest-basket/ckanext_harvest_basket-1.3.10.tar.gz/ckanext_harvest_basket-1.3.10/ckanext/harvest_basket/action/logic/get.py
@@ -0,0 +1,67 @@
+from __future__ import annotations
+
+import json
+from typing import Any, Optional
+
+from jsonschema import validate, ValidationError as SchemaValidationError
+
+import ckan.plugins.toolkit as tk
+
+from ckanext.harvest_basket.harvesters import (
+    DKANHarvester,
+    JunarHarvester,
+    SocrataHarvester,
+    ArcGISHarvester,
+    CustomCKANHarvester,
+    ODSHarvester,
+)
+from ckanext.transmute.utils import get_json_schema
+
+
+@tk.side_effect_free
+def check_source(ctx: dict[str, Any], data_dict: dict) -> dict[str, Any]:
+    tk.check_access("harvest_basket_check_source", ctx, data_dict)
+
+    source_name: str = tk.get_or_bust(data_dict, "source_name")
+    source_url: Optional[str] = data_dict.get("source_url")
+    config: dict = json.loads(data_dict.get("config", "").strip() or "{}")
+
+    if not source_url:
+        raise tk.ValidationError(f"The source URL must be provided to make a checkup")
+
+    sources = {
+        "dkan": DKANHarvester,
+        "junar": JunarHarvester,
+        "socrata": SocrataHarvester,
+        "arcgis": ArcGISHarvester,
+        "ckan": CustomCKANHarvester,
+        "ods": ODSHarvester,
+    }
+
+    harvester_class = sources.get(source_name)
+
+    if not harvester_class:
+        raise tk.ValidationError(
+            f"The source checkup for type `{source_name}` not implemented"
+        )
+
+    return harvester_class().make_checkup(source_url, source_name, config)
+
+
+@tk.side_effect_free
+def update_config(ctx: dict[str, Any], data_dict: dict) -> dict[str, Any]:
+    tk.check_access("harvest_basket_update_config", ctx, data_dict)
+
+    config_content: str = tk.get_or_bust(data_dict, "config")
+
+    try:
+        config: dict = json.loads(config_content)
+    except ValueError as e:
+        return str(e)
+
+    schema = get_json_schema()
+
+    try:
+        validate(config, schema)
+    except SchemaValidationError as e:
+        return f"{e.message}"
---
+++ packages/ckanext-harvest-basket/ckanext_harvest_basket-1.3.10.tar.gz/ckanext_harvest_basket-1.3.10/ckanext/harvest_basket/harvesters/__init__.py
@@ -0,0 +1,21 @@
+from ckanext.harvest_basket.harvesters.dkan_harvester import DKANHarvester
+from ckanext.harvest_basket.harvesters.junar_harvester import JunarHarvester
+from ckanext.harvest_basket.harvesters.socrata_harvester import SocrataHarvester
+from ckanext.harvest_basket.harvesters.arcgis_harvester import ArcGISHarvester
+from ckanext.harvest_basket.harvesters.ckan_harvester import CustomCKANHarvester
+from ckanext.harvest_basket.harvesters.ods_harvester import ODSHarvester
+from .dcat import BasketDcatJsonHarvester
+from .csw import BasketCswHarvester
+from .csiro import CsiroHarvester
+
+__all__ = [
+    "DKANHarvester",
+    "JunarHarvester",
+    "SocrataHarvester",
+    "ArcGISHarvester",
+    "CustomCKANHarvester",
+    "ODSHarvester",
+    "BasketDcatJsonHarvester",
+    "BasketCswHarvester",
+    "CsiroHarvester",
+]
