e364cbb58ddb22a173df79a04ad4d8c556ff7df3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-30 14:04:46 +0000

    Add package py-identity-model/0.8.2/py_identity_model-0.8.2.tar.gz

---
+++ packages/py-identity-model/py_identity_model-0.8.2.tar.gz/py_identity_model-0.8.2/tests/test_jwks.py
@@ -0,0 +1,27 @@
+import os
+
+from py_identity_model import JwksRequest, get_jwks
+from .test_utils import get_config
+
+TEST_JWKS_ADDRESS = get_config()["TEST_JWKS_ADDRESS"]
+
+
+def test_get_jwks_is_successful():
+    jwks_request = JwksRequest(address=TEST_JWKS_ADDRESS)
+    jwks_response = get_jwks(jwks_request)
+    assert jwks_response.is_successful
+    for key in jwks_response.keys:
+        assert key.kty
+        assert key.use
+        assert key.kid
+        assert key.n
+        assert key.e
+
+        if key.x5t:
+            assert key.x5c
+
+
+# def test_get_jwks_fails():
+#     jwks_request = JwksRequest(address='htt://not.a.real.address')
+#     jwks_response = get_jwks(jwks_request)
+#     assert jwks_response.is_successful is False
