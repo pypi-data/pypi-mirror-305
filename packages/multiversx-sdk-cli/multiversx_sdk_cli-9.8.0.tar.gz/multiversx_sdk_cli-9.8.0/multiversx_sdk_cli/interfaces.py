f555bd56d3889f325a078c3ddc8904acf1d4ee21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-30 04:05:10 +0000

    Add package multiversx-sdk-cli/9.8.0/multiversx_sdk_cli-9.8.0.tar.gz

---
+++ packages/multiversx-sdk-cli/multiversx_sdk_cli-9.8.0.tar.gz/multiversx_sdk_cli-9.8.0/multiversx_sdk_cli/interfaces.py
@@ -0,0 +1,42 @@
+from typing import Any, Dict, Protocol, Sequence
+
+
+class IAddress(Protocol):
+    def to_hex(self) -> str:
+        ...
+
+    def to_bech32(self) -> str:
+        ...
+
+
+class ITransaction(Protocol):
+    sender: str
+    receiver: str
+    gas_limit: int
+    chain_id: str
+    nonce: int
+    value: int
+    sender_username: str
+    receiver_username: str
+    gas_price: int
+    data: bytes
+    version: int
+    options: int
+    guardian: str
+    signature: bytes
+    guardian_signature: bytes
+    relayer: str
+
+    @property
+    def inner_transactions(self) -> Sequence["ITransaction"]:
+        ...
+
+
+class IAccount(Protocol):
+    def sign_transaction(self, transaction: ITransaction) -> str:
+        ...
+
+
+class ISimulateResponse(Protocol):
+    def to_dictionary(self) -> Dict[str, Any]:
+        ...
