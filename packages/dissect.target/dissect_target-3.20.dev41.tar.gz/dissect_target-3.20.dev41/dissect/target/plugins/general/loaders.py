6197383e2cd25ae7f35896d75da7233e797a5c39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-30 05:05:51 +0000

    Add package dissect.target/3.20.dev41/dissect_target-3.20.dev41.tar.gz

---
+++ packages/dissect.target/dissect_target-3.20.dev41.tar.gz/dissect_target-3.20.dev41/dissect/target/plugins/general/loaders.py
@@ -0,0 +1,26 @@
+from dissect.target.helpers.docs import INDENT_STEP, get_docstring
+from dissect.target.loader import LOADERS_BY_SCHEME
+from dissect.target.plugin import Plugin, export
+
+
+class LoaderListPlugin(Plugin):
+    """Plugin to list the available loaders."""
+
+    def check_compatible(self) -> None:
+        pass
+
+    @export(output="none")
+    def loaders(self) -> None:
+        """List the available loaders."""
+
+        loaders_info = {}
+        for key, loader in LOADERS_BY_SCHEME.items():
+            try:
+                docstring = get_docstring(loader, "No documentation.").splitlines()[0].strip()
+                loaders_info[key] = docstring
+            except ImportError:
+                continue
+
+        print("Available loaders:")
+        for loader_name, loader_description in sorted(loaders_info.items()):
+            print(f"{INDENT_STEP}{loader_name} - {loader_description}")
