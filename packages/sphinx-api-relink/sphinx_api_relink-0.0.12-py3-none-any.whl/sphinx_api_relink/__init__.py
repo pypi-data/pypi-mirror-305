af13eae407b291f2566eae857a1802c2fc094473
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-30 03:10:47 +0000

    Add package sphinx-api-relink/0.0.12/sphinx_api_relink-0.0.12-py3-none-any.whl

---
+++ packages/sphinx-api-relink/sphinx_api_relink-0.0.12-py3-none-any.whl/sphinx_api_relink/__init__.py
@@ -0,0 +1,225 @@
+# pyright: reportAttributeAccessIssue=false
+from __future__ import annotations
+
+import shutil
+from importlib.metadata import version
+from pathlib import Path
+from typing import TYPE_CHECKING, Any
+
+import sphinx.domains.python
+from docutils import nodes
+from sphinx.addnodes import pending_xref, pending_xref_condition
+from sphinx.ext.apidoc import main as sphinx_apidoc
+
+from sphinx_api_relink.linkcode import get_linkcode_resolve
+
+__SPHINX_VERSION = tuple(int(i) for i in version("sphinx").split(".") if i.isdigit())
+if __SPHINX_VERSION < (7, 3):
+    from sphinx.domains.python import parse_reftarget  # type:ignore[attr-defined]
+else:
+    from sphinx.domains.python._annotations import (  # type:ignore[import-not-found,no-redef]
+        parse_reftarget,  # noqa: PLC2701
+    )
+
+if TYPE_CHECKING:
+    from docutils.parsers.rst.states import Inliner
+    from sphinx.application import Sphinx
+    from sphinx.environment import BuildEnvironment
+    from sphinx.util.typing import RoleFunction
+
+
+def setup(app: Sphinx) -> dict[str, Any]:
+    app.add_config_value("api_github_repo", default=None, rebuild="env")
+    app.add_config_value("api_linkcode_debug", default=False, rebuild="env")
+    app.add_config_value("api_target_substitutions", default={}, rebuild="env")
+    app.add_config_value("api_target_types", default={}, rebuild="env")
+    app.add_config_value("generate_apidoc_directory", default="api", rebuild="env")
+    app.add_config_value("generate_apidoc_excludes", default=None, rebuild="env")
+    app.add_config_value("generate_apidoc_package_path", default=None, rebuild="env")
+    app.add_config_value("generate_apidoc_use_compwa_template", True, rebuild="env")
+    app.connect("config-inited", set_linkcode_resolve)
+    app.connect("config-inited", generate_apidoc)
+    app.connect("config-inited", replace_type_to_xref)
+    app.add_role("wiki", wiki_role("https://en.wikipedia.org/wiki/%s"))
+    return {
+        "parallel_read_safe": True,
+        "parallel_write_safe": True,
+    }
+
+
+def set_linkcode_resolve(app: Sphinx, _: BuildEnvironment) -> None:
+    github_repo: str | None = app.config.api_github_repo
+    if github_repo is None:
+        return
+    debug: bool = app.config.api_linkcode_debug
+    app.config.linkcode_resolve = get_linkcode_resolve(github_repo, debug)  # type:ignore[attr-defined]
+    app.setup_extension("sphinx.ext.linkcode")
+
+
+def generate_apidoc(app: Sphinx, _: BuildEnvironment) -> None:
+    config_key = "generate_apidoc_package_path"
+    package_path: list[str] | str | None = getattr(app.config, config_key, None)
+    if package_path is None:
+        return
+    apidoc_dir = Path(app.srcdir) / app.config.generate_apidoc_directory
+    shutil.rmtree(apidoc_dir, ignore_errors=True)
+    if isinstance(package_path, str):
+        package_dirs = [package_path]
+    else:
+        package_dirs = package_path
+    for rel_dir in package_dirs:
+        abs_package_path = Path(app.srcdir) / rel_dir
+        _run_sphinx_apidoc(
+            abs_package_path,
+            apidoc_dir,
+            excludes=app.config.generate_apidoc_excludes,
+            use_compwa_template=app.config.generate_apidoc_use_compwa_template,
+        )
+
+
+def _run_sphinx_apidoc(
+    package_path: Path,
+    apidoc_dir: str = "api",
+    excludes: list[str] | None = None,
+    use_compwa_template: bool = True,
+) -> None:
+    if not package_path.exists():
+        msg = f"Package under {package_path} does not exist"
+        raise FileNotFoundError(msg)
+    args: list[str] = [str(package_path)]
+    if excludes is None:
+        excludes = []
+    version_file = "version.py"
+    if (package_path / version_file).exists():
+        excludes.append(version_file)
+    for file in excludes:
+        excluded_path = package_path / file
+        if not excluded_path.exists():
+            msg = f"Excluded file {excluded_path} does not exist"
+            raise FileNotFoundError(msg)
+        args.append(str(package_path / file))
+    args.extend(f"-o {apidoc_dir} --force --no-toc --separate".split())
+    if use_compwa_template:
+        template_dir = Path(__file__).parent / "templates"
+        args.extend(f"--templatedir {template_dir}".split())
+    sphinx_apidoc(args)
+
+
+def replace_type_to_xref(app: Sphinx, _: BuildEnvironment) -> None:
+    target_substitutions = _get_target_substitutions(app)
+    ref_targets = {
+        k: v if isinstance(v, str) else v[1] for k, v in target_substitutions.items()
+    }
+    ref_types = _get_target_types(app)
+    ref_types.update({
+        v[1]: v[0] for v in target_substitutions.values() if isinstance(v, tuple)
+    })
+
+    def _new_type_to_xref(
+        target: str,
+        env: BuildEnvironment | None = None,  # type:ignore[assignment]
+        suppress_prefix: bool = False,
+    ) -> pending_xref:
+        reftype, target, title, refspecific = parse_reftarget(target, suppress_prefix)
+        target = ref_targets.get(target, target)
+        reftype = ref_types.get(target, reftype)
+        if env is None:
+            msg = "Environment cannot be None"
+            raise TypeError(msg)
+        return pending_xref(
+            "",
+            *_create_nodes(env, title),
+            refdomain="py",
+            reftype=reftype,
+            reftarget=target,
+            refspecific=refspecific,
+            **_get_env_kwargs(env),
+        )
+
+    if __SPHINX_VERSION < (7, 3):
+        sphinx.domains.python.type_to_xref = _new_type_to_xref  # pyright:ignore[reportPrivateImportUsage]
+    else:
+        sphinx.domains.python._annotations.type_to_xref = _new_type_to_xref  # noqa: SLF001
+
+
+def _get_target_substitutions(app: Sphinx) -> dict[str, str | tuple[str, str]]:
+    config_key = "api_target_substitutions"
+    target_substitutions = getattr(app.config, config_key, {})
+    if not isinstance(target_substitutions, dict):
+        msg = f"{config_key} must be a dict, got {type(target_substitutions).__name__}"
+        raise TypeError(msg)
+    for k, v in target_substitutions.items():
+        if not isinstance(k, str):
+            msg = f"{config_key} keys must be str, got {type(k).__name__}"
+            raise TypeError(msg)
+        if not isinstance(v, (str, tuple)):
+            msg = (
+                f"{config_key} values must be str or a tuple, got"
+                f" {type(v).__name__} for key {k!r}"
+            )
+            raise TypeError(msg)
+        if isinstance(v, tuple) and len(v) != 2:  # noqa: PLR2004
+            msg = (
+                f"If dict values of {config_key} are a tuple, they must have length 2,"
+                f" but got {len(v)} for key {k!r}"
+            )
+            raise TypeError(msg)
+    return target_substitutions
+
+
+def _get_target_types(app: Sphinx) -> dict[str, str]:
+    config_key = "api_target_types"
+    target_types = getattr(app.config, config_key, {})
+    if not isinstance(target_types, dict):
+        msg = f"{config_key} must be a dict, got {type(target_types).__name__}"
+        raise TypeError(msg)
+    for k, v in target_types.items():
+        if not isinstance(k, str):
+            msg = f"{config_key} keys must be str, got {type(k).__name__} for key {k!r}"
+            raise TypeError(msg)
+        if not isinstance(v, str):
+            msg = (
+                f"{config_key} values must be str, got {type(v).__name__} for key {k!r}"
+            )
+            raise TypeError(msg)
+    return target_types
+
+
+def _get_env_kwargs(env: BuildEnvironment) -> dict:
+    if env:
+        return {
+            "py:module": env.ref_context.get("py:module"),
+            "py:class": env.ref_context.get("py:class"),
+        }
+    return {}
+
+
+def _create_nodes(env: BuildEnvironment, title: str) -> list[nodes.Node]:
+    short_name = title.split(".")[-1]
+    if env.config.python_use_unqualified_type_names:
+        return [
+            pending_xref_condition("", short_name, condition="resolved"),
+            pending_xref_condition("", title, condition="*"),
+        ]
+    return [nodes.Text(short_name)]
+
+
+def wiki_role(pattern: str) -> RoleFunction:
+    def role(  # noqa: PLR0913, PLR0917
+        name: str,  # noqa: ARG001
+        rawtext: str,
+        text: str,
+        lineno: int,  # noqa: ARG001
+        inliner: Inliner,  # noqa: ARG001
+        options: dict | None = None,
+        content: list[str] | None = None,  # noqa: ARG001
+    ) -> tuple[list[nodes.Node], list[nodes.system_message]]:
+        output_text = text
+        output_text = output_text.replace("_", " ")
+        url = pattern % (text,)
+        if options is None:
+            options = {}
+        reference_node = nodes.reference(rawtext, output_text, refuri=url, **options)
+        return [reference_node], []
+
+    return role  # type:ignore[return-value]
---
+++ packages/sphinx-api-relink/sphinx_api_relink-0.0.12-py3-none-any.whl/sphinx_api_relink/linkcode.py
@@ -0,0 +1,170 @@
+"""A linkcode resolver for using :code:`sphinx.ext.linkcode` with GitHub."""
+
+from __future__ import annotations
+
+import inspect
+import subprocess
+import sys
+from functools import cache, lru_cache
+from os.path import dirname, relpath
+from typing import TYPE_CHECKING, Any, Callable, TypedDict
+from urllib.parse import quote
+
+import requests
+from colorama import Fore
+
+from sphinx_api_relink.helpers import print_once
+
+if TYPE_CHECKING:
+    from types import ModuleType
+
+
+class LinkcodeInfo(TypedDict, total=True):
+    module: str
+    fullname: str
+
+
+def get_linkcode_resolve(
+    github_repo: str, debug: bool
+) -> Callable[[str, LinkcodeInfo], str | None]:
+    def linkcode_resolve(domain: str, info: LinkcodeInfo) -> str | None:
+        path = _get_path(domain, info, debug)
+        if path is None:
+            return None
+        blob_url = get_blob_url(github_repo)
+        if debug:
+            msg = f"  {info['fullname']} --> {blob_url}/src/{path}"
+            print_once(msg, color=Fore.BLUE)
+        return f"{blob_url}/src/{path}"
+
+    return linkcode_resolve
+
+
+def _get_path(domain: str, info: LinkcodeInfo, debug: bool) -> str | None:
+    obj = __get_object(domain, info)
+    if obj is None:
+        return None
+    try:
+        source_file = inspect.getsourcefile(obj)
+    except TypeError:
+        if debug:
+            msg = f"  Cannot source file for {info['fullname']!r} of type {type(obj)}"
+            print_once(msg, color=Fore.MAGENTA)
+        return None
+    if not source_file:
+        return None
+
+    module_name = info["module"]
+    main_module_path = _get_package(module_name).__file__
+    if main_module_path is None:
+        msg = f"Could not find file for module {module_name!r}"
+        raise ValueError(msg)
+    path = quote(relpath(source_file, start=dirname(dirname(main_module_path))))
+    source, start_lineno = inspect.getsourcelines(obj)
+    end_lineno = start_lineno + len(source) - 1
+    linenumbers = f"L{start_lineno}-L{end_lineno}"
+    return f"{path}#{linenumbers}"
+
+
+@cache
+def _get_package(module_name: str) -> ModuleType:
+    package_name = module_name.split(".")[0]
+    return __get_module(package_name)
+
+
+@cache
+def __get_module(module_name: str) -> ModuleType:
+    module = sys.modules.get(module_name)
+    if module is None:
+        msg = f"Could not find module {module_name!r}"
+        raise ImportError(msg)
+    return module
+
+
+def __get_object(domain: str, info: LinkcodeInfo) -> Any | None:
+    if domain != "py":
+        print_once(f"Can't get the object for domain {domain!r}")
+        return None
+
+    module_name: str = info["module"]
+    fullname: str = info["fullname"]
+
+    obj = _get_object_from_module(module_name, fullname)
+    if obj is None:
+        print_once(f"Module {module_name} does not contain {fullname}")
+        return None
+    return inspect.unwrap(obj)
+
+
+def _get_object_from_module(module_name: str, fullname: str) -> Any | None:
+    module = __get_module(module_name)
+    name_parts = fullname.split(".")
+    if len(name_parts) == 1:
+        return getattr(module, fullname, None)
+    obj: Any = module
+    for sub_attr in name_parts[:-1]:
+        obj = getattr(obj, sub_attr, None)
+    if obj is None:
+        print_once(f"Module {module_name} does not contain {fullname}")
+        return None
+    return obj
+
+
+@lru_cache(maxsize=1)
+def get_blob_url(github_repo: str) -> str:
+    ref = _get_commit_sha()
+    repo_url = f"https://github.com/{github_repo}"
+    blob_url = f"{repo_url}/blob/{ref}"
+    if _url_exists(blob_url):
+        return blob_url
+    print_once(f"The URL {blob_url} seems not to exist", color=Fore.MAGENTA)
+    tag = _get_latest_tag()
+    if tag is not None:
+        blob_url = f"{repo_url}/blob/{tag}"
+        print_once(f"--> falling back to {blob_url}", color=Fore.MAGENTA)
+        if _url_exists(blob_url):
+            return blob_url
+    blob_url = f"{repo_url}/blob/main"
+    print_once(f"--> falling back to {blob_url}", color=Fore.MAGENTA)
+    if _url_exists(blob_url):
+        return blob_url
+    blob_url = f"{repo_url}/blob/master"
+    print_once(f"--> falling back to {blob_url}", color=Fore.MAGENTA)
+    return blob_url
+
+
+@lru_cache(maxsize=1)
+def _get_commit_sha() -> str:
+    result = subprocess.run(  # noqa: S603
+        ["git", "rev-parse", "HEAD"],  # noqa: S607
+        capture_output=True,
+        check=True,
+        text=True,
+    )
+    commit_hash = result.stdout.strip()
+    return commit_hash[:7]
+
+
+def _get_latest_tag() -> str | None:
+    try:
+        result = subprocess.check_output(  # noqa: S603
+            ["git", "describe", "--tags", "--exact-match"],  # noqa: S607
+            stderr=subprocess.PIPE,
+            universal_newlines=True,
+        )
+
+        return result.strip()
+    except subprocess.CalledProcessError:
+        return None
+
+
+@cache
+def _url_exists(url: str) -> bool:
+    try:
+        response = requests.head(url, timeout=5)
+        redirect_url = response.headers.get("Location")
+        if redirect_url is None:
+            return response.status_code < 400  # noqa: PLR2004
+        return _url_exists(redirect_url)
+    except requests.RequestException:
+        return False
