473e0f9588519e70aa8eae1623b78c73f694963f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-30 03:10:47 +0000

    Add package CrossDown/3.2.4/crossdown-3.2.4.tar.gz

---
+++ packages/CrossDown/crossdown-3.2.4.tar.gz/crossdown-3.2.4/CrossDown/__init__.py
@@ -0,0 +1,39 @@
+from typing import *
+from .Core import main
+
+
+__all__ = [
+    'main',  # 主函数
+    'indent',  # 添加空格
+    'HEAD',  #
+]
+__version__ = '0.11.2'
+__author__ = 'CrossDark'
+__email__ = 'liuhanbo333@icloud.com'
+__source__ = 'https://crossdark.net/'
+__license__ = """MIT"""
+
+
+HEAD = {
+    ('latex', 'js'): '<script type="text/javascript" async src="https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.7/MathJax.js?config=TeX-MML-AM_CHTML"></script>',
+
+    # mermaid
+    ('mermaid', 'js'): '<script src="https://cdn.jsdelivr.net/npm/mermaid/dist/mermaid.min.js"></script>',
+    ('mermaid', 'init'): '<script>mermaid.initialize({startOnLoad:true})</script>',
+
+    ('code-highlight', 'css'): '<link rel="stylesheet" href="../Static/styles.css">',  # 代码高亮css
+}
+
+
+def indent(input_: Union[str, List, Tuple], indent_spaces: int = 4) -> str:
+    """
+    给字符串中的每一行前面加上缩进。
+    :param input_: 原始字符串，可以包含多行。
+    :param indent_spaces: 每行前面要添加的空格数，默认为4。
+    :return: 带缩进的新字符串。
+    """
+    # 使用字符串的splitlines()方法分割原始字符串为行列表,如果是可迭代对象则直接遍历
+    # 遍历行列表，给每行前面加上相应的缩进，并重新组合成字符串
+    return "\n".join(
+        f"{' ' * indent_spaces}{line}" for line in (lambda x: x.splitlines() if isinstance(x, str) else x)(input_)
+    )
---
+++ packages/CrossDown/crossdown-3.2.4.tar.gz/crossdown-3.2.4/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="CrossDown",
+    version="3.2.4",
+    author="CrossDark",
+    author_email="liuhanbo333@icloud.com",
+    description="CrossDark's MarkDown",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/CrossDark/CrossDown",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'markdown',
+        'matplotlib',
+        'numpy',
+    ],
+    package_data={
+        '': ['Static/*'],  # 这将包含static文件夹下的所有子文件夹和文件
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'License :: OSI Approved :: MIT License',
+        "Operating System :: OS Independent"
+    ],
+)
