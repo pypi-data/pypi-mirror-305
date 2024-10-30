cfd66dbd9e5c3a77987a404bbb9f6a66a6e063c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-30 14:05:11 +0000

    Add package benfordslaw/2.0.0/benfordslaw-2.0.0-py3-none-any.whl

---
+++ packages/benfordslaw/benfordslaw-2.0.0-py3-none-any.whl/benfordslaw/examples.py
@@ -0,0 +1,175 @@
+"""Examples for benfords law."""
+
+# import benfordslaw
+# print(benfordslaw.__version__)
+from benfordslaw import benfordslaw
+
+# Initialize
+pos=-2
+bl = benfordslaw(pos=pos)
+
+# Load elections example
+df = bl.import_example(data='elections_usa')
+
+# Extract election information.
+X = df['votes'].loc[df['candidate']=='Donald Trump'].values
+
+# Make fit
+results = bl.fit(X)
+
+# Plot
+bl.plot(title=f'Results of Donald Trump based on digit={pos}', barcolor=[0.5, 0.5, 0.5], fontsize=12, barwidth=0.4)
+
+
+# %% Issue #13
+# https://github.com/erdogant/benfordslaw/issues/13
+from benfordslaw import benfordslaw
+import pandas as pd
+# import datzets as dz
+# df = dz.get(data='elections')
+
+bl = benfordslaw()
+df = bl.import_example(data='elections_usa')
+df = bl.import_example(data='elections_rus')
+
+
+# %% Issue #10
+import pandas as pd
+from benfordslaw import benfordslaw
+
+bl = benfordslaw(pos=-2)
+# USA example
+df = bl.import_example(data='elections_usa')
+Iloc = df['candidate']=='Donald Trump'
+X = df['votes'].loc[Iloc].values
+
+results = bl.fit(X)
+# Plot
+bl.plot(title='Donald Trump', barcolor=[0.5, 0.5, 0.5], fontsize=12, barwidth=0.4)
+
+# %% Issue #9
+import pandas as pd
+from benfordslaw import benfordslaw
+
+# Initialize
+bl = benfordslaw(pos=2)
+
+# Load elections example
+df = bl.import_example(data='elections_usa')
+
+# Extract election information.
+X = df['votes'].loc[df['candidate']=='Donald Trump'].values
+
+X = pd.DataFrame(X)
+
+# Make fit
+results = bl.fit(X)
+
+
+# %% Issue #8
+import pandas as pd
+from benfordslaw import benfordslaw
+
+bl = benfordslaw(alpha=0.05)
+
+data = pd.DataFrame({'value': [1,2,3,4,5]})
+bl.fit(data['value'].astype(int)) # this works fine 
+bl.fit(data['value'].astype(pd.Int64Dtype())) #this throws an error
+
+# %%
+import numpy as np
+from benfordslaw import benfordslaw
+
+# Create uniform random data
+X = np.random.randint(0, high=100, size=1000)
+bl = benfordslaw(alpha=0.05, method='chi2')
+# Fit
+results = bl.fit(X)
+# Plot
+bl.plot(title='Random data')
+
+# %% Analyze Second digit
+from benfordslaw import benfordslaw
+
+bl = benfordslaw(pos=2)
+# USA example
+df = bl.import_example(data='elections_usa')
+Iloc = df['candidate']=='Donald Trump'
+X = df['votes'].loc[Iloc].values
+results = bl.fit(X)
+# Plot
+bl.plot(title='Donald Trump', barcolor=[0.5, 0.5, 0.5], fontsize=12, barwidth=0.4)
+
+# %%
+import numpy as np
+from benfordslaw import benfordslaw
+bl = benfordslaw(alpha=0.05, method='chi2')
+x = np.linspace(0,1000,1001)
+x = np.append(x,[1,1,1,1,1,1,])
+isben2 = bl.fit(x)
+
+print(f"isben2 {isben2}")
+print(f"P_significant: {isben2['P_significant']}")
+if not isben2['P_significant']:
+    print("sorry, nope.")
+
+# %% USA example
+from benfordslaw import benfordslaw
+
+bl = benfordslaw(pos=1)
+
+# USA example
+df = bl.import_example(data='elections_usa')
+
+# Get data for candidate
+Iloc = df['candidate']=='Donald Trump'
+X = df['votes'].loc[Iloc].values
+
+# Fit
+results = bl.fit(X)
+# Plot
+bl.plot(title='Donald Trump', barcolor=[0.5, 0.5, 0.5], fontsize=12, barwidth=0.4)
+
+
+# %% Analyze last digit
+
+bl = benfordslaw(pos=-1)
+# USA example
+df = bl.import_example(data='elections_usa')
+Iloc = df['candidate']=='Donald Trump'
+X = df['votes'].loc[Iloc].values
+
+results = bl.fit(X)
+# Plot
+bl.plot(title='Donald Trump', barcolor=[0.5, 0.5, 0.5], fontsize=12, barwidth=0.4)
+
+# %% second last digit
+
+bl = benfordslaw(pos=-2)
+# USA example
+df = bl.import_example(data='elections_usa')
+Iloc = df['candidate']=='Donald Trump'
+X = df['votes'].loc[Iloc].values
+
+results = bl.fit(X)
+# Plot
+bl.plot(title='Donald Trump', barcolor=[0.5, 0.5, 0.5], fontsize=12, barwidth=0.4)
+
+# %% RUS
+df = bl.import_example('RUS')
+candidates=['Putin Vladimir Vladimirovich', 'Baburin Sergei Nikolaevich', 'Titov Boris Yurievich', 'Yavlinskiy Gregory Alekseivich']
+
+for candidate in candidates:
+    bl = benfordslaw(method='ks')
+    bl.fit(df[candidate].values)
+    bl.plot(title=candidate)
+
+# %% USA
+df = bl.import_example('elections_usa')
+for candidate in df['candidate'].unique():
+    Iloc = df['candidate']==candidate
+    X = df['votes'].loc[Iloc].values
+    bl.fit(X)
+    bl.plot(title=candidate)
+
+# %%
---
+++ packages/benfordslaw/benfordslaw-2.0.0-py3-none-any.whl/benfordslaw/tests/test_benfordslaw.py
@@ -0,0 +1,51 @@
+# pip install pytest
+# pytest test/test_benfordslaw.py
+
+import numpy as np
+from benfordslaw import benfordslaw
+import unittest
+
+class TestBENFORDSLAW(unittest.TestCase):
+	def test_nums(self):
+		bl = benfordslaw()
+		X = [12, 23, 34, 45, 56, 67, 78, 89, 90, 100]
+		out = bl.fit(np.array(X))
+		np.all(out['percentage_emp'][:,1]==[20, 10, 10, 10, 10, 10, 10, 10, 10])
+
+	def test_benfordslaw(self):
+		bl = benfordslaw()
+		df = bl.import_example()
+		Iloc = df['candidate']=='Donald Trump'
+		X = df['votes'].loc[Iloc].values
+
+		# TEST 1: check output is unchanged
+		methods=[None, 'chi2', 'ks']
+		for method in methods:
+			bl = benfordslaw(method=method)
+			out = bl.fit(X)
+			assert np.all(np.isin([*out.keys()], ['P', 't', 'percentage_emp', 'P_significant']))
+
+		# TEST 2: check chi2
+		bl = benfordslaw(method='chi2')
+		out = bl.fit(X)
+		assert out['P']==0.4150723856863902
+		assert out['t']==8.190646662956185
+		assert out['P_significant']==False
+
+		# TEST 3: check ks
+		bl = benfordslaw(method='ks')
+		out = bl.fit(X)
+		assert out['P']==1.0
+		assert out['t']==0.1111111111111111
+
+		# TEST 4: check percentages
+		assert np.all(out['percentage_emp'][:,1]==[31.64521544487969, 16.508114157806382, 12.479015109121432, 9.820928931169558, 7.862339115836598, 6.3794068270845, 5.8477895914941245, 4.616675993284835, 4.8405148293228875])
+
+	def test_dataframe(self):
+		bl = benfordslaw()
+		df = bl.import_example()
+		Iloc = df['candidate']=='Donald Trump'
+		X = df['votes'].loc[Iloc]
+		results = bl.fit(X)
+		np.all(np.isin([*results.keys()], ['P', 't', 'percentage_emp', 'P_significant']))
+		assert np.all(results['percentage_emp'][:,1]==[31.64521544487969, 16.508114157806382, 12.479015109121432, 9.820928931169558, 7.862339115836598, 6.3794068270845, 5.8477895914941245, 4.616675993284835, 4.8405148293228875])
