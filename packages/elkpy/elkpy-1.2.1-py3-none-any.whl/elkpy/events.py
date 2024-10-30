99b58d6f4d14a7500f4e76f5fe546aa64ef2052c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-30 03:10:38 +0000

    Add package elkpy/1.2.1/elkpy-1.2.1-py3-none-any.whl

---
+++ packages/elkpy/elkpy-1.2.1-py3-none-any.whl/elkpy/events.py
@@ -0,0 +1,45 @@
+import asyncio
+from sushierrors import SushiUnkownError
+
+
+class ElkpyEvent(asyncio.Event):
+    error: bool = False
+
+    async def wait(self):
+        if self.error: 
+            raise SushiUnkownError
+        return await super().wait()
+
+
+class TrackCreationEvent(ElkpyEvent):
+    sushi_id: int 
+
+    def __init__(self, name: str) -> None:
+        super().__init__()
+        self.name: str = name
+        self.action = 1
+        self.data: dict = {}
+
+
+class TrackDeletionEvent(ElkpyEvent):
+    def __init__(self, sushi_id: int) -> None:
+        super().__init__()
+        self.action = 2
+        self.sushi_id: int = sushi_id
+
+
+class ProcessorCreationEvent(ElkpyEvent):
+    sushi_id: int
+
+    def __init__(self, name: str) -> None:
+        super().__init__()
+        self.name: str = name
+        self.action = 1
+        self.data: dict = {}
+
+
+class ProcessorDeletionEvent(ElkpyEvent):
+    def __init__(self, sushi_id: int) -> None:
+        super().__init__()
+        self.action = 2
+        self.sushi_id: int = sushi_id
