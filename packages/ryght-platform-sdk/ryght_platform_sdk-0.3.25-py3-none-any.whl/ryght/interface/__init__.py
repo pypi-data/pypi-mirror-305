46ca289391ee7af5f9116589b1b4ae6ba9198302
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-30 08:04:56 +0000

    Add package ryght-platform-sdk/0.3.25/ryght_platform_sdk-0.3.25-py3-none-any.whl

---
+++ packages/ryght-platform-sdk/ryght_platform_sdk-0.3.25-py3-none-any.whl/ryght/interface/__init__.py
@@ -0,0 +1,15 @@
+
+from .api import NotesAPI
+from .api import ModelsAPI
+from .api import DocumentsAPI
+from .api import ApiInterface
+from .api import CompletionsAPI
+from .api import OrganizationAPI
+from .api import ConversationsAPI
+from .api import DocumentCollectionAPI
+from .api import PermissionManagementAPI
+from .api import PermissionManagementAPI
+from .api import PermissionManagementAPI
+
+from .client import BaseClient
+
