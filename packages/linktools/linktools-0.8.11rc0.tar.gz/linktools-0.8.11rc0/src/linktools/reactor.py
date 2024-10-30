ebef1991e8cccb8a69bcdc714e91f7cbebcd54ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-30 15:05:25 +0000

    Add package linktools/0.8.11rc0/linktools-0.8.11rc0.tar.gz

---
+++ packages/linktools/linktools-0.8.11rc0.tar.gz/linktools-0.8.11rc0/src/linktools/reactor.py
@@ -0,0 +1,143 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+import asyncio
+import atexit
+import functools
+import threading
+import time
+import traceback
+from collections import deque
+from typing import Optional, Callable, Any, Coroutine
+
+from ._environ import environ
+from .types import Event, Stoppable
+
+_logger = environ.get_logger("reactor")
+
+
+class Reactor(Stoppable):
+    """
+    Code stolen from frida_tools.application.Reactor
+    """
+
+    def __init__(self, on_stop=None, on_error=None):
+        self._running = False
+        self._on_stop = on_stop
+        self._on_error = on_error
+        self._pending = deque([])
+        self._lock = threading.Lock()
+        self._cond = threading.Condition(self._lock)
+        self._worker = None
+
+    def is_running(self) -> "bool":
+        with self._lock:
+            return self._running
+
+    def run(self):
+        with self._lock:
+            self._running = True
+        self._worker = threading.Thread(target=self._run)
+        self._worker.daemon = True
+        self._worker.start()
+
+    def _run(self):
+        running = True
+        while running:
+            now = time.time()
+            work = None
+            timeout = None
+            with self._lock:
+                for item in self._pending:
+                    (f, when) = item
+                    if now >= when:
+                        work = f
+                        self._pending.remove(item)
+                        break
+                if len(self._pending) > 0:
+                    timeout = max([min(map(lambda item: item[1], self._pending)) - now, 0])
+                previous_pending_length = len(self._pending)
+
+            if work is not None:
+                try:
+                    work()
+                except (KeyboardInterrupt, EOFError) as e:
+                    if self._on_error is not None:
+                        self._on_error(e, traceback.format_exc())
+                    self.stop()
+                except BaseException as e:
+                    if self._on_error is not None:
+                        self._on_error(e, traceback.format_exc())
+
+            with self._lock:
+                if self._running and len(self._pending) == previous_pending_length:
+                    self._cond.wait(timeout)
+                running = self._running
+
+        if self._on_stop is not None:
+            self._on_stop()
+
+    def stop(self, delay: float = None):
+        self.schedule(self._stop, delay)
+
+    def _stop(self):
+        with self._lock:
+            self._running = False
+
+    def schedule(self, fn: Callable[[], any], delay: float = None):
+        now = time.time()
+        if delay is not None:
+            when = now + delay
+        else:
+            when = now
+        with self._lock:
+            self._pending.append((functools.partial(self._work, fn), when))
+            self._cond.notify()
+
+    def _work(self, fn: Callable[[], any]):
+        fn()
+
+    def wait(self, timeout=5):
+        assert self._worker
+        self._worker.join(timeout)
+        if self._worker.is_alive():
+            _logger.warning("Worker did not finish normally")
+
+    def __enter__(self):
+        self.run()
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.stop()
+        self.wait()
+
+
+class ReactorThread(threading.Thread):
+
+    def __init__(self):
+        def run():
+            self._loop = asyncio.new_event_loop()
+            event.set()
+            self._loop.run_forever()
+
+        super().__init__(target=run)
+
+        event = Event()
+        self.daemon = True
+        self._loop: Optional[asyncio.AbstractEventLoop] = None
+        self.start()
+        event.wait()
+        atexit.register(self.stop)
+
+    def stop(self):
+        loop = self.get_event_loop()
+        loop.call_soon_threadsafe(lambda: loop.stop())
+
+    def call_soon(self, callback: Callable[..., Any]):
+        loop = self.get_event_loop()
+        return loop.call_soon_threadsafe(callback)
+
+    def call_task_soon(self, coro: Coroutine):
+        loop = self.get_event_loop()
+        return loop.call_soon_threadsafe(lambda: loop.create_task(coro))
+
+    def get_event_loop(self) -> asyncio.AbstractEventLoop:
+        return self._loop
---
+++ packages/linktools/linktools-0.8.11rc0.tar.gz/linktools-0.8.11rc0/src/linktools/utils/_subprocess.py
@@ -0,0 +1,213 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+import errno
+import os
+import queue
+import shlex
+import subprocess
+import threading
+from typing import AnyStr, Tuple, Optional, IO, Callable, Any, Dict, Union, List, Iterable
+
+from .._environ import environ
+from ..decorator import cached_property, timeoutable
+from ..types import TimeoutType
+
+
+def list2cmdline(args: Iterable[str]) -> str:
+    return subprocess.list2cmdline(args)
+
+
+def cmdline2list(cmdline: str) -> List[str]:
+    return shlex.split(cmdline)
+
+
+class Output:
+    STDOUT = 1
+    STDERR = 2
+
+    def __init__(self, stdout: IO[AnyStr], stderr: IO[AnyStr]):
+        self._queue = queue.Queue()
+        self._stdout_finished = None
+        self._stdout_thread = None
+        self._stderr_finished = None
+        self._stderr_thread = None
+        if stdout:
+            self._stdout_finished = threading.Event()
+            self._stdout_thread = threading.Thread(
+                target=self._iter_lines,
+                args=(stdout, self.STDOUT, self._stdout_finished,)
+            )
+            self._stdout_thread.daemon = True
+            self._stdout_thread.start()
+        if stderr:
+            self._stderr_finished = threading.Event()
+            self._stderr_thread = threading.Thread(
+                target=self._iter_lines,
+                args=(stderr, self.STDERR, self._stderr_finished,)
+            )
+            self._stderr_thread.daemon = True
+            self._stderr_thread.start()
+
+    @property
+    def is_alive(self):
+        if self._stdout_finished and not self._stdout_finished.is_set():
+            return True
+        if self._stderr_finished and not self._stderr_finished.is_set():
+            return True
+        return False
+
+    def _iter_lines(self, io: IO[AnyStr], code: int, event: threading.Event):
+        try:
+            while True:
+                data = io.readline()
+                if not data:
+                    break
+                self._queue.put((code, data))
+        except OSError as e:
+            if e.errno != errno.EBADF:
+                environ.logger.debug(f"Handle output error: {e}")
+        finally:
+            event.set()
+            self._queue.put((None, None))
+
+    def get_lines(self, timeout: TimeoutType):
+        while self.is_alive:
+            try:
+                # 给个1秒超时时间防止有多个线程消费的时候死锁
+                code, data = self._queue.get(timeout=min(timeout.remain or 1, 1))
+                if code is not None:
+                    yield code, data
+            except queue.Empty:
+                if not timeout.check():
+                    break
+
+        while True:
+            try:
+                # 需要把剩余可消费的数据消费完
+                code, data = self._queue.get_nowait()
+                if code is not None:
+                    yield code, data
+            except queue.Empty:
+                break
+
+
+class Process(subprocess.Popen):
+
+    @timeoutable
+    def call(self, timeout: TimeoutType = None) -> int:
+        with self:
+            try:
+                return self.wait(timeout=timeout.remain)
+            except Exception:
+                self.kill()
+                raise
+
+    @timeoutable
+    def call_as_daemon(self, timeout: TimeoutType = None) -> int:
+        try:
+            return self.wait(timeout=timeout.remain or .1)
+        except subprocess.TimeoutExpired:
+            return 0
+
+    @timeoutable
+    def check_call(self, timeout: TimeoutType = None) -> int:
+        with self:
+            try:
+                retcode = self.wait(timeout=timeout.remain)
+                if retcode:
+                    raise subprocess.CalledProcessError(retcode, self.args)
+                return retcode
+            except:
+                self.kill()
+                raise
+
+    @timeoutable
+    def exec(
+            self,
+            timeout: TimeoutType = None,
+            on_stdout: Callable[[str], Any] = None,
+            on_stderr: Callable[[str], Any] = None
+    ) -> Tuple[Optional[AnyStr], Optional[AnyStr]]:
+        """
+        执行命令
+        :param timeout: 超时时间
+        :param on_stdout: 把输出打印到logger中
+        :param on_stderr: 把输出打印到logger中
+        :return: 返回stdout输出内容
+        """
+
+        out = err = None
+
+        if self.stdout or self.stderr:
+
+            for code, data in self._output.get_lines(timeout):
+                if code == self._output.STDOUT:
+                    out = data if out is None else out + data
+                    if on_stdout:
+                        data = data.decode(errors="ignore") if isinstance(data, bytes) else data
+                        data = data.rstrip()
+                        if data:
+                            on_stdout(data)
+
+                elif code == self._output.STDERR:
+                    err = data if err is None else err + data
+                    if on_stderr:
+                        data = data.decode(errors="ignore") if isinstance(data, bytes) else data
+                        data = data.rstrip()
+                        if data:
+                            on_stderr(data)
+        else:
+
+            try:
+                self.wait(timeout.remain)
+            except subprocess.TimeoutExpired:
+                pass
+
+        return out, err
+
+    @cached_property
+    def _output(self):
+        return Output(self.stdout, self.stderr)
+
+
+def create_process(
+        *args: Any,
+        capture_output: bool = False,
+        stdin: Union[int, IO] = None, stdout: Union[int, IO] = None, stderr: Union[int, IO] = None,
+        shell: bool = False, cwd: str = None,
+        env: Dict[str, str] = None, append_env: Dict[str, str] = None, default_env: Dict[str, str] = None,
+        **kwargs) -> Process:
+    args = [str(arg) for arg in args]
+
+    if capture_output is True:
+        if stdout is not None or stderr is not None:
+            raise ValueError("stdout and stderr arguments may not be used "
+                             "with capture_output.")
+        stdout = subprocess.PIPE
+        stderr = subprocess.PIPE
+
+    if not cwd:
+        try:
+            cwd = os.getcwd()
+        except FileNotFoundError:
+            cwd = environ.temp_path
+            cwd.mkdir(parents=True, exist_ok=True)
+
+    if append_env or default_env:
+        env = dict(env) if env else dict(os.environ)
+        if default_env:
+            for key, value in default_env.items():
+                env.setdefault(key, value)
+        if append_env:
+            env.update(append_env)
+
+    environ.logger.debug(f"Exec cmdline: {list2cmdline(args)}")
+
+    return Process(
+        args,
+        stdin=stdin, stdout=stdout, stderr=stderr,
+        shell=shell, cwd=cwd,
+        env=env,
+        **kwargs
+    )
