2dd9e91f08f90ae1f00d2620c90fe8055c76c11c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-30 03:09:28 +0000

    Add package usethis/0.1.0/usethis-0.1.0.tar.gz

---
+++ packages/usethis/usethis-0.1.0.tar.gz/usethis-0.1.0/tests/usethis/_integrations/uv/deps.py
@@ -0,0 +1,62 @@
+from pathlib import Path
+
+from usethis._integrations.uv.deps import get_dep_groups
+from usethis._utils._test import change_cwd
+
+
+class TestGetDepGroups:
+    def test_no_dev_section(self, tmp_path: Path):
+        (tmp_path / "pyproject.toml").touch()
+
+        with change_cwd(tmp_path):
+            assert get_dep_groups() == {}
+
+    def test_empty_section(self, tmp_path: Path):
+        (tmp_path / "pyproject.toml").write_text("""\
+[dependency-groups]
+""")
+
+        with change_cwd(tmp_path):
+            assert get_dep_groups() == {}
+
+    def test_empty_group(self, tmp_path: Path):
+        (tmp_path / "pyproject.toml").write_text("""\
+[dependency-groups]
+test=[]
+""")
+
+        with change_cwd(tmp_path):
+            assert get_dep_groups() == {"test": []}
+
+    def test_single_dev_dep(self, tmp_path: Path):
+        (tmp_path / "pyproject.toml").write_text("""\
+[dependency-groups]
+test=['pytest']
+""")
+
+        with change_cwd(tmp_path):
+            assert get_dep_groups() == {"test": "pytest"}
+
+    def test_multiple_dev_deps(self, tmp_path: Path):
+        (tmp_path / "pyproject.toml").write_text("""\
+[dependency-groups]
+qa=["flake8", "black", "isort"]
+""")
+
+        with change_cwd(tmp_path):
+            assert get_dep_groups() == {"qa": ["flake8", "black", "isort"]}
+
+    def test_multiple_groups(self, tmp_path: Path):
+        (tmp_path / "pyproject.toml").write_text(
+            """\
+[dependency-groups]
+qa=["flake8", "black", "isort"]
+test=['pytest']
+"""
+        )
+
+        with change_cwd(tmp_path):
+            assert get_dep_groups() == {
+                "qa": ["flake8", "black", "isort"],
+                "test": ["pytest"],
+            }
