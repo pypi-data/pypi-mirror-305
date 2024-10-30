ff17485f045a6e47dbf45376ed7735dcc88e8945
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-30 16:04:47 +0000

    Add package cyj-test-pyxgdb/0.0.11/cyj_test_pyxgdb-0.0.11-py3-none-any.whl

---
+++ packages/cyj-test-pyxgdb/cyj_test_pyxgdb-0.0.11-py3-none-any.whl/cyj_test__pyxgdb/setup.py
@@ -0,0 +1,9 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name='cyj_test__pyxgdb',
+    version='0.0.11',
+    packages=find_packages(),
+    package_data={'': ['*.so', '__init__.py']},
+)
