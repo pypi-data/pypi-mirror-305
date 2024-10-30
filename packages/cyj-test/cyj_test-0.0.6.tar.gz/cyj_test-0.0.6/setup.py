2a0c9a6279553f99f09f8277507c0b652c0385c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-30 15:05:54 +0000

    Add package cyj-test/0.0.6/cyj_test-0.0.6.tar.gz

---
+++ packages/cyj-test/cyj_test-0.0.6.tar.gz/cyj_test-0.0.6/setup.py
@@ -0,0 +1,22 @@
+import platform
+from setuptools import setup, find_packages
+
+setup(
+    name="cyj_test",  # 你的包名称
+    version="0.0.6",  # 初始版本号
+    description="A description of cyj package",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="yinchenjue",
+    author_email="454194347@qq.com",
+    include_package_data=True,
+    packages=find_packages(),
+    install_requires=[],  # 如果有依赖包，在这里列出
+    package_data={
+        # 包含C动态库文件
+         'cyj_test': ['python39/*.so'],
+        #"cyj_test": ["libxugusql.so"] if platform.system() == "Linux" else ["xugusql.dll"],
+    },
+    python_requires=">=3.6",
+)
+
