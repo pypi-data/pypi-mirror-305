765673b95663745ab1cdc30651a8d06a25ddbef9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-30 07:06:00 +0000

    Add package weiser-ai/0.1.3/weiser_ai-0.1.3.tar.gz

---
+++ packages/weiser-ai/weiser_ai-0.1.3.tar.gz/weiser_ai-0.1.3/weiser/drivers/metric_stores/__init__.py
@@ -0,0 +1,18 @@
+from typing import Union
+from weiser.drivers.metric_stores.duckdb import DuckDBMetricStore
+from weiser.drivers.metric_stores.postgres import PostgresMetricStore
+from weiser.loader.models import MetricStore, MetricStoreType
+
+QUERY_TYPE_MAP = {
+    MetricStoreType.duckdb: DuckDBMetricStore,
+    MetricStoreType.postgresql: PostgresMetricStore,
+}
+
+MetricStoreDB = Union[DuckDBMetricStore, PostgresMetricStore]
+# MetricStoreDB = DuckDBMetricStore
+
+
+class MetricStoreFactory:
+    @staticmethod
+    def create_driver(metric_store: MetricStore) -> MetricStoreDB:
+        return QUERY_TYPE_MAP.get(metric_store.db_type, DuckDBMetricStore)(metric_store)
