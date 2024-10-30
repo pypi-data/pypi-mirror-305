cc7c8baf830812d24493c28f7b00cde20a7716eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-30 07:06:16 +0000

    Add package EilishWorld/2.0.0/EilishWorld-2.0.0-py3-none-any.whl

---
+++ packages/EilishWorld/EilishWorld-2.0.0-py3-none-any.whl/Mbali Phama/__init__.py
@@ -0,0 +1,4 @@
+
+from .module1 import *
+from .module2 import *
+from .module3 import *
---
+++ packages/EilishWorld/EilishWorld-2.0.0-py3-none-any.whl/Mbali Phama/module2.py
@@ -0,0 +1,11 @@
+NAME = "Mbali Phama"
+AGE = 22
+
+
+def greet():
+   
+    return f"Hello, my name is {NAME}."
+
+def age_in_dog_years():
+   
+    return AGE * 3
