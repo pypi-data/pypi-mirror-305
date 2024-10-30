2c7af93f00e30a1e1113355b3d3b789235511c6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-30 04:06:39 +0000

    Add package hydra-auto-schema/0.0.1/hydra_auto_schema-0.0.1-py3-none-any.whl

---
+++ packages/hydra-auto-schema/hydra_auto_schema-0.0.1-py3-none-any.whl/hydra_plugins/auto_schema/__init__.py
@@ -0,0 +1,9 @@
+"""
+TODO: Import the AutoSchemaPlugin once it is working (if it's actually useful).
+"""
+# from .auto_schema_plugin import AutoSchemaPlugin, AutoSchemaPluginConfig
+
+__all__ = [
+    # "AutoSchemaPlugin",
+    # "AutoSchemaPluginConfig",
+]
