bb5f904ee4f61d4f71e165a6b72884e40cf1460c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-30 07:06:20 +0000

    Add package online-store-cu-by-egor/0.1.0/online_store_cu_by_egor-0.1.0.tar.gz

---
+++ packages/online-store-cu-by-egor/online_store_cu_by_egor-0.1.0.tar.gz/online_store_cu_by_egor-0.1.0/online_store/order_management.py
@@ -0,0 +1,32 @@
+
+class OrderManager:
+    def __init__(self):
+        self.orders = {}
+
+    def create_order(self, order_id, order_data):
+        if order_id in self.orders:
+            print(f'Заказ с ID {order_id} уже существует')
+        else:
+            self.orders[order_id] = order_data
+            print(f'Заказ с ID {order_id} добавлен')
+
+    def update_order(self, order_id, order_data):
+        if order_id in self.orders:
+            self.orders[order_id].update(order_data)
+            print(f'Заказ с ID {order_id} обновлён')
+        else:
+            print(f'Заказ с ID {order_id} не найден')
+
+    def find_order(self, order_id):
+        if order_id in self.orders:
+            return self.orders[order_id]
+        else:
+            print(f'Заказ с ID {order_id} не найден')
+            return None
+
+    def cancel_order(self, order_id):
+        if order_id in self.orders:
+            del self.orders[order_id]
+            print(f'Заказ с ID {order_id} отменён')
+        else:
+            print(f'Заказ с ID {order_id} не найден')
