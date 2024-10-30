3183c8460667866ae40e77024d9d6db2c9aab202
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-30 14:04:34 +0000

    Add package gama-config/2.5.0/gama_config-2.5.0.tar.gz

---
+++ packages/gama-config/gama_config-2.5.0.tar.gz/gama_config-2.5.0/gama_config/transform_cameras.py
@@ -0,0 +1,30 @@
+from typing import List
+from greenstream_config import Camera
+from gama_config.gama_vessel import Mode
+
+
+def transform_camera_pipelines(cameras: List[Camera], mode: Mode, namespace: str):
+    """
+    Transforms the camera pipelines based on the mode of operation.
+    """
+
+    if len(cameras) == 0:
+        raise ValueError("No cameras found in the vessel config yml")
+
+    if mode == Mode.STUBS:
+        # Use a test pattern for the camera
+        for camera in cameras:
+            camera.elements = [
+                "videotestsrc pattern=ball",
+                "video/x-raw, format=RGB,width=1920,height=1080",
+            ]
+    elif mode == Mode.SIMULATOR:
+        for camera in cameras:
+            # Use the ROS image source for the camera
+            camera.elements = [
+                f"rosimagesrc ros-topic=sensors/cameras/{camera.name}_{camera.type}/image_raw ros-name='gst_rosimagesrc_{camera.name}_{camera.type}' ros-namespace='{namespace}'"
+            ]
+            # Don't publish the image back out to ROS as it came from there
+            camera.camera_frame_topic = None
+
+    return cameras
