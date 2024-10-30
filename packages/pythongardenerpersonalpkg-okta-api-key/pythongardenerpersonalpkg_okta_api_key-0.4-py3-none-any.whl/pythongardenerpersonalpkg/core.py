1b9eff93f093ba077d34aa64531f4bfd4308421b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-30 15:05:20 +0000

    Add package pythongardenerpersonalpkg-okta-api-key/0.4/pythongardenerpersonalpkg_okta_api_key-0.4-py3-none-any.whl

---
+++ packages/pythongardenerpersonalpkg-okta-api-key/pythongardenerpersonalpkg_okta_api_key-0.4-py3-none-any.whl/pythongardenerpersonalpkg/core.py
@@ -0,0 +1,7 @@
+def say_hello(name):
+    github_pat = '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'
+    aws_access_key = 'AKIAYZZGSY75LDOLKSUF'
+    aws_secret_key = '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'
+    # https://trial-4038288-admin.okta.com/
+    okta_api_key = '00El6PC_2eCRcexhNjAsD_x6zaL8ZZ47FFpkrySYgm'
+    return f"Hello, {name}!"
