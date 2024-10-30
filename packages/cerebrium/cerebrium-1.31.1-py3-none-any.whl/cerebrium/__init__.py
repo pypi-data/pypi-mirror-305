022270e7d5800773f1c1f1c88b06ad67b106fd99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-30 05:04:40 +0000

    Add package cerebrium/1.31.1/cerebrium-1.31.1-py3-none-any.whl

---
+++ packages/cerebrium/cerebrium-1.31.1-py3-none-any.whl/cerebrium/__init__.py
@@ -0,0 +1,5 @@
+__version__ = "1.31.1"
+
+# Ignore linting in this file
+# flake8: noqa
+from cerebrium.core import get_secret
---
+++ packages/cerebrium/cerebrium-1.31.1-py3-none-any.whl/cerebrium/files.py
@@ -0,0 +1,13 @@
+PREDICT_FILE = "_cerebrium_predict.json"
+PIP_REQUIREMENTS_FILE = "requirements.txt"
+CONDA_REQUIREMENTS_FILE = "conda_pkglist.txt"
+APT_REQUIREMENTS_FILE = "pkglist.txt"
+SHELL_COMMANDS_FILE = "shell_commands.sh"
+
+INTERNAL_FILES = [
+    PIP_REQUIREMENTS_FILE,
+    CONDA_REQUIREMENTS_FILE,
+    APT_REQUIREMENTS_FILE,
+    PREDICT_FILE,
+    SHELL_COMMANDS_FILE,
+]
