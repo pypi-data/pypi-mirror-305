3417e836bb63fd53bb5608fcdb7aa022c2c4f1a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-30 05:05:26 +0000

    Add package treams/0.4.5/treams-0.4.5-cp310-cp310-win_amd64.whl

---
+++ packages/treams/treams-0.4.5-cp310-cp310-win_amd64.whl/treams/__init__.py
@@ -0,0 +1,111 @@
+"""TREAMS: T-Matrix scattering code for nanophotonic computations.
+
+.. currentmodule:: treams
+
+Classes
+=======
+
+The top-level classes and functions allow a high-level access to the functionality.
+
+Basis sets
+----------
+
+.. autosummary::
+   :toctree: generated/
+
+   CylindricalWaveBasis
+   PlaneWaveBasisByUnitVector
+   PlaneWaveBasisByComp
+   SphericalWaveBasis
+
+Matrices and Arrays
+-------------------
+
+.. autosummary::
+   :toctree: generated/
+
+   PhysicsArray
+   SMatrix
+   SMatrices
+   TMatrix
+   TMatrixC
+
+Other
+-----
+
+.. autosummary::
+   :toctree: generated/
+
+   Lattice
+   Material
+
+Functions
+=========
+
+.. autosummary::
+   :toctree: generated/
+
+   bfield
+   changepoltype
+   chirality_density
+   dfield
+   efield
+   expand
+   expandlattice
+   hfield
+   permute
+   plane_wave
+   rotate
+   translate
+
+"""
+
+from treams._core import (  # noqa: F401
+    CylindricalWaveBasis,
+    PhysicsArray,
+    PlaneWaveBasisByComp,
+    PlaneWaveBasisByUnitVector,
+    SphericalWaveBasis,
+)
+from treams._lattice import Lattice, WaveVector  # noqa: F401
+from treams._material import Material  # noqa: F401
+from treams._operators import (  # noqa: F401
+    BField,
+    ChangePoltype,
+    DField,
+    EField,
+    Expand,
+    ExpandLattice,
+    FField,
+    GField,
+    HField,
+    Permute,
+    Rotate,
+    Translate,
+    bfield,
+    changepoltype,
+    dfield,
+    efield,
+    expand,
+    expandlattice,
+    ffield,
+    gfield,
+    hfield,
+    permute,
+    rotate,
+    translate,
+)
+from treams._smatrix import (  # noqa: F401
+    SMatrices,
+    SMatrix,
+    chirality_density,
+    poynting_avg_z,
+)
+from treams._tmatrix import (  # noqa: F401
+    TMatrix,
+    TMatrixC,
+    cylindrical_wave,
+    plane_wave,
+    plane_wave_angle,
+    spherical_wave,
+)
