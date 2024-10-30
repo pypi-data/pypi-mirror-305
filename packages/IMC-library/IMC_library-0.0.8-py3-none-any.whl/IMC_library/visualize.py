28aa262fbe7259e498accc7cbe70186b92cfd368
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-30 05:04:51 +0000

    Add package IMC-library/0.0.8/IMC_library-0.0.8-py3-none-any.whl

---
+++ packages/IMC-library/IMC_library-0.0.8-py3-none-any.whl/IMC_library/visualize.py
@@ -0,0 +1,12 @@
+from IMC_library import function
+import sys
+
+
+cofactor=input("Saisir le cofacteur appliquer dans la transformation Arcsinh: ")
+thresh=input("Saisir le seuil: ")
+kernel=input("Saisir la taille du kernel: ")
+path=input("Saisir le chemin d'acces du dossier crée: ")
+path_raw=input("Saisir le chemin d'acces du dossier contenant les images brutes: ")
+
+
+function.visualize_roi(int(cofactor),int(thresh),int(kernel),path,path_raw)
