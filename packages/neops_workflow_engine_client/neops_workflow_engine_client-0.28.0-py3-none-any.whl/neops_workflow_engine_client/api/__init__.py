680c46e5af5a2cbe5cf8014dadad18d36ac0a68f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-30 04:05:11 +0000

    Add package neops_workflow_engine_client/0.28.0/neops_workflow_engine_client-0.28.0-py3-none-any.whl

---
+++ packages/neops_workflow_engine_client/neops_workflow_engine_client-0.28.0-py3-none-any.whl/neops_workflow_engine_client/api/__init__.py
@@ -0,0 +1,7 @@
+# flake8: noqa
+
+# import apis into api package
+from neops_workflow_engine_client.api.default_api import DefaultApi
+from neops_workflow_engine_client.api.function_block_api import FunctionBlockApi
+from neops_workflow_engine_client.api.health_api import HealthApi
+
