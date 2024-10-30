902d8a5186e81cfd4bb045208425d1343b56d94f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-30 15:05:39 +0000

    Add package piper-sdk/0.0.7/piper_sdk-0.0.7.tar.gz

---
+++ packages/piper-sdk/piper_sdk-0.0.7.tar.gz/piper_sdk-0.0.7/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='piper_sdk',
+    version='0.0.7',
+    packages=find_packages(include=['piper_sdk', 'piper_sdk.*']),
+    include_package_data=True,
+    install_requires=[
+        'python-can>=4.3.1',
+    ],
+    entry_points={
+    },
+    author='RosenYin',
+    author_email='yinruocheng321@gmail.com',
+    description='A sdk to control piper',
+    classifiers=[
+        'Programming Language :: Python :: 3.8',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
+
