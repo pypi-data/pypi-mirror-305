bb8018e9cf89cdc0553b8ed7dfc95aeb9348031d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-30 08:04:10 +0000

    Add package flet/0.25.0.dev3609/flet-0.25.0.dev3609-py3-none-any.whl

---
+++ packages/flet/flet-0.25.0.dev3609-py3-none-any.whl/flet/__init__.py
@@ -0,0 +1,11 @@
+from flet.app import app, app_async
+from flet_core import *
+from flet_core.pubsub import PubSubClient
+from flet_core.types import (
+    FLET_APP,
+    FLET_APP_HIDDEN,
+    FLET_APP_WEB,
+    WEB_BROWSER,
+    AppView,
+    WebRenderer,
+)
