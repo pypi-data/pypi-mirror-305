3c3e2c60237fa609b439b81925e23cacecbb12c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-30 08:04:33 +0000

    Add package expit/1.0.8/expit-1.0.8.tar.gz

---
+++ packages/expit/expit-1.0.8.tar.gz/expit-1.0.8/src/expit/core/__init__.py
@@ -0,0 +1,24 @@
+import math
+
+import click
+import preparse
+
+__all__ = ["function", "main"]
+
+
+def function(x: float):
+    try:
+        p = math.exp(-x)
+    except OverflowError:
+        p = float("+inf")
+    return 1 / (1 + p)
+
+
+@preparse.PreParser(posix=False).click()
+@click.command(add_help_option=False)
+@click.help_option("-h", "--help")
+@click.version_option(None, "-V", "--version")
+@click.argument("x", type=float)
+def main(x: float):
+    """applies the expit function to x"""
+    click.echo(function(x))
