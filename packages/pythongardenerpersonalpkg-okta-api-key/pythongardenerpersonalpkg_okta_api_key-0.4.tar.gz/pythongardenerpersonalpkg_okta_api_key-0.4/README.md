08d1a582481dbe104a99238eade39e64517b7957
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-30 15:05:20 +0000

    Add package pythongardenerpersonalpkg-okta-api-key/0.4/pythongardenerpersonalpkg_okta_api_key-0.4.tar.gz

---
+++ packages/pythongardenerpersonalpkg-okta-api-key/pythongardenerpersonalpkg_okta_api_key-0.4.tar.gz/pythongardenerpersonalpkg_okta_api_key-0.4/README.md
@@ -0,0 +1,4 @@
+# pythongardenerpersonalpkg
+
+This is a simple example package. You can use GitHub to host your project.
+
---
+++ packages/pythongardenerpersonalpkg-okta-api-key/pythongardenerpersonalpkg_okta_api_key-0.4.tar.gz/pythongardenerpersonalpkg_okta_api_key-0.4/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pythongardenerpersonalpkg-okta-api-key',
+    version='0.4',
+    packages=find_packages(),
+    description='A simple example package okta api key',
+    author='Your Name',
+    author_email='your.email@example.com',
+    url='https://github.com/yourusername/pythongardenerpersonalpkg',  # Optional
+    install_requires=[],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
