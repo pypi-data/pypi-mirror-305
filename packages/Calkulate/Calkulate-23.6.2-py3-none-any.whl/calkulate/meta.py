7a1e70e5b57b70f719a27de3422f970e05153737
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-30 05:05:11 +0000

    Add package Calkulate/23.6.2/Calkulate-23.6.2-py3-none-any.whl

---
+++ packages/Calkulate/Calkulate-23.6.2-py3-none-any.whl/calkulate/meta.py
@@ -0,0 +1,27 @@
+# Calkulate: seawater total alkalinity from titration data
+# Copyright (C) 2019--2024  Matthew P. Humphreys  (GNU GPLv3)
+"""Package metadata."""
+
+_authorlist = ["Humphreys, Matthew P.", "Matthews, Ruth S."]
+__author__ = " and ".join(_authorlist)
+__version__ = "23.6.2"
+__year__ = 2024
+
+
+def hello():
+    """Report the version number and DOI."""
+    print(
+        r"""
+   .--.     . .         .      .      
+  :         | |         |     _|_     
+  |    .-.  | |.-. .  . | .-.  |  .-. 
+  :   (   ) | |-.' |  | |(   ) | (.-' 
+   `--'`-'`-`-'  `-`--`-`-`-'`-`-'`--'
+
+    Humphreys, M.P. & Matthews, R.S.
+       doi:10.5281/zenodo.2634304
+           Version {} ({})
+""".format(
+            __version__, __year__
+        )
+    )
