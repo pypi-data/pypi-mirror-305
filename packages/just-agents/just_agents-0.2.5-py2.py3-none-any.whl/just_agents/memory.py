a6b8e55dfe34b87f8fcb245a7fe88ab2ed351013
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-30 05:05:09 +0000

    Add package just-agents/0.2.5/just_agents-0.2.5-py2.py3-none-any.whl

---
+++ packages/just-agents/just_agents-0.2.5-py2.py3-none-any.whl/just_agents/memory.py
@@ -0,0 +1,66 @@
+from dataclasses import dataclass, field
+from typing import Callable, Optional
+from litellm.types.utils import Function
+
+OnMessageCallable = Callable[[dict], None]
+OnFunctionCallable = Callable[[Function], None]
+
+@dataclass
+class Memory:
+
+    on_message: list[OnMessageCallable] = field(default_factory=list)
+    messages: list[dict] = field(default_factory=list)
+
+    def add_on_message(self, handler: OnMessageCallable):
+        self.on_message.append(handler)
+
+    def add_on_tool_result(self, handler: OnMessageCallable):
+        self.add_on_message(lambda m: handler(m) if m['role'] == "tool" else None)
+
+    def add_on_tool_call(self, fun: OnFunctionCallable):
+        """
+        Adds handler only to function calls to track what exactly was called
+        :param fun:
+        :return:
+        """
+        def tool_handler(message: dict) -> None:
+            if hasattr(message, 'tool_calls') and message.tool_calls is not None:
+                for call in message.tool_calls:
+                    #if call.function is Function:
+                    fun(call.function)
+        self.add_on_message(tool_handler)
+
+
+    def remove_on_message(self, handler: OnMessageCallable):
+        self.on_message = [m for m in self.on_message if m == handler]
+
+    def add_system_message(self, prompt: str):
+        return self.add_message({"role":"system", "content":prompt})
+
+    def add_user_message(self, prompt: str):
+        return self.add_message({"role":"user", "content":prompt})
+
+    def add_message(self, message: dict):
+        """
+        adds message to the memory
+        :param message:
+        :param run_callbacks:
+        :return:
+        """
+        self.messages.append(message)
+        for handler in self.on_message:
+            handler(message)
+
+    @property
+    def last_message(self) -> Optional[dict]:
+        return self.messages[-1] if len(self.messages) > 0 else None
+
+
+    def add_messages(self, messages: list[dict]):
+        for message in messages:
+            self.messages.append(message)
+            for handler in self.on_message:
+                handler(message)
+
+    def clear_messages(self):
+        self.messages.clear()
---
+++ packages/just-agents/just_agents-0.2.5-py2.py3-none-any.whl/just_agents/utils.py
@@ -0,0 +1,136 @@
+import os
+import random
+
+import yaml
+from pathlib import Path
+from typing import Optional, Dict, Any
+import importlib.resources as resources
+from dotenv import load_dotenv
+import importlib
+from typing import Callable
+from litellm import Message, ModelResponse, completion
+import copy
+
+#
+# class RotateKeys():
+#     keys:list[str]
+#
+#     def __init__(self, file_path:str):
+#         with open(file_path) as f:
+#             text = f.read().strip()
+#             self.keys = text.split("\n")
+#
+#     def __call__(self, *args, **kwargs):
+#         return random.choice(self.keys)
+#
+#     def remove(self, key:str):
+#         self.keys.remove(key)
+#
+#     def len(self):
+#         return len(self.keys)
+
+def resolve_agent_schema(agent_schema: str | Path | dict):
+    """
+    Resolve the agent schema from a string, path (to yaml file) or dict.
+    """
+    if isinstance(agent_schema, str):
+        agent_schema = Path(agent_schema)
+    if isinstance(agent_schema, Path):
+        if not agent_schema.exists():
+            raise ValueError(
+                f"In constructor agent_schema path is not exists: ({str(agent_schema)})!")
+        with open(agent_schema) as f:
+            agent_schema = yaml.safe_load(f)
+    if not isinstance(agent_schema, dict):
+        raise ValueError(
+            f"In constructor agent_schema parameter should be string, Path or dict!")
+
+    return agent_schema
+
+def _resolve_agent_schema(agent_schema: str | Path | dict | None, default_file_name: str):
+    if agent_schema is None:
+        agent_schema = Path(Path(__file__).parent, "config", default_file_name)
+
+    return resolve_agent_schema(agent_schema)
+
+
+def resolve_llm_options(agent_schema: dict, llm_options: dict):
+    if llm_options is None:
+        llm_options = agent_schema.get("options", None)
+    if llm_options is None:
+        raise ValueError(
+            "llm_options should not be None. You should pass it through llm_options or agent_schema['options'].")
+
+    return llm_options
+
+
+def resolve_system_prompt(agent_schema: dict):
+    system_prompt = agent_schema.get("system_prompt", None)
+    system_prompt_path = agent_schema.get("system_prompt_path", None)
+    if (system_prompt is not None) and (system_prompt_path is not None):
+        raise ValueError("You should use only one of system_prompt or system_prompt_path not both together.")
+    if system_prompt_path is not None:
+        path = Path(system_prompt_path)
+        if path.exists():
+            system_prompt = path.read_text(encoding="utf8")
+    return system_prompt
+
+
+def resolve_tools(agent_schema: dict) -> list[Callable]:
+    function_list:list[Callable] = []
+    tools = agent_schema.get('tools', None)
+    if tools is None:
+        return None
+    for entry in tools:
+        package_name: str = entry['package']
+        function_name: str = entry['function']
+        try:
+            # Dynamically import the package
+            package = importlib.import_module(package_name)
+            # Get the function from the package
+            func = getattr(package, function_name)
+            function_list.append(func)
+        except (ImportError, AttributeError) as e:
+            print(f"Error importing {function_name} from {package_name}: {e}")
+
+    return function_list
+
+
+def rotate_env_keys() -> str:
+    load_dotenv()
+    keys = []
+    index = 1
+
+    while True:
+        # Determine the environment variable name
+        key_name = 'KEY' if index == 0 else f'KEY_{index}'
+        key_value = os.getenv(key_name)
+
+        # Break the loop if the key is not found
+        if key_value is None:
+            break
+
+        # Add the found key to the list
+        keys.append(key_value)
+        index += 1
+
+    # Raise an error if no keys are found
+    if not keys:
+        raise ValueError("No keys found in environment variables")
+
+    # Randomly choose one of the available keys
+    return random.choice(keys)
+
+def load_config(resource: str, package: str = "just_agents.config") -> Dict[str, Any]:
+    """
+    :rtype: yaml config
+    """
+    if Path(resource).exists():
+        return yaml.safe_load(Path(resource).open("r"))
+    in_config = Path("config") / resource
+    if in_config.exists():
+        return yaml.safe_load( (Path("config") / resource).open("r") )
+    else:
+        # Load from package resources
+        with resources.open_text(package, 'agent_prompts.yaml') as file:
+            return yaml.safe_load(file)
