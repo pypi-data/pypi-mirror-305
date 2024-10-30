05ada406ef4420328d3d1ab48148d82f066d27d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-30 05:06:17 +0000

    Add package dopynion/0.1.10/dopynion-0.1.10-py3-none-any.whl

---
+++ packages/dopynion/dopynion-0.1.10-py3-none-any.whl/dopynion/data_model.py
@@ -0,0 +1,33 @@
+from datetime import datetime
+
+from pydantic import BaseModel, Field
+
+
+class Cards(BaseModel):
+    quantities: dict[str, int]
+
+
+class Player(BaseModel):
+    name: str
+    hand: Cards | None
+
+
+class Game(BaseModel):
+    finished: bool
+    players: list[Player]
+    stock: Cards
+
+
+class ActionRecord(BaseModel):
+    action: str
+    player: Player
+    score: int
+
+
+class PlayerTurnRecord(BaseModel):
+    actions: list[ActionRecord] = Field(default_factory=list)
+
+
+class GameRecord(BaseModel):
+    date: datetime
+    turns: list[PlayerTurnRecord] = Field(default_factory=list)
