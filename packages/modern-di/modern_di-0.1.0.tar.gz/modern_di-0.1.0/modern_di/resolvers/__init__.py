ac935043c2d4a5f381018aeaa9cba959d493cac5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-30 04:06:00 +0000

    Add package modern-di/0.1.0/modern_di-0.1.0.tar.gz

---
+++ packages/modern-di/modern_di-0.1.0.tar.gz/modern_di-0.1.0/modern_di/resolvers/__init__.py
@@ -0,0 +1,11 @@
+from modern_di.resolvers.base import AbstractResolver, BaseCreatorResolver
+from modern_di.resolvers.factory import Factory
+from modern_di.resolvers.resource import Resource
+
+
+__all__ = [
+    "AbstractResolver",
+    "BaseCreatorResolver",
+    "Factory",
+    "Resource",
+]
