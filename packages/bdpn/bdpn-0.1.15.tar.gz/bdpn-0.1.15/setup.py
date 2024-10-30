4e56ed41cb44447f2d59b3aa008b1771882951e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-30 16:04:57 +0000

    Add package bdpn/0.1.15/bdpn-0.1.15.tar.gz

---
+++ packages/bdpn/bdpn-0.1.15.tar.gz/bdpn-0.1.15/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='bdpn',
+    packages=find_packages(),
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+        'Topic :: Software Development',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    version='0.1.15',
+    description='Estimation of BDPN parameters from phylogenetic trees.',
+    author='Anna Zhukova',
+    author_email='anna.zhukova@pasteur.fr',
+    url='https://github.com/evolbioinfo/bdpn',
+    keywords=['phylogenetics', 'birth-death model', 'partner notification'],
+    install_requires=['six', 'ete3', 'numpy', "scipy==1.10.0-rc1", 'biopython'],
+    entry_points={
+            'console_scripts': [
+                'bdpn_infer = bdpn.bdpn_model:main',
+                'bd_infer = bdpn.bd_model:main',
+                'bdmult_infer = bdpn.bdmult_model:main',
+                'bdpn_loglikelihood = bdpn.bdpn_model:loglikelihood_main',
+                'bd_loglikelihood = bdpn.bd_model:loglikelihood_main',
+                'bdmult_loglikelihood = bdpn.bdmult_model:loglikelihood_main',
+                'pn_test = bdpn.model_distinguisher:main',
+            ]
+    },
+)
