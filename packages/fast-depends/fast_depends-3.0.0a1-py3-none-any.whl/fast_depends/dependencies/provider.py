06e0299f13b7176c14490836aad700e79c0981c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-30 07:04:59 +0000

    Add package fast-depends/3.0.0a1/fast_depends-3.0.0a1-py3-none-any.whl

---
+++ packages/fast-depends/fast_depends-3.0.0a1-py3-none-any.whl/fast_depends/dependencies/provider.py
@@ -0,0 +1,70 @@
+from contextlib import contextmanager
+from typing import TYPE_CHECKING, Any, Callable, Dict, Hashable, Iterator, TypeAlias
+
+from fast_depends.core import build_call_model
+
+if TYPE_CHECKING:
+    from fast_depends.core import CallModel
+
+
+Key: TypeAlias = Hashable
+
+
+class Provider:
+    dependencies: Dict[Key, "CallModel"]
+    overrides: Dict[Key, "CallModel"]
+
+    def __init__(self) -> None:
+        self.dependencies = {}
+        self.overrides = {}
+
+    def clear(self) -> None:
+        self.dependencies = {}
+        self.overrides = {}
+
+    def add_dependant(
+        self,
+        dependant: "CallModel",
+    ) -> Key:
+        key = self.__get_original_key(dependant.call)
+        self.dependencies[key] = dependant
+        return key
+
+    def get_dependant(self, key: Key) -> "CallModel":
+        return self.overrides.get(key) or self.dependencies[key]
+
+    def override(
+        self,
+        original: Callable[..., Any],
+        override: Callable[..., Any],
+    ) -> None:
+        key = self.__get_original_key(original)
+
+        override_model = build_call_model(
+            override,
+            dependency_provider=self,
+        )
+
+        if (original_dependant := self.dependencies.get(key)):
+            override_model.serializer = original_dependant.serializer
+
+        else:
+            self.dependencies[key] = build_call_model(
+                original,
+                dependency_provider=self,
+            )
+
+        self.overrides[key] = override_model
+
+    @contextmanager
+    def scope(
+        self,
+        original: Callable[..., Any],
+        override: Callable[..., Any],
+    ) -> Iterator[None]:
+        self.override(original, override)
+        yield
+        self.overrides.pop(self.__get_original_key(original), None)
+
+    def __get_original_key(self, original: Callable[..., Any]) -> Key:
+        return original
