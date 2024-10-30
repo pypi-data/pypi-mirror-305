cb5408508bbdbe2d93f2c49c31ad5d66931d65b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-30 15:05:52 +0000

    Add package async_yookassa/0.2.5/async_yookassa-0.2.5-py3-none-any.whl

---
+++ packages/async_yookassa/async_yookassa-0.2.5-py3-none-any.whl/async_yookassa/__init__.py
@@ -0,0 +1,9 @@
+from async_yookassa.configuration import Configuration
+from async_yookassa.invoice import Invoice
+from async_yookassa.payment import Payment
+from async_yookassa.refund import Refund
+
+__author__ = "Ivan Ashikhmin and YooMoney"
+__email__ = "sushkoos@gmail.com and cms@yoomoney.ru"
+__version__ = "0.2.5"
+__all__ = ["Configuration", "Payment", "Invoice", "Refund"]
---
+++ packages/async_yookassa/async_yookassa-0.2.5-py3-none-any.whl/async_yookassa/models/payment_submodels/deal_submodels/settlements.py
@@ -0,0 +1,11 @@
+from pydantic import BaseModel, ConfigDict
+
+from async_yookassa.enums.settlement import SettlementTypeEnum
+from async_yookassa.models.payment_submodels.amount import Amount
+
+
+class Settlement(BaseModel):
+    type: str = SettlementTypeEnum.payout
+    amount: Amount
+
+    model_config = ConfigDict(use_enum_values=True)
