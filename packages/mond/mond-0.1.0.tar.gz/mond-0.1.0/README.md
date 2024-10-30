8f1e087337531854243b9fe3545296a837300752
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-30 09:04:06 +0000

    Add package mond/0.1.0/mond-0.1.0.tar.gz

---
+++ packages/mond/mond-0.1.0.tar.gz/mond-0.1.0/README.md
@@ -0,0 +1,12 @@
+# monte-solver
+
+Monte Carlo dissolver
+
+## OpenMM 
+
+Installing OpenMM from PyPi is nice to avoid conda, but you need to have numpy<2 installed. In case
+of errors, you may run: 
+
+```bash
+pip install "numpy<2"
+```
---
+++ packages/mond/mond-0.1.0.tar.gz/mond-0.1.0/src/mond/monte_carlo/utils.py
@@ -0,0 +1,43 @@
+from typing import List 
+import math
+import numpy as np 
+from random import uniform
+
+def random_rotate(mol_xyz:List[List[float]])->List[List[float]]:
+    """
+    Random rotation of a list of xyz coordinates
+    """
+    rot_matrix = generate_random_rotation_matrix()
+    rotated_mol = rotate(rot_mat, mol_xyz)
+    return rotated_mol
+
+def rotate(rot_mat:List[List[float]], mol_xyz:List[List[float]])->List[List[float]]: 
+    """
+    Rotates a molecule given a specific rotation matrix
+    """
+    raise NotImplementedError
+
+def generate_random_rotation_matrix()-> List[List[float]]: 
+    
+    alpha = uniform(0, 360)
+    beta = uniform(0, 360)
+    gamma = uniform(0, 360)
+
+    r_x = [[1 , 0 , 0], 
+           [0, cos(alpha), -sin(alpha)], 
+           [0, sin(alpha), cos(alpha)]]
+           
+    r_y = [[cos(beta), 0, sin(beta)],
+            [0, 1, 0], 
+            [-sin(beta), 0, cos(beta)]]
+
+    r_z = [[cos(gamma), -sin(gamma), 0], 
+           [sin(gamma), cos(gamma), 0],
+           [0, 0, 1]]
+
+    random_matrix = list(np.dot(r_x, r_y, r_z))
+    return random_matrix
+
+def generate_random_translation_matrix()->: 
+    raise NotImplementedError
+    
