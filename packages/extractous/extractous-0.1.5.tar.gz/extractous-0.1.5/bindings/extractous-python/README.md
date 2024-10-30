823a093401034e715e54b7bbc26b9540f2c3321b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-30 03:09:46 +0000

    Add package extractous/0.1.5/extractous-0.1.5.tar.gz

---
+++ packages/extractous/extractous-0.1.5.tar.gz/extractous-0.1.5/bindings/extractous-python/README.md
@@ -0,0 +1,43 @@
+# Extractous Python Bindings
+
+This project provides Python bindings for the Extractous library, allowing you to use extractous functionality in 
+your Python applications.
+
+## Installation
+
+To install the extractous Python bindings, you can use pip:
+
+```bash
+pip install extractous
+```
+
+## Usage
+
+Extracting a file to string:
+
+```python
+from extractous import Extractor
+
+extractor = Extractor()
+extractor.set_extract_string_max_length(1000)
+result = extractor.extract_file_to_string("README.md")
+
+print(result)
+```
+
+Extracting a file to a buffered stream:
+
+```python
+from extractous import Extractor
+
+extractor = Extractor()
+reader = extractor.extract_file("tests/quarkus.pdf")
+
+result = ""
+buffer = reader.read(4096)
+while len(buffer) > 0:
+    result += buffer.decode("utf-8")
+    buffer = reader.read(4096)
+
+print(result)
+```
---
+++ packages/extractous/extractous-0.1.5.tar.gz/extractous-0.1.5/extractous-core/src/lib.rs
@@ -0,0 +1,69 @@
+//! Extractous is a library that extracts text from various file formats.
+//! * Supports many file formats such as Word, Excel, PowerPoint, PDF, and many more.
+//! * Strives to be simple fast and efficient
+//!
+//! # Quick Start
+//! Extractous API entry point is the [`Extractor`] struct.
+//! All public apis are accessible through an extractor.
+//! The extractor provides functions to extract text from files, Urls, and byte arrays.
+//! To use an extractor, you need to:
+//! - [create and configure new the extractor](#create-and-config-an-extractor)
+//! - [use the extractor to extract text](#extract-text)
+//!
+//! ## Create and config an extractor
+//!
+//! ```no_run
+//! use extractous::Extractor;
+//! use extractous::PdfParserConfig;
+//!
+//! // Create a new extractor. Note it uses a consuming builder pattern
+//! let mut extractor = Extractor::new()
+//!                       .set_extract_string_max_length(1000);
+//!
+//! // can also perform conditional configuration
+//! let custom_pdf_config = true;
+//! if custom_pdf_config {
+//!     extractor = extractor.set_pdf_config(
+//!         PdfParserConfig::new().set_extract_annotation_text(false)
+//!     );
+//! }
+//!
+//! ```
+//!
+//! ## Extract text
+//!
+//! ```no_run
+//! use extractous::Extractor;
+//! use extractous::PdfParserConfig;
+//!
+//! // Create a new extractor. Note it uses a consuming builder pattern
+//! let mut extractor = Extractor::new().set_extract_string_max_length(1000);
+//!
+//! // Extract text from a file
+//! let text = extractor.extract_file_to_string("README.md").unwrap();
+//! println!("{}", text);
+//!
+//! ```
+
+/// Default buffer size
+pub const DEFAULT_BUF_SIZE: usize = 32768;
+
+// errors module
+mod errors;
+pub use errors::*;
+
+// extractor module is the config interface
+mod config;
+pub use config::*;
+// extractor module is the main public api interface
+mod extractor;
+pub use extractor::*;
+
+// tika module, not exposed outside this crate
+mod tika {
+    mod jni_utils;
+    mod parse;
+    mod wrappers;
+    pub use parse::*;
+    pub use wrappers::JReaderInputStream;
+}
