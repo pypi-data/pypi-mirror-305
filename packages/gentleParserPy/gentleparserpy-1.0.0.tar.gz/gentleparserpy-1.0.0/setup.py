f91c9b94ee5dabb670b179713724e0ef326e028f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-30 04:04:59 +0000

    Add package gentleParserPy/1.0.0/gentleparserpy-1.0.0.tar.gz

---
+++ packages/gentleParserPy/gentleparserpy-1.0.0.tar.gz/gentleparserpy-1.0.0/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+def readme():
+  with open('README.md', 'r') as f:
+    return f.read()
+
+setup(
+  name='gentleParserPy',
+  version='1.0.0',
+  author='lopaet',
+  author_email='',
+  description='This is my first module',
+  long_description=readme(),
+  long_description_content_type='text/markdown',
+  packages=find_packages(),
+  install_requires=[],
+  classifiers=[
+    'Programming Language :: Python :: 3.11',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent'
+  ],
+  keywords='example python',
+  # project_urls={
+  #   'Documentation': 'link'
+  # },
+  python_requires='>=3.7'
+)
