9cc8491faf9b440c31a3e7f3c78189480ca6d0a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-30 08:04:49 +0000

    Add package copkmeans-ht/1.5/copkmeans-ht-1.5.tar.gz

---
+++ packages/copkmeans-ht/copkmeans-ht-1.5.tar.gz/copkmeans-ht-1.5/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='copkmeans-ht',
+    version='1.5',
+    description='This is just a copy of the original repo https://github.com/Behrouz-Babaki/COP-Kmeans',
+    author='Behrouz Babaki',
+    author_email='',
+    url='https://github.com/hongtaoh/COP-Kmeans',
+    license='MIT',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False
+)
