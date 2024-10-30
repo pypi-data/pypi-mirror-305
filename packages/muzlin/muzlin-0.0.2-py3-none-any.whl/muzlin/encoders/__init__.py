2150df679de16abcd777b790cd896c3d9013bce6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-30 08:04:12 +0000

    Add package muzlin/0.0.2/muzlin-0.0.2-py3-none-any.whl

---
+++ packages/muzlin/muzlin-0.0.2-py3-none-any.whl/muzlin/encoders/__init__.py
@@ -0,0 +1,14 @@
+import apipkg
+
+# Implement lazy loading
+apipkg.initpkg(__name__, {
+    'BaseEncoder': 'muzlin.encoders.base:BaseEncoder',
+    'AzureOpenAIEncoder': 'muzlin.encoders.zure:AzureOpenAIEncoder',
+    'BedrockEncoder': 'muzlin.encoders.bedrock:BedrockEncoder',
+    'CohereEncoder': 'muzlin.encoders.cohere:CohereEncoder',
+    'FastEmbedEncoder': 'muzlin.encoders.fastembed:FastEmbedEncoder',
+    'GoogleEncoder': 'muzlin.encoders.google:GoogleEncoder',
+    'HuggingFaceEncoder': 'muzlin.encoders.huggingface:HuggingFaceEncoder',
+    'HFEndpointEncoder': 'muzlin.encoders.huggingface:HFEndpointEncoder',
+    'OpenAIEncoder': 'muzlin.encoders.openai:OpenAIEncoder',
+})
