116430b4607b971bc8afe5ec003db0cc1c6a2e18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-30 04:05:59 +0000

    Add package lago-python-client/1.15.0/lago_python_client-1.15.0-py3-none-any.whl

---
+++ packages/lago-python-client/lago_python_client-1.15.0-py3-none-any.whl/lago_python_client/models/billable_metric.py
@@ -0,0 +1,41 @@
+from typing import List, Optional
+
+from lago_python_client.base_model import BaseModel
+
+from ..base_model import BaseResponseModel
+
+
+class BillableMetricFilter(BaseModel):
+    key: Optional[str]
+    values: Optional[List[str]]
+
+
+class BillableMetricFilters(BaseModel):
+    __root__: List[BillableMetricFilter]
+
+
+class BillableMetric(BaseModel):
+    name: Optional[str]
+    code: Optional[str]
+    description: Optional[str]
+    recurring: Optional[bool]
+    aggregation_type: Optional[str]
+    weighted_interval: Optional[str]
+    field_name: Optional[str]
+    filters: Optional[BillableMetricFilters]
+
+
+class BillableMetricResponse(BaseResponseModel):
+    lago_id: str
+    name: str
+    code: str
+    description: Optional[str]
+    recurring: Optional[bool]
+    aggregation_type: Optional[str]
+    weighted_interval: Optional[str]
+    field_name: Optional[str]
+    created_at: str
+    filters: BillableMetricFilters
+    active_subscriptions_count: int
+    draft_invoices_count: int
+    plans_count: int
