6eacece8309b1bc1380c04105e964e2289a37e2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-30 04:06:08 +0000

    Add package mapaction-logging-module/0.2.4/mapaction_logging_module-0.2.4.tar.gz

---
+++ packages/mapaction-logging-module/mapaction_logging_module-0.2.4.tar.gz/mapaction_logging_module-0.2.4/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='mapaction_logging_module',
+    version='0.2.4',
+    author='Harry Sibbenga',             
+    author_email='hsibbenga@mapaction.org',
+    description='A structured logging module for MapAction applications with a Dashboard',
+    long_description=long_description,  
+    long_description_content_type="text/markdown", 
+    url='https://github.com/mapaction/mapaction_logging_module.git',  
+    packages=find_packages(),          
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",  
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',           
+    install_requires=["streamlit", "pandas"]              
+)
