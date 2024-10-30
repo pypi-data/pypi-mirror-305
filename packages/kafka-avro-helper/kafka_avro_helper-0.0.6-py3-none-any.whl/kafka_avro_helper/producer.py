086afa8ed54ac365a23d022a096a911375d47b57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-30 03:11:40 +0000

    Add package kafka-avro-helper/0.0.6/kafka_avro_helper-0.0.6-py3-none-any.whl

---
+++ packages/kafka-avro-helper/kafka_avro_helper-0.0.6-py3-none-any.whl/kafka_avro_helper/producer.py
@@ -0,0 +1,63 @@
+from os import environ
+from typing import Any, Optional
+from aiokafka import AIOKafkaProducer
+from dataclasses_avroschema import AvroModel
+import struct
+import logging
+
+MAGIC_BYTE = 0
+__producer__: AIOKafkaProducer = None   # NOQA
+
+
+def value_serializer(value: Any) -> Optional[bytes]:
+    if value is None:
+        return None
+    if isinstance(value, bytes):
+        return value
+    if isinstance(value, AvroModel):
+        value.validate()
+        serialized_data = value.serialize()
+        schema_id = value.get_metadata().schema_id  # NOQA
+        prefix_bytes = struct.pack(">bI", MAGIC_BYTE, schema_id)
+        return prefix_bytes + serialized_data
+    raise NotImplementedError(f"Value {value} of type {type(value)} not supported")
+
+
+def key_serializer(value: Any) -> Optional[bytes]:
+    if value is None:
+        return None
+    if isinstance(value, bytes):
+        return value
+    else:
+        return str(value).encode('utf-8')
+
+
+async def get_producer() -> AIOKafkaProducer:
+    """ Get a Kafka producer instance, it will be created if it does not exist and started if it is not ready """
+    global __producer__
+    if not __producer__:
+        KAFKA_BROKERS = environ.get("KAFKA_BROKERS")
+        __producer__ = AIOKafkaProducer(
+            bootstrap_servers=KAFKA_BROKERS or "localhost",
+            value_serializer=value_serializer,
+            key_serializer=key_serializer
+        )
+        if KAFKA_BROKERS is None:
+            logging.warning("KAFKA_BROKERS environment variable not set, producer not started")
+            return __producer__
+    await __producer__.start()
+    return __producer__
+
+
+async def send_message(
+        topic: str,
+        key: Any,
+        value: AvroModel,
+        headers: dict = None,
+        wait=True
+):
+    producer = await get_producer()
+    if wait:
+        await producer.send_and_wait(topic, key=key, value=value, headers=headers)
+    else:
+        await producer.send(topic, key=key, value=value, headers=headers)
