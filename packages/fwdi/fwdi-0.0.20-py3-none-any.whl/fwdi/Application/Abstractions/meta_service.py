8d4022a87adabda3a304ea90cf8160ce27fa35f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-30 14:04:56 +0000

    Add package fwdi/0.0.20/fwdi-0.0.20-py3-none-any.whl

---
+++ packages/fwdi/fwdi-0.0.20-py3-none-any.whl/fwdi/Application/Abstractions/meta_service.py
@@ -0,0 +1,27 @@
+#        _       __        __    __________    _____     __   __     __
+#   	/ \	    |  |	  |__|  |__________|  |  _  \   |__|  \ \   / /
+#      / _ \	|  |	   __       |  |      | |_)  |   __    \ \_/ /   Alitrix - Modern NLP
+#     / /_\ \	|  |	  |  |      |  |      |  _  /   |  |    } _ {    Languages: Python, C#
+#    / _____ \	|  |____  |  |      |  |      | | \ \   |  |   / / \ \   http://github.com/Alitrix
+#   /_/     \_\	|_______| |__|	    |__|      |_|  \_\  |__|  /_/   \_\
+
+# Licensed under the MIT License <http://opensource.org/licenses/MIT>
+# SPDX-License-Identifier: MIT
+# Copyright (c) 2020-2021 The Alitrix Authors <http://github.com/Alitrix>
+
+from ...Utilites.ext_reflection import ExtReflection
+
+class MetaServiceFWDI(type):
+    def __init__(cls, name, bases, ns):
+        for attr, value in cls.__dict__.items():
+            if callable(value):
+                if attr == '__call__':
+                    setattr(cls, attr, ExtReflection.init_inject(value))
+                    
+                if attr == '__init__':
+                    setattr(cls, attr, ExtReflection.init_inject(value))
+
+                if not attr.startswith('__'):
+                    setattr(cls, attr, ExtReflection.method_inject(value))
+
+        super().__init__(name, bases, ns)
