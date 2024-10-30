7e4e8c05273f3b6a0a0bbf3903e115c56a7bbe17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-30 14:05:31 +0000

    Add package faker-file/0.17.13/faker-file-0.17.13.tar.gz

---
+++ packages/faker-file/faker-file-0.17.13.tar.gz/faker-file-0.17.13/scripts/black.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+#black conftest.py
+#black setup.py
+#black examples/
+#black src/
+black .
---
+++ packages/faker-file/faker-file-0.17.13.tar.gz/faker-file-0.17.13/scripts/ruff.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+ruff conftest.py
+ruff setup.py
+ruff examples/
+ruff src/
