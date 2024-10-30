e30117ab0a5dd040b284ba9fd1238b92f755cc4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-30 03:11:07 +0000

    Add package mtbacktest/0.0.11/mtbacktest-0.0.11-py3-none-any.whl

---
+++ packages/mtbacktest/mtbacktest-0.0.11-py3-none-any.whl/mtbacktest/lib/metrics.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from simulation import simulate
+
+def cagr(data:pd.Series, periods=252):
+    return (data.iloc[-1] / data.iloc[0]) ** (periods / len(data)) - 1
+def sharpe_ratio(data:pd.Series, risk_free_rate=0.004):
+    returns = cagr(data)
+    return (np.mean(returns) - risk_free_rate) / np.std(returns)
+
+np.random.seed(123)
+data = pd.Series(simulate())
+print(sharpe_ratio(data))
