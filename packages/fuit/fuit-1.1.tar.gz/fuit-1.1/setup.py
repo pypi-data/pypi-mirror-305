4b04985400a2a74ab417ecca2a2aac6fb98a34a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-30 04:05:15 +0000

    Add package fuit/1.1/fuit-1.1.tar.gz

---
+++ packages/fuit/fuit-1.1.tar.gz/fuit-1.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+libs = open("requirements.txt").read().splitlines()
+description = open("README.md", encoding="utf-8").read()
+setup(
+    name="fuit",
+    version="1.01",
+    url="",
+    license="MIT",
+    author="DaSh-More",
+    author_email="",
+    description=description,
+    install_requires=libs,
+    platforms=["any"],
+)
