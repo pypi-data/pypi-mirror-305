145797a96af380efcaa8b93f4e976a795877d059
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-30 14:05:42 +0000

    Add package tyba-cvxpy/1.6.0/tyba_cvxpy-1.6.0-cp312-cp312-win_amd64.whl

---
+++ packages/tyba-cvxpy/tyba_cvxpy-1.6.0-cp312-cp312-win_amd64.whl/cvxpy/version.py
@@ -0,0 +1,10 @@
+
+# THIS FILE IS GENERATED FROM CVXPY SETUP.PY
+short_version = '1.6.0'
+version = '1.6.0'
+full_version = '1.6.0.dev0+0.2ac40d4'
+git_revision = '2ac40d4'
+commit_count = '0'
+release = False
+if not release:
+    version = full_version
