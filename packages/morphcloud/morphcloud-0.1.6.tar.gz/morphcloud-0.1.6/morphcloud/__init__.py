ab7025a7a050b12f51767503dc8c89caad2ad3d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-30 15:05:49 +0000

    Add package morphcloud/0.1.6/morphcloud-0.1.6.tar.gz

---
+++ packages/morphcloud/morphcloud-0.1.6.tar.gz/morphcloud-0.1.6/morphcloud/__init__.py
@@ -0,0 +1,9 @@
+"""
+Copyright (c) 2024 Morph Labs. All rights reserved.
+Released under the license as described in the file LICENSE.
+"""
+
+from .runtime import Runtime
+
+__version__ = "0.1.0"
+__all__ = ["Runtime"]
