a69c6c80bc6998030316640d3a5ccd0913145eda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-30 04:06:57 +0000

    Add package MbaliHelloPkg/1.0.0/mbalihellopkg-1.0.0.tar.gz

---
+++ packages/MbaliHelloPkg/mbalihellopkg-1.0.0.tar.gz/mbalihellopkg-1.0.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+#get the long description from the README file
+long_description = "Testing the packaging process"
+
+setup(
+    name ="MbaliHelloPkg",
+    version ="1.0.0",
+ description="A sample of python Hello world",
+long_description=long_description,
+author = "Mbali Python",
+author_email="mbaliphama8@gmail.com",
+classifier = [
+"development status::1- Planning",
+    "Intended audience::Developers",
+    "Topic::Software  Development::Build Tools",
+    "License :: OSI Approved;;MIT License",
+    "Programming Language ::Python::3",
+    "Operating System::Microsoft::Windows",
+    
+   ],
+)
+ 
