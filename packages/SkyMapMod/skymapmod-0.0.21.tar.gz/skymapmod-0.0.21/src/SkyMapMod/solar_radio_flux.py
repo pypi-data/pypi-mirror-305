ee28ebb896aadec21f8f32bc0c802bc254425ffe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-30 05:05:05 +0000

    Add package SkyMapMod/0.0.21/skymapmod-0.0.21.tar.gz

---
+++ packages/SkyMapMod/skymapmod-0.0.21.tar.gz/skymapmod-0.0.21/src/SkyMapMod/solar_radio_flux.py
@@ -0,0 +1,23 @@
+import requests
+
+url = "https://www.spaceweather.gc.ca/solar_flux_data/daily_flux_values/fluxtable.txt"
+response = requests.get(url)
+
+a = response.text
+a = a.split('\n')
+
+fluxdate = []
+fluxtime = []
+fluxjulian = []
+fluxcarrington = []
+fluxobsflux = []
+fluxadjflux = []
+fluxursi = []
+for i in range(2, len(a)-1):
+    fluxdate.append(a[i].split()[0])
+    fluxtime.append(a[i].split()[1])
+    fluxjulian.append(a[i].split()[2])
+    fluxcarrington.append(a[i].split()[3])
+    fluxobsflux.append(a[i].split()[4])
+    fluxadjflux.append(a[i].split()[5])
+    fluxursi.append(a[i].split()[6])
