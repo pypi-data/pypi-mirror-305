6220bac30d9b34fa2d75b7cf3c2d172d768157ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-30 04:06:15 +0000

    Add package django-petra/0.0.1/django_petra-0.0.1-py3-none-any.whl

---
+++ packages/django-petra/django_petra-0.0.1-py3-none-any.whl/django_petra/petra_core/index.py
@@ -0,0 +1,20 @@
+from rest_framework.viewsets import ViewSet as View
+from rest_framework.response import Response
+from rest_framework.serializers import ModelSerializer
+from django_petra.status import Status as MyStatus
+
+class ViewSet(View):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+class Response(Response):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        
+class status(MyStatus):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        
+class ModelSerializer(ModelSerializer):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
