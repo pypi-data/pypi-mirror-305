61c84bf97a9a61da57769a59dec2ecf130bdf248
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-30 09:04:08 +0000

    Add package PyViCare/2.35.0/pyvicare-2.35.0-py3-none-any.whl

---
+++ packages/PyViCare/pyvicare-2.35.0-py3-none-any.whl/PyViCare/PyViCareDevice.py
@@ -0,0 +1,23 @@
+from PyViCare.PyViCareService import ViCareService
+from PyViCare.PyViCareUtils import handleNotSupported
+
+
+class Device:
+    """This is the base class for all simple devices.
+    This class connects to the Viessmann ViCare API.
+    The authentication is done through OAuth2.
+    Note that currently, a new token is generated for each run.
+    """
+
+    def __init__(self, service: ViCareService) -> None:
+        self.service = service
+
+    @handleNotSupported
+    def getSerial(self):
+        return self.service.getProperty("device.serial")["properties"]["value"]["value"]
+
+    def isLegacyDevice(self) -> bool:
+        return self.service.hasRoles(["type:legacy"])
+
+    def isE3Device(self) -> bool:
+        return self.service.hasRoles(["type:E3"])
