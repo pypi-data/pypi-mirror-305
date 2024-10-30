2e449638633e6a33fceb426ddf20d7654bb78dda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-30 05:04:43 +0000

    Add package buteo/0.9.63/buteo-0.9.63.tar.gz

---
+++ packages/buteo/buteo-0.9.63.tar.gz/buteo-0.9.63/buteo/ai/__init__.py
@@ -0,0 +1,10 @@
+"""Tools for working with sentinel data"""
+from .augmentation import *
+from .augmentation_funcs import *
+from .augmentation_utils import *
+from .scalers import *
+from .selection import *
+from .encoding import *
+from .masking_funcs import *
+from .masking import *
+from .spatial_label_smoothing import *
---
+++ packages/buteo/buteo-0.9.63.tar.gz/buteo-0.9.63/buteo/eo/__init__.py
@@ -0,0 +1,6 @@
+"""Tools for working with sentinel data"""
+from .s1_utils import *
+from .s2_utils import *
+from .s1_preprocess import *
+from .s2_indices import *
+from .xyz_tiles import *
