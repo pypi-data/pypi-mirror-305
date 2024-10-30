f3375ef3377d831134d7f4df7308d5f1a8344cc2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-30 16:04:33 +0000

    Add package blexus/0.0.5/blexus-0.0.5.tar.gz

---
+++ packages/blexus/blexus-0.0.5.tar.gz/blexus-0.0.5/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="blexus",
+    version="0.0.5",
+    author="Blexus",
+    author_email="mmmmmm505090@gmail.com",
+    description="Blexus official package",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/Blexus-org/pkg",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
