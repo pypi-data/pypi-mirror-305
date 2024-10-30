b80559b71609e6c9af45b56df893cc27a848ff55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-30 08:04:53 +0000

    Add package configurationlib/1.0.0/configurationlib-1.0.0.tar.gz

---
+++ packages/configurationlib/configurationlib-1.0.0.tar.gz/configurationlib-1.0.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='configurationlib',
+    version='1.0.0',
+    author='kokodev',
+    author_email='koko@kokodev.cc',
+    description='A simple configuration manager',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    install_requires=[],  # Add any dependencies if needed
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
