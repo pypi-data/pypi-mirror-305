6c7de30ef70934be0b03170d701dc9346b29e05d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-30 03:09:24 +0000

    Add package simplemind/0.1.0/simplemind-0.1.0-py3-none-any.whl

---
+++ packages/simplemind/simplemind-0.1.0-py3-none-any.whl/simplemind/providers/__init__.py
@@ -0,0 +1,9 @@
+from typing import List, Type
+
+from simplemind.providers._base import BaseProvider
+from simplemind.providers.anthropic import Anthropic
+from simplemind.providers.groq import Groq
+from simplemind.providers.openai import OpenAI
+from simplemind.providers.xai import XAI
+
+providers: List[Type[BaseProvider]] = [Anthropic, Groq, OpenAI, XAI]
