9d23905da3137d89574920792a79ed610af65975
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-30 16:04:51 +0000

    Add package com.hayatapps.pa.service.api/0.10/com_hayatapps_pa_service_api-0.10.tar.gz

---
+++ packages/com.hayatapps.pa.service.api/com_hayatapps_pa_service_api-0.10.tar.gz/com_hayatapps_pa_service_api-0.10/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="com.hayatapps.pa.service.api",
+    version="0.10",
+    author="Roman Połchowski",
+    author_email="rp@hayatapps.com",
+    description="PA proto package",
+    packages=find_packages(),
+    install_requires=[
+        'protobuf>=5.26.1',
+        'grpcio>=1.56.2',
+        'grpcio-tools>=1.56.2'
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.9',
+)
