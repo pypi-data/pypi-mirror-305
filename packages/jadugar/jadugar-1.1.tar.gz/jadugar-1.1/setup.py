cce98092df81435aa499e84ef7c53bf286222a00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-30 07:07:39 +0000

    Add package jadugar/1.1/jadugar-1.1.tar.gz

---
+++ packages/jadugar/jadugar-1.1.tar.gz/jadugar-1.1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+
+# Read the content of your README file
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name='jadugar',
+    version='1.1',  # Update the version number
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={'file': ['../data/*.txt']},
+    install_requires=[
+        # list your dependencies here
+    ],
+    author='Raj N',
+    description='Jadugar is a fun project for experimentation with Python packages, read more at project links below',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    project_urls={ 
+        'Linktree': 'https://linktr.ee/rajnandale',
+        'GitHub': 'https://github.com/rajnandale',
+        'Gist': 'https://gist.github.com/rajnandale/471c9f2fe5e6012b5fdd8990ef761239',
+        'LinkedIn': 'https://www.linkedin.com/in/rajnandale',
+    },
+)
