c13db8560cd788a50e6d2a66e9f961c1ae63a157
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-30 14:04:48 +0000

    Add package common-email-check/0.1.4/common_email_check-0.1.4.tar.gz

---
+++ packages/common-email-check/common_email_check-0.1.4.tar.gz/common_email_check-0.1.4/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="common_email_check",       # 패키지 이름
+    version="0.1.4",                 # 버전
+    description="A module for email checking",  # 설명
+    author="Your Name",              # 작성자 이름
+    author_email="your_email@example.com",  # 작성자 이메일
+    packages=find_packages(),        # 패키지 목록 자동 탐색
+    install_requires=[
+        "pyo3",  # 필요한 라이브러리
+    ],
+)
