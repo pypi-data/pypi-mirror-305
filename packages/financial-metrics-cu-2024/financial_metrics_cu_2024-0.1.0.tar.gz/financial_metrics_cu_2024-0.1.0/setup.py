bb1dd70f00867d386e8b3b63fbe4763d06690cb6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-30 05:05:08 +0000

    Add package financial-metrics-cu-2024/0.1.0/financial_metrics_cu_2024-0.1.0.tar.gz

---
+++ packages/financial-metrics-cu-2024/financial_metrics_cu_2024-0.1.0.tar.gz/financial_metrics_cu_2024-0.1.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='financial_metrics_cu_2024',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[],
+    author='Anastasia',
+    author_email='a.opushneva@edu.centraluniversity.ru',
+    description='Python package for calculating financial metrics.',
+    url='https://github.com/your-username/financial_metrics',
+    license='MIT',
+    entry_points={
+        'console_scripts': [
+            'financial_metrics = financial_metrics.main:main',
+        ],
+    },
+)
