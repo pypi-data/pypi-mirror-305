e687c892f75378a72aa9bb54e991fe8889a566c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-30 03:11:05 +0000

    Add package sskie/0.3.45/sskie-0.3.45-py3-none-any.whl

---
+++ packages/sskie/sskie-0.3.45-py3-none-any.whl/sskie/spatial_operators.py
@@ -0,0 +1,13 @@
+import numpy as np
+from scipy.spatial import distance
+
+# 데이터 구조화 함수
+def get_coordinates(self, df):
+    x_coords = np.array(df['X'])
+    y_coords = np.array(df['Y'])
+    return np.stack([x_coords, y_coords], axis=1)
+
+# 두 지점 집합 간 거리 행렬을 계산함
+def operator_distance_matrix(self, pts_matrix_1, pts_matrix_2):
+    OD_Dist = distance.cdist(pts_matrix_1, pts_matrix_2)
+    return OD_Dist
