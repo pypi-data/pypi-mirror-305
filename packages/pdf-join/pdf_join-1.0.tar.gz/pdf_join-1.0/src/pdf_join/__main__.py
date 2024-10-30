8664b58ce1a49bc77c3e1419e9f204dae7be6b97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-30 08:04:09 +0000

    Add package pdf-join/1.0/pdf_join-1.0.tar.gz

---
+++ packages/pdf-join/pdf_join-1.0.tar.gz/pdf_join-1.0/src/pdf_join/__main__.py
@@ -0,0 +1,19 @@
+# __main__.py
+
+import sys
+
+from .pdf_join import pdf_join
+
+def main():
+	'''
+	Run pdfmerge to merge PDFs.
+	'''
+	out = [arg for i, arg in enumerate(sys.argv[1:]) if sys.argv[i-1] == '--out']
+	if not out:
+		out = 'merged.pdf'
+	
+	args = [arg for i, arg in enumerate(sys.argv[1:]) if arg not in ['pdf_join.py', '--out'] and not sys.argv[i-1] == '--out']
+	pdf_join(out=out, *args)
+
+if __name__ == '__main__':
+	main()
