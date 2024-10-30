8da2899ec27da04b8b4208df719ae60e91d7748f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-30 07:05:02 +0000

    Add package finbourne-insights-sdk-preview/0.0.807/finbourne_insights_sdk_preview-0.0.807-py3-none-any.whl

---
+++ packages/finbourne-insights-sdk-preview/finbourne_insights_sdk_preview-0.0.807-py3-none-any.whl/finbourne_insights/api/__init__.py
@@ -0,0 +1,10 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from finbourne_insights.api.access_evaluations_api import AccessEvaluationsApi
+from finbourne_insights.api.application_metadata_api import ApplicationMetadataApi
+from finbourne_insights.api.auditing_api import AuditingApi
+from finbourne_insights.api.requests_api import RequestsApi
+from finbourne_insights.api.vendor_logs_api import VendorLogsApi
