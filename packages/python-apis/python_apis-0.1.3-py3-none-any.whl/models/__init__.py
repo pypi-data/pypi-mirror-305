89d4c1eb004ca3cbab4d42992dc9d61939044fbe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-30 03:11:43 +0000

    Add package python-apis/0.1.3/python_apis-0.1.3-py3-none-any.whl

---
+++ packages/python-apis/python_apis-0.1.3-py3-none-any.whl/models/__init__.py
@@ -0,0 +1,10 @@
+"""
+Here the models for the Python-APIs package are available, if you are creating a new services
+you can import the model from here if it is available.
+"""
+
+from .ad_user import ADUser
+
+__all__ = [
+    "ADUser",
+]
