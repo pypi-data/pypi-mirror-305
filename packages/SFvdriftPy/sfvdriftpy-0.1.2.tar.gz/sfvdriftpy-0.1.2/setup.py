72fc12b2d96985ff280dab6660c6fd8591e32d7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-30 14:05:16 +0000

    Add package SFvdriftPy/0.1.2/sfvdriftpy-0.1.2.tar.gz

---
+++ packages/SFvdriftPy/sfvdriftpy-0.1.2.tar.gz/sfvdriftpy-0.1.2/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='SFvdriftPy',
+    version='0.1.2',
+    description="""Model for equatorial vertical drift based on paper published by Scherliess & Fejer.
+                Refactored from sami2py development team.""",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Mateo Cardona Serrano (them)',
+    author_email='mcardonaserrano@berkeley.edu',
+    url='https://github.com/mcardonaserrano/SFvdriftPy',
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={'SFvdriftPy': ['data/*.txt']},
+    install_requires=[
+        'numpy'
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
