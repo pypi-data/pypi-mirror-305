c621ac0ee5ee2d62fac391e1587512e70f33f8ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-30 16:04:38 +0000

    Add package dig-spider/0.0.2/dig_spider-0.0.2.tar.gz

---
+++ packages/dig-spider/dig_spider-0.0.2.tar.gz/dig_spider-0.0.2/test/test.py
@@ -0,0 +1,14 @@
+from scrapy.crawler import CrawlerProcess
+from scrapy.utils.project import get_project_settings
+import os
+if __name__ == "__main__":
+    # os.environ.setdefault("XDG_CONFIG_HOME", "/Users/bytedance/learn/git/dig/src")
+    process = CrawlerProcess(get_project_settings())
+    # 'followall' is the name of one of the spiders of the project.
+    process.crawl("website", config="config/allsharktankproducts.yaml",
+                  url="https://allsharktankproducts.com/sharktankproducts/season-12-products/",
+                  # LOG_LEVEL="INFO"
+                  # DOWNLOAD_DELAY=3
+                  )
+    process.start()
+    process.stop()
