552e3f87cd66d991c5945af10daa66b0cb9eec31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-30 14:05:31 +0000

    Add package grss/4.3.4/grss-4.3.4.tar.gz

---
+++ packages/grss/grss-4.3.4.tar.gz/grss-4.3.4/setup.py
@@ -0,0 +1,46 @@
+""" Setup script for grss package. """
+# taken and adapted from setup.py at https://github.com/pybind/cmake_example
+import os
+import subprocess
+from pathlib import Path
+
+from setuptools import Extension, setup
+from setuptools.command.build_ext import build_ext
+
+class CMakeExtension(Extension):
+    """
+    CMake extension for setuptools.
+
+    Parameters
+    ----------
+    Extension : setuptools.Extension
+        Extension object from setuptools.
+    """
+    def __init__(self, name: str, sourcedir: str = "") -> None:
+        super().__init__(name, sources=[])
+        self.sourcedir = os.fspath(Path(sourcedir).resolve())
+
+class CMakeBuild(build_ext):
+    """
+    CMake build class for setuptools.
+
+    Parameters
+    ----------
+    build_ext : setuptools.command.build_ext.build_ext
+        Build extension object from setuptools.
+    """
+
+    def build_extension(self, ext: CMakeExtension) -> None:
+        subprocess.run(["./build_cpp.sh"], cwd=ext.sourcedir, check=True)
+        binary_created = [f for f in os.listdir(f"{ext.sourcedir}/build/")
+                            if f.startswith("libgrss")]
+        if not binary_created:
+            raise FileNotFoundError("libgrss binary for C++ source code not found "
+                                    "in cmake build directory")
+        os.system(f"cp {ext.sourcedir}/build/libgrss.* {self.build_lib}/grss/")
+        return
+
+setup(
+    ext_modules=[CMakeExtension("libgrss")],
+    cmdclass={"build_ext": CMakeBuild}
+)
