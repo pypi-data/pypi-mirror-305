256720bca7583befa279dde807aabfee88e7e5a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-30 07:05:02 +0000

    Add package jadugar/0.5/jadugar-0.5.tar.gz

---
+++ packages/jadugar/jadugar-0.5.tar.gz/jadugar-0.5/setup.py
@@ -0,0 +1,18 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name='jadugar',
+    version='0.5',  # Update the version number
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={'file': ['../data/*.txt']},
+    install_requires=[
+        # list your dependencies here
+    ],
+    author='Raj N',
+    author_email='mail6164@duck.com',
+    description='Jadugar is fun project for experimentation with python packages, read more at project links below ',
+    url='https://gist.github.com/rajnandale/471c9f2fe5e6012b5fdd8990ef761239'
+)
