33d512ce3b2c33ad9dba0616fe2e9a22ad16a334
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-31 15:05:33 +0000

    Add package adafruit-fingerprint/1.0.3/adafruit-fingerprint-1.0.3.tar.gz

---
+++ packages/adafruit-fingerprint/adafruit-fingerprint-1.0.3.tar.gz/adafruit-fingerprint-1.0.3/setup.py
@@ -0,0 +1,32 @@
+from os import path
+from setuptools import setup, find_packages
+
+README = f'{path.abspath(path.dirname(__file__))}/README.md'
+
+with open(README, 'r') as readme_file:
+    README_CONTENT = readme_file.read()
+
+setup(
+    name='adafruit-fingerprint',
+    version='1.0.3',
+    packages=find_packages(),
+
+    # meta
+    author='Faith Odonghanro, Promise Nwanozie, Joshua Adegoke',
+    author_email='toriboi.fo@gmail.com',
+    description='Interface with the adafruit fingerprint r305 module from upper computer over serial connection',
+    long_description_content_type='text/markdown',
+    long_description=README_CONTENT,
+    license='MIT',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6'
+    ],
+    url='https://github.com/cerebrohivetech/adafruit-fingerprint',
+    project_urls={
+        'Documentation': 'https://adafruit-fingerprint.readthedocs.io',
+        'Repository': 'https://github.com/cerebrohivetech/adafruit-fingerprint'
+    },
+    install_requires=['pyserial==3.4.0']
+)
