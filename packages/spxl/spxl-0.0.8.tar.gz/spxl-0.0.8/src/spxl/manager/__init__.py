a155348512eb624758010b3e8326de6a1b60860e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-31 15:04:58 +0000

    Add package spxl/0.0.8/spxl-0.0.8.tar.gz

---
+++ packages/spxl/spxl-0.0.8.tar.gz/spxl-0.0.8/src/spxl/manager/__init__.py
@@ -0,0 +1,15 @@
+#--------------------------------------------------------------------------------
+# 참조 모듈 목록.
+#--------------------------------------------------------------------------------
+from __future__ import annotations
+from typing import Awaitable, Callable, Final, Generic, Iterator, Optional, Sequence, Type, TypeVar, Union, Tuple, Any, List, Dict, Set, cast, overload
+import builtins
+
+
+#--------------------------------------------------------------------------------
+# 패키지 안의 클래스 별칭 목록.
+#--------------------------------------------------------------------------------
+from .baserepository import BaseRepository
+from .baserepository import BaseRepository as Repository
+from .sharedclass import SharedClass
+from .singleton import Singleton
