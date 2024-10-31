7f33d24524bcfc32190af77acfdd86bf98e5c99a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-31 17:05:39 +0000

    Add package foundry-platform-sdk/0.33.0/foundry_platform_sdk-0.33.0-py3-none-any.whl

---
+++ packages/foundry-platform-sdk/foundry_platform_sdk-0.33.0-py3-none-any.whl/foundry/v2/connectivity/models/__init__.py
@@ -0,0 +1,92 @@
+#  Copyright 2024 Palantir Technologies, Inc.
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+
+from foundry.v2.connectivity.models._agent_proxy_runtime import AgentProxyRuntime
+from foundry.v2.connectivity.models._agent_proxy_runtime_dict import AgentProxyRuntimeDict  # NOQA
+from foundry.v2.connectivity.models._agent_rid import AgentRid
+from foundry.v2.connectivity.models._agent_worker_runtime import AgentWorkerRuntime
+from foundry.v2.connectivity.models._agent_worker_runtime_dict import AgentWorkerRuntimeDict  # NOQA
+from foundry.v2.connectivity.models._connection import Connection
+from foundry.v2.connectivity.models._connection_dict import ConnectionDict
+from foundry.v2.connectivity.models._connection_display_name import ConnectionDisplayName  # NOQA
+from foundry.v2.connectivity.models._connection_rid import ConnectionRid
+from foundry.v2.connectivity.models._direct_connection_runtime import (
+    DirectConnectionRuntime,
+)  # NOQA
+from foundry.v2.connectivity.models._direct_connection_runtime_dict import (
+    DirectConnectionRuntimeDict,
+)  # NOQA
+from foundry.v2.connectivity.models._file_import import FileImport
+from foundry.v2.connectivity.models._file_import_custom_filter import FileImportCustomFilter  # NOQA
+from foundry.v2.connectivity.models._file_import_custom_filter_dict import (
+    FileImportCustomFilterDict,
+)  # NOQA
+from foundry.v2.connectivity.models._file_import_dict import FileImportDict
+from foundry.v2.connectivity.models._file_import_display_name import FileImportDisplayName  # NOQA
+from foundry.v2.connectivity.models._file_import_filter import FileImportFilter
+from foundry.v2.connectivity.models._file_import_filter_dict import FileImportFilterDict
+from foundry.v2.connectivity.models._file_import_mode import FileImportMode
+from foundry.v2.connectivity.models._file_import_rid import FileImportRid
+from foundry.v2.connectivity.models._file_last_modified_after_filter import (
+    FileLastModifiedAfterFilter,
+)  # NOQA
+from foundry.v2.connectivity.models._file_last_modified_after_filter_dict import (
+    FileLastModifiedAfterFilterDict,
+)  # NOQA
+from foundry.v2.connectivity.models._file_path_matches_filter import FilePathMatchesFilter  # NOQA
+from foundry.v2.connectivity.models._file_path_matches_filter_dict import (
+    FilePathMatchesFilterDict,
+)  # NOQA
+from foundry.v2.connectivity.models._file_size_filter import FileSizeFilter
+from foundry.v2.connectivity.models._file_size_filter_dict import FileSizeFilterDict
+from foundry.v2.connectivity.models._network_egress_policy_rid import NetworkEgressPolicyRid  # NOQA
+from foundry.v2.connectivity.models._plaintext_value import PlaintextValue
+from foundry.v2.connectivity.models._runtime_platform import RuntimePlatform
+from foundry.v2.connectivity.models._runtime_platform_dict import RuntimePlatformDict
+from foundry.v2.connectivity.models._secret_name import SecretName
+
+__all__ = [
+    "AgentProxyRuntime",
+    "AgentProxyRuntimeDict",
+    "AgentRid",
+    "AgentWorkerRuntime",
+    "AgentWorkerRuntimeDict",
+    "Connection",
+    "ConnectionDict",
+    "ConnectionDisplayName",
+    "ConnectionRid",
+    "DirectConnectionRuntime",
+    "DirectConnectionRuntimeDict",
+    "FileImport",
+    "FileImportCustomFilter",
+    "FileImportCustomFilterDict",
+    "FileImportDict",
+    "FileImportDisplayName",
+    "FileImportFilter",
+    "FileImportFilterDict",
+    "FileImportMode",
+    "FileImportRid",
+    "FileLastModifiedAfterFilter",
+    "FileLastModifiedAfterFilterDict",
+    "FilePathMatchesFilter",
+    "FilePathMatchesFilterDict",
+    "FileSizeFilter",
+    "FileSizeFilterDict",
+    "NetworkEgressPolicyRid",
+    "PlaintextValue",
+    "RuntimePlatform",
+    "RuntimePlatformDict",
+    "SecretName",
+]
