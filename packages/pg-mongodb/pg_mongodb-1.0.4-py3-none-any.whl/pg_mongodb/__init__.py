b4bb902bc0f382f42c99c54fcab22ac89b0d4534
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-31 17:05:52 +0000

    Add package pg-mongodb/1.0.4/pg_mongodb-1.0.4-py3-none-any.whl

---
+++ packages/pg-mongodb/pg_mongodb-1.0.4-py3-none-any.whl/pg_mongodb/__init__.py
@@ -0,0 +1,3 @@
+from pg_mongodb.mongo import MongoManager
+from pg_mongodb.define import KEY_MONGODB_DEFAULT_KEY, KEY_MONGODB_DEFAULT_DB
+VERSION = "1.0.4"
---
+++ packages/pg-mongodb/pg_mongodb-1.0.4-py3-none-any.whl/pg_mongodb/define.py
@@ -0,0 +1,15 @@
+KEY_MONGODB = "mongodb"
+KEY_MONGODB_URI = "uri"
+KEY_MONGODB_DEFAULT_KEY = "default"
+KEY_MONGODB_DEFAULT_DB = "default"
+"""
+mongodb configuration format
+====
+{
+  "mongodb": {
+    "default": {
+      "uri": "mongodb://localhost:27017"
+    }
+  }
+}
+"""
