87c9d593c2d576447c2e97d069d6918762023c27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-31 14:04:41 +0000

    Add package python-utils-traveler/0.0.10/python_utils_traveler-0.0.10.tar.gz

---
+++ packages/python-utils-traveler/python_utils_traveler-0.0.10.tar.gz/python_utils_traveler-0.0.10/python_utils/decorators/utils.py
@@ -0,0 +1,21 @@
+import time
+from functools import wraps
+
+from python_utils.logging.logging import init_logger
+
+logger = init_logger()
+
+def timeit(fn):
+    '''
+    Description: decorator function that times the functions process time
+
+    Args:
+        - fn: the function being passed
+    '''
+    @wraps(fn)
+    def get_time(*args, **kwargs): 
+        start = time.time() 
+        output = fn(*args, **kwargs)
+        logger.info(f"Time taken in {fn.__name__}: {time.time() - start:.7f}")
+        return output  # make sure that the decorator returns the output of fn
+    return get_time 
