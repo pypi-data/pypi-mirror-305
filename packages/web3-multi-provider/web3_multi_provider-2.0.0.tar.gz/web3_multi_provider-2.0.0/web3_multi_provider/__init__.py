6633970fcc5c5b8592bc11dbb0f42b94746ced93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-31 16:05:03 +0000

    Add package web3-multi-provider/2.0.0/web3_multi_provider-2.0.0.tar.gz

---
+++ packages/web3-multi-provider/web3_multi_provider-2.0.0.tar.gz/web3_multi_provider-2.0.0/web3_multi_provider/__init__.py
@@ -0,0 +1,13 @@
+from .multi_http_provider import (
+    FallbackProvider,
+    MultiProvider,
+    NoActiveProviderError,
+    ProtocolNotSupported,
+)
+
+__all__ = (
+    "FallbackProvider",
+    "MultiProvider",
+    "NoActiveProviderError",
+    "ProtocolNotSupported",
+)
