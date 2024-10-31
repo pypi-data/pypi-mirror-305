91fa672328515b61a740d0a67c54451289654664
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-31 06:05:55 +0000

    Add package odxtools/8.3.4/odxtools-8.3.4-py3-none-any.whl

---
+++ packages/odxtools/odxtools-8.3.4-py3-none-any.whl/odxtools/cli/_print_utils.py
@@ -0,0 +1,257 @@
+# SPDX-License-Identifier: MIT
+import re
+import textwrap
+from typing import Any, Callable, Dict, List, Optional, Union
+
+import markdownify
+from tabulate import tabulate  # TODO: switch to rich tables
+
+from ..description import Description
+from ..diaglayers.diaglayer import DiagLayer
+from ..diagservice import DiagService
+from ..parameters.codedconstparameter import CodedConstParameter
+from ..parameters.nrcconstparameter import NrcConstParameter
+from ..parameters.parameter import Parameter
+from ..parameters.physicalconstantparameter import PhysicalConstantParameter
+from ..parameters.systemparameter import SystemParameter
+from ..parameters.valueparameter import ValueParameter
+from ..singleecujob import SingleEcuJob
+
+
+def format_desc(description: Description, indent: int = 0) -> str:
+    # Collapse whitespaces
+    desc = re.sub(r"\s+", " ", str(description))
+    # Covert XHTML to Markdown
+    desc = markdownify.markdownify(desc)
+    # Collapse blank lines
+    desc = re.sub(r"(\n\s*)+\n+", "\n", desc).strip()
+    # add indentation
+    desc = textwrap.indent(desc, " " * indent)
+
+    return desc
+
+
+def print_diagnostic_service(service: DiagService,
+                             print_params: bool = False,
+                             print_pre_condition_states: bool = False,
+                             print_state_transitions: bool = False,
+                             print_audiences: bool = False,
+                             allow_unknown_bit_lengths: bool = False,
+                             print_fn: Callable[..., Any] = print) -> None:
+
+    print_fn(f" Service '{service.short_name}':")
+
+    if service.description:
+        desc = format_desc(service.description, indent=3)
+        print_fn(f"  Description: " + desc)
+
+    if print_pre_condition_states and len(service.pre_condition_states) > 0:
+        pre_condition_states_short_names = [
+            pre_condition_state.short_name for pre_condition_state in service.pre_condition_states
+        ]
+        print_fn(f"  Pre-Condition States: {', '.join(pre_condition_states_short_names)}")
+
+    if print_state_transitions and len(service.state_transitions) > 0:
+        state_transitions = [
+            f"{state_transition.source_snref} -> {state_transition.target_snref}"
+            for state_transition in service.state_transitions
+        ]
+        print_fn(f"  State Transitions: {', '.join(state_transitions)}")
+
+    if print_audiences and service.audience:
+        enabled_audiences_short_names = [
+            enabled_audience.short_name for enabled_audience in service.audience.enabled_audiences
+        ]
+        print_fn(f"  Enabled Audiences: {', '.join(enabled_audiences_short_names)}")
+
+    if print_params:
+        print_service_parameters(
+            service, allow_unknown_bit_lengths=allow_unknown_bit_lengths, print_fn=print_fn)
+
+
+def print_service_parameters(service: DiagService,
+                             allow_unknown_bit_lengths: bool = False,
+                             print_fn: Callable[..., Any] = print) -> None:
+    # prints parameter details of request, positive response and negative response of diagnostic service
+
+    # Request
+    if service.request:
+        print_fn(f"  Request '{service.request.short_name}':")
+        const_prefix = service.request.coded_const_prefix()
+        print_fn(
+            f"    Identifying Prefix: 0x{const_prefix.hex().upper()} ({bytes(const_prefix)!r})")
+        print_fn(f"    Parameters:")
+        table = extract_parameter_tabulation_data(list(service.request.parameters))
+        table_str = textwrap.indent(tabulate(table, headers='keys', tablefmt='presto'), "    ")
+        print_fn()
+        print_fn(table_str)
+        print_fn()
+    else:
+        print_fn(f"  No Request!")
+
+    # Positive Responses
+    if not service.positive_responses:
+        print_fn(f"  No positive responses")
+
+    for resp in service.positive_responses:
+        print_fn(f"  Positive Response '{resp.short_name}':")
+        print_fn(f"   Parameters:\n")
+        table = extract_parameter_tabulation_data(list(resp.parameters))
+        table_str = textwrap.indent(tabulate(table, headers='keys', tablefmt='presto'), "    ")
+        print_fn(table_str)
+        print_fn()
+
+    # Negative Response
+    if not service.negative_responses:
+        print_fn(f"  No negative responses")
+
+    for resp in service.negative_responses:
+        print_fn(f" Negative Response '{resp.short_name}':")
+        print_fn(f"   Parameters:\n")
+        table = extract_parameter_tabulation_data(list(resp.parameters))
+        table_str = textwrap.indent(tabulate(table, headers='keys', tablefmt='presto'), "    ")
+        print_fn(table_str)
+        print_fn()
+
+    print_fn("\n")
+
+
+def extract_service_tabulation_data(services: List[DiagService]) -> Dict[str, Any]:
+    # extracts data of diagnostic services into Dictionary which can be printed by tabulate module
+    # TODO: consider indentation
+
+    name = []
+    semantic = []
+    request: List[Optional[str]] = []
+
+    for service in services:
+        name.append(service.short_name)
+        semantic.append(service.semantic)
+
+        if service.request:
+            prefix = service.request.coded_const_prefix()
+            request.append(f"0x{str(prefix.hex().upper())[:32]}...") if len(
+                prefix) > 32 else request.append(f"0x{str(prefix.hex().upper())}")
+        else:
+            request.append(None)
+
+    return {'Name': name, 'Semantic': semantic, 'Hex-Request': request}
+
+
+def extract_parameter_tabulation_data(parameters: List[Parameter]) -> Dict[str, Any]:
+    # extracts data of parameters of diagnostic services into Dictionary which can be printed by tabulate module
+    # TODO: consider indentation
+
+    name = []
+    byte = []
+    bit_length: List[Optional[int]] = []
+    semantic = []
+    param_type = []
+    value: List[Optional[str]] = []
+    value_type: List[Optional[str]] = []
+    data_type: List[Optional[str]] = []
+    dop: List[Optional[str]] = []
+
+    for param in parameters:
+        name.append(param.short_name)
+        byte.append(param.byte_position)
+        semantic.append(param.semantic)
+        param_type.append(param.parameter_type)
+        length = 0
+        if param.get_static_bit_length() is not None:
+            bit_length.append(param.get_static_bit_length())
+            length = (param.get_static_bit_length() or 0) // 4
+        else:
+            bit_length.append(None)
+        if isinstance(param, CodedConstParameter):
+            if isinstance(param.coded_value, int):
+                value.append(f"0x{param.coded_value:0{length}X}")
+            elif isinstance(param.coded_value, bytes) or isinstance(param.coded_value, bytearray):
+                value.append(f"0x{param.coded_value.hex().upper()}")
+            else:
+                value.append(f"{param.coded_value!r}")
+            data_type.append(param.diag_coded_type.base_data_type.name)
+            value_type.append('coded value')
+            dop.append(None)
+        elif isinstance(param, NrcConstParameter):
+            data_type.append(param.diag_coded_type.base_data_type.name)
+            value.append(str(param.coded_values))
+            value_type.append('coded values')
+            dop.append(None)
+        elif isinstance(param, (PhysicalConstantParameter, SystemParameter, ValueParameter)):
+            # this is a hack to make this routine work for parameters
+            # which reference DOPs of a type that a is not yet
+            # internalized. (all parameter objects of the tested types
+            # are supposed to have a DOP.)
+            param_dop = getattr(param, "_dop", None)
+
+            if param_dop is not None:
+                dop.append(param_dop.short_name)
+
+            if param_dop is not None and (phys_type := getattr(param, "physical_type",
+                                                               None)) is not None:
+                data_type.append(phys_type.base_data_type.name)
+            else:
+                data_type.append(None)
+            if isinstance(param, PhysicalConstantParameter):
+                if isinstance(param.physical_constant_value, bytes) or isinstance(
+                        param.physical_constant_value, bytearray):
+                    value.append(f"0x{param.physical_constant_value.hex().upper()}")
+                else:
+                    value.append(f"{param.physical_constant_value!r}")
+                value_type.append('constant value')
+            elif isinstance(param, ValueParameter) and param.physical_default_value is not None:
+                if isinstance(param.physical_default_value, bytes) or isinstance(
+                        param.physical_default_value, bytearray):
+                    value.append(f"0x{param.physical_default_value.hex().upper()}")
+                else:
+                    value.append(f"{param.physical_default_value!r}")
+                value_type.append('default value')
+            else:
+                value.append(None)
+                value_type.append(None)
+        else:
+            value.append(None)
+            data_type.append(None)
+            value_type.append(None)
+            dop.append(None)
+
+    return {
+        'Name': name,
+        'Byte Position': byte,
+        'Bit Length': bit_length,
+        'Semantic': semantic,
+        'Parameter Type': param_type,
+        'Data Type': data_type,
+        'Value': value,
+        'Value Type': value_type,
+        'Linked DOP': dop
+    }
+
+
+def print_dl_metrics(variants: List[DiagLayer], print_fn: Callable[..., Any] = print) -> None:
+
+    name = []
+    type = []
+    num_services = []
+    num_dops = []
+    num_comparam_refs = []
+    for variant in variants:
+        assert isinstance(variant, DiagLayer)
+        all_services: List[Union[DiagService, SingleEcuJob]] = sorted(
+            variant.services, key=lambda x: x.short_name)
+        name.append(variant.short_name)
+        type.append(variant.variant_type.value)
+        num_services.append(len(all_services))
+        ddds = variant.diag_data_dictionary_spec
+        num_dops.append(len(ddds.data_object_props))
+        num_comparam_refs.append(len(getattr(variant, "comparams_refs", [])))
+
+    table = {
+        'Name': name,
+        'Variant Type': type,
+        'Number of Services': num_services,
+        'Number of DOPs': num_dops,
+        'Number of communication parameters': num_comparam_refs
+    }
+    print_fn(tabulate(table, headers='keys', tablefmt='presto'))
---
+++ packages/odxtools/odxtools-8.3.4-py3-none-any.whl/odxtools/cli/browse.py
@@ -0,0 +1,379 @@
+# SPDX-License-Identifier: MIT
+import argparse
+import logging
+import sys
+from typing import List, Optional, Union, cast
+
+import InquirerPy.prompt as IP_prompt
+from tabulate import tabulate  # TODO: switch to rich tables
+
+from ..database import Database
+from ..dataobjectproperty import DataObjectProperty
+from ..diaglayer import DiagLayer
+from ..diagservice import DiagService
+from ..exceptions import odxraise, odxrequire
+from ..hierarchyelement import HierarchyElement
+from ..odxtypes import AtomicOdxType, DataType, ParameterValueDict
+from ..parameters.matchingrequestparameter import MatchingRequestParameter
+from ..parameters.parameter import Parameter
+from ..parameters.parameterwithdop import ParameterWithDOP
+from ..parameters.valueparameter import ValueParameter
+from ..request import Request
+from ..response import Response
+from . import _parser_utils
+from ._parser_utils import SubparsersList
+from ._print_utils import extract_parameter_tabulation_data
+
+# name of the tool
+_odxtools_tool_name_ = "browse"
+
+
+def _convert_string_to_odx_type(string_value: str, odx_type: DataType) -> AtomicOdxType:
+    """Similar to odx_type.from_string(string_value) but more relaxed to parse user input"""
+    if odx_type == DataType.A_UINT32:
+        return int(string_value, 0)
+    elif odx_type == DataType.A_BYTEFIELD:
+        return _convert_string_to_bytes(string_value)
+    else:
+        return odx_type.from_string(string_value)
+
+
+def _convert_string_to_bytes(string_value: str) -> bytes:
+    if all(len(x) <= 2 for x in string_value.split(" ")):
+        return bytes(int(x, 16) for x in string_value.split(" ") if len(x) > 0)
+    else:
+        return int(string_value, 16).to_bytes((int(string_value, 16).bit_length() + 7) // 8, "big")
+
+
+def _validate_string_value(input: str, parameter: Parameter) -> bool:
+    if not parameter.is_required and input == "":
+        return True
+    elif isinstance(parameter, ParameterWithDOP):
+        try:
+            phys_type = odxrequire(parameter.physical_type)
+            val = _convert_string_to_odx_type(input, phys_type.base_data_type)
+        except:  # noqa: E722
+            return False
+        dop = parameter.dop
+        if isinstance(dop, DataObjectProperty):
+            return dop.is_valid_physical_value(val)
+        else:
+            raise NotImplementedError("Validation of complex DOPs")
+    else:
+        logging.info("This value is not validated precisely: Parameter {parameter}")
+        return input != ""
+
+
+def prompt_single_parameter_value(parameter: Parameter) -> Optional[AtomicOdxType]:
+    if not isinstance(parameter, ValueParameter):
+        odxraise("Only the value of ValueParameters can be queried")
+    if parameter.physical_type is None:
+        odxraise("Only ValueParameters which define a physical data type can be queried")
+
+    # TODO: add valid choices for the parameter
+    #        "choices": parameter.get_valid_physical_values(),
+    param_prompt = [{
+        "type":
+            "input",
+        "name":
+            parameter.short_name,
+        "message":
+            f"Value for parameter '{parameter.short_name}' (Type: {parameter.physical_type.base_data_type})"
+            + (f"[optional]" if not parameter.is_required else ""),
+        # TODO: improve validation
+        "validate":
+            lambda x: _validate_string_value(x, parameter),
+        # TODO: do type conversion?
+        "filter":
+            lambda x: x
+        # x if x == "" or p.physical_type.base_data_type is None
+        # else _convert_string_to_odx_type(x, p.physical_type.base_data_type, param=p) # This does not work because the next parameter to be promted is used (for some reason?)
+    }]
+
+    if (dop := getattr(parameter, "dop", None)) and \
+       (compu_method := getattr(dop, "compu_method", None)):
+        scales = compu_method.internal_to_phys
+        choices = [scale.compu_const for scale in scales if scale is not None]
+        if (cdv := compu_method.compu_default_value) is not None:
+            choices.append(cdv.compu_const)
+        param_prompt[0]["choices"] = choices
+
+    answer = IP_prompt(param_prompt)
+    if answer.get(parameter.short_name) == "" and not parameter.is_required:
+        return None
+    elif parameter.physical_type.base_data_type is not None:
+        return _convert_string_to_odx_type(
+            cast(str, answer.get(parameter.short_name)), parameter.physical_type.base_data_type)
+    else:
+        logging.warning(
+            f"Parameter {parameter.short_name} does not have a physical data type. Param details: {parameter}"
+        )
+        return cast(str, answer.get(parameter.short_name))
+
+
+def encode_message_interactively(sub_service: Union[Request, Response],
+                                 ask_user_confirmation: bool = False) -> None:
+    if sys.__stdin__ is None or sys.__stdout__ is None or not sys.__stdin__.isatty(
+    ) or not sys.__stdout__.isatty():
+        raise SystemError("This command can only be used in an interactive shell!")
+    param_dict = sub_service.parameter_dict()
+
+    exists_definable_param = False
+    for param_or_dict in param_dict.values():
+        if isinstance(param_or_dict, dict):
+            for param in param_or_dict.values():
+                if isinstance(param, Parameter) and param.is_settable:
+                    exists_definable_param = True
+        elif param_or_dict.is_settable:
+            exists_definable_param = True
+
+    param_values: ParameterValueDict = {}
+    if exists_definable_param > 0:
+        # Ask whether user wants to encode a message
+        if ask_user_confirmation:
+            encode_message_prompt = [{
+                "type": "list",
+                "name": "yes_no_prompt",
+                "message": f"Do you want to encode a message? [y/n]",
+                "choices": ["yes", "no"],
+            }]
+            answer = IP_prompt(encode_message_prompt)
+            if answer.get("yes_no_prompt") == "no":
+                return
+
+        answered_request = b''
+        if isinstance(sub_service, Response):
+            answered_request_prompt = [{
+                "type":
+                    "input",
+                "name":
+                    "request",
+                "message":
+                    f"What is the request you want to answer? (Enter the coded request as integer in hexadecimal format (e.g. 12 3B 5)",
+                "filter":
+                    lambda input: _convert_string_to_bytes(input),
+            }]
+            answer = IP_prompt(answered_request_prompt)
+            answered_request = cast(bytes, answer.get("request"))
+            print(f"Input interpretation as list: {list(answered_request)}")
+
+        # Request values for parameters
+        for key, param_or_structure in param_dict.items():
+            if isinstance(param_or_structure, dict):
+                # param_or_structure refers to a structure (represented as dict of params)
+                print(
+                    f"The next {len(param_or_structure)} parameters belong to the structure '{key}'"
+                )
+                structure_param_values: ParameterValueDict = {}
+                for param_sn, param in param_or_structure.items():
+                    if not isinstance(param, dict) and param.is_settable:
+                        val = prompt_single_parameter_value(param)
+                        if val is not None:
+                            structure_param_values[param_sn] = val
+                param_values[key] = structure_param_values
+            elif param_or_structure.is_settable:
+                # param_or_structure is a parameter
+                val = prompt_single_parameter_value(param_or_structure)
+                if val is not None:
+                    param_values[key] = val
+        if isinstance(sub_service, Response):
+            payload = sub_service.encode(coded_request=answered_request, **param_values)
+        else:
+            payload = sub_service.encode(coded_request=b'', **param_values)
+    else:
+        # There are no optional parameters that need to be defined by the user -> Just print message
+        payload = sub_service.encode()
+    print(f"Message payload: 0x{bytes(payload).hex()}")
+
+
+def encode_message_from_string_values(
+    sub_service: Union[Request, Response],
+    parameter_values: Optional[ParameterValueDict] = None,
+) -> None:
+    if parameter_values is None:
+        parameter_values = {}
+    parameter_values = parameter_values.copy()
+    param_dict = sub_service.parameter_dict()
+
+    # Check if all needed parameters are given
+    missing_parameter_names = []
+    for param_sn, param in param_dict.items():
+        if isinstance(param, dict):
+            # param_value refers to a structure (represented as dict of params)
+            for simple_param_sn, simple_param in param.items():
+                structured_value = parameter_values.get(param_sn)
+                if not isinstance(simple_param, Parameter):
+                    continue
+                if simple_param.is_required and (not isinstance(structured_value, dict) or
+                                                 structured_value.get(simple_param_sn) is None):
+                    missing_parameter_names.append(f"{param_sn} :: {simple_param_sn}")
+        else:
+            if param.is_required and parameter_values.get(param_sn) is None:
+                missing_parameter_names.append(param_sn)
+
+    if len(missing_parameter_names) > 0:
+        print("The following parameters are required but missing!")
+        print(" - " + "\n - ".join(missing_parameter_names))
+        return
+
+    # Request values for parameters
+    for parameter_sn, parameter_value in parameter_values.items():
+        parameter = param_dict.get(parameter_sn)
+        if parameter is None:
+            print(f"I don't know the parameter {parameter_sn}")
+            continue
+
+        if isinstance(parameter_value, dict):
+            # parameter_value refers to a structure (represented as dict of params)
+            typed_dict = parameter_value.copy()
+            for simple_param_sn, simple_param_val in parameter_value.items():
+                simple_parameter = param_dict.get(simple_param_sn)
+                if simple_parameter is None:
+                    print(f"Unknown sub-parameter {simple_param_sn}")
+                    continue
+                if not isinstance(simple_param_val, str):
+                    print(f"The value specified for parameter {simple_param_sn} is not a string")
+                    continue
+
+                typed_dict[simple_param_sn] = _convert_string_to_odx_type(
+                    simple_param_val,
+                    simple_parameter.physical_type.base_data_type  # type: ignore[union-attr]
+                )
+                parameter_values[parameter_sn] = typed_dict
+        else:
+            assert isinstance(parameter, Parameter)
+
+            if not isinstance(parameter_value, str):
+                print(f"Value for parameter {parameter_sn} is not a string")
+                continue
+
+            if not isinstance(parameter, MatchingRequestParameter):
+                parameter_values[parameter_sn] = _convert_string_to_odx_type(
+                    parameter_value,
+                    parameter.physical_type.base_data_type,  # type: ignore[attr-defined]
+                )
+            else:
+                parameter_values[parameter_sn] = _convert_string_to_odx_type(
+                    parameter_value, DataType.A_BYTEFIELD)
+
+    payload = sub_service.encode(coded_request=b'\xff' * 100, **parameter_values)
+
+    print(f"Message payload: 0x{bytes(payload).hex()}")
+
+
+def browse(odxdb: Database) -> None:
+    if sys.__stdin__ is None or sys.__stdout__ is None or not sys.__stdin__.isatty(
+    ) or not sys.__stdout__.isatty():
+        raise SystemError("This command can only be used in an interactive shell!")
+    dl_names = [dl.short_name for dl in odxdb.diag_layers]
+    while True:
+        # Select an ECU
+        selection = [{
+            "type": "list",
+            "name": "variant",
+            "message": "Select a Variant.",
+            "choices": list(dl_names) + ["[exit]"],
+        }]
+        answer = IP_prompt(selection)
+        if answer.get("variant") == "[exit]":
+            return
+
+        variant_name = answer.get("variant")
+        assert isinstance(variant_name, str)
+        variant = odxdb.diag_layers[variant_name]
+        print(f"{type(answer.get('variant'))=}")
+        assert isinstance(variant, DiagLayer)
+
+        if isinstance(variant, HierarchyElement):
+            if (rx_id := variant.get_receive_id()) is not None:
+                recv_id = hex(rx_id)
+            else:
+                recv_id = "None"
+
+            if (tx_id := variant.get_send_id()) is not None:
+                send_id = hex(tx_id)
+            else:
+                send_id = "None"
+
+            print(
+                f"{variant.variant_type.value} '{variant.short_name}' (Receive ID: {recv_id}, Send ID: {send_id})"
+            )
+
+        while True:
+            services: List[DiagService] = [
+                s for s in variant.services if isinstance(s, DiagService)
+            ]
+            # Select a service of the ECU
+            selection = [{
+                "type":
+                    "list",
+                "name":
+                    "service",
+                "message":
+                    f"The variant {variant.short_name} offers the following services. Select one!",
+                "choices": [s.short_name for s in services] + ["[back]"],
+            }]
+            answer = IP_prompt(selection)
+            if answer.get("service") == "[back]":
+                break
+
+            service_sn = answer.get("service")
+            assert isinstance(service_sn, str)
+
+            service = variant.services[service_sn]
+            assert isinstance(service, DiagService)
+            assert service.request is not None
+            assert service.positive_responses is not None
+            assert service.negative_responses is not None
+
+            # Select a request/ response of the service
+            selection = [{
+                "type":
+                    "list",
+                "name":
+                    "message_type",
+                "message":
+                    "This service offers the following messages.",
+                "choices": [{
+                    "name": f"Request: {service.request.short_name}",
+                    "value": service.request,
+                    "short": f"Request: {service.request.short_name}",
+                }] + [{
+                    "name": f"Positive response: {pr.short_name}",
+                    "value": pr,
+                    "short": f"Positive response: {pr.short_name}",
+                } for pr in service.positive_responses] + [{
+                    "name": f"Negative response: {nr.short_name}",
+                    "value": nr,
+                    "short": f"Negative response: {nr.short_name}",
+                } for nr in service.negative_responses] + ["[back]"],  # type: ignore
+            }]
+            answer = IP_prompt(selection)
+            if answer.get("message_type") == "[back]":
+                continue
+
+            codec = answer.get("message_type")
+            if codec is not None:
+                assert isinstance(codec, (Request, Response))
+                table = extract_parameter_tabulation_data(codec.parameters)
+                table_str = tabulate(table, headers='keys', tablefmt='presto')
+                print(table_str)
+
+                encode_message_interactively(codec, ask_user_confirmation=True)
+
+
+def add_subparser(subparsers: SubparsersList) -> None:
+    # Browse interactively to avoid spamming the console.
+    parser = subparsers.add_parser(
+        "browse",
+        description="Interactively browse the content of automotive diagnostic files (*.pdx).",
+        help="Interactively browse the content of automotive diagnostic files.",
+        formatter_class=argparse.RawTextHelpFormatter,
+    )
+
+    _parser_utils.add_pdx_argument(parser)
+
+
+def run(args: argparse.Namespace) -> None:
+    odxdb = _parser_utils.load_file(args)
+    browse(odxdb)
