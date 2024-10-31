ff35aa027f3289acb07b98ac8415041f83f2a5cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-31 03:10:49 +0000

    Add package completor/1.0.0/completor-1.0.0-py3-none-any.whl

---
+++ packages/completor/completor-1.0.0-py3-none-any.whl/completor/exceptions/__init__.py
@@ -0,0 +1,4 @@
+from completor.exceptions.clean_exceptions import CompletorError
+from completor.exceptions.exceptions import CaseReaderFormatError
+
+__all__ = ["CompletorError", "CaseReaderFormatError"]
