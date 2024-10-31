0604e1a7c4788b756409e5f980a001cec4bae32a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-31 15:04:55 +0000

    Add package async_yookassa/0.3.5/async_yookassa-0.3.5.tar.gz

---
+++ packages/async_yookassa/async_yookassa-0.3.5.tar.gz/async_yookassa-0.3.5/async_yookassa/__init__.py
@@ -0,0 +1,11 @@
+from async_yookassa.configuration import Configuration
+from async_yookassa.invoice import Invoice
+from async_yookassa.payment import Payment
+from async_yookassa.payout import Payout
+from async_yookassa.refund import Refund
+from async_yookassa.receipt import Receipt
+
+__author__ = "Ivan Ashikhmin and YooMoney"
+__email__ = "sushkoos@gmail.com and cms@yoomoney.ru"
+__version__ = "0.3.5"
+__all__ = ["Configuration", "Payment", "Invoice", "Refund", "Receipt", "Payout"]
