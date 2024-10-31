37cb9f694e002efb94580e75311ca680116741a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-31 06:06:14 +0000

    Add package beam-client/0.2.98/beam_client-0.2.98-py3-none-any.whl

---
+++ packages/beam-client/beam_client-0.2.98-py3-none-any.whl/beam/cli/configure.py
@@ -0,0 +1,65 @@
+import click
+from beta9 import terminal
+from beta9.cli.extraclick import ClickCommonGroup
+from beta9.config import ConfigContext, get_settings, load_config, save_config
+
+
+def validate_token(ctx: click.Context, param: click.Parameter, value: str):
+    token = value.strip()
+    if not token or len(token) < 64:
+        raise click.BadParameter("A valid token is required", ctx=ctx, param=param)
+    return token
+
+
+@click.group(cls=ClickCommonGroup)
+def common(**_):
+    pass
+
+
+@common.command(
+    name="configure",
+    help="""
+    Configure a beam context
+    """,
+    epilog="""
+      Examples:
+
+        {cli_name} configure default --token MY_TOKEN
+
+        {cli_name} configure production --token MY_TOKEN
+        \b
+    """,
+)
+@click.option(
+    "--token",
+    "-n",
+    type=click.STRING,
+    help="The token.",
+    required=True,
+    callback=validate_token,
+)
+@click.argument(
+    "name",
+    nargs=1,
+    required=True,
+)
+def configure(token: str, name: str):
+    settings = get_settings()
+    config_path = settings.config_path
+    contexts = load_config(config_path)
+
+    if name in contexts and contexts[name].is_valid():
+        text = f"Context '{name}' already exists. Overwrite?"
+        if terminal.prompt(text=text, default="n").lower() in ["n", "no"]:
+            return
+
+    context = ConfigContext(
+        token=token, gateway_host=settings.gateway_host, gateway_port=settings.gateway_port
+    )
+
+    # Save context to config
+    contexts[name] = context
+
+    save_config(contexts=contexts, path=config_path)
+
+    terminal.success("Configured beam context 🎉!")
