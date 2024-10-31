61fd807f97f3e24cd5b1f748a472639208d7d3ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-31 15:05:30 +0000

    Add package aidataup/0.0.4/aidataup-0.0.4.tar.gz

---
+++ packages/aidataup/aidataup-0.0.4.tar.gz/aidataup-0.0.4/tests/test_cleansing.py
@@ -0,0 +1,23 @@
+#--- Say Hello
+# def test_say_hello():
+#     from jack_sayhello.sayhello import say_hello
+#     assert say_hello("Jack") == "Hello, Jack!"
+# #--- Say Hi
+# def test_hi():
+#     from jack_sayhello.sayhello import hi
+#     assert hi("Jack") == "Hi, Jack!"
+# #--- Say bye ---
+# def test_bye():
+#     from jack_sayhello.sayhello import bye
+#     assert bye("Jack") == "Bye, Jack!"
+# #--- Say name ---
+# def test_sayname():
+#     from jack_sayhello.sayhello import sayname
+#     assert sayname("Jack") == "Ohayo Jack!!!!!"
+# #--- Say message ---
+# def test_sayit():
+#     from jack_sayhello.sayhello import sayit
+#     assert sayit("Jack") == "Jack"
+
+
+
