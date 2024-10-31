6c6c3b6a58965394ef29022595e9c8d771b75ad8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-31 14:05:14 +0000

    Add package qrev-email-verification/0.2.1.3/qrev_email_verification-0.2.1.3-py3-none-any.whl

---
+++ packages/qrev-email-verification/qrev_email_verification-0.2.1.3-py3-none-any.whl/qrev_email_verification/__init__.py
@@ -0,0 +1,68 @@
+from qrev_email_verification.models.models import (
+    APIResponse,
+    InsufficientCreditsError,
+    InvalidEmailError,
+)
+
+...  # Above code should be called first
+
+from qrev_email_verification.email_verification import (
+    EmailVerification,
+    EmailVerificationSettings,
+)
+from qrev_email_verification.services.millionverifier.api import (
+    MillionVerifierService as MVService,
+)
+from qrev_email_verification.services.millionverifier.api import (
+    MillionVerifierSettings as MVSettings,
+)
+from qrev_email_verification.services.millionverifier.models import (
+    EmailResponse as MVEmailResponse,
+)
+from qrev_email_verification.services.millionverifier.models import (
+    InvalidEmailError as MVInvalidEmailError,
+)
+from qrev_email_verification.services.millionverifier.mongo_api import (
+    MillionVerifierMongoService as MVMongoService,
+)
+from qrev_email_verification.services.millionverifier.mongo_api import (
+    MongoSettings as MVMongoSettings,
+)
+from qrev_email_verification.services.zerobounce.api import (
+    ZeroBounceService as ZBService,
+)
+from qrev_email_verification.services.zerobounce.api import (
+    ZeroBounceSettings as ZBSettings,
+)
+from qrev_email_verification.services.zerobounce.models import (
+    EmailResponse as ZBEmailResponse,
+)
+from qrev_email_verification.services.zerobounce.models import (
+    InvalidEmailError as ZBInvalidEmailError,
+)
+from qrev_email_verification.services.zerobounce.mongo_api import (
+    MongoSettings as ZBMongoSettings,
+)
+from qrev_email_verification.services.zerobounce.mongo_api import (
+    ZeroBounceMongoService as ZBMongoService,
+)
+
+__all__ = [
+    "APIResponse",
+    "EmailVerification",
+    "EmailVerificationSettings",
+    "InvalidEmailError",
+    "InsufficientCreditsError",
+    "MVEmailResponse",
+    "MVInvalidEmailError",
+    "MVMongoService",
+    "MVMongoSettings",
+    "MVService",
+    "MVSettings",
+    "ZBEmailResponse",
+    "ZBInvalidEmailError",
+    "ZBMongoService",
+    "ZBMongoSettings",
+    "ZBService",
+    "ZBSettings",
+]
