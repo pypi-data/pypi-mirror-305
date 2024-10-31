9bc1c07e731339e9c393e51bd5099c91e7fe1d48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-31 06:06:10 +0000

    Add package moocore/0.1.4/moocore-0.1.4-py3-none-macosx_10_9_universal2.whl

---
+++ packages/moocore/moocore-0.1.4-py3-none-macosx_10_9_universal2.whl/moocore/__init__.py
@@ -0,0 +1,65 @@
+# ruff: noqa: D104
+from ._moocore import (
+    ReadDatasetsError,
+    apply_within_sets,
+    avg_hausdorff_dist,
+    eaf,
+    eafdiff,
+    epsilon_additive,
+    epsilon_mult,
+    filter_dominated,
+    filter_dominated_within_sets,
+    get_dataset,
+    get_dataset_path,
+    hypervolume,
+    Hypervolume,
+    hv_approx,
+    igd,
+    igd_plus,
+    is_nondominated,
+    is_nondominated_within_sets,
+    normalise,
+    pareto_rank,
+    read_datasets,
+    vorobDev,
+    vorobT,
+    whv_hype,
+    whv_rect,
+    total_whv_rect,
+)
+
+from importlib.metadata import version as _metadata_version
+
+__version__ = _metadata_version("moocore")
+# Remove symbols imported for internal use
+del _metadata_version
+
+
+__all__ = [
+    "ReadDatasetsError",
+    "apply_within_sets",
+    "avg_hausdorff_dist",
+    "eaf",
+    "eafdiff",
+    "epsilon_additive",
+    "epsilon_mult",
+    "filter_dominated",
+    "filter_dominated_within_sets",
+    "get_dataset",
+    "get_dataset_path",
+    "hypervolume",
+    "Hypervolume",
+    "hv_approx",
+    "igd",
+    "igd_plus",
+    "is_nondominated",
+    "is_nondominated_within_sets",
+    "normalise",
+    "pareto_rank",
+    "read_datasets",
+    "vorobDev",
+    "vorobT",
+    "whv_hype",
+    "whv_rect",
+    "total_whv_rect",
+]
