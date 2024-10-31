13a81a7016754cd9f6e7e830eb82f29b2ebf68ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-31 06:05:29 +0000

    Add package tractorun/0.42.0/tractorun-0.42.0-py3-none-any.whl

---
+++ packages/tractorun/tractorun-0.42.0-py3-none-any.whl/tractorun/backend/tractorch/environment.py
@@ -0,0 +1,29 @@
+import torch
+import torch.distributed
+
+from tractorun.base_backend import EnvironmentBase
+from tractorun.private.closet import Closet as _Closet
+
+
+__all__ = ["Environment"]
+
+
+class Environment(EnvironmentBase):
+    def prepare(self, closet: _Closet) -> None:
+        if closet.mesh.gpu_per_process > 0:
+            assert torch.cuda.is_available()
+
+            if closet.mesh.gpu_per_process > 1:
+                raise RuntimeError("not supported")
+
+            device_index = closet.coordinator.get_process_index()
+            assert device_index < torch.cuda.device_count()
+            torch.cuda.set_device(closet.coordinator.get_process_index())
+
+        backend = "gloo" if closet.mesh.gpu_per_process == 0 else "nccl"
+        torch.distributed.init_process_group(
+            backend=backend,
+            init_method="tcp://" + closet.coordinator.get_primary_endpoint(),
+            rank=closet.coordinator.get_self_index(),
+            world_size=closet.coordinator.get_total_peer_count(),
+        )
