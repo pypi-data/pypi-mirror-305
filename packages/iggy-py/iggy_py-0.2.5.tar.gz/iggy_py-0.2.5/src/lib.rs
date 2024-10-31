75f2a0df84925b00212de869994a2c6b58b946dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-31 16:04:41 +0000

    Add package iggy-py/0.2.5/iggy_py-0.2.5.tar.gz

---
+++ packages/iggy-py/iggy_py-0.2.5.tar.gz/iggy_py-0.2.5/src/lib.rs
@@ -0,0 +1,23 @@
+mod client;
+mod receive_message;
+mod send_message;
+mod stream;
+mod topic;
+
+use client::IggyClient;
+use pyo3::prelude::*;
+use receive_message::ReceiveMessage;
+use send_message::SendMessage;
+use stream::StreamDetails;
+use topic::TopicDetails;
+
+/// A Python module implemented in Rust.
+#[pymodule]
+fn iggy_py(_py: Python, m: &Bound<'_, PyModule>) -> PyResult<()> {
+    m.add_class::<SendMessage>()?;
+    m.add_class::<ReceiveMessage>()?;
+    m.add_class::<IggyClient>()?;
+    m.add_class::<StreamDetails>()?;
+    m.add_class::<TopicDetails>()?;
+    Ok(())
+}
