7bda54d033eca9f12506dbdeae86dd4ba24971dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-31 06:05:33 +0000

    Add package quakelm/0.1.0/quakelm-0.1.0.tar.gz

---
+++ packages/quakelm/quakelm-0.1.0.tar.gz/quakelm-0.1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="quakelm",
+    version="0.1.0",
+    long_description="quakelm",
+    long_description_content_type="text/markdown",
+    packages=["quakelm"],
+    install_requires=["numpy",  "h5py", "matplotlib", "pandas"],
+)
