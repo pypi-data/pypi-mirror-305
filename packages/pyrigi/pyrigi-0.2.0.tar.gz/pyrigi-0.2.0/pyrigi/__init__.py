a52f58ac01531c760b8624bf38ac68a63ef56a99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-31 16:06:36 +0000

    Add package pyrigi/0.2.0/pyrigi-0.2.0.tar.gz

---
+++ packages/pyrigi/pyrigi-0.2.0.tar.gz/pyrigi-0.2.0/pyrigi/__init__.py
@@ -0,0 +1,9 @@
+# flake8: noqa
+
+"""
+This is a package for the rigidity and flexibility of graphs
+and bar-and-joint frameworks.
+"""
+
+from pyrigi.graph import Graph
+from pyrigi.framework import Framework
