ef58d56216a43be1046828548eed28c787644a91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-31 17:06:04 +0000

    Add package cyclarity-sdk/1.0.62a1/cyclarity_sdk-1.0.62a1-py3-none-any.whl

---
+++ packages/cyclarity-sdk/cyclarity_sdk-1.0.62a1-py3-none-any.whl/cyclarity_sdk/platform_api/logger/models.py
@@ -0,0 +1,54 @@
+from abc import ABC, abstractmethod
+from datetime import datetime
+from enum import IntEnum, Enum
+import logging
+from typing import Union, Optional
+
+from cyclarity_sdk.sdk_models import ExecutionMetadata, MessageType
+from pydantic import BaseModel
+
+
+class LogLevel(IntEnum):
+    CRITICAL = logging.CRITICAL
+    FATAL = CRITICAL
+    ERROR = logging.ERROR
+    WARNING = logging.WARNING
+    WARN = WARNING
+    INFO = logging.INFO
+    DEBUG = logging.DEBUG
+    NOTSET = logging.NOTSET
+
+
+class LogChannelType(str, Enum):
+    '''
+    Channels are logical separators to the log screens in the UI.
+    '''
+    DEFAULT = "DEFAULT"
+    TRACE = "TRACE"
+
+
+class LogChannel(BaseModel):
+    type: LogChannelType = LogChannelType.DEFAULT
+    topic: Optional[str] = ''  # grouping logs by topic name for future use
+
+
+class LogInformation(BaseModel):
+    logger_name: str
+    log_level: LogLevel
+    time_stamp: datetime
+    message: str
+
+
+class ExecutionLog(BaseModel):
+    '''All the needed attributes from logs to sent via mqtt'''
+    metadata: ExecutionMetadata
+    data: Union[LogInformation, dict]
+    type: MessageType = MessageType.LOG
+    channel: Optional[LogChannel] = LogChannel(type=LogChannelType.DEFAULT)
+
+
+class LogPublisher(ABC):
+    @abstractmethod
+    def publish_log(self, execution_log: ExecutionLog):
+        raise NotImplementedError(
+            f'publish_log was not defined for class {self.__class__.__name__}')  # noqa
