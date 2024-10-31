877459b6304804cedeaf2ac2328f56aa5fd6e72d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-31 04:05:04 +0000

    Add package uplogic/4.0b3/uplogic-4.0b3.tar.gz

---
+++ packages/uplogic/uplogic-4.0b3.tar.gz/uplogic-4.0b3/uplogic/animation/rig.py
@@ -0,0 +1,171 @@
+from bge.types import BL_ArmatureChannel, BL_ArmatureObject
+from bge.logic import ROT_MODE_XYZ
+from uplogic.utils.objects import GameObject
+from mathutils import Vector, Quaternion, Matrix
+
+
+class RigBone():
+
+    def __init__(self, bone: BL_ArmatureChannel, armature: BL_ArmatureObject) -> None:
+        self.bone = bone
+        self.armature = armature
+        self._armature = self.armature.blenderObject.data
+        # self.bone.rotation_mode = rotation_mode
+
+    @property
+    def name(self):
+        return self.bone.name
+
+    @name.setter
+    def name(self, val):
+        self._armature.blenderObject.bones[self.bone.name].name = val
+
+    @property
+    def constraints(self):
+        return self.armature.blenderObject.pose.bones[self.bone.name].constraints
+
+    @property
+    def head(self):
+        return self._armature.bones[self.bone.name].head
+
+    @head.setter
+    def head(self, value):
+        self._armature.bones[self.bone.name].head = value
+
+    @property
+    def head_local(self):
+        return self._armature.bones[self.bone.name].head_local
+
+    @head_local.setter
+    def head_local(self, value):
+        self._armature.bones[self.bone.name].head_local = value
+
+    @property
+    def tail(self):
+        return self._armature.bones[self.bone.name].tail
+
+    @tail.setter
+    def tail(self, value):
+        self._armature.bones[self.bone.name].tail = value
+
+    @property
+    def tail_local(self):
+        return self._armature.bones[self.bone.name].tail_local
+
+    @tail_local.setter
+    def tail_local(self, value):
+        self._armature.bones[self.bone.name].tail_local = value
+
+    @property
+    def inherit_rotation(self):
+        return self._armature.bones[self.bone.name].use_inherit_rotation
+
+    @inherit_rotation.setter
+    def inherit_rotation(self, value):
+        self._armature.bones[self.bone.name].use_inherit_rotation = value
+
+    @property
+    def inherit_scale(self):
+        return self._armature.bones[self.bone.name].inherit_scale
+
+    @inherit_scale.setter
+    def inherit_scale(self, value):
+        self._armature.bones[self.bone.name].inherit_scale = value
+
+    @property
+    def connected(self):
+        return self._armature.bones[self.bone.name].use_connect
+
+    @connected.setter
+    def connected(self, value):
+        self._armature.bones[self.bone.name].use_connect = value
+
+    @property
+    def deform(self):
+        return self._armature.bones[self.bone.name].use_deform
+
+    @deform.setter
+    def deform(self, value):
+        self._armature.bones[self.bone.name].use_deform = value
+
+    @property
+    def local_location(self):
+        return self._armature.bones[self.bone.name].use_local_location
+
+    @local_location.setter
+    def local_location(self, value):
+        self._armature.bones[self.bone.name].use_local_location = value
+
+    @property
+    def relative_parent(self):
+        return self._armature.bones[self.bone.name].use_relative_parent
+
+    @relative_parent.setter
+    def relative_parent(self, value):
+        self._armature.bones[self.bone.name].use_relative_parent = value
+
+    @property
+    def scale_easing(self):
+        return self._armature.bones[self.bone.name].use_scale_easing
+
+    @scale_easing.setter
+    def scale_easing(self, value):
+        self._armature.bones[self.bone.name].use_scale_easing = value
+
+    @property
+    def pose_matrix_cleaned(self):
+        offset_m4 = (Matrix.Translation(self.bone.location) @ Quaternion(self.bone.rotation_quaternion).to_matrix().to_4x4())
+        return self.bone.pose_matrix @ offset_m4.inverted()
+
+    @property
+    def localPosition(self):
+        return self.bone.location @ self.bone.channel_matrix.inverted()
+
+    @localPosition.setter
+    def localPosition(self, val):
+        self.bone.location = self.pose_matrix_cleaned.inverted() @ self.armature.worldTransform.inverted() @ (Vector(val) + self.armature.worldPosition)
+        self.armature.blenderObject.update_tag()
+
+    @property
+    def worldPosition(self):
+        return self.bone.location @ self.bone.pose_matrix.inverted()
+
+    @worldPosition.setter
+    def worldPosition(self, val):
+        self.bone.location = self.pose_matrix_cleaned.inverted() @ self.armature.worldTransform.inverted() @ Vector(val)
+        self.armature.blenderObject.update_tag()
+
+    @property
+    def worldOrientation(self):
+        return self.bone.location @ self.bone.pose_matrix.inverted()
+
+    @worldOrientation.setter
+    def worldOrientation(self, val: Matrix):
+        _mode = self.bone.rotation_mode
+        self.bone.rotation_mode = ROT_MODE_XYZ
+        self.bone.rotation_euler = Vector(val.to_euler())
+        self.armature.blenderObject.update_tag()
+        self.bone.rotation_mode = _mode
+
+
+class RigBones(dict):
+    pass
+
+
+class Rig(GameObject):
+
+    def __init__(
+        self,
+        armature
+    ) -> None:
+        self.game_object: BL_ArmatureObject = armature
+        self.bones: RigBones[str, RigBone] = RigBones()
+        for bone in self.game_object.channels:
+            self.bones[bone.name] = RigBone(bone, armature)
+
+    def set_bone_pos(self, bone: str, pos: Vector):
+        self.bones[bone].worldPosition = pos
+
+    def set_bone_rot(self, bone: str, rot: Quaternion):
+        bone = self.game_object.channels[bone]
+        bone.rotation_quaternion = bone.pose_matrix.inverted() @ Quaternion(rot)
---
+++ packages/uplogic/uplogic-4.0b3.tar.gz/uplogic-4.0b3/uplogic/nodes/conditions/collision.py
@@ -0,0 +1,121 @@
+from bge.types import KX_GameObject
+from uplogic.nodes import ULConditionNode
+from uplogic.nodes import ULLogicContainer
+from bpy.types import Material
+from mathutils import Vector
+
+
+class ULCollision(ULConditionNode):
+    def __init__(self):
+        ULConditionNode.__init__(self)
+        self.game_object = None
+        self.use_mat = None
+        self.prop = None
+        self.material = None
+        self._collision = False
+        self.pulse = False
+        self._target = None
+        self._point = None
+        self._normal = None
+        self._collision_triggered = False
+        self._consumed = False
+        self._game_object = None
+        self._objects = []
+        self.COLLISION = self.add_output(self.get_collision)
+        self.TARGET = self.add_output(self.get_target)
+        self.POINT = self.add_output(self.get_point)
+        self.NORMAL = self.add_output(self.get_normal)
+        self.OBJECTS = self.add_output(self.get_objects)
+        
+    def get_collision(self):
+        return self._collision
+
+    def get_point(self):
+        return self._point
+
+    def get_normal(self):
+        return self._normal
+
+    def get_target(self):
+        return self._target
+
+    def get_objects(self):
+        return self._objects
+
+    def _collision_callback(self, obj: KX_GameObject, point: Vector, normal: Vector):
+        self._objects.append(obj)
+        use_mat = self.get_input(self.use_mat)
+        if use_mat:
+            material: Material = self.get_input(self.material)
+            if material:
+                for obj in self._objects:
+                    bo = obj.blenderObject
+                    if material.name not in [
+                        slot.material.name for
+                        slot in
+                        bo.material_slots
+                    ]:
+                        self._objects.remove(obj)
+                    else:
+                        self._collision_triggered = True
+                        self._target = obj
+                        self._point = point
+                        self._normal = normal
+                        return
+                self._collision_triggered = False
+                return
+        else:
+            prop = self.get_input(self.prop)
+            if prop:
+                for obj in self._objects:
+                    if prop not in obj:
+                        self._objects.remove(obj)
+                    else:
+                        self._collision_triggered = True
+                        self._target = obj
+                        self._point = point
+                        self._normal = normal
+                        return
+                self._collision_triggered = False
+                return
+        self._collision_triggered = True
+        self._target = obj
+        self._point = point
+        self._normal = normal
+
+    def reset(self):
+        super().reset()
+        self._collision_triggered = False
+        self._objects = []
+
+    def _reset_game_object(self, game_object: KX_GameObject):
+        if self._game_object:
+            self._game_object.collisionCallbacks.remove(
+                self._collision_callback
+            )
+        game_object.collisionCallbacks.append(
+            self._collision_callback
+        )
+        self._game_object = game_object
+        self._collision = False
+        self._target = None
+        self._point = None
+        self._normal = None
+        self._collision_triggered = False
+
+    def evaluate(self):
+        last_target = self._target
+        game_object = self.get_input(self.game_object)
+        if game_object is not self._game_object:
+            self._reset_game_object(game_object)
+        collision = self._collision_triggered
+        if last_target is not self._target:
+            self._consumed = False
+        if collision and not self.pulse:
+            self._collision = collision and not self._consumed
+            self._consumed = True
+        elif self.pulse:
+            self._collision = collision
+        else:
+            self._consumed = False
+            self._collision = False
