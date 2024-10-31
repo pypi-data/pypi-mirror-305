f4f8d5315064fd061c9e47185ebe4ea11ac7317b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-31 07:04:51 +0000

    Add package labelbox/6.0.1/labelbox-6.0.1.tar.gz

---
+++ packages/labelbox/labelbox-6.0.1.tar.gz/labelbox-6.0.1/tests/integration/test_legacy_project.py
@@ -0,0 +1,13 @@
+from os import name
+
+import pytest
+from pydantic import ValidationError
+
+from labelbox.schema.media_type import MediaType
+
+
+def test_project_name_parameter(client, rand_gen):
+    with pytest.raises(
+        ValidationError, match="project name must be a valid string"
+    ):
+        client.create_project(name="     ", media_type=MediaType.Image)
