5e1fd1cfb3393b283ab5c60a476aa872453399c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-31 07:05:47 +0000

    Add package elastic_stacker/0.3.0/elastic_stacker-0.3.0.tar.gz

---
+++ packages/elastic_stacker/elastic_stacker-0.3.0.tar.gz/elastic_stacker-0.3.0/README.md
@@ -0,0 +1,52 @@
+# Elastic Stacker
+
+## Purpose
+
+This tool is used to export configuration objects (dashboards, pipelines, alerts, etc) out of one Elastic Stack and then allow importing in to another Stack.
+
+A main use case is for moving from development done in a PRE environment, and in to a PROD environment. Another use case is for keeping configurations in sync between multiple production stacks, but that might live on separate networks.
+
+## Installation
+
+```bash
+pip install elastic-stacker
+```
+
+## Contributing
+
+1. Clone the repository
+3. Set up Python environment: `pipenv install --dev`
+4. Activate the environment: `pipenv shell`
+5. Concrete-ize the configuration file, specifically replacing any occurences of `<REDACTED>` with real values:
+    ```sh
+    cp stacker.example.yaml stacker.yaml
+    # Update any configuration options needed:
+    nano stacker.yaml
+    ```
+6. Run the tool (more detailed usage below): `stacker -p <pre|prod> system_dump`
+
+
+## Usage
+
+Running the tools
+
+```bash
+# Export all the user configurations from LC Elastic Pre
+# Export should go in to the `files/` directory at the top of this repo
+# this is so that the resulting exported objects can be tracked in git.
+# You can also set the data directory in the config file (see the example).
+stacker system_dump -p pre --data-directory $(git rev-parse --show-toplevel)/files/
+
+# Check the changes that resulted from the export, make sure things look expected
+git diff
+
+# Add and commit any changes
+git add $(git rev-parse --show-toplevel)/files/
+git commit -m"Updated elastic repo with exported content"
+```
+
+## License
+
+Stacker is released under the Apache 2.0 license with LLVM exception. For more details see the LICENSE file.
+
+LLNL-CODE-850537
---
+++ packages/elastic_stacker/elastic_stacker-0.3.0.tar.gz/elastic_stacker-0.3.0/elastic_stacker/elasticsearch/watches.py
@@ -0,0 +1,146 @@
+import getpass
+import logging
+import os
+
+from httpx import HTTPStatusError
+
+from elastic_stacker.utils.controller import ElasticsearchAPIController
+
+logger = logging.getLogger("elastic_stacker")
+
+
+def substitute_passwords(
+    d: dict, password_map: dict = {}, prompt: bool = False, watch_id: str = ""
+):
+    """
+    Search through a nested dict looking for redacted passwords.
+    If it finds one, substitutes it with a value, which may be provided
+    from a mapping or prompted from the user.
+    """
+    for key, value in d.items():
+        if isinstance(value, dict):
+            d[key] = substitute_passwords(
+                value, password_map=password_map, prompt=prompt, watch_id=watch_id
+            )
+        if value == "::es_redacted::":
+            username = d.get("username", "")
+            if prompt:
+                d[key] = getpass.getpass(
+                    "Password for {} in watch {}:".format(username, watch_id)
+                )
+            elif username in password_map:
+                d[key] = password_map[username]
+            else:
+                logger.error(
+                    "No password found for user {user} in watch {watch_name}"
+                    "Set `options.watcher_users.{user}` in the config file, "
+                    "or use the `--prompt-credentials flag to enter them at "
+                    "the command line.`".format(user=username, watch_name=watch_id)
+                )
+                raise KeyError(
+                    "No password for {user} in watch {watch_name}".format(
+                        user=username, watch_name=watch_id
+                    )
+                )
+    return d
+
+
+class WatchController(ElasticsearchAPIController):
+    """
+    WatchController manages the import and export of Watches from Elasticsearch.
+    https://www.elastic.co/guide/en/elasticsearch/reference/current/watcher-api.html
+    """
+
+    _resource_directory = "watches"
+
+    def query(
+        self,
+        offset: int = None,
+        size: int = None,
+        query: dict = None,
+        sort: dict = None,
+        search_after: dict = None,
+    ):
+        """
+        Get a list of all watches that match a certain query.
+        https://www.elastic.co/guide/en/elasticsearch/reference/current/watcher-api-query-watches.html
+        """
+        post_body = {
+            "from": offset,
+            "size": size,
+            "query": query,
+            "sort": sort,
+            "search_after": search_after,
+        }
+        post_body = self._clean_params(post_body)
+        response = self._client.post("/_watcher/_query/watches", json=post_body)
+        return response.json()
+
+    def create(self, watch_id: str, watch: dict, active: bool = None):
+        """
+        Create a new watch, or update an existing one.
+        https://www.elastic.co/guide/en/elasticsearch/reference/current/watcher-api-put-watch.html
+        """
+        endpoint = "_watcher/watch/{}".format(watch_id)
+        query_params = {"active": active}
+        query_params = self._clean_params(query_params)
+        response = self._client.put(endpoint, json=watch, params=query_params)
+        return response.json()
+
+    def load(
+        self,
+        data_directory: os.PathLike = None,
+        allow_failure: bool = True,
+        delete_after_import: bool = False,
+        prompt_credentials: bool = False,
+        **kwargs,
+    ):
+        """
+        Load in watches from files in the data directory and create them in Elasticsearch.
+        """
+        working_directory = self._get_working_dir(data_directory, create=False)
+
+        for watch_file in working_directory.glob("*.json"):
+            watch_id = watch_file.stem
+
+            watch = self._read_file(watch_file)
+
+            watch = substitute_passwords(
+                watch,
+                prompt=prompt_credentials,
+                password_map=self._options.get("watcher_users", {}),
+                watch_id=watch_id,
+            )
+
+            logger.info("Loading watch {}".format(watch_id))
+            # TODO store watch active state on dump
+            try:
+                self.create(watch_id, watch)
+            except HTTPStatusError as e:
+                if allow_failure:
+                    logger.info(
+                        "Experienced an error importing watch {}; "
+                        "continuing because allow_failure is True".format(watch_id)
+                    )
+                else:
+                    raise e
+            else:
+                if delete_after_import:
+                    watch_file.unlink()
+
+    def dump(
+        self,
+        data_directory: os.PathLike = None,
+        purge: bool = False,
+        force_purge: bool = False,
+        **kwargs,
+    ):
+        """
+        Dump out Watches from Elasticsearch to files in the data directory.
+        """
+        working_directory = self._get_working_dir(data_directory, create=True)
+        for watch in self._depaginate(self.query, "watches", page_size=10):
+            file_path = working_directory / (watch["_id"] + ".json")
+            self._write_file(file_path, watch["watch"])
+        if purge or force_purge:
+            self._purge_untouched_files(force=force_purge)
