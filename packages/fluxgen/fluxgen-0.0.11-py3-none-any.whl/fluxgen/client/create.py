1ddf3e2ae6dba27fd24d506db9b55ef3405e8b81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-31 04:05:06 +0000

    Add package fluxgen/0.0.11/fluxgen-0.0.11-py3-none-any.whl

---
+++ packages/fluxgen/fluxgen-0.0.11-py3-none-any.whl/fluxgen/client/create.py
@@ -0,0 +1,19 @@
+import fluxgen.command.create as create
+import fluxgen.utils as utils
+
+
+def main(args, parser, command, subparser):
+    # Assemble options we know are important
+    options = {"lead-broker": args.lead_broker, "command": command or None, "brokers": args.brokers}
+
+    # This will raise an error if the member type (e.g., minicluster) is not known
+    generator = create.FluxInstallScript()
+    result = generator.render(**options)
+
+    # This is a preview only
+    if args.dry_run:
+        print(result)
+        return
+
+    print(f"Writing install script to {args.outfile}")
+    utils.write_file(result, args.outfile)
