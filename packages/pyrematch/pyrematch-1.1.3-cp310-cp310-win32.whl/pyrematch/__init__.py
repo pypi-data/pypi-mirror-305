50f33289f7dbe78c89a57f1dbca1b1d7d85982f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-31 04:05:41 +0000

    Add package pyrematch/1.1.3/pyrematch-1.1.3-cp310-cp310-win32.whl

---
+++ packages/pyrematch/pyrematch-1.1.3-cp310-cp310-win32.whl/pyrematch/__init__.py
@@ -0,0 +1,12 @@
+from .pyrematch import Flags, multi_reql, reql
+
+__version__ = "1.1.3"
+
+from ._pyrematch import REmatchException
+
+__all__ = [
+    "Flags",
+    "multi_reql",
+    "reql",
+    "REmatchException",
+]
