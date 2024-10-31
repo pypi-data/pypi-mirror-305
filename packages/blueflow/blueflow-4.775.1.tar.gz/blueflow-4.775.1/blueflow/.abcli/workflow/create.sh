ca87132d45ea79878b6f391a310444793553bba1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-31 14:04:49 +0000

    Add package blueflow/4.775.1/blueflow-4.775.1.tar.gz

---
+++ packages/blueflow/blueflow-4.775.1.tar.gz/blueflow-4.775.1/blueflow/.abcli/workflow/create.sh
@@ -0,0 +1,31 @@
+#! /usr/bin/env bash
+
+function notebooks_and_scripts_workflow_create() {
+    local options=$1
+
+    if [ $(abcli_option_int "$options" help 0) == 1 ]; then
+        local args="[--publish_as <public-object-name>]"
+        options="pattern=$NBS_PATTRENS_LIST$EOP,~upload"
+        abcli_show_usage "workflow create$ABCUL$options$ABCUL.|<job-name>$ABCUL$args$EOPE" \
+            "create a <pattern> workflow."
+        return
+    fi
+
+    local do_upload=$(abcli_option_int "$options" upload 1)
+    local pattern=$(abcli_option "$options" pattern $NBS_PATTREN_DEFAULT)
+
+    local job_name=$(abcli_clarify_object $2 .)
+
+    abcli_log "📜 workflow.create: $pattern -> $job_name"
+
+    python3 -m blueflow.workflow \
+        create \
+        --job_name $job_name \
+        --pattern "$pattern" \
+        "${@:3}"
+
+    [[ "$do_upload" == 1 ]] &&
+        abcli_upload - $job_name
+
+    return 0
+}
---
+++ packages/blueflow/blueflow-4.775.1.tar.gz/blueflow-4.775.1/blueflow/.abcli/workflow/monitor.sh
@@ -0,0 +1,43 @@
+#! /usr/bin/env bash
+
+function notebooks_and_scripts_workflow_monitor() {
+    local options=$1
+
+    if [ $(abcli_option_int "$options" help 0) == 1 ]; then
+        local options="~download,node=<node>,publish_as=<public-object-name>,~upload"
+        abcli_show_usage "workflow monitor$ABCUL$EOP[$options]$ABCUL[.|<job-name>]$ABCUL[<command-line>]$EOPE" \
+            "monitor <job-name>/workflow and run <command-line>."
+        return
+    fi
+
+    local do_download=$(abcli_option_int "$options" download 1)
+    local publish_as=$(abcli_option "$options" publish_as)
+    local do_upload=$(abcli_option_int "$options" upload 1)
+    local node=$(abcli_option "$options" node void)
+
+    local job_name=$(abcli_clarify_object $2 .)
+
+    abcli_log "📜 workflow.monitor: $job_name @ $node ..."
+
+    [[ "$do_download" == 1 ]] &&
+        abcli_download - $job_name
+
+    python3 -m blueflow.workflow.runners \
+        monitor \
+        --hot_node $node \
+        --job_name $job_name
+
+    [[ "$do_upload" == 1 ]] &&
+        abcli_upload - $job_name
+
+    [[ ! -z "$publish_as" ]] &&
+        abcli_publish as=$publish_as,~download,suffix=.gif $job_name
+
+    local command_line="${@:3}"
+    if [[ ! -z "$command_line" ]]; then
+        abcli_eval - \
+            "$command_line"
+    else
+        return 0
+    fi
+}
