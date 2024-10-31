67ce05fb8c986bdecef8c2128613ae42dd89b69a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-31 07:05:31 +0000

    Add package streamlit-button-with-images-and-links/0.1.3/streamlit_button_with_images_and_links-0.1.3.tar.gz

---
+++ packages/streamlit-button-with-images-and-links/streamlit_button_with_images_and_links-0.1.3.tar.gz/streamlit_button_with_images_and_links-0.1.3/setup.py
@@ -0,0 +1,22 @@
+from pathlib import Path
+
+import setuptools
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setuptools.setup(
+    name="streamlit_button_with_images_and_links",
+    version="0.1.3",
+    author="Usman DG",
+    author_email="dimausman@inbox.ru",
+    description="Streamlit component which should allow you to replace buttons with images with caption",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(where="src"),
+    package_dir={"": "src"},
+    include_package_data=True,
+    classifiers=[],
+    python_requires=">=3.7",
+    install_requires=["streamlit>=1.2", "jinja2"],
+)
