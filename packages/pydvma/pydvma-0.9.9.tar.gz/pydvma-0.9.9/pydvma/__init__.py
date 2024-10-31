6c18d62debc08a40d52196b8fcbc4eea4ff57f67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-31 17:06:13 +0000

    Add package pydvma/0.9.9/pydvma-0.9.9.tar.gz

---
+++ packages/pydvma/pydvma-0.9.9.tar.gz/pydvma-0.9.9/pydvma/__init__.py
@@ -0,0 +1,16 @@
+from .gui import Logger, Oscilloscope
+from .options import MySettings, Output_Signal_Settings ,set_plot_colours
+from .file import load_data, save_data, save_fig, export_to_matlab_jwlogger, export_to_matlab, export_to_csv
+# from .oscilloscope import Oscilloscope
+from .acquisition import log_data, output_signal, signal_generator, stream_snapshot
+from .datastructure import DataSet, TimeData, FreqData, CrossSpecData, TfData, SonoData, MetaData, ModalData, update_dataset
+from .testdata import create_test_impulse_data, create_test_impulse_ensemble, create_test_noise_data
+from .plotting import PlotData
+from .analysis import calculate_fft, calculate_cross_spectrum_matrix, calculate_cross_spectra_averaged, clean_impulse
+from .analysis import calculate_tf, calculate_tf_averaged, multiply_by_power_of_iw, best_match, calculate_sonogram, calculate_damping_from_sono
+from .streams import Recorder, Recorder_NI, start_stream, REC, setup_output_NI, setup_output_soundcard, list_available_devices, get_devices_NI, get_devices_soundcard
+from .modal import modal_fit_single_channel, modal_fit_all_channels
+# import faulthandler
+# faulthandler.enable()
+# from .gui_tk_test import Logger
+
---
+++ packages/pydvma/pydvma-0.9.9.tar.gz/pydvma-0.9.9/setup.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep  6 15:06:51 2018
+
+@author: tb267
+"""
+from setuptools import setup, find_packages
+
+requires = ['peakutils', 'numpy', 'scipy', 'pyqtgraph', 'matplotlib', 'seaborn', 'sounddevice']
+#requires = ['peakutils', 'numpy', 'scipy', 'pyqtgraph', 'matplotlib', 'seaborn', 'sounddevicem', 'qtpy', 'pyqt5', 'qdarktheme']
+
+setup(
+    name='pydvma',
+    version='0.9.9', # keep in sync with datastructure.py
+    install_requires=requires,
+    packages=['pydvma'],
+    package_data={'': ['icon.png']},
+    license='BSD 3-Clause License',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Tore Butlin',
+    author_email='tb267@cam.ac.uk',
+    url='https://github.com/torebutlin/pydvma'
+)
