6894c7443be2fc8a998cced08d5fe1fcc4ac59a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-31 14:04:28 +0000

    Add package jaxloop/0.0.1/jaxloop-0.0.1-py3-none-any.whl

---
+++ packages/jaxloop/jaxloop-0.0.1-py3-none-any.whl/jaxloop/actions.py
@@ -0,0 +1,17 @@
+"""Some loop actions."""
+
+class ActionOne:
+  """ActionOne is an action."""
+
+  def __init__(self):
+    self.f = 'f'
+
+
+class ActionTwo:
+  """ActionTwo is an action."""
+  pass
+
+
+def foo():
+  """Foo is a function."""
+  return 1
