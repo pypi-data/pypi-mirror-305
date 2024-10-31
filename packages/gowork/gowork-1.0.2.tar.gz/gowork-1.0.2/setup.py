ef30dff31874c1861d70a694b9ee44a6fcf37004
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-31 14:04:34 +0000

    Add package gowork/1.0.2/gowork-1.0.2.tar.gz

---
+++ packages/gowork/gowork-1.0.2.tar.gz/gowork-1.0.2/setup.py
@@ -0,0 +1,36 @@
+import setuptools
+import subprocess
+
+gowork_version = (
+    subprocess.run(["git", "describe", "--tags"], stdout=subprocess.PIPE)
+        .stdout.decode("utf-8")
+        .strip()
+)
+
+setuptools.setup(
+    name="gowork",
+    version=gowork_version,
+    author="Patrick Amaral",
+    author_email="patrick.dev.atom@gmail.com",
+    description="Library to help track your credentials and database engines",
+    url="https://github.com/Phomint/GoWork",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    license='GPLv3',
+    keywords=['databases', 'athena', 'mysql', 'mongodb'],
+    install_requires=[
+        "numpy",
+        "pandas",
+        "pyathena",
+        "pymysql",
+        "glob2",
+        "sqlalchemy",
+        "mongudb"
+    ],
+)
