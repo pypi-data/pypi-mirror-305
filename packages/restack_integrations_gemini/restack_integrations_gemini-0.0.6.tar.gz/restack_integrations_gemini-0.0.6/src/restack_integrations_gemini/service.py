778c07dc0ca8b7ad61df86a2ab761c8643de00b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-31 15:04:57 +0000

    Add package restack_integrations_gemini/0.0.6/restack_integrations_gemini-0.0.6.tar.gz

---
+++ packages/restack_integrations_gemini/restack_integrations_gemini-0.0.6.tar.gz/restack_integrations_gemini-0.0.6/src/restack_integrations_gemini/service.py
@@ -0,0 +1,33 @@
+from restack_ai import Restack
+from pydantic import BaseModel
+from .functions.generate_content import gemini_generate_content
+
+
+class GeminiServiceOptions(BaseModel):
+    rate_limit: int
+
+class RestackWrapper(BaseModel):
+    client: Restack
+    
+    class Config:
+        arbitrary_types_allowed = True
+
+
+class GeminiServiceInput(BaseModel):
+    client: RestackWrapper
+    options: GeminiServiceOptions
+
+
+async def gemini_service(input: GeminiServiceInput):
+    return await input.client.start_service(
+        functions=[
+            gemini_generate_content
+        ],
+        options=input.options
+    )
+
+if __name__ == "__main__":
+    gemini_service(
+        client=Restack(),
+        options=GeminiServiceOptions(rate_limit=100000)
+    )
