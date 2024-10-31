69ecb34ff30749345f96c7a56c9d1660c7900a55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-31 15:04:56 +0000

    Add package bomiot/0.4.1/bomiot-0.4.1-py3-none-any.whl

---
+++ packages/bomiot/bomiot-0.4.1-py3-none-any.whl/bomiot/server/core/__init__.py
@@ -0,0 +1,6 @@
+from django.dispatch import Signal, receiver
+from .signal import bomiot_signals
+
+@receiver(bomiot_signals)
+def bomiot_signal_callback(sender, **kwargs):
+    print(sender, kwargs)
