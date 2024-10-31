8912172c736df847f642d9b945577290dd4d5aa4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-31 15:05:17 +0000

    Add package pyDolarVenezuela/2.0.2/pyDolarVenezuela-2.0.2-py3-none-any.whl

---
+++ packages/pyDolarVenezuela/pyDolarVenezuela-2.0.2-py3-none-any.whl/pyDolarVenezuela/network.py
@@ -0,0 +1,46 @@
+from typing import Literal
+import requests
+from curl_cffi import requests as cffi
+
+_headers = {
+    'User-Agent': 'Mozilla/5.0 (X11; CrOS x86_64 8172.45.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.64 Safari/537.36'
+}
+
+def get(url: str, params: dict = None, verify: bool = True):
+    """
+    Realiza una solicitud HTTP GET utilizando la biblioteca requests.
+
+    Args:
+        - url (str): La URL a la que se enviará la solicitud GET.
+        - params (dict, opcional): Parámetros que se incluirán en la solicitud. Por defecto es None.
+        - verify (bool, opcional): Si se debe verificar el certificado SSL. Por defecto es True.
+
+    Returns:
+        bytes: El contenido de la respuesta en formato de bytes.
+    """
+    response = requests.get(url, params=params, verify=verify, timeout=10.0, headers=_headers)
+    response.raise_for_status()
+    
+    return response.content
+
+def curl(method: Literal['GET', 'POST'], url: str, headers: dict = None, data: dict = None, json: dict = None, impersonate: str = "chrome110"):
+    """
+    Realiza una solicitud HTTP utilizando cffi y permite la opción de impersonar un navegador.
+
+    Args:
+        - method (Literal['GET', 'POST']): El método HTTP a utilizar (GET o POST).
+        - url (str): La URL a la que se enviará la solicitud.
+        - headers (dict, opcional): Encabezados HTTP a incluir en la solicitud. Por defecto es None.
+        - json (dict, opcional): Datos JSON a incluir en la solicitud. Por defecto es None.
+        - impersonate (str, opcional): El navegador a impersonar. Por defecto es "chrome110".
+
+    Returns:
+        bytes: El contenido de la respuesta en formato de bytes.
+    """
+    if headers is None:
+        headers = _headers
+        
+    response = cffi.request(method=method, url=url, impersonate=impersonate, headers=headers, data=data, json=json, timeout=10.0)
+    response.raise_for_status()
+
+    return response.content
---
+++ packages/pyDolarVenezuela/pyDolarVenezuela-2.0.2-py3-none-any.whl/pyDolarVenezuela/providers/bcv.py
@@ -0,0 +1,61 @@
+from typing import Any, Dict, List
+from bs4 import BeautifulSoup
+
+from ..network import requests, get
+from ..utils.extras import currencies, list_monitors_images, bank_dict
+from ..utils.time import get_formatted_date_bcv, get_datestring_to_datetime
+from ._base import Base
+from ..pages import BCV as BCVPage
+
+requests.packages.urllib3.disable_warnings()
+
+def _get_rate_by_id(tag_id: str, soup: BeautifulSoup):
+    return float(soup.find(id=tag_id).find('strong').text.strip().replace(',', '.'))
+
+def _get_time(soup: BeautifulSoup):
+    date = soup.find('span', 'date-display-single').get('content')
+    return get_formatted_date_bcv(date)
+
+class BCV(Base):
+    PAGE = BCVPage
+
+    @classmethod
+    def _load(cls, **kwargs) -> List[Dict[str, Any]]:
+        response = get(f'{cls.PAGE.provider}tasas-informativas-sistema-bancario', verify=False)
+        soup = BeautifulSoup(response, 'html.parser')
+        rates = []
+
+        section_tipo_de_cambio_oficial = soup.find('div', 'view-tipo-de-cambio-oficial-del-bcv')
+        section_sistema_bancario = soup.find('div', 'table-responsive')
+
+        for bank in section_sistema_bancario.find('tbody').find_all('tr'):
+            title = str(bank.find('td', 'views-field views-field-views-conditional').text).strip()
+            key = bank_dict.get(title)
+
+            if key and key not in [bank['key'] for bank in rates]:
+                field_tasa_venta = bank.find('td', 'views-field views-field-field-tasa-venta').text
+                if field_tasa_venta.count(',') == 1:
+                    price = float(field_tasa_venta.replace(',', '.'))
+                    price_round = round(price, 2)
+                    image = next((image.image for image in list_monitors_images if image.provider == 'bcv' and image.title == key), None)
+                    last_update = get_datestring_to_datetime(bank.find('td', 'views-field views-field-field-fecha-del-indicador').text.strip().replace('-', '/'))
+
+                    rates.append({
+                        'key': key,
+                        'title': title,
+                        'price': price_round,
+                        'image': image,
+                        'last_update': last_update
+                    })
+
+        for code, values in currencies.items():
+            image = next((image.image for image in list_monitors_images if image.provider == 'bcv' and image.title == code), None)
+            rates.append({
+                'key': code,
+                'title': values['name'],
+                'price': round(_get_rate_by_id(values['id'], section_tipo_de_cambio_oficial), 2),
+                'image': image,
+                'last_update': _get_time(section_tipo_de_cambio_oficial)
+            })
+
+        return rates 
