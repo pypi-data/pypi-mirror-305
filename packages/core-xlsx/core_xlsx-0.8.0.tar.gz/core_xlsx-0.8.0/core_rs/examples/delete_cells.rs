3ec7a351df35bcfba94675759c31a81f6e9f5321
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-31 05:06:21 +0000

    Add package core-xlsx/0.8.0/core_xlsx-0.8.0.tar.gz

---
+++ packages/core-xlsx/core_xlsx-0.8.0.tar.gz/core_xlsx-0.8.0/core_rs/examples/delete_cells.rs
@@ -0,0 +1,39 @@
+use std::time::Instant;
+
+use core_rs::writer::sheet::XLSXSheet;
+
+fn main() -> anyhow::Result<()> {
+    let start = Instant::now();
+    let sheet = XLSXSheet::new("A".to_string(), 1, 50, 30);
+
+    let mut guard_sheet = sheet.lock().unwrap();
+
+    for cell in guard_sheet.cells() {
+        let mut cell = cell.lock().unwrap();
+        let f = format!("Row: {}, Col: {}", cell.row, cell.column);
+
+        cell.set_value(f)?;
+    }
+
+    guard_sheet.delete_cols(3, 28)?;
+    guard_sheet.delete_rows(6, 45)?;
+
+    for cell in guard_sheet.cells() {
+        let cell = cell.lock().unwrap();
+
+        println!(
+            "Cell {:?}, Row: {}, Col: {}, Value: {:?}",
+            cell.cell, cell.row, cell.column, cell.value
+        );
+    }
+
+    let end = start.elapsed();
+    println!(
+        "Выполнено за: {}.{:03} сек. Кол-во ячеек: {}",
+        end.as_secs(),
+        end.subsec_millis(),
+        guard_sheet.cells().collect::<Vec<_>>().len()
+    );
+
+    Ok(())
+}
---
+++ packages/core-xlsx/core_xlsx-0.8.0.tar.gz/core_xlsx-0.8.0/core_rs/examples/merged_cells.rs
@@ -0,0 +1,39 @@
+use std::time::Instant;
+
+use core_rs::writer::sheet::XLSXSheet;
+
+fn main() -> anyhow::Result<()> {
+    let start = Instant::now();
+    let sheet = XLSXSheet::new("A".to_string(), 1, 50, 30);
+
+    let mut guard_sheet = sheet.lock().unwrap();
+    guard_sheet.set_merged_cells(1, 10, 1, 10)?;
+
+    for cell in guard_sheet.cells() {
+        let cell = cell.lock().unwrap();
+
+        if cell.is_merge {
+            println!(
+                "{} -> {}:{} __ is_merge: {:?} [{:?}, {:?}, {:?}, {:?}]",
+                cell.cell,
+                cell.row,
+                cell.column,
+                cell.is_merge,
+                cell.start_row,
+                cell.end_row,
+                cell.start_column,
+                cell.end_column
+            );
+        }
+    }
+
+    let end = start.elapsed();
+    println!(
+        "Выполнено за: {}.{:03} сек. Кол-во ячеек: {}",
+        end.as_secs(),
+        end.subsec_millis(),
+        guard_sheet.cells().collect::<Vec<_>>().len()
+    );
+
+    Ok(())
+}
