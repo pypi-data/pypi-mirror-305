363ffeb35e11d4730f7e3aa38e6f0297085bdd30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-31 14:05:14 +0000

    Add package goallteam-library-entity/1.0.7/goallteam_library_entity-1.0.7.tar.gz

---
+++ packages/goallteam-library-entity/goallteam_library_entity-1.0.7.tar.gz/goallteam_library_entity-1.0.7/library_entity/__init__.py
@@ -0,0 +1,7 @@
+from .country import Country
+from .department import Department
+from .city import City
+from .player import Player
+from .user import User
+from .team import Team
+from .teamplayer import TeamPlayer
