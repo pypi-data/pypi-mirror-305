3160b2de487c3c4ddccf53ada0bfe3cd4cfd628e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-31 17:05:24 +0000

    Add package simplemath4test/0.1.0/simplemath4test-0.1.0-py3-none-any.whl

---
+++ packages/simplemath4test/simplemath4test-0.1.0-py3-none-any.whl/simplemath4test/__init__.py
@@ -0,0 +1,7 @@
+# simplemath4test/__init__.py
+
+from .operations import add, subtract
+
+__all__ = ['add', 'subtract']
+
+
---
+++ packages/simplemath4test/simplemath4test-0.1.0-py3-none-any.whl/simplemath4test/operations.py
@@ -0,0 +1,9 @@
+# simplemath4test/operations.py
+
+def add(a, b):
+    """Return the sum of two numbers."""
+    return a + b
+
+def subtract(a, b):
+    """Return the difference between two numbers."""
+    return a - b
