d86cb75687bbd4989ca860632ab2f090445858f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-31 03:11:18 +0000

    Add package slurpit_netbox/1.0.33/slurpit_netbox-1.0.33-py3-none-any.whl

---
+++ packages/slurpit_netbox/slurpit_netbox-1.0.33-py3-none-any.whl/slurpit_netbox/views/__init__.py
@@ -0,0 +1,19 @@
+from .onboarding import SlurpitImportedDeviceListView, SlurpitImportedDeviceOnboardView, ImportDevices
+from .setting import SettingsView
+from .datamapping import DataMappingView
+from .reconcile import (
+    ReconcileView, 
+    ReconcileDetailView, 
+    SlurpitInitIPAddressListView, 
+    SlurpitPrefixListView, 
+    SlurpitInterfaceListView, 
+    SlurpitVlanListView,
+    SlurpitPrefixEditView, 
+    SlurpitInterfaceEditView, 
+    SlurpitIPAddressEditView, 
+    SlurpitInterfaceBulkEditView, 
+    SlurpitPrefixBulkEditView, 
+    SlurpitIPAddressBulkEditView,
+    SlurpitVLANEditView,
+    SlurpitVLANBulkEditView
+)
