f7494569c6f420da703a6c2fa4907807f20a2e1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-31 03:10:54 +0000

    Add package tksel/1.1.3/tksel-1.1.3-py3-none-any.whl

---
+++ packages/tksel/tksel-1.1.3-py3-none-any.whl/TkSel/__about__.py
@@ -0,0 +1,4 @@
+# SPDX-FileCopyrightText: 2023-present Marceau-h <pypi@marceau-h.fr>
+#
+# SPDX-License-Identifier: AGPL-3.0-or-later
+__version__ = "1.1.3"
---
+++ packages/tksel/tksel-1.1.3-py3-none-any.whl/TkSel/__init__.py
@@ -0,0 +1,7 @@
+# SPDX-FileCopyrightText: 2023-present Marceau-h <pypi@marceau-h.fr>
+#
+# SPDX-License-Identifier: AGPL-3.0-or-later
+from .__about__ import __version__
+from .TkSel import TkSel
+
+__all__ = ["TkSel", "__version__"]
