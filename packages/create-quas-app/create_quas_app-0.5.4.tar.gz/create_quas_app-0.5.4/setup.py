ebcabbf878d1ea53546565ebc64f5373a7e5bdb9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-31 05:06:26 +0000

    Add package create-quas-app/0.5.4/create_quas_app-0.5.4.tar.gz

---
+++ packages/create-quas-app/create_quas_app-0.5.4.tar.gz/create_quas_app-0.5.4/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+with open("QUAS-template/README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name="create-quas-app",
+    version="0.5.4",
+    packages=find_packages(),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=["Click"],
+    entry_points={
+        "console_scripts": [
+            "create-quas-app=app:create_quas_app",
+        ],
+    },
+    author="Emmanuel Esho",
+    description="A basic Quick API Setup using Flask, intended for rapid project initiation.",
+    url="https://github.com/zeddyemy/QUAS",
+)
