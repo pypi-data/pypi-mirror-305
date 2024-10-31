1a971ade8c473746b0332801dc1245c74be624d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-31 04:05:10 +0000

    Add package dbnl-bear/0.4/dbnl_bear-0.4.tar.gz

---
+++ packages/dbnl-bear/dbnl_bear-0.4.tar.gz/dbnl_bear-0.4/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='dbnl_bear',
+    version='0.4',
+    packages=find_packages(),
+    install_requires=["lxml", "tqdm", "python-dotenv", "langchain_core", "langchain_openai", "tqdm",
+                      "typing", "langchain_text_splitters", "docx", "fuzzywuzzy", "pydantic",
+                     "python-docx", "python-Levenshtein"],
+    description='A Python package to use an AI assistant for historical analysis',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Arjan van Dalfsen',
+    author_email='j.a.vandalfsen@uu.nl',
+    url='https://github.com/trister95/dbnl_bear',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.10',
+)
