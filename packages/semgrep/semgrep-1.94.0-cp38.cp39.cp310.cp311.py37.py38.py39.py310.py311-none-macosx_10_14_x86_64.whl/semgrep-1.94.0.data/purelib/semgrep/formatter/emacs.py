86e2beaa714e066e5443ba8a0cc7ed9c44e111bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-31 14:05:18 +0000

    Add package semgrep/1.94.0/semgrep-1.94.0-cp38.cp39.cp310.cp311.py37.py38.py39.py310.py311-none-macosx_10_14_x86_64.whl

---
+++ packages/semgrep/semgrep-1.94.0-cp38.cp39.cp310.cp311.py37.py38.py39.py310.py311-none-macosx_10_14_x86_64.whl/semgrep-1.94.0.data/purelib/semgrep/formatter/emacs.py
@@ -0,0 +1,26 @@
+from typing import Any
+from typing import Iterable
+from typing import Mapping
+from typing import Sequence
+
+import semgrep.rpc_call
+import semgrep.semgrep_interfaces.semgrep_output_v1 as out
+from semgrep.error import SemgrepError
+from semgrep.formatter.base import BaseFormatter
+from semgrep.formatter.base import to_CliOutput
+from semgrep.rule import Rule
+from semgrep.rule_match import RuleMatch
+
+
+class EmacsFormatter(BaseFormatter):
+    def format(
+        self,
+        rules: Iterable[Rule],
+        rule_matches: Iterable[RuleMatch],
+        semgrep_structured_errors: Sequence[SemgrepError],
+        cli_output_extra: out.CliOutputExtra,
+        extra: Mapping[str, Any],
+        is_ci_invocation: bool,
+    ) -> str:
+        output = to_CliOutput(rule_matches, semgrep_structured_errors, cli_output_extra)
+        return semgrep.rpc_call.format(out.OutputFormat(out.Emacs()), output)
