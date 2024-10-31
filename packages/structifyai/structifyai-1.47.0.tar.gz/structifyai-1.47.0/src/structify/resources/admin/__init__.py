3fc2620e2a2a94be22593ac141c738efeafe7c98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-31 06:06:38 +0000

    Add package structifyai/1.47.0/structifyai-1.47.0.tar.gz

---
+++ packages/structifyai/structifyai-1.47.0.tar.gz/structifyai-1.47.0/src/structify/resources/admin/__init__.py
@@ -0,0 +1,47 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from .admin import (
+    AdminResource,
+    AsyncAdminResource,
+    AdminResourceWithRawResponse,
+    AsyncAdminResourceWithRawResponse,
+    AdminResourceWithStreamingResponse,
+    AsyncAdminResourceWithStreamingResponse,
+)
+from .users import (
+    UsersResource,
+    AsyncUsersResource,
+    UsersResourceWithRawResponse,
+    AsyncUsersResourceWithRawResponse,
+    UsersResourceWithStreamingResponse,
+    AsyncUsersResourceWithStreamingResponse,
+)
+from .training_datasets import (
+    TrainingDatasetsResource,
+    AsyncTrainingDatasetsResource,
+    TrainingDatasetsResourceWithRawResponse,
+    AsyncTrainingDatasetsResourceWithRawResponse,
+    TrainingDatasetsResourceWithStreamingResponse,
+    AsyncTrainingDatasetsResourceWithStreamingResponse,
+)
+
+__all__ = [
+    "UsersResource",
+    "AsyncUsersResource",
+    "UsersResourceWithRawResponse",
+    "AsyncUsersResourceWithRawResponse",
+    "UsersResourceWithStreamingResponse",
+    "AsyncUsersResourceWithStreamingResponse",
+    "TrainingDatasetsResource",
+    "AsyncTrainingDatasetsResource",
+    "TrainingDatasetsResourceWithRawResponse",
+    "AsyncTrainingDatasetsResourceWithRawResponse",
+    "TrainingDatasetsResourceWithStreamingResponse",
+    "AsyncTrainingDatasetsResourceWithStreamingResponse",
+    "AdminResource",
+    "AsyncAdminResource",
+    "AdminResourceWithRawResponse",
+    "AsyncAdminResourceWithRawResponse",
+    "AdminResourceWithStreamingResponse",
+    "AsyncAdminResourceWithStreamingResponse",
+]
