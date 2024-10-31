51d4d7fe19fdb111d1cfc69e768a08f5cbf6f7ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-31 14:05:10 +0000

    Add package boto3-stubs-full/1.35.52/boto3_stubs_full-1.35.52.tar.gz

---
+++ packages/boto3-stubs-full/boto3_stubs_full-1.35.52.tar.gz/boto3_stubs_full-1.35.52/mypy_boto3_taxsettings/paginator.py
@@ -0,0 +1,64 @@
+"""
+Type annotations for taxsettings service client paginators.
+
+[Open documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_taxsettings/paginators/)
+
+Usage::
+
+    ```python
+    from boto3.session import Session
+
+    from mypy_boto3_taxsettings.client import TaxSettingsClient
+    from mypy_boto3_taxsettings.paginator import (
+        ListTaxRegistrationsPaginator,
+    )
+
+    session = Session()
+    client: TaxSettingsClient = session.client("taxsettings")
+
+    list_tax_registrations_paginator: ListTaxRegistrationsPaginator = client.get_paginator("list_tax_registrations")
+    ```
+"""
+
+import sys
+from typing import Generic, Iterator, TypeVar
+
+from botocore.paginate import PageIterator, Paginator
+
+from .type_defs import (
+    ListTaxRegistrationsRequestListTaxRegistrationsPaginateTypeDef,
+    ListTaxRegistrationsResponseTypeDef,
+)
+
+if sys.version_info >= (3, 12):
+    from typing import Unpack
+else:
+    from typing_extensions import Unpack
+
+
+__all__ = ("ListTaxRegistrationsPaginator",)
+
+
+_ItemTypeDef = TypeVar("_ItemTypeDef")
+
+
+class _PageIterator(Generic[_ItemTypeDef], PageIterator):
+    def __iter__(self) -> Iterator[_ItemTypeDef]:
+        """
+        Proxy method to specify iterator item type.
+        """
+
+
+class ListTaxRegistrationsPaginator(Paginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/taxsettings.html#TaxSettings.Paginator.ListTaxRegistrations)
+    [Show boto3-stubs-full documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_taxsettings/paginators/#listtaxregistrationspaginator)
+    """
+
+    def paginate(
+        self, **kwargs: Unpack[ListTaxRegistrationsRequestListTaxRegistrationsPaginateTypeDef]
+    ) -> _PageIterator[ListTaxRegistrationsResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/taxsettings.html#TaxSettings.Paginator.ListTaxRegistrations.paginate)
+        [Show boto3-stubs-full documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_taxsettings/paginators/#listtaxregistrationspaginator)
+        """
