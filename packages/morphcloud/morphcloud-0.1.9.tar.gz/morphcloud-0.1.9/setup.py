6fcdc8aa43ec0327f6ddc44e87ecfc5778184cb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-31 06:05:47 +0000

    Add package morphcloud/0.1.9/morphcloud-0.1.9.tar.gz

---
+++ packages/morphcloud/morphcloud-0.1.9.tar.gz/morphcloud-0.1.9/setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup, find_packages
+from setuptools.command.install import install
+
+class PostInstallCommand(install):
+    def run(self):
+        install.run(self)
+
+setup(
+    name="morphcloud",
+    version="0.1.9",
+    use_scm_version=True,
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "requests",
+        "tqdm",
+        "argparse",
+        "shutil",
+        "psutil",
+        "streamlit",
+        "anthropic",
+        "openai",
+        "pillow",
+        "httpx",
+        "fire",
+        "playwright",
+        "bs4",
+        "click",
+        "pytest",
+        "pydantic",
+        "httpx",
+    ],
+    entry_points={
+        "console_scripts": [
+            "morphcloud=morphcloud.cli:main",
+        ],
+    },
+    cmdclass={
+        'install': PostInstallCommand,
+    },
+)
