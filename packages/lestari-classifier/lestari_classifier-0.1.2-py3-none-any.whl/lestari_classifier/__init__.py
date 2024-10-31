38f3bd4392129518b69f265cba0a1f38fe91ec8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-31 15:05:31 +0000

    Add package lestari-classifier/0.1.2/lestari_classifier-0.1.2-py3-none-any.whl

---
+++ packages/lestari-classifier/lestari_classifier-0.1.2-py3-none-any.whl/lestari_classifier/__init__.py
@@ -0,0 +1,11 @@
+"""
+LESTARI (Layered Ensemble Stacking Technique with Adaptive Regression of Individual-errors)
+"""
+
+from .classifier import LestariClassifier
+
+__version__ = "0.1.2"
+__author__ = "Rio Nur Arifin"
+__email__ = "arifinrio95@gmail.com"
+
+__all__ = ["LestariClassifier"]
