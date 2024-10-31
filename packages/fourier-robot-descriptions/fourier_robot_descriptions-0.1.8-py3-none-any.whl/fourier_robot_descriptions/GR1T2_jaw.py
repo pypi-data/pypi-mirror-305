4835ff04ccff8bdcfd8da4a4a91a7f307186b814
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-31 15:05:29 +0000

    Add package fourier-robot-descriptions/0.1.8/fourier_robot_descriptions-0.1.8-py3-none-any.whl

---
+++ packages/fourier-robot-descriptions/fourier_robot_descriptions-0.1.8-py3-none-any.whl/fourier_robot_descriptions/GR1T2_jaw.py
@@ -0,0 +1,16 @@
+"""GR1T2 description."""
+
+from os import getenv as _getenv
+from os import path as _path
+
+from ._cache import clone_to_cache as _clone_to_cache
+
+REPOSITORY_PATH: str = _clone_to_cache(
+    "fourier_grx_descriptions",
+    commit=_getenv("ROBOT_DESCRIPTION_COMMIT", None),
+)
+
+
+PACKAGE_PATH: str = _path.join(REPOSITORY_PATH, "GRX/GR1/GR1T2")
+
+URDF_PATH: str = _path.join(PACKAGE_PATH, "urdf", "GR1T2_jaw.urdf")
