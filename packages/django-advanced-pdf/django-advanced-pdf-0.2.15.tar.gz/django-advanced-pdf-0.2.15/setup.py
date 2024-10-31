a23f8a6ff663503932d487622545accba12517d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-31 14:04:50 +0000

    Add package django-advanced-pdf/0.2.15/django-advanced-pdf-0.2.15.tar.gz

---
+++ packages/django-advanced-pdf/django-advanced-pdf-0.2.15.tar.gz/django-advanced-pdf-0.2.15/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="django-advanced-pdf",
+    version="0.2.15",
+    author="Thomas Turner",
+    description="Django app that helps one create pdf",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/django-advance-utils/django-advanced-pdf",
+    include_package_data=True,
+    packages=['django_advanced_pdf'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
