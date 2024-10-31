8d11717681b98c4840bc3ff9c7c7042fa64cf69d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-31 03:11:19 +0000

    Add package cglbtrial5/0.2.0/cglbtrial5-0.2.0-py3-none-any.whl

---
+++ packages/cglbtrial5/cglbtrial5-0.2.0-py3-none-any.whl/cglbtrial5/conversion.py
@@ -0,0 +1,6 @@
+def unit_one_2_unit_two(value_unit_one):A=3.22*(value_unit_one-12.)+111.11;return A
+from importlib.metadata import entry_points
+def load_plugins():
+	A=[]
+	for B in entry_points().get('cglbtrial5.reader_plugins',[]):C=B.load();A.append(C)
+	return A
