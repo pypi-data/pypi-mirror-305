a1c3cbeb1d8c260de2ba153306dd01141aa6f3d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-31 16:05:42 +0000

    Add package seal5/0.2.0/seal5-0.2.0-py3-none-any.whl

---
+++ packages/seal5/seal5-0.2.0-py3-none-any.whl/seal5/backends/llvmir/writer.py
@@ -0,0 +1,173 @@
+# SPDX-License-Identifier: Apache-2.0
+#
+# This file is part of the M2-ISA-R project: https://github.com/tum-ei-eda/M2-ISA-R
+#
+# Copyright (C) 2022
+# Chair of Electrical Design Automation
+# Technical University of Munich
+
+"""Clean M2-ISA-R/Seal5 metamodel to .core_desc file."""
+
+import os
+import argparse
+import logging
+import pathlib
+import pickle
+from typing import Union
+
+from m2isar.metamodel import arch
+
+from seal5.tools import cdsl2llvm
+from seal5.model import Seal5InstrAttribute
+from seal5.riscv_utils import build_riscv_mattr, get_riscv_defaults
+
+logger = logging.getLogger("llvmir_behavior_writer")
+
+
+def main():
+    """Main app entrypoint."""
+
+    # read command line args
+    parser = argparse.ArgumentParser()
+    parser.add_argument("top_level", help="A .m2isarmodel or .seal5model file.")
+    parser.add_argument("--log", default="info", choices=["critical", "error", "warning", "info", "debug"])
+    parser.add_argument("--output", "-o", type=str, default=None)
+    parser.add_argument("--splitted", action="store_true", help="Split per set and instruction")
+    parser.add_argument("--metrics", default=None, help="Output metrics to file")
+    parser.add_argument("--ext", type=str, default="ll", help="Default file extension (if using --splitted)")
+    args = parser.parse_args()
+
+    # initialize logging
+    logging.basicConfig(level=getattr(logging, args.log.upper()))
+
+    # resolve model paths
+    top_level = pathlib.Path(args.top_level)
+    # abs_top_level = top_level.resolve()
+
+    is_seal5_model = False
+    # print("top_level", top_level)
+    # print("suffix", top_level.suffix)
+    if top_level.suffix == ".seal5model":
+        is_seal5_model = True
+    if args.output is not None:
+        out_path = pathlib.Path(args.output)
+    else:
+        assert top_level.suffix in [".m2isarmodel", ".seal5model"], "Can not infer model type from file extension."
+        # out_path = top_level.parent / (top_level.stem + ".core_desc")
+        raise NotImplementedError
+
+    logger.info("loading models")
+    if not is_seal5_model:
+        raise NotImplementedError
+
+    # load models
+    with open(top_level, "rb") as f:
+        # models: "dict[str, arch.CoreDef]" = pickle.load(f)
+        if is_seal5_model:
+            model: "dict[str, Union[arch.InstructionSet, ...]]" = pickle.load(f)
+            model["cores"] = {}
+        else:  # TODO: core vs. set!
+            temp: "dict[str, Union[arch.InstructionSet, arch.CoreDef]]" = pickle.load(f)
+            assert len(temp) > 0, "Empty model!"
+            if isinstance(list(temp.values())[0], arch.CoreDef):
+                model = {"cores": temp, "sets": {}}
+            elif isinstance(list(temp.values())[0], arch.InstructionSet):
+                model = {"sets": temp, "cores": {}}
+            else:
+                assert False
+
+    # preprocess model
+    # print("model", model)
+    metrics = {
+        "n_sets": 0,
+        "n_instructions": 0,
+        "n_skipped": 0,
+        "n_failed": 0,
+        "n_success": 0,
+    }
+    settings = model.get("settings", None)
+    if args.splitted:
+        # errs = []
+        # model_includes = []
+        # errs = []
+        if settings:
+            riscv_settings = settings.riscv
+        else:
+            riscv_settings = None
+        default_features, default_xlen = get_riscv_defaults(riscv_settings)
+
+        assert out_path.is_dir(), "Expecting output directory when using --splitted"
+        for set_name, set_def in model["sets"].items():
+            xlen = set_def.xlen
+            if xlen is None:
+                xlen = default_xlen
+            metrics["n_sets"] += 1
+            ext_settings = set_def.settings
+            for instr_def in set_def.instructions.values():
+                metrics["n_instructions"] += 1
+                attrs = instr_def.attributes
+                if len(attrs) > 0:
+                    skip = False
+                    if Seal5InstrAttribute.MAY_LOAD in attrs:
+                        skip = True
+                    elif Seal5InstrAttribute.MAY_STORE in attrs:
+                        skip = True
+                    elif arch.InstrAttribute.COND in attrs:
+                        skip = True
+                    elif arch.InstrAttribute.NO_CONT in attrs:
+                        skip = True
+                    if skip:
+                        metrics["n_skipped"] += 1
+                        continue
+                input_file = out_path / set_name / f"{instr_def.name}.core_desc"
+                if not input_file.is_file():
+                    metrics["n_skipped"] += 1
+                    # errs.append(TODO)
+                output_file = out_path / set_name / f"{instr_def.name}.{args.ext}"
+
+                features = [*default_features]
+                if ext_settings is not None:
+                    arch_ = ext_settings.get_arch(name=set_name)
+                    if arch is not None:
+                        features.append(arch_)
+                mattr = build_riscv_mattr(features, xlen)
+
+                install_dir = os.getenv("CDSL2LLVM_DIR", None)
+                assert install_dir is not None
+                install_dir = pathlib.Path(install_dir)
+                try:
+                    cdsl2llvm.run_pattern_gen(
+                        # install_dir / "llvm" / "build",
+                        install_dir,
+                        input_file,
+                        output_file,
+                        skip_patterns=True,
+                        skip_formats=True,
+                        mattr=mattr,
+                        xlen=xlen,
+                    )
+                    metrics["n_success"] += 1
+                except AssertionError:
+                    metrics["n_failed"] += 1
+                    # errs.append((insn_name, str(ex)))
+        # if len(errs) > 0:
+        #     # print("errs", errs)
+        #     for insn_name, err_str in errs:
+        #         print("Err:", insn_name, err_str)
+        #         input("!")
+        #         cdsl2llvm.
+        #         out_path_ = out_path / set_name / f"{instr_def.name}.{args.ext}"
+        #         out_path_.parent.mkdir(exist_ok=True)
+    else:
+        raise NotImplementedError
+    if args.metrics:
+        metrics_file = args.metrics
+        with open(metrics_file, "w", encoding="utf-8") as f:
+            f.write(",".join(metrics.keys()))
+            f.write("\n")
+            f.write(",".join(map(str, metrics.values())))
+            f.write("\n")
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/seal5/seal5-0.2.0-py3-none-any.whl/seal5/backends/riscv_isa_info/writer.py
@@ -0,0 +1,166 @@
+# SPDX-License-Identifier: Apache-2.0
+#
+# This file is part of the M2-ISA-R project: https://github.com/tum-ei-eda/M2-ISA-R
+#
+# Copyright (C) 2022
+# Chair of Electrical Design Automation
+# Technical University of Munich
+
+"""Clean M2-ISA-R/Seal5 metamodel to .core_desc file."""
+
+import argparse
+import logging
+import pathlib
+import pickle
+from typing import Union
+
+from mako.template import Template
+
+from m2isar.metamodel import arch
+
+from seal5.index import NamedPatch, write_index_yaml
+from seal5.settings import ExtensionsSettings, LLVMVersion
+
+logger = logging.getLogger("riscv_isa_info")
+
+
+MAKO_TEMPLATE = '    {"${arch}", RISCVExtensionVersion{${version_major}, ${version_minor}}},'
+MAKO_TEMPLATE_LLVM18 = '    {"${arch}", {${version_major}, ${version_minor}}},'
+
+
+def gen_riscv_isa_info_str(name: str, ext_settings: ExtensionsSettings, llvm_version: LLVMVersion):
+    # print("name", name)
+    # print("ext_settings", ext_settings)
+    arch_ = ext_settings.get_arch(name=name)
+    version = ext_settings.get_version()
+    if not isinstance(version, str):
+        assert isinstance(version, (int, float))
+        version = str(float(version))
+    version_major, version_minor = str(version).split(".", 1)
+
+    llvm_major = llvm_version.major
+    template = MAKO_TEMPLATE_LLVM18 if llvm_major >= 18 else MAKO_TEMPLATE
+    content_template = Template(template)
+    content_text = content_template.render(arch=arch_, version_major=version_major, version_minor=version_minor)
+    # content_text = content_text.rstrip("\n")
+    return arch, (content_text)
+
+
+def main():
+    """Main app entrypoint."""
+
+    # read command line args
+    parser = argparse.ArgumentParser()
+    parser.add_argument("top_level", help="A .m2isarmodel or .seal5model file.")
+    parser.add_argument("--log", default="info", choices=["critical", "error", "warning", "info", "debug"])
+    parser.add_argument("--output", "-o", type=str, default=None)
+    parser.add_argument("--splitted", action="store_true", help="Split per set")
+    parser.add_argument("--formats", action="store_true", help="Also generate instruction formats")
+    parser.add_argument("--metrics", default=None, help="Output metrics to file")
+    parser.add_argument("--index", default=None, help="Output index to file")
+    parser.add_argument("--ext", type=str, default="td", help="Default file extension (if using --splitted)")
+    args = parser.parse_args()
+
+    # initialize logging
+    logging.basicConfig(level=getattr(logging, args.log.upper()))
+
+    # resolve model paths
+    top_level = pathlib.Path(args.top_level)
+    # abs_top_level = top_level.resolve()
+
+    is_seal5_model = False
+    # print("top_level", top_level)
+    # print("suffix", top_level.suffix)
+    if top_level.suffix == ".seal5model":
+        is_seal5_model = True
+    if args.output is not None:
+        out_path = pathlib.Path(args.output)
+    else:
+        assert top_level.suffix in [".m2isarmodel", ".seal5model"], "Can not infer model type from file extension."
+        # out_path = top_level.parent / (top_level.stem + ".core_desc")
+        raise NotImplementedError
+
+    logger.info("loading models")
+    if not is_seal5_model:
+        raise NotImplementedError
+
+    # load models
+    with open(top_level, "rb") as f:
+        # models: "dict[str, arch.CoreDef]" = pickle.load(f)
+        if is_seal5_model:
+            model: "dict[str, Union[arch.InstructionSet, ...]]" = pickle.load(f)
+            model["cores"] = {}
+        else:  # TODO: core vs. set!
+            temp: "dict[str, Union[arch.InstructionSet, arch.CoreDef]]" = pickle.load(f)
+            assert len(temp) > 0, "Empty model!"
+            if isinstance(list(temp.values())[0], arch.CoreDef):
+                model = {"cores": temp, "sets": {}}
+            elif isinstance(list(temp.values())[0], arch.InstructionSet):
+                model = {"sets": temp, "cores": {}}
+            else:
+                assert False
+
+    metrics = {
+        "n_sets": 0,
+        "n_skipped": 0,
+        "n_failed": 0,
+        "n_success": 0,
+    }
+    # preprocess model
+    # print("model", model)
+    artifacts = {}
+    artifacts[None] = []  # used for global artifacts
+    if not args.splitted:
+        # content = ""
+        contents = []  # Extensions need to be sorted!
+        # errs = []
+        settings = model.get("settings", None)
+        llvm_version = None
+        if settings:
+            llvm_settings = settings.llvm
+            if llvm_settings:
+                llvm_state = llvm_settings.state
+                if llvm_state:
+                    llvm_version = llvm_state.version
+        for set_name, set_def in model["sets"].items():
+            artifacts[set_name] = []
+            metrics["n_sets"] += 1
+            ext_settings = set_def.settings
+            if ext_settings is None:
+                metrics["n_skipped"] += 1
+                continue
+            metrics["n_success"] += 1
+            key, new_content = gen_riscv_isa_info_str(set_name, ext_settings=ext_settings, llvm_version=llvm_version)
+            contents.append((key, new_content))
+        contents = sorted(contents, key=lambda x: x[0])
+        content = "\n".join([x[1] for x in contents])
+        if len(content) > 0:
+            with open(out_path, "w", encoding="utf-8") as f:
+                f.write(content)
+            if ext_settings.experimental:
+                key = "riscv_isa_info_experimental"
+            else:
+                key = "riscv_isa_info"
+            riscv_isa_info_patch = NamedPatch("llvm/lib/Support/RISCVISAInfo.cpp", key=key, src_path=out_path)
+            artifacts[None].append(riscv_isa_info_patch)
+    else:
+        raise NotImplementedError
+    if args.metrics:
+        metrics_file = args.metrics
+        with open(metrics_file, "w", encoding="utf-8") as f:
+            f.write(",".join(metrics.keys()))
+            f.write("\n")
+            f.write(",".join(map(str, metrics.values())))
+            f.write("\n")
+    if args.index:
+        if sum(map(len, artifacts.values())) > 0:
+            global_artifacts = artifacts.get(None, [])
+            set_artifacts = {key: value for key, value in artifacts.items() if key is not None}
+            index_file = args.index
+            write_index_yaml(index_file, global_artifacts, set_artifacts, content=True)
+        else:
+            logger.warning("No patches generated. No index file will be written.")
+
+
+if __name__ == "__main__":
+    main()
