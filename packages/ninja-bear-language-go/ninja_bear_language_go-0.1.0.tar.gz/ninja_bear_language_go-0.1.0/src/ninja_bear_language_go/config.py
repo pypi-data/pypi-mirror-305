13f1bfa2fc0f6cb663d13c797ff7290f5fe7f5ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-31 16:06:01 +0000

    Add package ninja-bear-language-go/0.1.0/ninja_bear_language_go-0.1.0.tar.gz

---
+++ packages/ninja-bear-language-go/ninja_bear_language_go-0.1.0.tar.gz/ninja_bear_language_go-0.1.0/src/ninja_bear_language_go/config.py
@@ -0,0 +1,22 @@
+from typing import Type
+
+from .generator import Generator
+from ninja_bear import LanguageConfigBase, NamingConventionType
+
+
+class Config(LanguageConfigBase):
+    """
+    Go specific config. For more information about the config methods, refer to LanguageConfigBase.
+    """
+
+    def _file_extension(self) -> str:
+        return 'go'
+
+    def _generator_type(self) -> Type[Generator]:
+        return Generator
+    
+    def _default_file_naming_convention(self) -> NamingConventionType:
+        return NamingConventionType.SNAKE_CASE
+
+    def _allowed_file_name_pattern(self) -> str:
+        return r'^([a-z]_*)+$'
