6b276a5448ffd290d8b8b9cf1ae9dfa4991e53e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-31 14:04:51 +0000

    Add package pyscfad/0.1.8/pyscfad-0.1.8.tar.gz

---
+++ packages/pyscfad/pyscfad-0.1.8.tar.gz/pyscfad-0.1.8/setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup, find_packages
+
+_dct = {}
+with open('pyscfad/version.py') as f:
+  exec(f.read(), _dct)
+__version__ = _dct['__version__']
+
+setup(
+    name='pyscfad',
+    version=__version__,
+    description='PySCF with autodiff',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Xing Zhang',
+    author_email='xzhang8@caltech.edu',
+    include_package_data=True,
+    packages=find_packages(exclude=["examples","*test*"]),
+    python_requires='>=3.9',
+    install_requires=[
+        'numpy>=1.17',
+        'scipy',
+        'h5py',
+        'jax>=0.4.14',
+        'jaxlib>=0.4.14',
+        'pyscf>=2.3',
+        'pyscfadlib>=0.1.4',
+        #'pyscf-properties @ git+https://github.com/fishjojo/properties.git@ad',
+    ],
+    url='https://github.com/fishjojo/pyscfad',
+    license='MIT',
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Scientific/Engineering",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
+    ],
+    zip_safe=False,
+)
