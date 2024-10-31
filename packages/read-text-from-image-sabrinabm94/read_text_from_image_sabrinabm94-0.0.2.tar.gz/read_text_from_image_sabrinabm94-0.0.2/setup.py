d1996d29d1878f08e2007eaa8505f81a15830fea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-31 14:05:12 +0000

    Add package read-text-from-image-sabrinabm94/0.0.2/read_text_from_image_sabrinabm94-0.0.2.tar.gz

---
+++ packages/read-text-from-image-sabrinabm94/read_text_from_image_sabrinabm94-0.0.2.tar.gz/read_text_from_image_sabrinabm94-0.0.2/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    page_description = f.read()
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name="read_text_from_image_sabrinabm94",
+    version="0.0.2",
+    author="Sabrina B. M.",
+    author_email="sabrinabm94@gmail.com",
+    description="Package to extract text from images using Python",
+    long_description=page_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/sabrinabm94/python/tree/master/data-engineering/challenges/package",
+    packages=find_packages(),
+    install_requires=requirements,
+    python_requires=">=3.8",
+)
