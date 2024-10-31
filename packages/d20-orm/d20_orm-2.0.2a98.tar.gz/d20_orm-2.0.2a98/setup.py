df8638762abc4015263625aa09293337a16a5c0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-31 15:05:09 +0000

    Add package d20-orm/2.0.2a98/d20_orm-2.0.2a98.tar.gz

---
+++ packages/d20-orm/d20_orm-2.0.2a98.tar.gz/d20_orm-2.0.2a98/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="d20-orm",
+    version="2.0.2a98",
+    author="Alex Sánchez Vega",
+    author_email="alex@d20.services",
+    description="A small ORM for multimodel DBs",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/d20services/arangodb_python_orm",
+    project_urls={
+        "Bug Tracker": "https://github.com/d20services/arangodb_python_orm/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.8",
+    install_requires=['pyArango', 'datetime'],
+)
