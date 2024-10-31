900e60058078f324b844e568c9a99905e3aab471
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-31 16:05:14 +0000

    Add package code2flowdiagram/1.0.1/code2flowdiagram-1.0.1.tar.gz

---
+++ packages/code2flowdiagram/code2flowdiagram-1.0.1.tar.gz/code2flowdiagram-1.0.1/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+from code2flowdiagram.engine import VERSION
+
+url_base = 'https://github.com/scottrogowski/code2flowdiagram'
+download_url = '%s/archive/code2flowdiagram-%s.tar.gz' % (url_base, VERSION)
+
+setup(
+    name='code2flowdiagram',
+    version=VERSION,
+    description='Visualize your source code as DOT flowcharts',
+    long_description=open('README.md', encoding='utf-8').read(),
+    long_description_content_type="text/markdown",
+    entry_points={
+        'console_scripts': ['code2flowdiagram=code2flowdiagram.engine:main'],
+    },
+    license='MIT',
+    author='Scott Rogowski',
+    author_email='scottmrogowski@gmail.com',
+    url=url_base,
+    download_url=download_url,
+    packages=find_packages(),
+    python_requires='>=3.6',
+    include_package_data=True,
+    package_data={
+        'code2flowdiagram': ['graphviz/bin/*'],
+    },
+    classifiers=[
+        'Natural Language :: English',
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
