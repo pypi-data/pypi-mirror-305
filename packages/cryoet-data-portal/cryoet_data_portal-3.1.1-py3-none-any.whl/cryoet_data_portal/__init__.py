127221c8facc2d6db188817a5938c6d7cb193be7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-31 06:06:14 +0000

    Add package cryoet-data-portal/3.1.1/cryoet_data_portal-3.1.1-py3-none-any.whl

---
+++ packages/cryoet-data-portal/cryoet_data_portal-3.1.1-py3-none-any.whl/cryoet_data_portal/__init__.py
@@ -0,0 +1,41 @@
+"""
+An API client library to facilitate use of the Cryo-Electron Tomography portal API. The Portal is a collection of high quality tomograms and metadata.
+
+For more information on the API, visit the cryoet-data-portal repo: https://github.com/chanzuckerberg/cryoet-data-portal/
+"""
+
+from ._client import Client
+from ._models import (
+    Annotation,
+    AnnotationAuthor,
+    AnnotationFile,
+    Dataset,
+    DatasetAuthor,
+    DatasetFunding,
+    Deposition,
+    DepositionAuthor,
+    Run,
+    TiltSeries,
+    Tomogram,
+    TomogramAuthor,
+    TomogramVoxelSpacing,
+)
+
+__version__ = "3.1.1"
+
+__all__ = [
+    "Client",
+    "Annotation",
+    "AnnotationFile",
+    "AnnotationAuthor",
+    "Dataset",
+    "DatasetAuthor",
+    "DatasetFunding",
+    "Deposition",
+    "DepositionAuthor",
+    "Run",
+    "TiltSeries",
+    "Tomogram",
+    "TomogramAuthor",
+    "TomogramVoxelSpacing",
+]
