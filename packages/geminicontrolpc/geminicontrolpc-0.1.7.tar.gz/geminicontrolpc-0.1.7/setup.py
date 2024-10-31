2e94601e83c1c18a4ffda0fff7605fee83fed4cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-31 06:06:20 +0000

    Add package geminicontrolpc/0.1.7/geminicontrolpc-0.1.7.tar.gz

---
+++ packages/geminicontrolpc/geminicontrolpc-0.1.7.tar.gz/geminicontrolpc-0.1.7/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='geminicontrolpc',
+    version='0.1.7',
+    author='Trey Leonard',
+    author_email='allanleonardiii@gmail.com',
+    description='A Python package for controlling your PC using Google Gemini AI.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/gigachadtrey/geminicontrolpc',  # Your GitHub URL
+    packages=find_packages(),
+    install_requires=[
+        'google-generativeai>=0.1.0',  # Specify a minimum version
+        'Pillow>=8.0.0',                # Specify a minimum version
+        'opencv-python>=4.0.0',         # Required for image capture if using OpenCV
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+    entry_points={
+        'console_scripts': [
+            'gemctrlpc=geminicontrolpc.geminicontrolpc:main',  # Adjust this to match the main function in geminicontrolpc.py
+        ]
+    },
+)
