cab1d62063eccb83b7aebf2eb99a7d7ff39a5c2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-31 06:05:47 +0000

    Add package blue-options/4.144.1/blue_options-4.144.1.tar.gz

---
+++ packages/blue-options/blue_options-4.144.1.tar.gz/blue_options-4.144.1/blue_options/help/parsing.py
@@ -0,0 +1,36 @@
+import os
+from typing import List
+
+list_of_modules: List[str] = [
+    "abcli",
+    "blue_options",
+    "blue_objects",
+    "blue_geo",
+    "blueflow",
+] + [
+    item
+    for item in os.getenv(
+        "BLUE_OPTIONS_HELP_MODULE_LIST",
+        "",
+    ).split("+")
+    if item
+]
+
+
+def get_callable_module(callable: str) -> str:
+    for module in list_of_modules:
+        if callable.startswith(module):
+            return module
+
+    return callable
+
+
+def get_callable_suffix(callable: str) -> str:
+    module = get_callable_module(callable)
+
+    suffix = callable.split(module)[1]
+
+    if suffix.startswith("_"):
+        suffix = suffix[1:]
+
+    return suffix
