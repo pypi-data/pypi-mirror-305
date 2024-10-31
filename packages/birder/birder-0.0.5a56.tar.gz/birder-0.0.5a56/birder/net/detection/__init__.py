fade611dc5a8732407b21eb34aa106c840f58619
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-31 06:05:53 +0000

    Add package birder/0.0.5a56/birder-0.0.5a56.tar.gz

---
+++ packages/birder/birder-0.0.5a56.tar.gz/birder-0.0.5a56/birder/net/detection/__init__.py
@@ -0,0 +1,13 @@
+from birder.net.detection.efficientdet import EfficientDet
+from birder.net.detection.faster_rcnn import Faster_RCNN
+from birder.net.detection.retinanet import RetinaNet
+from birder.net.detection.ssd import SSD
+from birder.net.detection.vitdet import ViTDet
+
+__all__ = [
+    "EfficientDet",
+    "Faster_RCNN",
+    "RetinaNet",
+    "SSD",
+    "ViTDet",
+]
