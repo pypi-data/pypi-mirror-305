ede2368ffbb1198259ed84b69d546f0ba4364eae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-31 15:05:07 +0000

    Add package hellodddd/2.32.6/hellodddd-2.32.6.tar.gz

---
+++ packages/hellodddd/hellodddd-2.32.6.tar.gz/hellodddd-2.32.6/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='hellodddd',
+    version='2.32.6',
+    description='This is a new library',
+    author='New User',
+    packages=['hellodddd'],
+)
