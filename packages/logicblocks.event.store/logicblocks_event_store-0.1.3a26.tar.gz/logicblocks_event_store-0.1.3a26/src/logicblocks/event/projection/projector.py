1c0e89a3843b84a953f2bad56d277f48bdc6dcc4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-31 04:05:08 +0000

    Add package logicblocks.event.store/0.1.3a26/logicblocks_event_store-0.1.3a26.tar.gz

---
+++ packages/logicblocks.event.store/logicblocks_event_store-0.1.3a26.tar.gz/logicblocks_event_store-0.1.3a26/src/logicblocks/event/projection/projector.py
@@ -0,0 +1,22 @@
+import functools
+
+from typing import Any, Dict, List
+
+from logicblocks.event.types import StoredEvent
+
+
+class Projection:
+    def __init__(self, state: Dict[str, Any]):
+        self.state = state
+
+
+class Projector:
+    def call_handler_func(self, state: Dict[str, Any], event: StoredEvent):
+        handler_function = getattr(self, event.name)
+
+        return handler_function(state, event)
+
+    def project(self, state: Dict[str, Any], events: List[StoredEvent]):
+        return Projection(
+            functools.reduce(self.call_handler_func, events, state)
+        )
