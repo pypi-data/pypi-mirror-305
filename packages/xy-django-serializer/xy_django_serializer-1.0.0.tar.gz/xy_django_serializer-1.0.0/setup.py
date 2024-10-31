2dc931f50c111e1e245596cb0811d051838eeeb6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-31 16:06:36 +0000

    Add package xy-django-serializer/1.0.0/xy_django_serializer-1.0.0.tar.gz

---
+++ packages/xy-django-serializer/xy_django_serializer-1.0.0.tar.gz/xy_django_serializer-1.0.0/setup.py
@@ -0,0 +1,16 @@
+# -*- coding: UTF-8 -*-
+__author__ = "余洋"
+__doc__ = "setup"
+"""
+  * @File    :   setup.py
+  * @Time    :   2024/10/31 09:04:29
+  * @Author  :   余洋
+  * @Version :   0.0.1
+  * @Contact :   yuyangit.0515@qq.com
+  * @License :   (C)Copyright 2019-2024, 希洋 (Ship of Ocean)
+  * @Desc    :   
+"""
+
+from setuptools import setup
+
+setup()
