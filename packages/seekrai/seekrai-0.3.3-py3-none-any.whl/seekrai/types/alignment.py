20d156887437117fadf1ece4b5dfe894c5693952
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-31 07:04:34 +0000

    Add package seekrai/0.3.3/seekrai-0.3.3-py3-none-any.whl

---
+++ packages/seekrai/seekrai-0.3.3-py3-none-any.whl/seekrai/types/alignment.py
@@ -0,0 +1,59 @@
+from __future__ import annotations
+
+from datetime import datetime
+from enum import Enum
+from typing import List, Literal, Optional
+
+from pydantic import Field
+
+from seekrai.types.abstract import BaseModel
+
+
+class AlignmentRequest(BaseModel):
+    instructions: str = Field(
+        default=..., description="Task description/instructions for the alignment task"
+    )
+    files: List[str] = Field(
+        default=..., description="List of file ids to use for alignment"
+    )
+
+
+class AlignmentEstimationRequest(BaseModel):
+    files: List[str] = Field(
+        default=...,
+        description="List of file ids to use to generate an alignment estiamte",
+    )
+
+
+class AlignmentEstimationResponse(BaseModel):
+    input_tokens: int = Field(
+        default=...,
+        description="Estimated number of input tokens for the given files",
+    )
+    output_tokens: int = Field(
+        default=...,
+        description="Estimated number of output tokens for the given files",
+    )
+
+
+class AlignmentJobStatus(str, Enum):
+    STATUS_PENDING = "pending"
+    STATUS_QUEUED = "queued"
+    STATUS_RUNNING = "running"
+    STATUS_CANCEL_REQUESTED = "cancel_requested"
+    STATUS_CANCELLED = "cancelled"
+    STATUS_FAILED = "failed"
+    STATUS_COMPLETED = "completed"
+
+
+class AlignmentResponse(BaseModel):
+    id: Optional[str] = Field(default=..., description="Alignment job ID")
+    created_at: datetime | None = None
+    status: AlignmentJobStatus | None = None
+
+
+class AlignmentList(BaseModel):
+    # object type
+    object: Literal["list"] | None = None
+    # list of fine-tune job objects
+    data: List[AlignmentResponse] | None = None
