53f9b72bff325bb5fcf00b6f883c55d34dee1d4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-31 06:06:19 +0000

    Add package meltingplot.duet-simplyprint-connector/1.1.0/meltingplot.duet_simplyprint_connector-1.1.0-py3-none-any.whl

---
+++ packages/meltingplot.duet-simplyprint-connector/meltingplot.duet_simplyprint_connector-1.1.0-py3-none-any.whl/meltingplot/duet_simplyprint_connector/__main__.py
@@ -0,0 +1,43 @@
+"""Main entry point for the Duet SimplyPrint connector."""
+import logging
+
+from simplyprint_ws_client.cli import ClientCli
+from simplyprint_ws_client.client import ClientApp, ClientMode, ClientOptions
+from simplyprint_ws_client.client.config import ConfigManagerType
+from simplyprint_ws_client.client.logging import ClientHandler
+from simplyprint_ws_client.helpers.url_builder import SimplyPrintBackend
+
+from . import __version__
+from .virtual_client import VirtualClient, VirtualConfig
+
+
+def main():
+    """Initiate the connector as the main entry point."""
+    client_options = ClientOptions(
+        name="DuetConnector",
+        version=__version__,
+        mode=ClientMode.MULTI_PRINTER,
+        client_t=VirtualClient,
+        config_t=VirtualConfig,
+        allow_setup=True,
+        config_manager_type=ConfigManagerType.JSON,
+        backend=SimplyPrintBackend.PRODUCTION,
+    )
+
+    logging.basicConfig(
+        level=logging.INFO,
+        format="[%(asctime)s] %(levelname)s %(name)s.%(funcName)s: %(message)s",
+        handlers=[
+            logging.StreamHandler(),
+            ClientHandler.root_handler(client_options),
+        ],
+    )
+
+    app = ClientApp(client_options)
+    cli = ClientCli(app)
+    cli.start_client = lambda: app.run_blocking()
+    cli(prog_name="python -m meltingplot.duet_simplyprint_connector")
+
+
+if __name__ == "__main__":
+    main()
