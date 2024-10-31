3b418c4af499d4739352e3254570865e3571d9c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-31 14:04:29 +0000

    Add package panpipelines/1.0.7/panpipelines-1.0.7.tar.gz

---
+++ packages/panpipelines/panpipelines-1.0.7.tar.gz/panpipelines-1.0.7/src/panpipelines/scripts/__init__.py
@@ -0,0 +1,11 @@
+__all__ = [
+    "panscript",
+    "fmriprep_panscript",
+    "aslprep_panscript",
+    "pancontainer_panscript",
+    "sdcflows_fieldmap",
+    "mne_make_surfaces",
+    "paninfo_bidsqc",
+    "additional_transforms",
+    "ftp_upload_pan"
+]
