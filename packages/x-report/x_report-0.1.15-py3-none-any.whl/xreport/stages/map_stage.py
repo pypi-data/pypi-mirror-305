7533d9a6ea9d8457979af92b6372b449d77d048e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-31 17:05:24 +0000

    Add package x-report/0.1.15/x_report-0.1.15-py3-none-any.whl

---
+++ packages/x-report/x_report-0.1.15-py3-none-any.whl/xreport/stages/map_stage.py
@@ -0,0 +1,52 @@
+import pandas as pd
+
+from xreport.stages.base_stage import BaseStage
+
+
+class MapStage(BaseStage):
+    def __init__(self, stage_id, name, description, mappings):
+        """
+        :param stage_id: The unique identifier for the stage.
+        :param name: The name of the stage.
+        :param description: A brief description of the stage.
+        :param mappings: A dictionary of column names and their mapping functions.
+        """
+        super().__init__(stage_id, name, description)
+        self.mappings = mappings  # A dictionary of column names and their mapping functions
+
+    def _process_stage(self, input_df):
+        """
+        :param input_df: The input DataFrame containing the data to be processed.
+        :return: A DataFrame where specified columns have been transformed using their associated mapping functions and an additional computation DataFrame with previous column values is concatenated.
+        """
+
+        # Check if the input DataFrame is empty
+        if input_df.empty:
+            # Initialize empty computation and output DataFrames with expected structure
+            self.computation_df = pd.DataFrame(columns=input_df.columns.tolist() + ["#"] +[f"{col} (Prev)" for col in self.mappings.keys()])
+            self.output_df = pd.DataFrame(columns=input_df.columns)
+            return self.output_df
+
+        self.computation_df = pd.DataFrame()
+        df = input_df.copy()
+        # Initialize computation DataFrame
+
+        for column, mapping_func in self.mappings.items():
+            # Create a copy of the original column for the computation DataFrame
+            pre_mapped_column = df[column].copy()
+
+            # Apply the mapping function
+            df[column] = df[column].apply(mapping_func)
+
+            # Concatenate the mapped column and the previous column into computation_df
+            self.computation_df = pd.concat(
+                [ self.computation_df,
+
+                  pd.DataFrame({f"{column} (Prev)": input_df[column]})],
+                axis=1
+            )
+
+        self.computation_df = pd.concat([df, pd.DataFrame({'#': ['#'] * len(df)}),self.computation_df], axis=1)
+
+        self.output_df = df
+        return self.output_df
