f336a987b0405fb228cadac69a32d22eec350d95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-31 16:05:50 +0000

    Add package czsc/0.9.60/czsc-0.9.60-py3-none-any.whl

---
+++ packages/czsc/czsc-0.9.60-py3-none-any.whl/examples/develop/weight_backtest.py
@@ -0,0 +1,15 @@
+# https://s0cqcxuy3p.feishu.cn/wiki/Pf1fw1woQi4iJikbKJmcYToznxb
+import sys
+
+sys.path.insert(0, r"A:\ZB\git_repo\waditu\czsc")
+import czsc
+import pandas as pd
+
+czsc.welcome()
+
+
+def test_ensemble_weight():
+    """从持仓权重样例数据中回测"""
+    dfw = pd.read_feather(r"C:\Users\zengb\Downloads\weight_example.feather")
+    wb = czsc.WeightBacktest(dfw, digits=1, fee_rate=0.0002, res_path=r"C:\Users\zengb\Desktop\weight_example")
+    # res = wb.backtest()
