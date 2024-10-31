a5dd109a1782e91af99a52989a3f5b0458f3f475
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-31 05:06:28 +0000

    Add package gHangman/1.1.1/gHangman-1.1.1.tar.gz

---
+++ packages/gHangman/gHangman-1.1.1.tar.gz/gHangman-1.1.1/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+setup(
+    name='gHangman',
+    version='1.1.1',
+    py_modules=['start', 'data', 'efx', 'game', 'sides'],
+    install_requires=[],
+    data_files=[('', ['Words.dat'])],
+    author='a-good-omen',
+    author_email='student.rohn@gmail.com',
+    description='A classical word guessing game.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/a-good-omen/hangman-remastered',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.7',
+    entry_points={
+        'console_scripts': [
+            'hangman=start:main',
+        ],
+    },
+    include_package_data=True,
+    package_data={
+        '': ['Words.dat'],
+    },
+)
