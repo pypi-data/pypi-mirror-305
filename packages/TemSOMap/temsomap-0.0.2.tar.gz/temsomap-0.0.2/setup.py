13b43e4269b7f4cf883b89f9bcf30b7db7a8b869
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-31 06:06:19 +0000

    Add package TemSOMap/0.0.2/temsomap-0.0.2.tar.gz

---
+++ packages/TemSOMap/temsomap-0.0.2.tar.gz/temsomap-0.0.2/setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+d = {}
+with open("src/_version_.py") as f:
+    exec(f.read(), d)
+
+setuptools.setup(
+    name="TemSOMap",
+    version=d["__version__"],
+    author="Xinhai Pan",
+    author_email="xpan78@gatech.edu",
+    description="Temporal-Spatial-Omics Mapping of single cells.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="",
+    packages=setuptools.find_namespace_packages(),
+    classifiers=["Programming Language :: Python :: 3.11", "Operating System :: Unix",],
+    python_requires=">=3.8.5",
+    install_requires=[
+        "pip",
+        "torch",
+        "pandas",
+        "numpy",
+        "scipy",
+        "matplotlib",
+        "seaborn",
+        "scanpy",
+        "scikit-learn",
+        "tqdm",
+    ],
+)
