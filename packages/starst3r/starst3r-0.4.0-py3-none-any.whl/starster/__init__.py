335a957d9ea7aff7b40419be57195e52eb402fdb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-31 04:05:28 +0000

    Add package starst3r/0.4.0/starst3r-0.4.0-py3-none-any.whl

---
+++ packages/starst3r/starst3r-0.4.0-py3-none-any.whl/starster/__init__.py
@@ -0,0 +1,9 @@
+__version__ = "0.4.0"
+
+from mast3r.model import AsymmetricMASt3R as Mast3rModel
+
+from . import gs
+from .image import *
+from .reconstruct import *
+from .scene import *
+from .utils import *
