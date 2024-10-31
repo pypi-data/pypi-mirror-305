e70330d5396cff2d6b3028b92fb12bc48b8b91a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-31 03:10:55 +0000

    Add package slumbernana-monkey/0.1.0/slumbernana_monkey-0.1.0-py3-none-any.whl

---
+++ packages/slumbernana-monkey/slumbernana_monkey-0.1.0-py3-none-any.whl/Slumbernana-Monkey/Slumbernana.py
@@ -0,0 +1,17 @@
+text="""
+蕉蕉🍌🍌長滿香蕉的小島🍌🍌
+蕉蕉🍌🍌睡蕉小猴的愛巢🍌🍌
+蕉蕉🍌🍌吃完香蕉就睡覺🍌🍌
+蕉蕉🍌🍌快快樂樂沒煩惱🍌🍌
+蕉蕉🍌🍌睡蕉小猴真是好🍌🍌
+
+蕉蕉🍌🍌幸福美滿沒煩惱🍌🍌
+"""
+
+print(text)
+
+import webbrowser
+
+url='https://www.youtube.com/watch?v=k5tM80krHqs'
+
+webbrowser.open(url)
