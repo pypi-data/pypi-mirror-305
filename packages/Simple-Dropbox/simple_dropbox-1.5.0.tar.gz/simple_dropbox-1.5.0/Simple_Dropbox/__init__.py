fab101606fb32ebc46ff84eef1adda06bf45a9c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-31 07:05:31 +0000

    Add package Simple-Dropbox/1.5.0/simple_dropbox-1.5.0.tar.gz

---
+++ packages/Simple-Dropbox/simple_dropbox-1.5.0.tar.gz/simple_dropbox-1.5.0/Simple_Dropbox/__init__.py
@@ -0,0 +1,6 @@
+# __init__.py
+"""
+Simple_Dropbox est un module qui fournit un accès simplifié à Dropbox.
+Il contient des fonctions pour interagir avec l'API de Dropbox.
+"""
+from .Script import *
