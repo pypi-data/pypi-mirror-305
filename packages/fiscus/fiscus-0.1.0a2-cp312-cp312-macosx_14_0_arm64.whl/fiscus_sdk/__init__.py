5c0ee58d99655cc45ce3700cea04de57022e8cd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-31 16:06:17 +0000

    Add package fiscus/0.1.0a2/fiscus-0.1.0a2-cp312-cp312-macosx_14_0_arm64.whl

---
+++ packages/fiscus/fiscus-0.1.0a2-cp312-cp312-macosx_14_0_arm64.whl/fiscus_sdk/__init__.py
@@ -0,0 +1,91 @@
+# fiscus_sdk/__init__.py
+
+"""
+Fiscus SDK - API Gateway for the AI World
+"""
+
+__version__ = '0.1.0-alpha.2'
+__version_info__ = (0, 1, 0, 'alpha', 2)
+__author__ = "Fiscus Flows, Inc."
+__license__ = "Proprietary"
+__description__ = "Fiscus is a powerful platform designed to be the API Gateway for the AI World. It enables developers to integrate AI agents and language models with any API or service, without the need for manual mapping or code adjustments"
+
+# Explicit relative imports for better internal structure and control
+from fiscus_sdk.client import FiscusClient
+from fiscus_sdk.user import FiscusUser
+from fiscus_sdk.connector import FiscusConnector
+from fiscus_sdk.response import FiscusResponse
+from fiscus_sdk.audit import FiscusAuditTrail
+from fiscus_sdk.exceptions import (
+    FiscusError,
+    FiscusAuthenticationError,
+    FiscusAuthorizationError,
+    FiscusValidationError,
+)
+from fiscus_sdk.callbacks import (
+	FiscusCallback,
+	FiscusOnSuccess,
+	FiscusOnError,
+	FiscusOnAuth,
+	FiscusOnLog,
+	FiscusOnStream,
+	FiscusOnResponse,
+	FiscusAIFetchCategories,
+	FiscusAICategorySelection,
+	FiscusAIFetchConnectors,
+	FiscusAIConnectorSelection,
+	FiscusAIFetchOperations,
+	FiscusAIOperationSelection,
+	FiscusAIFetchOperationDetails,
+	FiscusAITaskCreation,
+	FiscusAIComplete
+)
+from fiscus_sdk.enums import (
+	FiscusResponseType,
+	FiscusConnectionType,
+	FiscusExecutionType,
+	FiscusInitType,
+	FiscusLogLevel,
+	FiscusLLMType,
+	FiscusMemoryRetrievalType,
+	FiscusMemoryStorageType,
+	FiscusCallbackType
+)
+
+# Only expose essential classes and functions in __all__
+__all__ = [
+    'FiscusClient',
+    'FiscusUser',
+    'FiscusConnector',
+    'FiscusResponse',
+    'FiscusError',
+    'FiscusAuthenticationError',
+    'FiscusAuthorizationError',
+    'FiscusValidationError',
+    'FiscusCallback',
+    'FiscusOnSuccess',
+	'FiscusOnError',
+	'FiscusOnAuth',
+	'FiscusOnLog',
+	'FiscusOnStream',
+	'FiscusOnResponse',
+	'FiscusAIFetchCategories',
+	'FiscusAICategorySelection',
+	'FiscusAIFetchConnectors',
+	'FiscusAIConnectorSelection',
+	'FiscusAIFetchOperations',
+	'FiscusAIOperationSelection',
+	'FiscusAIFetchOperationDetails',
+	'FiscusAITaskCreation',
+	'FiscusAIComplete',
+    'FiscusAuditTrail',
+    'FiscusResponseType',
+    'FiscusConnectionType',
+	'FiscusExecutionType',
+    'FiscusInitType',
+    'FiscusLogLevel',
+	'FiscusLLMType',
+	'FiscusMemoryRetrievalType',
+	'FiscusMemoryStorageType',
+	'FiscusCallbackType'
+]
