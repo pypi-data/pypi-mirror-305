c463a823adb22ed7817f830e2e2d3c16c37a8061
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-31 03:11:02 +0000

    Add package django-hstore-widget/0.0.10/django_hstore_widget-0.0.10-py3-none-any.whl

---
+++ packages/django-hstore-widget/django_hstore_widget-0.0.10-py3-none-any.whl/django_hstore_widget/widgets.py
@@ -0,0 +1,35 @@
+try:
+    from django.contrib.admin.templatetags.admin_static import static
+except ImportError:
+    from django.templatetags.static import static
+from django.contrib.admin.widgets import AdminTextareaWidget
+from django.contrib.postgres.forms import forms
+from django.template.loader import get_template
+from django.utils.safestring import mark_safe
+
+
+class HStoreFormWidget(AdminTextareaWidget):
+    def render(self, name, value, attrs=None, renderer=None):
+        if attrs is None:
+            attrs = {}
+            # it's called "original" because it will be replaced by a copy
+
+        # get default HTML from AdminTextareaWidget
+        html = super().render(name, value, attrs)
+
+        # prepare template context
+        template_context = {
+            "field_name": name,
+            "field_data": value,
+        }
+
+        # get template object
+        template = get_template("django_hstore_widget.html")
+        # render additional html
+        additional_html = template.render(template_context)
+
+        # append additional HTML and mark as safe
+        html = additional_html
+        html = mark_safe(html)
+
+        return html
