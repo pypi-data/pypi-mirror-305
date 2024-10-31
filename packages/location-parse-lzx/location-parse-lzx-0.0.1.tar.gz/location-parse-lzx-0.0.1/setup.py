5ee0ec8768bbe19ffac8914cd332ccb3cb42a390
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-31 17:06:08 +0000

    Add package location-parse-lzx/0.0.1/location-parse-lzx-0.0.1.tar.gz

---
+++ packages/location-parse-lzx/location-parse-lzx-0.0.1.tar.gz/location-parse-lzx-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='location-parse-lzx',
+    version='0.0.1',
+    packages=find_packages(),
+    install_requires=[],
+    author='lzx',
+    author_email='2739638173@qq.com',
+    description='处理地区字符串，将其解析拆分为省、市、县',
+    url='https://github.com/dongrixinyu/JioNLP'
+)
