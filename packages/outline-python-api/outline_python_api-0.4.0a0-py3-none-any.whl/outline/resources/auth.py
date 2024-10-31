e1a8ecde08bebde0c06ba982838a799475a21129
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-31 05:05:51 +0000

    Add package outline-python-api/0.4.0a0/outline_python_api-0.4.0a0-py3-none-any.whl

---
+++ packages/outline-python-api/outline_python_api-0.4.0a0-py3-none-any.whl/outline/resources/auth.py
@@ -0,0 +1,186 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from __future__ import annotations
+
+import httpx
+
+from .._types import NOT_GIVEN, Body, Query, Headers, NotGiven
+from .._compat import cached_property
+from .._resource import SyncAPIResource, AsyncAPIResource
+from .._response import (
+    to_raw_response_wrapper,
+    to_streamed_response_wrapper,
+    async_to_raw_response_wrapper,
+    async_to_streamed_response_wrapper,
+)
+from .._base_client import make_request_options
+from ..types.auth_info_response import AuthInfoResponse
+from ..types.auth_config_response import AuthConfigResponse
+
+__all__ = ["AuthResource", "AsyncAuthResource"]
+
+
+class AuthResource(SyncAPIResource):
+    @cached_property
+    def with_raw_response(self) -> AuthResourceWithRawResponse:
+        """
+        This property can be used as a prefix for any HTTP method call to return the
+        the raw response object instead of the parsed content.
+
+        For more information, see https://www.github.com/Yoshino-s/outline-python-api#accessing-raw-response-data-eg-headers
+        """
+        return AuthResourceWithRawResponse(self)
+
+    @cached_property
+    def with_streaming_response(self) -> AuthResourceWithStreamingResponse:
+        """
+        An alternative to `.with_raw_response` that doesn't eagerly read the response body.
+
+        For more information, see https://www.github.com/Yoshino-s/outline-python-api#with_streaming_response
+        """
+        return AuthResourceWithStreamingResponse(self)
+
+    def config(
+        self,
+        *,
+        # Use the following arguments if you need to pass additional parameters to the API that aren't available via kwargs.
+        # The extra values given here take precedence over values defined on the client or passed to this method.
+        extra_headers: Headers | None = None,
+        extra_query: Query | None = None,
+        extra_body: Body | None = None,
+        timeout: float | httpx.Timeout | None | NotGiven = NOT_GIVEN,
+    ) -> AuthConfigResponse:
+        """Retrieve authentication options"""
+        return self._post(
+            "/auth.config",
+            options=make_request_options(
+                extra_headers=extra_headers, extra_query=extra_query, extra_body=extra_body, timeout=timeout
+            ),
+            cast_to=AuthConfigResponse,
+        )
+
+    def info(
+        self,
+        *,
+        # Use the following arguments if you need to pass additional parameters to the API that aren't available via kwargs.
+        # The extra values given here take precedence over values defined on the client or passed to this method.
+        extra_headers: Headers | None = None,
+        extra_query: Query | None = None,
+        extra_body: Body | None = None,
+        timeout: float | httpx.Timeout | None | NotGiven = NOT_GIVEN,
+    ) -> AuthInfoResponse:
+        """Retrieve authentication details for the current API key"""
+        return self._post(
+            "/auth.info",
+            options=make_request_options(
+                extra_headers=extra_headers, extra_query=extra_query, extra_body=extra_body, timeout=timeout
+            ),
+            cast_to=AuthInfoResponse,
+        )
+
+
+class AsyncAuthResource(AsyncAPIResource):
+    @cached_property
+    def with_raw_response(self) -> AsyncAuthResourceWithRawResponse:
+        """
+        This property can be used as a prefix for any HTTP method call to return the
+        the raw response object instead of the parsed content.
+
+        For more information, see https://www.github.com/Yoshino-s/outline-python-api#accessing-raw-response-data-eg-headers
+        """
+        return AsyncAuthResourceWithRawResponse(self)
+
+    @cached_property
+    def with_streaming_response(self) -> AsyncAuthResourceWithStreamingResponse:
+        """
+        An alternative to `.with_raw_response` that doesn't eagerly read the response body.
+
+        For more information, see https://www.github.com/Yoshino-s/outline-python-api#with_streaming_response
+        """
+        return AsyncAuthResourceWithStreamingResponse(self)
+
+    async def config(
+        self,
+        *,
+        # Use the following arguments if you need to pass additional parameters to the API that aren't available via kwargs.
+        # The extra values given here take precedence over values defined on the client or passed to this method.
+        extra_headers: Headers | None = None,
+        extra_query: Query | None = None,
+        extra_body: Body | None = None,
+        timeout: float | httpx.Timeout | None | NotGiven = NOT_GIVEN,
+    ) -> AuthConfigResponse:
+        """Retrieve authentication options"""
+        return await self._post(
+            "/auth.config",
+            options=make_request_options(
+                extra_headers=extra_headers, extra_query=extra_query, extra_body=extra_body, timeout=timeout
+            ),
+            cast_to=AuthConfigResponse,
+        )
+
+    async def info(
+        self,
+        *,
+        # Use the following arguments if you need to pass additional parameters to the API that aren't available via kwargs.
+        # The extra values given here take precedence over values defined on the client or passed to this method.
+        extra_headers: Headers | None = None,
+        extra_query: Query | None = None,
+        extra_body: Body | None = None,
+        timeout: float | httpx.Timeout | None | NotGiven = NOT_GIVEN,
+    ) -> AuthInfoResponse:
+        """Retrieve authentication details for the current API key"""
+        return await self._post(
+            "/auth.info",
+            options=make_request_options(
+                extra_headers=extra_headers, extra_query=extra_query, extra_body=extra_body, timeout=timeout
+            ),
+            cast_to=AuthInfoResponse,
+        )
+
+
+class AuthResourceWithRawResponse:
+    def __init__(self, auth: AuthResource) -> None:
+        self._auth = auth
+
+        self.config = to_raw_response_wrapper(
+            auth.config,
+        )
+        self.info = to_raw_response_wrapper(
+            auth.info,
+        )
+
+
+class AsyncAuthResourceWithRawResponse:
+    def __init__(self, auth: AsyncAuthResource) -> None:
+        self._auth = auth
+
+        self.config = async_to_raw_response_wrapper(
+            auth.config,
+        )
+        self.info = async_to_raw_response_wrapper(
+            auth.info,
+        )
+
+
+class AuthResourceWithStreamingResponse:
+    def __init__(self, auth: AuthResource) -> None:
+        self._auth = auth
+
+        self.config = to_streamed_response_wrapper(
+            auth.config,
+        )
+        self.info = to_streamed_response_wrapper(
+            auth.info,
+        )
+
+
+class AsyncAuthResourceWithStreamingResponse:
+    def __init__(self, auth: AsyncAuthResource) -> None:
+        self._auth = auth
+
+        self.config = async_to_streamed_response_wrapper(
+            auth.config,
+        )
+        self.info = async_to_streamed_response_wrapper(
+            auth.info,
+        )
