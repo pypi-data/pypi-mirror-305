8293916c7dd2313bf7a41f667a43d0f7191a97b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-31 14:04:42 +0000

    Add package sbeditor/0.0.0b6/sbeditor-0.0.0b6.tar.gz

---
+++ packages/sbeditor/sbeditor-0.0.0b6.tar.gz/sbeditor-0.0.0b6/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+import subprocess
+
+# Copied from https://www.youtube.com/watch?v=U-aIPTS580s
+# remote_version = subprocess.run(['git', 'describe', '--tags'], stdout=subprocess.PIPE).stdout.decode('utf-8').strip()
+# assert '.' in remote_version
+
+with open('README.md', 'r', encoding="utf-8") as readme:
+    long_description = readme.read()
+
+# print(remote_version)
+
+setup(
+    name='sbeditor',
+    version="v0.0.0-beta.6",
+    packages=['sbeditor'],
+    url='https://github.com/FAReTek1/sbeditor',
+    license='MIT',
+    author='faretek1',
+    author_email='',
+    description='A parser for all things sb3',
+    long_description=long_description
+)
