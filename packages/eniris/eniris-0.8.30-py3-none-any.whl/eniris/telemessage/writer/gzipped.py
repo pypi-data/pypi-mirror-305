8b21596bb73ff6e8f0e50fba4fce30727f35c101
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-31 17:06:13 +0000

    Add package eniris/0.8.30/eniris-0.8.30-py3-none-any.whl

---
+++ packages/eniris/eniris-0.8.30-py3-none-any.whl/eniris/telemessage/writer/gzipped.py
@@ -0,0 +1,32 @@
+from eniris.telemessage.writer.writer import TelemessageWriter
+from eniris.telemessage import Telemessage
+
+import gzip
+
+
+class GZipTelemessageWriter:
+    """TelemessageWriter that compresses Telemessages using the gzip algorithm, and then passes them on to another TelemessageWriter"""
+
+    def __init__(self, output: TelemessageWriter, compresslevel: int = 9):
+        """Optional argument is the compression level, in range of 0-9.
+
+        Args:
+            output (TelemessageWriter): _description_
+        """
+        self.output = output
+        self.compresslevel = compresslevel
+
+    def writeTelemessage(self, message: Telemessage):
+        gzippedData = gzip.compress(message.data, compresslevel=self.compresslevel)
+        ammendedHeaders = {**message.headers, "Content-Encoding": "gzip"}
+        gzippedMessage = Telemessage(
+            message.parameters, gzippedData, headers=ammendedHeaders
+        )
+
+        self.output.writeTelemessage(gzippedMessage)
+        
+    def close(self):
+        self.output.close()
+
+    def flush(self):
+        self.output.flush()
