d04240b812698d3f41d373f8700f5411956eaaef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-31 14:04:49 +0000

    Add package blueflow/4.775.1/blueflow-4.775.1-py3-none-any.whl

---
+++ packages/blueflow/blueflow-4.775.1-py3-none-any.whl/blueflow/.abcli/workflow.sh
@@ -0,0 +1,23 @@
+#! /usr/bin/env bash
+
+function notebooks_and_scripts_workflow() {
+    local task=$(abcli_unpack_keyword $1)
+
+    if [ "$task" == "help" ]; then
+        notebooks_and_scripts_workflow_create "$@"
+        notebooks_and_scripts_workflow_monitor "$@"
+        notebooks_and_scripts_workflow_submit "$@"
+        return
+    fi
+
+    local function_name=notebooks_and_scripts_workflow_$task
+    if [[ $(type -t $function_name) == "function" ]]; then
+        $function_name "${@:2}"
+        return
+    fi
+
+    abcli_log_error "-workflow: $task: command not found."
+    return 1
+}
+
+abcli_source_caller_suffix_path /workflow
