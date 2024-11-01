2972b07d70f2c334a637d41f9b466886abc4f1d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-01 06:06:22 +0000

    Add package wxbtool/0.1.4/wxbtool-0.1.4.tar.gz

---
+++ packages/wxbtool/wxbtool-0.1.4.tar.gz/wxbtool-0.1.4/tests/test_dataset.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+
+import unittest
+import os
+import sys
+import pathlib
+import unittest.mock as mock
+
+from unittest.mock import patch
+
+
+class TestTest(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    @mock.patch.dict(
+        os.environ, {"WXBHOME": str(pathlib.Path(__file__).parent.absolute())}
+    )
+    def test_dataset(self):
+        import wxbtool.wxb as wxb
+
+        testargs = [
+            "wxb",
+            "dserve",
+            "-m",
+            "models.model",
+            "-s",
+            "Setting3d",
+            "-t",
+            "true",
+        ]
+        with patch.object(sys, "argv", testargs):
+            wxb.main()
+
+        testargs = ["wxb", "test", "-m", "models.model", "-b", "10", "-t", "true"]
+        with patch.object(sys, "argv", testargs):
+            wxb.main()
