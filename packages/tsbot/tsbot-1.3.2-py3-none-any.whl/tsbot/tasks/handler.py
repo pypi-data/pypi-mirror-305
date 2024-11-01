fded0fff9176c1bfc339b635165f3f79f3ad5aac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-01 14:05:34 +0000

    Add package tsbot/1.3.2/tsbot-1.3.2-py3-none-any.whl

---
+++ packages/tsbot/tsbot-1.3.2-py3-none-any.whl/tsbot/tasks/handler.py
@@ -0,0 +1,50 @@
+from __future__ import annotations
+
+import asyncio
+from typing import TYPE_CHECKING
+
+from tsbot import logging
+
+if TYPE_CHECKING:
+    from tsbot import bot, tasks
+
+
+logger = logging.get_logger(__name__)
+
+
+class TasksHandler:
+    def __init__(self) -> None:
+        self._started = False
+        self._tasks: set[asyncio.Task[None]] = set()
+        self._starting_tasks: list[tasks.TSTask] = []
+
+    def _start_task(self, bot: bot.TSBot, task: tasks.TSTask) -> None:
+        task.task = asyncio.create_task(task.handler(bot), name=task.name)
+        self._tasks.add(task.task)
+        task.task.add_done_callback(self._tasks.remove)
+        logger.debug("Started a task handler %r", getattr(task.handler, "__name__", task.handler))
+
+    def register_task(self, bot: bot.TSBot, task: tasks.TSTask) -> None:
+        self._start_task(bot, task) if self._started else self._starting_tasks.append(task)
+
+    def remove_task(self, task: tasks.TSTask) -> None:
+        if task.task and not task.task.done():
+            task.task.cancel()
+
+    def start(self, bot: bot.TSBot) -> None:
+        while self._starting_tasks:
+            self._start_task(bot, self._starting_tasks.pop())
+
+        self._started = True
+        logger.debug("Task handler started")
+
+    async def close(self) -> None:
+        self._started = False
+        self._starting_tasks.clear()
+
+        for task in self._tasks:
+            task.cancel()
+
+        # Sleep until all the tasks are removed from tasks list
+        while self._tasks:
+            await asyncio.sleep(0)
