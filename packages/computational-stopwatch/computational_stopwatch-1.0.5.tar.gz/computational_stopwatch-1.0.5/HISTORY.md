b456f1e8d3ab83204e4837fbb6b60630435c6995
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-01 06:05:14 +0000

    Add package computational-stopwatch/1.0.5/computational_stopwatch-1.0.5.tar.gz

---
+++ packages/computational-stopwatch/computational_stopwatch-1.0.5.tar.gz/computational_stopwatch-1.0.5/HISTORY.md
@@ -0,0 +1,26 @@
+# Versions History
+
+## v1.0.5
+
+- minor fix
+
+## v1.0.4
+
+- added `HISTORY.md` in the pip package for compatibility with the conda packaging
+
+## v1.0.3
+
+- added ```__str__ ``` function to easily include just the time stamp inside other strings
+
+## v1.0.2
+
+- updated the README
+- added multiple streams functionality
+
+## v1.0.1
+
+- minor fixes
+
+## v1.0.0
+
+- first reelease
---
+++ packages/computational-stopwatch/computational_stopwatch-1.0.5.tar.gz/computational_stopwatch-1.0.5/setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup
+
+VERSION = '1.0.5'
+
+with open('README.md') as readme_file:
+    README = readme_file.read()
+
+with open('HISTORY.md') as history_file:
+    HISTORY = history_file.read()
+
+setup_args = dict(
+    name='computational_stopwatch',
+    version=VERSION,
+    description='Simple stopwatch to easily print the elapsed time of a set of operations',
+    long_description_content_type="text/markdown",
+    long_description=README + '\n\n' + HISTORY,
+    license='GNUv3',
+    packages=['computational_stopwatch'],
+    include_package_data=False,
+    author='Luca Baronti',
+    author_email='lbaronti@gmail.com',
+    keywords=['computation', 'time', 'elapsed time'],
+    url='https://gitlab.com/luca.baronti/computational-stopwatch',
+    download_url='https://pypi.org/project/computational_stopwatch/',
+		classifiers=[
+			# How mature is this project? Common values are
+			'Development Status :: 5 - Production/Stable',
+			# Indicate who your project is intended for
+			'Intended Audience :: Developers',
+            'Topic :: Software Development',
+			# Pick your license as you wish (should match "license" above)
+			'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+			# Specify all Python versions you support here.
+			'Programming Language :: Python :: 3',
+		]
+)
+
+install_requires = [ ]
+
+if __name__ == '__main__':
+    setup(**setup_args, install_requires=install_requires)
