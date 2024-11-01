3bb0b68f4740f82f2c6eef31fcfae5ddd0f4fc03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-01 03:17:16 +0000

    Add package interactflow/1.0.0/interactflow-1.0.0.tar.gz

---
+++ packages/interactflow/interactflow-1.0.0.tar.gz/interactflow-1.0.0/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+requirements = ["pynput>=1.7.6", "python-xlib>=0.33"]
+
+setup(
+    name="interactflow",
+    version="1.0.0",
+    author="InteractFlow Team",
+    author_email="yashchouriya131+github@gmail.com",
+    description="A powerful tool for recording and replaying user interactions",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/yashChouriya/interactflow",
+    packages=find_packages(),
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Testing",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: System :: Monitoring",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=requirements,
+    entry_points={
+        "console_scripts": [
+            "interactflow=src.main:main",
+        ],
+    },
+)
