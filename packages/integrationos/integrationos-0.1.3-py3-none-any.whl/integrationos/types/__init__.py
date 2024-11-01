601ed67c82cc79ebece34637666e61d4f0a5eb7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-01 07:05:20 +0000

    Add package integrationos/0.1.3/integrationos-0.1.3-py3-none-any.whl

---
+++ packages/integrationos/integrationos-0.1.3-py3-none-any.whl/integrationos/types/__init__.py
@@ -0,0 +1,13 @@
+from .generic import *
+
+__all__ = [
+    'UnifiedOptions',
+    'ListFilter',
+    'Pagination',
+    'DeleteOptions',
+    'Count',
+    'ListResponse',
+    'Response',
+    'ResponseError',
+    'HttpStatusCode',
+]
