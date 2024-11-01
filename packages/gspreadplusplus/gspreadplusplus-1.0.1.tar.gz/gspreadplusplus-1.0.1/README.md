2556d08eb22c87ced80ee156d19774d80b7c49e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-01 07:05:56 +0000

    Add package gspreadplusplus/1.0.1/gspreadplusplus-1.0.1.tar.gz

---
+++ packages/gspreadplusplus/gspreadplusplus-1.0.1.tar.gz/gspreadplusplus-1.0.1/README.md
@@ -0,0 +1,11 @@
+# gspreadplusplus
+
+A Python library for utilities related to gspread.
+
+## Installation
+
+You can install the package using pip:
+
+```bash
+pip install gspreadplusplus
+
---
+++ packages/gspreadplusplus/gspreadplusplus-1.0.1.tar.gz/gspreadplusplus-1.0.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='gspreadplusplus',
+    version='1.0.1',
+    author='Daniel Simanek',
+    author_email='daniel.simanek@decathlon.com',
+    description='Advanced functions using the gspread library, used for making the code less bloated.',
+    long_description=open('README.md').read(),  # Long description from the README file
+    long_description_content_type='text/markdown',
+    url='https://github.com/daniel-simanek/gspreadplusplus',
+    packages=find_packages(),
+    py_modules=['gspreadplusplus'],
+    install_requires=[
+        'gspread',
+        'oauth2client',
+        'pandas',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
