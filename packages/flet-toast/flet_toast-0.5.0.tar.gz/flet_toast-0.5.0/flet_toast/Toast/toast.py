55b3b3e2862470f46f74ad0be3d0c61c8b5714bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-01 06:06:19 +0000

    Add package flet-toast/0.5.0/flet_toast-0.5.0.tar.gz

---
+++ packages/flet-toast/flet_toast-0.5.0.tar.gz/flet_toast-0.5.0/flet_toast/Toast/toast.py
@@ -0,0 +1,75 @@
+from ..Controls.controls import Toast
+from ..Controls.controls import ft
+from ..Types.types import Position
+
+class FletToast:
+    
+    """
+    Crie toasts personalizados usando a biblioteca flet do python.
+    
+    ---
+    Consulte a documentação pelo site: https://pypi.org/project/flet-toast/
+    """
+
+    @property
+    def Position(self) -> Position:
+        return Position
+
+    def sucess(
+        self,
+        page: ft.Page,
+        message: str,
+        position: Position,
+        duration: int = 3
+    ) -> Toast:
+        
+        toast = Toast(
+            page=page,
+            color=ft.colors.BLUE,
+            text=message,
+            icon=ft.icons.CHECK_CIRCLE_OUTLINED,
+            position=position,
+            duration=duration
+        )
+
+        return toast
+    
+    def warning(
+        self,
+        page: ft.Page,
+        message: str,
+        position: Position,
+        duration: int = 3
+    ) -> Toast:
+        
+        toast = Toast(
+            page=page,
+            color=ft.colors.PURPLE,
+            text=message,
+            icon=ft.icons.WARNING_ROUNDED,
+            position=position,
+            duration=duration
+        )
+
+        return toast
+    
+    def error(
+        self,
+        page: ft.Page,
+        message: str,
+        position: Position,
+        duration: int = 3
+    ) -> Toast:
+        
+        toast = Toast(
+            page=page,
+            color=ft.colors.RED,
+            text=message,
+            icon=ft.icons.ERROR,
+            position=position,
+            duration=duration
+        )
+
+        return toast
+
+flet_toast = FletToast()
