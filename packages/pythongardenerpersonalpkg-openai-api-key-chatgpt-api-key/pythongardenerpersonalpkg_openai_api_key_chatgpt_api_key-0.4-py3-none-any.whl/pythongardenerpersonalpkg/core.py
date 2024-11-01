58ec1938c83c8dd8799447cb7a53e9210f51318d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-01 03:18:13 +0000

    Add package pythongardenerpersonalpkg-openai-api-key-chatgpt-api-key/0.4/pythongardenerpersonalpkg_openai_api_key_chatgpt_api_key-0.4-py3-none-any.whl

---
+++ packages/pythongardenerpersonalpkg-openai-api-key-chatgpt-api-key/pythongardenerpersonalpkg_openai_api_key_chatgpt_api_key-0.4-py3-none-any.whl/pythongardenerpersonalpkg/core.py
@@ -0,0 +1,11 @@
+def say_hello(name):
+    github_pat = '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'
+    aws_access_key = 'AKIAYZZGSY75LDOLKSUF'
+    aws_secret_key = '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'
+    # https://trial-4038288-admin.okta.com/
+    okta_api_key = '00El6PC_2eCRcexhNjAsD_x6zaL8ZZ47FFpkrySYgm'
+    OPENAI_API_KEY = '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'
+    OPENAI_API = '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'
+    OPENAI_API_TOKEN = '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'
+    OPENAI_TOKEN = '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'
+    return f"Hello, {name}!"
