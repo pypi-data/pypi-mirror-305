391d97d2f92c7d40fce8b4ac9c30b9503871ea8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-01 15:05:23 +0000

    Add package aertjhk/1.0/aertjhk-1.0.tar.gz

---
+++ packages/aertjhk/aertjhk-1.0.tar.gz/aertjhk-1.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="aertjhk",
+    version="1.0",
+    packages=find_packages(),
+    description="",
+    author="yashksaini",
+    author_email="ys3853428@gmail.com",
+    license="GPL 3.0",
+    install_requires=[],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
