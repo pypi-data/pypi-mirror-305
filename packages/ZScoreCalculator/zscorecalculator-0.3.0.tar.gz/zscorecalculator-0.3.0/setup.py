2e7daefbead3f59e1f3ec2e1b5947a096f786d67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-01 08:06:04 +0000

    Add package ZScoreCalculator/0.3.0/zscorecalculator-0.3.0.tar.gz

---
+++ packages/ZScoreCalculator/zscorecalculator-0.3.0.tar.gz/zscorecalculator-0.3.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ZScoreCalculator',
+    version='0.3.0',
+    author='Lillian Tatka',
+    description="""A package for calculating peptide and protein z scores.""",
+    packages=find_packages(), 
+    install_requires=[ 
+        'pandas',
+        'numpy',
+        'tqdm',
+        'matplotlib',
+        'seaborn'
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',  
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+
+)
