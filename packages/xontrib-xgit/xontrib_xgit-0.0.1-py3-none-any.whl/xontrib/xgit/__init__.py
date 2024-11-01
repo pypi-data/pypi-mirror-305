2e45a61a80dcba35e0cfb2a2c66866e68880403b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-01 15:05:37 +0000

    Add package xontrib-xgit/0.0.1/xontrib_xgit-0.0.1-py3-none-any.whl

---
+++ packages/xontrib-xgit/xontrib_xgit-0.0.1-py3-none-any.whl/xontrib/xgit/__init__.py
@@ -0,0 +1,33 @@
+"""
+Package doc
+"""
+
+from xontrib.xgit.xgit_main import (
+    _load_xontrib_,
+    _unload_xontrib_,
+    GitId,
+    GitObject,
+    GitBlob,
+    GitTree,
+    GitRepository,
+    GitWorktree,
+    GitContext,
+    GitEntryMode,
+    GitObjectType,
+    GitTreeEntry,
+)
+
+__all__ = (
+    "_load_xontrib_",
+    "_unload_xontrib_",
+    "GitId",
+    "GitObject",
+    "GitBlob",
+    "GitTree",
+    "GitRepository",
+    "GitWorktree",
+    "GitContext",
+    "GitEntryMode",
+    "GitObjectType",
+    "GitTreeEntry",
+)
