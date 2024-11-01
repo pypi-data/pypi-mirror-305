ea46c6a2a6a49f2bed8cd61dc9acb6e4f9d2a819
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-01 14:06:08 +0000

    Add package vptq/0.0.3/vptq-0.0.3-cp38-cp38-manylinux1_x86_64.whl

---
+++ packages/vptq/vptq-0.0.3-cp38-cp38-manylinux1_x86_64.whl/vptq/layers/__init__.py
@@ -0,0 +1,6 @@
+# -------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+# --------------------------------------------------------------------------
+
+from vptq.layers.model_base import AutoModelForCausalLM as AutoModelForCausalLM
