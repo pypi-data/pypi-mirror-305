b66ac8a4022815eafca1ce373ff7746186160862
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-01 06:06:16 +0000

    Add package openlayer/0.2.0a32/openlayer-0.2.0a32.tar.gz

---
+++ packages/openlayer/openlayer-0.2.0a32.tar.gz/openlayer-0.2.0a32/src/openlayer/types/__init__.py
@@ -0,0 +1,13 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from __future__ import annotations
+
+from .project_list_params import ProjectListParams as ProjectListParams
+from .commit_create_params import CommitCreateParams as CommitCreateParams
+from .project_create_params import ProjectCreateParams as ProjectCreateParams
+from .project_list_response import ProjectListResponse as ProjectListResponse
+from .commit_create_response import CommitCreateResponse as CommitCreateResponse
+from .project_create_response import ProjectCreateResponse as ProjectCreateResponse
+from .inference_pipeline_update_params import InferencePipelineUpdateParams as InferencePipelineUpdateParams
+from .inference_pipeline_update_response import InferencePipelineUpdateResponse as InferencePipelineUpdateResponse
+from .inference_pipeline_retrieve_response import InferencePipelineRetrieveResponse as InferencePipelineRetrieveResponse
