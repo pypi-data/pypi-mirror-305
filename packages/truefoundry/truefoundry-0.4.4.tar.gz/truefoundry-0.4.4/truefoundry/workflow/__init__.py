8b05edcf8b2a8b5715f633a6c234ccbdcf4645ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-01 15:05:41 +0000

    Add package truefoundry/0.4.4/truefoundry-0.4.4.tar.gz

---
+++ packages/truefoundry/truefoundry-0.4.4.tar.gz/truefoundry-0.4.4/truefoundry/workflow/__init__.py
@@ -0,0 +1,44 @@
+try:
+    import fsspec
+    from flytekit import task as _
+except ImportError:
+    print("To use workflows, please run 'pip install truefoundry[workflow]'.")
+
+from flytekit import conditional
+from flytekit.types.directory import FlyteDirectory
+
+from truefoundry.common.constants import ENV_VARS
+from truefoundry.common.tfy_signed_url_fs import SignedURLFileSystem
+from truefoundry.deploy.v2.lib.patched_models import (
+    ContainerTaskConfig,
+    PythonTaskConfig,
+    TaskDockerFileBuild,
+    TaskPythonBuild,
+)
+from truefoundry.workflow.container_task import ContainerTask
+from truefoundry.workflow.map_task import map_task
+from truefoundry.workflow.python_task import PythonFunctionTask
+from truefoundry.workflow.task import task
+from truefoundry.workflow.workflow import ExecutionConfig, workflow
+
+__all__ = [
+    "task",
+    "ContainerTask",
+    "PythonFunctionTask",
+    "map_task",
+    "workflow",
+    "conditional",
+    "FlyteDirectory",
+    "TaskDockerFileBuild",
+    "TaskPythonBuild",
+    "ContainerTaskConfig",
+    "PythonTaskConfig",
+    "ExecutionConfig",
+]
+
+
+# Register the SignedURLFileSystem implementation for fsspec
+if ENV_VARS.TFY_INTERNAL_SIGNED_URL_SERVER_HOST:
+    fsspec.register_implementation("s3", SignedURLFileSystem, clobber=True)
+    fsspec.register_implementation("gs", SignedURLFileSystem, clobber=True)
+    fsspec.register_implementation("abfs", SignedURLFileSystem, clobber=True)
