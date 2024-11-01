b2c559717d05c2a223999fcab463a42d5acff19b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-01 14:06:14 +0000

    Add package yapper-tts/0.1.1/yapper_tts-0.1.1.tar.gz

---
+++ packages/yapper-tts/yapper_tts-0.1.1.tar.gz/yapper_tts-0.1.1/yapper/constants.py
@@ -0,0 +1,35 @@
+from yapper.enums import Persona
+
+PLATFORM_LINUX = "linux"
+PLATFORM_MAC = "mac"
+PLATFORM_WINDOWS = "windows"
+
+VOICE_MALE = "m"
+VOICE_FEMALE = "f"
+
+SPEECH_RATE = 165
+SPEECH_VOLUME = 1
+
+FLD_ROLE = "role"
+FLD_CONTENT = "content"
+
+ROLE_SYSTEM = "system"
+ROLE_USER = "user"
+
+GEMINI_MODEL_DEFAULT = "gemini-1.5-pro-002"
+GPT_MODEL_DEFAULT = "gpt-3.5-turbo"
+
+persona_instrs = {
+    Persona.DEFAULT: "You are a programmer's funny coding companion",
+    Persona.JARVIS: "You are J.A.R.V.I.S, Iron Man's AI assiatant",
+    Persona.FRIDAY: "You are F.R.I.D.A.Y, Iron Man's AI assiatant",
+    Persona.ALFRED: "You are Alfred, Bruce Wayne's butler",
+    Persona.HAL: 'You are HAL-9000, the AI from "2000: A space odyssey"',
+    Persona.CORTANA: "You are Cortana, the AI from Halo games",
+    Persona.SAMANTHA: "You are Samantha, tHE AI from the film 'Her'",
+    Persona.TARS: "You are T.A.R.S, the AI from the film 'Interstellar'",
+}
+for persona in persona_instrs:
+    persona_instrs[persona] += "\n" + "I am a programmer and you are tasked"
+    persona_instrs[persona] += " to be my translator you are tasked to be my"
+    persona_instrs[persona] += " translator, say what I say in your own way"
