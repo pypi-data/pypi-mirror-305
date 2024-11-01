e5080a652ad3b3952eda36a8a0c8a5b70212b45f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-01 04:05:38 +0000

    Add package nomenclature-iamc/0.20.0/nomenclature_iamc-0.20.0-py3-none-any.whl

---
+++ packages/nomenclature-iamc/nomenclature_iamc-0.20.0-py3-none-any.whl/nomenclature/config.py
@@ -0,0 +1,197 @@
+from enum import Enum
+from pathlib import Path
+from typing import Annotated, Optional
+
+import yaml
+from git import Repo
+from pydantic import (
+    BaseModel,
+    Field,
+    ValidationInfo,
+    field_validator,
+    model_validator,
+    ConfigDict,
+    BeforeValidator,
+)
+
+
+def convert_to_set(v: str | list[str] | set[str]) -> set[str]:
+    match v:
+        case set(v):
+            return v
+        case list(v):
+            return set(v)
+        case str(v):
+            return {v}
+        case _:
+            raise TypeError("`repositories` must be of type str, list or set.")
+
+
+class CodeListConfig(BaseModel):
+    dimension: str | None = None
+    repositories: Annotated[set[str], BeforeValidator(convert_to_set)] = Field(
+        default_factory=set, alias="repository"
+    )
+    model_config = ConfigDict(populate_by_name=True)
+
+    @property
+    def repository_dimension_path(self) -> str:
+        return f"definitions/{self.dimension}"
+
+
+class RegionCodeListConfig(CodeListConfig):
+    country: bool = False
+    nuts: dict[str, str | list[str]] | None = None
+
+    @field_validator("nuts")
+    @classmethod
+    def check_nuts(
+        cls, v: dict[str, str | list[str]] | None
+    ) -> dict[str, str | list[str]] | None:
+        if v and not all(k in ["nuts-1", "nuts-2", "nuts-3"] for k in v.keys()):
+            raise ValueError(
+                "Invalid fields for `nuts` in configuration. "
+                "Allowed values are: 'nuts-1', 'nuts-2' and 'nuts-3'."
+            )
+        return v
+
+
+class Repository(BaseModel):
+    url: str
+    hash: str | None = None
+    release: str | None = None
+    local_path: Path | None = Field(default=None, validate_default=True)
+    # defined via the `repository` name in the configuration
+
+    @model_validator(mode="after")
+    @classmethod
+    def check_hash_and_release(cls, v: "Repository") -> "Repository":
+        if v.hash and v.release:
+            raise ValueError("Either `hash` or `release` can be provided, not both.")
+        return v
+
+    @field_validator("local_path")
+    @classmethod
+    def check_path_empty(cls, v):
+        if v is not None:
+            raise ValueError("The `local_path` must not be set as part of the config.")
+        return v
+
+    @property
+    def revision(self):
+        return self.hash or self.release or "main"
+
+    def fetch_repo(self, to_path):
+        to_path = to_path if isinstance(to_path, Path) else Path(to_path)
+
+        if not to_path.is_dir():
+            repo = Repo.clone_from(self.url, to_path)
+        else:
+            repo = Repo(to_path)
+            repo.remotes.origin.fetch()
+        self.local_path = to_path
+        repo.git.reset("--hard")
+        repo.git.checkout(self.revision)
+        repo.git.reset("--hard")
+        repo.git.clean("-xdf")
+        if self.revision == "main":
+            repo.remotes.origin.pull()
+        self.check_external_repo_double_stacking()
+
+    def check_external_repo_double_stacking(self):
+        nomenclature_config = self.local_path / "nomenclature.yaml"
+        if nomenclature_config.is_file():
+            with open(nomenclature_config, "r", encoding="utf-8") as f:
+                config = yaml.safe_load(f)
+            if config.get("repositories"):
+                raise ValueError(
+                    (
+                        "External repos cannot again refer to external repos, "
+                        f"found in nomenclature.yaml in '{self.url}'"
+                    )
+                )
+
+
+class DataStructureConfig(BaseModel):
+    """A class for configuration of a DataStructureDefinition
+
+    Attributes
+    ----------
+    region : RegionCodeListConfig
+        Attributes for configuring the RegionCodeList
+
+    """
+
+    model: Optional[CodeListConfig] = Field(default_factory=CodeListConfig)
+    scenario: Optional[CodeListConfig] = Field(default_factory=CodeListConfig)
+    region: Optional[RegionCodeListConfig] = Field(default_factory=RegionCodeListConfig)
+    variable: Optional[CodeListConfig] = Field(default_factory=CodeListConfig)
+
+    @field_validator("model", "scenario", "region", "variable", mode="before")
+    @classmethod
+    def add_dimension(cls, v, info: ValidationInfo):
+        return {"dimension": info.field_name, **v}
+
+    @property
+    def repos(self) -> dict[str, str]:
+        return {
+            dimension: getattr(self, dimension).repositories
+            for dimension in ("model", "scenario", "region", "variable")
+            if getattr(self, dimension).repositories
+        }
+
+
+class RegionMappingConfig(BaseModel):
+    repositories: Annotated[set[str], BeforeValidator(convert_to_set)] = Field(
+        default_factory=set, alias="repository"
+    )
+    model_config = ConfigDict(populate_by_name=True)
+
+
+class DimensionEnum(str, Enum):
+    model = "model"
+    scenario = "scenario"
+    variable = "variable"
+    region = "region"
+    subannual = "subannual"
+
+
+class NomenclatureConfig(BaseModel):
+    dimensions: None | list[DimensionEnum] = None
+    repositories: dict[str, Repository] = Field(default_factory=dict)
+    definitions: DataStructureConfig = Field(default_factory=DataStructureConfig)
+    mappings: RegionMappingConfig = Field(default_factory=RegionMappingConfig)
+
+    model_config = ConfigDict(use_enum_values=True)
+
+    @model_validator(mode="after")
+    @classmethod
+    def check_definitions_repository(
+        cls, v: "NomenclatureConfig"
+    ) -> "NomenclatureConfig":
+        mapping_repos = {"mappings": v.mappings.repositories} if v.mappings else {}
+        repos = {**v.definitions.repos, **mapping_repos}
+        for use, repositories in repos.items():
+            if repositories - v.repositories.keys():
+                raise ValueError((f"Unknown repository {repositories} in '{use}'."))
+        return v
+
+    def fetch_repos(self, target_folder: Path):
+        for repo_name, repo in self.repositories.items():
+            repo.fetch_repo(target_folder / repo_name)
+
+    @classmethod
+    def from_file(cls, file: Path):
+        """Read a DataStructureConfig from a file
+
+        Parameters
+        ----------
+        file : :class:`pathlib.Path` or path-like
+            Path to config file
+
+        """
+        with open(file, "r", encoding="utf-8") as stream:
+            config = yaml.safe_load(stream)
+        instance = cls(**config)
+        instance.fetch_repos(file.parent)
+        return instance
