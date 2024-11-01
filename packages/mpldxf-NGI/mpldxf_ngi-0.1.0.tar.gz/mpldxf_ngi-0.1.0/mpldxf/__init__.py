d02f9ed36f985d6982ad6f65322cf133ad0b15f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-01 08:05:51 +0000

    Add package mpldxf-NGI/0.1.0/mpldxf_ngi-0.1.0.tar.gz

---
+++ packages/mpldxf-NGI/mpldxf_ngi-0.1.0.tar.gz/mpldxf_ngi-0.1.0/mpldxf/__init__.py
@@ -0,0 +1,36 @@
+"""A matplotlib backend to enable writing matplotlib figures to DXF format.
+
+   DXF is a format that can be read by computer aided design (CAD) software.
+
+   This package builds on the ``ezdxf`` package writen by Manfred Moitzi:
+
+     http://bitbucket.org/mozman/ezdxf
+
+Copyright (C) 2014 David M Kent
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+"""
+import mpldxf.backend_dxf
+from .backend_dxf import FigureCanvasDxf
+
+
+__version__ = '0.1.0'
+
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
---
+++ packages/mpldxf-NGI/mpldxf_ngi-0.1.0.tar.gz/mpldxf_ngi-0.1.0/setup.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+"""Setup file for the mpldxf package.
+
+Copyright (C) 2014 David M Kent
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+"""
+
+import os
+from setuptools import setup
+
+import versioneer
+
+
+AUTHOR_NAME = "Jon-Michael Josefsen"
+AUTHOR_EMAIL = "jmj@ngi.no"
+
+
+def read(fname):
+    try:
+        with open(os.path.join(os.path.dirname(__file__), fname)) as f:
+            return f.read()
+    except IOError:
+        return "File '%s' not found.\n" % fname
+
+
+setup(
+    name="mpldxf-NGI",
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    description="A fork of mpldxf - a matplotlib backend to write DXF drawings. Modified by NGI to handle geotechnical plots",
+    author=AUTHOR_NAME,
+    url="https://github.com/dmkent/mpldxf",
+    download_url="https://github.com/dmkent/mpldxf",
+    author_email=AUTHOR_EMAIL,
+    packages=["mpldxf"],
+    provides=["mpldxf"],
+    keywords=["matplotlib", "DXF", "CAD"],
+    long_description=read("README.rst"),
+    platforms="OS Independent",
+    license="MIT License",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        "ezdxf>1.0.0",
+        "matplotlib",
+    ],
+    dependency_links=[
+        "hg+https://bitbucket.org/mozman/ezdxf@default#egg=ezdxf-0.7.0alpha",
+    ],
+)
