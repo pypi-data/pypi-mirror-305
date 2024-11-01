e44c89a8cd3ce500d72cb246c08d91e47987e3d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-01 05:05:24 +0000

    Add package nixm/40/nixm-40.tar.gz

---
+++ packages/nixm/nixm-40.tar.gz/nixm-40/nixm/command.py
@@ -0,0 +1,156 @@
+# This file is placed in the Public Domain.
+# pylint: disable=C,R,W0105,W0718
+
+
+"command"
+
+
+import inspect
+import os
+
+
+from nixt.object  import Obj
+from nixt.persist import Workdir
+from nixt.runtime import later, launch
+
+
+"defines"
+
+
+NAME = __file__.rsplit(os.sep, maxsplit=2)[-2]
+Workdir.wdr = os.path.expanduser(f"~/.{NAME}")
+
+
+"commands"
+
+
+class Commands:
+
+    cmds = {}
+
+    @staticmethod
+    def add(func):
+        Commands.cmds[func.__name__] = func
+
+    @staticmethod
+    def scan(mod):
+        for key, cmdz in inspect.getmembers(mod, inspect.isfunction):
+            if key.startswith("cb"):
+                continue
+            if 'event' in cmdz.__code__.co_varnames:
+                Commands.add(cmdz)
+
+
+def command(bot, evt):
+    parse(evt, evt.txt)
+    if "ident" in dir(bot):
+        evt.orig = bot.ident
+    func = Commands.cmds.get(evt.cmd, None)
+    if func:
+        try:
+            func(evt)
+            bot.display(evt)
+        except Exception as ex:
+            later(ex)
+    evt.ready()
+
+
+"utilities"
+
+
+def modloop(*pkgs, disable=""):
+    for pkg in pkgs:
+        for modname in dir(pkg):
+            if modname in spl(disable):
+                continue
+            if modname.startswith("__"):
+                continue
+            yield getattr(pkg, modname)
+
+
+def parse(obj, txt=None):
+    if txt is None:
+        txt = ""
+    args = []
+    obj.args    = []
+    obj.cmd     = ""
+    obj.gets    = Obj()
+    obj.hasmods = False
+    obj.index   = None
+    obj.mod     = ""
+    obj.opts    = ""
+    obj.result  = []
+    obj.sets    = Obj()
+    obj.txt     = txt or ""
+    obj.otxt    = obj.txt
+    _nr = -1
+    for spli in obj.otxt.split():
+        if spli.startswith("-"):
+            try:
+                obj.index = int(spli[1:])
+            except ValueError:
+                obj.opts += spli[1:]
+            continue
+        if "==" in spli:
+            key, value = spli.split("==", maxsplit=1)
+            val = getattr(obj.gets, key, None)
+            if val:
+                value = val + "," + value
+                setattr(obj.gets, key, value)
+            continue
+        if "=" in spli:
+            key, value = spli.split("=", maxsplit=1)
+            if key == "mod":
+                obj.hasmods = True
+                if obj.mod:
+                    obj.mod += f",{value}"
+                else:
+                    obj.mod = value
+                continue
+            setattr(obj.sets, key, value)
+            continue
+        _nr += 1
+        if _nr == 0:
+            obj.cmd = spli
+            continue
+        args.append(spli)
+    if args:
+        obj.args = args
+        obj.txt  = obj.cmd or ""
+        obj.rest = " ".join(obj.args)
+        obj.txt  = obj.cmd + " " + obj.rest
+    else:
+        obj.txt = obj.cmd or ""
+    return obj
+
+
+def scanner(*pkgs, init=False, disable=""):
+    result = []
+    for mod in modloop(*pkgs, disable=disable):
+        Commands.scan(mod)
+        thr = None
+        if init and "init" in dir(mod):
+            thr = launch(mod.init)
+        result.append((mod, thr))
+    return result
+
+
+def spl(txt):
+    try:
+        result = txt.split(',')
+    except (TypeError, ValueError):
+        result = txt
+    return [x for x in result if x]
+
+
+"interface"
+
+
+def __dir__():
+    return (
+        'Commands',
+        'command',
+        'parse',
+        'scanner',
+        'spl'
+    )
