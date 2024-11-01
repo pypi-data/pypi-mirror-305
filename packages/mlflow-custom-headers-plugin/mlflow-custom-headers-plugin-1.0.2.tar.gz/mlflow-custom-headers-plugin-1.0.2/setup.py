2e14adb4f158bb1dc3d8c67a8d435c1c89d0fa1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-01 03:17:15 +0000

    Add package mlflow-custom-headers-plugin/1.0.2/mlflow-custom-headers-plugin-1.0.2.tar.gz

---
+++ packages/mlflow-custom-headers-plugin/mlflow-custom-headers-plugin-1.0.2.tar.gz/mlflow-custom-headers-plugin-1.0.2/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="mlflow-custom-headers-plugin",
+    version="1.0.2",
+    description="MLFlow plugin that allows adding extra headers to requests to MLFlow in order to enable authentication and other use-cases.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Yakko Majuri",
+    author_email="yakko.majuri@protonmail.com",
+    license="MIT",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    install_requires=[
+        "mlflow>=2.8.0",
+    ],
+    python_requires=">=3.9",
+    entry_points={
+        "mlflow.request_header_provider": [
+            "unused = mlflow_custom_headers_plugin.headers:CustomRequestHeaderProvider"
+        ]
+    },
+    project_urls={
+        "Source": "https://github.com/yakkomajuri/mlflow_custom_headers_plugin",
+        "Bug Tracker": "https://github.com/yakkomajuri/mlflow_custom_headers_plugin/issues",
+    },
+)
