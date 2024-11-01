7009023ce2073fced2e9dc361fc458408dd7f6f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-01 14:05:46 +0000

    Add package dharmamitra-sanskrit-grammar/0.1.4/dharmamitra_sanskrit_grammar-0.1.4.tar.gz

---
+++ packages/dharmamitra-sanskrit-grammar/dharmamitra_sanskrit_grammar-0.1.4.tar.gz/dharmamitra_sanskrit_grammar-0.1.4/dharmamitra_sanskrit_grammar/__init__.py
@@ -0,0 +1,8 @@
+"""
+Dharmamitra Sanskrit Grammar Processing package.
+"""
+
+from .sanskrit_grammar import DharmamitraSanskritProcessor
+
+__version__ = "0.1.4"
+__all__ = ["DharmamitraSanskritProcessor"]
