98685a2fd55747dbc28a07f57ea26be658ff9489
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-01 08:05:43 +0000

    Add package casify/0.1.12/casify-0.1.12.tar.gz

---
+++ packages/casify/casify-0.1.12.tar.gz/casify-0.1.12/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="casify",  # Replace with your desired package name
+    version="0.1.12",
+    author="René Alexander Ask",
+    author_email="rene.ask@icloud.com",
+    description="A CAS-library wrapped around sympy to simplify use of CAS-functionality in mathematics.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/reneaas/casify",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        "sympy",
+    ],
+    python_requires=">=3.7",
+)
