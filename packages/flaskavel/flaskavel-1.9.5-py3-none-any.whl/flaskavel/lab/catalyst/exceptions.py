861e39aa8cbc54b2050586f5a045ff26a2a2081d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-01 08:06:04 +0000

    Add package flaskavel/1.9.5/flaskavel-1.9.5-py3-none-any.whl

---
+++ packages/flaskavel/flaskavel-1.9.5-py3-none-any.whl/flaskavel/lab/catalyst/exceptions.py
@@ -0,0 +1,11 @@
+class DumpFlaskavelException(Exception):
+    def __init__(self, response):
+        self.response = response
+
+class AuthorizeFlaskavelException(Exception):
+    def __init__(self, response):
+        self.response = response
+
+class ValidateFlaskavelException(Exception):
+    def __init__(self, response):
+        self.response = response
