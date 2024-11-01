974c0cdbb2f9f6937f1e4ced9496af5c76888d28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-01 14:06:12 +0000

    Add package table-differ/0.1.6/table_differ-0.1.6.tar.gz

---
+++ packages/table-differ/table_differ-0.1.6.tar.gz/table_differ-0.1.6/Table_Differ/modules/exceptions.py
@@ -0,0 +1,16 @@
+class InvalidArgument(Exception):
+    def __init__(self, message, error_code):
+        super().__init__(message)
+        self.error_code = error_code
+
+    def __str__(self):
+        return f"{self.message} (Error Code: {self.error_code})"
+
+class MissingArgument(Exception):
+    def __init__(self, message, error_code):
+        super().__init__(message)
+        self.error_code = error_code
+
+    def __str__(self):
+        return f"{self.message} (Error Code: {self.error_code})"
+
