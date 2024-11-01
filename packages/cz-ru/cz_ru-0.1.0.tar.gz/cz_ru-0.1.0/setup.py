4a9db9e38da016ac1947ce9fae667447c087671c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-01 06:05:15 +0000

    Add package cz-ru/0.1.0/cz_ru-0.1.0.tar.gz

---
+++ packages/cz-ru/cz_ru-0.1.0.tar.gz/cz_ru-0.1.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+
+setup(
+    name="cz_ru",
+    version="0.1.0",
+    py_modules=["cz_ru"],
+    license="MIT",
+    long_description="Конфиг для Commitizen на русском языке",
+    url="https://github.com/teatov/cz-ru",
+    install_requires=["commitizen"],
+    entry_points={
+        "commitizen.plugin": [
+            "cz_ru = cz_ru:CzRu"
+        ]
+    },
+)
