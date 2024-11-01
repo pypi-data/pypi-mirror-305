a64fe6769d2e995bee0bd1e81b48315d23e2d4a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-01 16:04:48 +0000

    Add package bayspec/0.1.5/bayspec-0.1.5-py3-none-any.whl

---
+++ packages/bayspec/bayspec-0.1.5-py3-none-any.whl/bayspec/__init__.py
@@ -0,0 +1,9 @@
+from .util import *
+from .data import *
+from .model import *
+from .infer import *
+from .__info__ import __version__
+
+
+from os.path import dirname, abspath
+__app__ = dirname(dirname(abspath(__file__))) + 'app/app.py'
