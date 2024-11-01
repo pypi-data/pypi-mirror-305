85ef21dac6c78fb7cfb4e38b6f72c36223ec315c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-01 06:06:11 +0000

    Add package laboneq/2.40.0/laboneq-2.40.0-cp39-abi3-macosx_10_12_x86_64.macosx_11_0_arm64.macosx_10_12_universal2.whl

---
+++ packages/laboneq/laboneq-2.40.0-cp39-abi3-macosx_10_12_x86_64.macosx_11_0_arm64.macosx_10_12_universal2.whl/laboneq/dsl/quantum/__init__.py
@@ -0,0 +1,6 @@
+# Copyright 2023 Zurich Instruments AG
+# SPDX-License-Identifier: Apache-2.0
+
+from laboneq.dsl.quantum.quantum_element import QuantumElement, QuantumElementSignalMap
+from laboneq.dsl.quantum.qubit import Qubit, QubitParameters
+from laboneq.dsl.quantum.transmon import Transmon, TransmonParameters
