7508c7f4b2b4e7c44abddb4dcc87706f748ac62d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-01 14:06:02 +0000

    Add package pipui/0.1.5/pipui-0.1.5.tar.gz

---
+++ packages/pipui/pipui-0.1.5.tar.gz/pipui-0.1.5/setup.py
@@ -0,0 +1,46 @@
+"""
+python setup.py sdist bdist_wheel
+"""
+import os
+import shutil
+import sys
+from setuptools import setup, find_packages
+from datetime import datetime
+from setuptools.command.install import install
+import subprocess
+import os
+
+with open("README.md", "r",encoding='utf-8') as f:
+    long_description = f.read()
+if __name__ == '__main__':
+    sys.argv.extend(["sdist", "bdist_wheel", "-d", "./dist/"])
+if os.path.exists("./build"):
+    shutil.rmtree("./build")
+setup(
+    name="pipui",
+    # version=datetime.now().strftime("%Y%m%d"),
+    version="0.1.5",
+    description="pipui",
+    long_description=long_description,
+    author="merlon",
+    license="BSD",
+    packages=find_packages(),
+    # packages=["pygcbs"],
+    package_data={"pipui": ["templates/*", ], },
+    # include_package
+    install_requires=[
+        "uvicorn",
+        "jinja2",
+        "fastapi",
+        "requests",
+        "beautifulsoup4",
+
+    ],
+    setup_requires=["setuptools", "wheel"],
+    entry_points={"console_scripts": ["pipui = pipui.main:main"]},
+    python_requires=">=3.6, <4",
+    # include_package_data=True,
+    cmdclass={
+        # "install": PostInstallCommand,
+    },
+)
