258fa717cf918f0afd9d9bf3b5307a9cefec9c39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-01 15:06:21 +0000

    Add package spectacularAI/1.37.0/spectacularAI-1.37.0-cp312-cp312-win_amd64.whl

---
+++ packages/spectacularAI/spectacularAI-1.37.0-cp312-cp312-win_amd64.whl/spectacularAI/__init__.py
@@ -0,0 +1,26 @@
+from spectacularAI_native import *
+
+__all__ = [
+    'cli', # Python module
+    'mapping',
+    'depthai',
+    # TODO: auto-populate
+    'VioOutput',
+    'Pose',
+    'Camera',
+    'CameraPose',
+    'Vector3d',
+    'Vector3f',
+    'Quaternion',
+    'TrackingStatus',
+    'ColorFormat',
+    'Bitmap',
+    'Frame',
+    'FrameSet',
+    'Replay',
+    'WgsCoordinates',
+    'GnssVioOutput',
+    'PixelCoordinates',
+    'FeaturePoint',
+    'VisualMarker'
+]
