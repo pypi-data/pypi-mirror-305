985a2fc00465bffb408a958bf9ec194c5c5b8190
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-01 08:05:48 +0000

    Add package ceo-alfred/0.0.4b0/ceo_alfred-0.0.4b0.tar.gz

---
+++ packages/ceo-alfred/ceo_alfred-0.0.4b0.tar.gz/ceo_alfred-0.0.4b0/alfred/main.py
@@ -0,0 +1,24 @@
+import sys
+
+from process_manager.agent import process_manager
+from alfred.action import find_information_about_the_assistant
+
+process_manager.grant_ability(find_information_about_the_assistant)
+
+
+def main():
+    print('\n\r')
+    if len(sys.argv) > 1:
+        query = ''
+        for arg in sys.argv[1:]:
+            query += arg
+        process_manager.assign(query)
+    try:
+        process_manager.just_do_it()
+    except KeyboardInterrupt:
+        pass
+    exit(0)
+
+
+if __name__ == "__main__":
+    main()
