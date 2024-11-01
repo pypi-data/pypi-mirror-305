449d5815c79cd6be60d3537275c91409c9111d39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-01 14:05:55 +0000

    Add package ersilia/0.1.38/ersilia-0.1.38-py3-none-any.whl

---
+++ packages/ersilia/ersilia-0.1.38-py3-none-any.whl/ersilia/utils/paths.py
@@ -0,0 +1,77 @@
+import re
+import os
+import collections
+from pathlib import Path
+from ersilia import logger
+from .docker import resolve_pack_method_docker
+from ..default import PACK_METHOD_BENTOML, PACK_METHOD_FASTAPI
+
+MODELS_DEVEL_DIRNAME = "models"
+
+
+class Paths(object):
+    def __init__(self):
+        self.essentials = ["setup.py", "README.md", "CODE_OF_CONDUCT.md"]
+
+    @staticmethod
+    def _eos_regex():
+        return re.compile(r"eos[0-9][a-z0-9]{3}")
+
+    @staticmethod
+    def home():
+        """Get home directory"""
+        return os.path.abspath(str(Path.home()))
+
+    def model_id_from_path(self, path):
+        """Guess model identifier based on the path"""
+        regex = self._eos_regex()
+        path = os.path.abspath(path)
+        model_ids = sorted(set(regex.findall(path)))
+        if len(model_ids) == 1:
+            return model_ids[0]
+        else:
+            return None
+
+    def org_development_path(self):
+        """Guess generic development path"""
+        path = self.ersilia_development_path()
+        if path is None:
+            return
+        else:
+            return os.path.split(path)[0]
+
+    def ersilia_development_path(self):
+        """Try to guess the package development path in the local computer"""
+        path = os.path.dirname(__file__)
+        for _ in range(2):
+            path = os.path.split(path)[0]
+        for essential in self.essentials:
+            if not os.path.exists(os.path.join(path, essential)):
+                return None
+        return path
+
+    @staticmethod
+    def exists(path):
+        if path is None:
+            return False
+        if os.path.exists(path):
+            return True
+        else:
+            return False
+
+def resolve_pack_method_source(model_path):
+    if os.path.exists(os.path.join(model_path, "installs", "install.sh")):
+        return PACK_METHOD_FASTAPI
+    elif os.path.exists(os.path.join(model_path, "bentoml.yml")):
+        return PACK_METHOD_BENTOML
+    logger.warning("Could not resolve pack method")
+    return None
+
+def resolve_pack_method(model_path):
+    with open(os.path.join(model_path, "service_class.txt"), "r") as f:
+        service_class = f.read().strip()
+    if service_class == "pulled_docker":
+        model_id = Paths().model_id_from_path(model_path)
+        return resolve_pack_method_docker(model_id)
+    else:
+        return resolve_pack_method_source(model_path)
---
+++ packages/ersilia/ersilia-0.1.38-py3-none-any.whl/ersilia/utils/terminal.py
@@ -0,0 +1,91 @@
+import os
+import subprocess
+import json
+import tempfile
+import shutil
+
+try:
+    from inputimeout import inputimeout, TimeoutOccurred
+except:
+    inputimeout = None
+    TimeoutOccurred = None
+
+from ..default import VERBOSE_FILE
+from ..utils.session import get_session_dir
+from ..utils.logging import make_temp_dir
+
+
+def is_quiet():
+    verbose_file = os.path.join(get_session_dir(), VERBOSE_FILE)
+    if not os.path.exists(verbose_file):
+        return False
+    with open(verbose_file, "r") as f:
+        d = json.load(f)
+    if d["verbose"]:
+        return False
+    else:
+        return True
+
+
+def run_command(cmd, quiet=None):
+    if quiet is None:
+        quiet = is_quiet()
+    if type(cmd) == str:
+        if quiet:
+            with open(os.devnull, "w") as fp:
+                subprocess.Popen(
+                    cmd, stdout=fp, stderr=fp, shell=True, env=os.environ
+                ).wait()
+        else:
+            subprocess.Popen(cmd, shell=True, env=os.environ).wait()
+    else:
+        if quiet:
+            subprocess.check_call(
+                cmd,
+                stdout=subprocess.DEVNULL,
+                stderr=subprocess.DEVNULL,
+                env=os.environ,
+            )
+        else:
+            subprocess.check_call(cmd, env=os.environ)
+
+
+def run_command_check_output(cmd):
+    if type(cmd) is str:
+        assert ">" not in cmd
+        tmp_folder = make_temp_dir(prefix="ersilia-")
+        os.listdir(tmp_folder)
+        tmp_file = os.path.join(tmp_folder, "out.txt")
+        cmd = cmd + " > " + tmp_file
+        with open(os.devnull, "w") as fp:
+            subprocess.Popen(cmd, shell=True, stderr=fp, stdout=fp).wait()
+        with open(tmp_file, "r") as f:
+            result = f.read()
+        shutil.rmtree(tmp_folder)
+        return result
+    else:
+        result = subprocess.run(cmd, stdout=subprocess.PIPE, env=os.environ)
+        return result.stdout
+
+
+def raw_input_with_timeout(prompt, default_answer, timeout=5):
+    if inputimeout is None:
+        return input(prompt)
+    try:
+        answer = inputimeout(prompt=prompt, timeout=timeout)
+    except TimeoutOccurred:
+        answer = default_answer
+    return answer
+
+
+def yes_no_input(prompt, default_answer, timeout=5):
+    ans = raw_input_with_timeout(
+        prompt=prompt, default_answer=default_answer, timeout=timeout
+    )
+    if ans is None or ans == "":
+        ans = default_answer
+    ans = str(ans).lower()
+    if ans[0] == "n":
+        return False
+    else:
+        return True
