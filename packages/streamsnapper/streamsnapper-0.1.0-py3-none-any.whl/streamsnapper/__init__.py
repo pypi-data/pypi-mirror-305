e54f1a7f09d941b8f64e93cbabce31655412d85f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-01 04:05:43 +0000

    Add package streamsnapper/0.1.0/streamsnapper-0.1.0-py3-none-any.whl

---
+++ packages/streamsnapper/streamsnapper-0.1.0-py3-none-any.whl/streamsnapper/__init__.py
@@ -0,0 +1,7 @@
+# Local imports
+from .streamsnapper import YouTube, SoundCloud, Downloader, Merger
+from .exceptions import StreamBaseError, InvalidDataError, ScrapingError, DownloadError, MergeError
+
+
+__version__ = '0.1.0'
+__license__ = 'MIT'
