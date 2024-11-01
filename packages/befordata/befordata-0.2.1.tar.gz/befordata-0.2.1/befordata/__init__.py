67e14d4c3104578bcf3f7e1bbf0727c752c6cb0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-01 03:17:24 +0000

    Add package befordata/0.2.1/befordata-0.2.1.tar.gz

---
+++ packages/befordata/befordata-0.2.1.tar.gz/befordata-0.2.1/befordata/__init__.py
@@ -0,0 +1,7 @@
+"""BeFoR-Data structure for handling behavioural force data"""
+
+__author__ = "Oliver Lindemann"
+__version__ = "0.2.1"
+
+from ._record import BeForRecord
+from ._epochs import BeForEpochs
