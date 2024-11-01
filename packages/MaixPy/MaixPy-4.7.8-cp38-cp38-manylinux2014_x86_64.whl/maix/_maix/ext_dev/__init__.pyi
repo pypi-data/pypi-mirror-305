a8a131fb8ef1eb40d4628bde94f828873d307b9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-01 03:17:04 +0000

    Add package MaixPy/4.7.8/MaixPy-4.7.8-cp38-cp38-manylinux2014_x86_64.whl

---
+++ packages/MaixPy/MaixPy-4.7.8-cp38-cp38-manylinux2014_x86_64.whl/maix/_maix/ext_dev/__init__.pyi
@@ -0,0 +1,9 @@
+"""
+maix.ext_dev module
+"""
+from __future__ import annotations
+from . import bm8563
+from . import imu
+from . import qmi8658
+from . import tmc2209
+__all__ = ['bm8563', 'imu', 'qmi8658', 'tmc2209']
