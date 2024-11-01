cfbdac7d129a5c0aa60fa632f2b43da885dac813
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-01 06:05:27 +0000

    Add package gspreadplusplus/1.0.0/gspreadplusplus-1.0.0.tar.gz

---
+++ packages/gspreadplusplus/gspreadplusplus-1.0.0.tar.gz/gspreadplusplus-1.0.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='gspreadplusplus',
+    version='1.0.0',
+    author='Daniel Simanek',
+    author_email='daniel.simanek@decathlon.com',
+    description='Advanced functions using the gspread library, used for making the code less bloated.',
+    long_description=open('README.md').read(),  # Long description from the README file
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    install_requires=[
+        'gspread',
+        'oauth2client',
+        'pandas',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
