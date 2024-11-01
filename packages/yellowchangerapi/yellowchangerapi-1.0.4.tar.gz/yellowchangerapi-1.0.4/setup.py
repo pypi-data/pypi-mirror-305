e01c9b3ed7536f56212a9691308c37e405b7e1db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-01 14:05:45 +0000

    Add package yellowchangerapi/1.0.4/yellowchangerapi-1.0.4.tar.gz

---
+++ packages/yellowchangerapi/yellowchangerapi-1.0.4.tar.gz/yellowchangerapi-1.0.4/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+# Read the contents of the README file
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="yellowchangerapi",
+    version="1.0.4",
+    author="Fluf",
+    author_email="support@yellowchanger.com",
+    description="A Python library for interacting with the YellowChanger service API.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/yellowfluf/YellowChangerAPI",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.9',
+    install_requires=[
+        "requests",
+    ],
+    project_urls={
+        'Bug Reports': 'https://github.com/yellowfluf/YellowChangerAPI/issues',
+        'Source': 'https://github.com/yellowfluf/YellowChangerAPI/tree/main',
+    },
+)
