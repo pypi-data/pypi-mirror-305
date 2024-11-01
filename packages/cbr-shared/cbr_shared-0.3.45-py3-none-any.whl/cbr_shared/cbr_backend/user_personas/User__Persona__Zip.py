1ef45bd0ce272487e28ab55e07ea54ca03a72840
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-01 06:05:06 +0000

    Add package cbr-shared/0.3.45/cbr_shared-0.3.45-py3-none-any.whl

---
+++ packages/cbr-shared/cbr_shared-0.3.45-py3-none-any.whl/cbr_shared/cbr_backend/user_personas/User__Persona__Zip.py
@@ -0,0 +1,9 @@
+from cbr_shared.cbr_backend.user_personas.User__Persona import User__Persona
+from osbot_utils.base_classes.Type_Safe import Type_Safe
+
+
+class User__Persona__Zip(Type_Safe):
+    user_persona : User__Persona = None
+
+    def persona_to__zip_bytes(self):
+        return 'will be here'
