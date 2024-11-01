7ce95647ebcc0c30a14f222384bcea09eea26fcd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-01 08:05:48 +0000

    Add package hms-utils/1.4.37/hms_utils-1.4.37.tar.gz

---
+++ packages/hms-utils/hms_utils-1.4.37.tar.gz/hms_utils-1.4.37/src/hms_utils/portal/portal_validation_errors.py
@@ -0,0 +1,43 @@
+from dcicutils.ff_utils import search_metadata
+from dcicutils.portal_utils import Portal
+from hms_utils.argv import ARGV
+from hms_utils.chars import chars
+
+
+def main():
+
+    argv = ARGV({
+        ARGV.REQUIRED(str): "--env",
+        ARGV.OPTIONAL([str]): "uuids"
+    })
+
+    portal = Portal(argv.env)
+
+    if argv.uuids:
+        status = 0
+        for uuid in argv.uuids:
+            if item := portal.get_metadata(uuid):
+                if _check_for_validation_errors(item):
+                    status
+        exit(status)
+
+    query = "/search/?type=Item&validation_errors.name%21=No+value"
+    ignored_types = []
+    for item in search_metadata(query, key=portal.key, is_generator=True):
+        if isinstance(item, dict) and (item_uuid := item.get("uuid")):
+            if (item_type := portal.get_schema_type(item)) not in ignored_types:
+                print(f"{item_uuid}: {item_type}")
+                if item := portal.get_metadata(item_uuid):
+                    if _check_for_validation_errors(item):
+                        status = 1
+
+
+def _check_for_validation_errors(item: dict) -> bool:
+    if errors := item.get("validation-errors"):
+        for error in errors:
+            print(f"{chars.rarrow_hollow} ERROR: {error.get('description')}")
+
+
+if __name__ == "__main__":
+    main()
+    pass
