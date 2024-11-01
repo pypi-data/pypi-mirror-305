8e9c077de8a505f51c026002ae4659f9c5c37c8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-01 08:05:40 +0000

    Add package chalkpy/2.55.7/chalkpy-2.55.7.tar.gz

---
+++ packages/chalkpy/chalkpy-2.55.7.tar.gz/chalkpy-2.55.7/chalk/_reporting/rich/theme.py
@@ -0,0 +1,11 @@
+from rich.style import Style
+from rich.theme import Theme
+
+from chalk._reporting.rich.color import PASTELY_CYAN, SERENDIPITOUS_PURPLE
+
+CHALK_THEME = Theme(
+    {
+        "progress.elapsed": Style(color=SERENDIPITOUS_PURPLE),
+        "progress.download": Style(color=PASTELY_CYAN),
+    }
+)
