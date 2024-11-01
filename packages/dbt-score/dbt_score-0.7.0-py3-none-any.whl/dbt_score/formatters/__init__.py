d12f1ed68b1b24150e495ab407f281d4ea996129
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-01 03:17:58 +0000

    Add package dbt-score/0.7.0/dbt_score-0.7.0-py3-none-any.whl

---
+++ packages/dbt-score/dbt_score-0.7.0-py3-none-any.whl/dbt_score/formatters/__init__.py
@@ -0,0 +1,34 @@
+"""Formatters are used to output CLI results."""
+
+from __future__ import annotations
+
+import typing
+from abc import ABC, abstractmethod
+
+from dbt_score.config import Config
+from dbt_score.scoring import Score
+
+if typing.TYPE_CHECKING:
+    from dbt_score.evaluation import ModelResultsType
+from dbt_score.models import ManifestLoader, Model
+
+
+class Formatter(ABC):
+    """Abstract class to define a formatter."""
+
+    def __init__(self, manifest_loader: ManifestLoader, config: Config):
+        """Instantiate a formatter."""
+        self._manifest_loader = manifest_loader
+        self._config = config
+
+    @abstractmethod
+    def model_evaluated(
+        self, model: Model, results: ModelResultsType, score: Score
+    ) -> None:
+        """Callback when a model has been evaluated."""
+        raise NotImplementedError
+
+    @abstractmethod
+    def project_evaluated(self, score: Score) -> None:
+        """Callback when a project has been evaluated."""
+        raise NotImplementedError
---
+++ packages/dbt-score/dbt_score-0.7.0-py3-none-any.whl/dbt_score/rule.py
@@ -0,0 +1,220 @@
+"""Rule definitions."""
+
+import inspect
+import typing
+from dataclasses import dataclass, field
+from enum import Enum
+from typing import Any, Callable, Iterable, Type, TypeAlias, overload
+
+from dbt_score.model_filter import ModelFilter
+from dbt_score.models import Model
+
+
+class Severity(Enum):
+    """The severity/weight of a rule."""
+
+    LOW = 1
+    MEDIUM = 2
+    HIGH = 3
+    CRITICAL = 4
+
+
+@dataclass
+class RuleConfig:
+    """Configuration for a rule."""
+
+    severity: Severity | None = None
+    config: dict[str, Any] = field(default_factory=dict)
+    model_filter_names: list[str] = field(default_factory=list)
+
+    @staticmethod
+    def from_dict(rule_config: dict[str, Any]) -> "RuleConfig":
+        """Create a RuleConfig from a dictionary."""
+        config = rule_config.copy()
+        severity = (
+            Severity(config.pop("severity", None))
+            if "severity" in rule_config
+            else None
+        )
+        filter_names = (
+            config.pop("model_filter_names", None)
+            if "model_filter_names" in rule_config
+            else []
+        )
+
+        return RuleConfig(
+            severity=severity, config=config, model_filter_names=filter_names
+        )
+
+
+@dataclass
+class RuleViolation:
+    """The violation of a rule."""
+
+    message: str | None = None
+
+
+RuleEvaluationType: TypeAlias = Callable[[Model], RuleViolation | None]
+
+
+class Rule:
+    """The rule base class."""
+
+    description: str
+    severity: Severity = Severity.MEDIUM
+    model_filter_names: list[str]
+    model_filters: frozenset[ModelFilter] = frozenset()
+    default_config: typing.ClassVar[dict[str, Any]] = {}
+
+    def __init__(self, rule_config: RuleConfig | None = None) -> None:
+        """Initialize the rule."""
+        self.config: dict[str, Any] = {}
+        if rule_config:
+            self.process_config(rule_config)
+
+    def __init_subclass__(cls, **kwargs) -> None:  # type: ignore
+        """Initializes the subclass."""
+        super().__init_subclass__(**kwargs)
+        if not hasattr(cls, "description"):
+            raise AttributeError("Subclass must define class attribute `description`.")
+
+    def process_config(self, rule_config: RuleConfig) -> None:
+        """Process the rule config."""
+        config = self.default_config.copy()
+
+        # Overwrite default rule configuration
+        for k, v in rule_config.config.items():
+            if k in self.default_config:
+                config[k] = v
+            else:
+                raise AttributeError(
+                    f"Unknown rule parameter: {k} for rule {self.source()}."
+                )
+
+        self.set_severity(
+            rule_config.severity
+        ) if rule_config.severity else rule_config.severity
+        self.model_filter_names = rule_config.model_filter_names
+        self.config = config
+
+    def evaluate(self, model: Model) -> RuleViolation | None:
+        """Evaluates the rule."""
+        raise NotImplementedError("Subclass must implement method `evaluate`.")
+
+    @classmethod
+    def should_evaluate(cls, model: Model) -> bool:
+        """Checks if all filters in the rule allow evaluation."""
+        if cls.model_filters:
+            return all(f.evaluate(model) for f in cls.model_filters)
+        return True
+
+    @classmethod
+    def set_severity(cls, severity: Severity) -> None:
+        """Set the severity of the rule."""
+        cls.severity = severity
+
+    @classmethod
+    def set_filters(cls, model_filters: Iterable[ModelFilter]) -> None:
+        """Set the filters of the rule."""
+        cls.model_filters = frozenset(model_filters)
+
+    @classmethod
+    def source(cls) -> str:
+        """Return the source of the rule, i.e. a fully qualified name."""
+        return f"{cls.__module__}.{cls.__name__}"
+
+    def __hash__(self) -> int:
+        """Compute a unique hash for a rule."""
+        return hash(self.source())
+
+
+# Use @overload to have proper typing for both @rule and @rule(...)
+# https://mypy.readthedocs.io/en/stable/generics.html#decorator-factories
+
+
+@overload
+def rule(__func: RuleEvaluationType) -> Type[Rule]:
+    ...
+
+
+@overload
+def rule(
+    *,
+    description: str | None = None,
+    severity: Severity = Severity.MEDIUM,
+    model_filters: set[ModelFilter] | None = None,
+) -> Callable[[RuleEvaluationType], Type[Rule]]:
+    ...
+
+
+def rule(
+    __func: RuleEvaluationType | None = None,
+    *,
+    description: str | None = None,
+    severity: Severity = Severity.MEDIUM,
+    model_filters: set[ModelFilter] | None = None,
+) -> Type[Rule] | Callable[[RuleEvaluationType], Type[Rule]]:
+    """Rule decorator.
+
+    The rule decorator creates a rule class (subclass of Rule) and returns it.
+
+    Using arguments or not are both supported:
+    - ``@rule``
+    - ``@rule(description="...")``
+
+    Args:
+        __func: The rule evaluation function being decorated.
+        description: The description of the rule.
+        severity: The severity of the rule.
+        model_filters: Set of ModelFilter that filters the rule.
+    """
+
+    def decorator_rule(
+        func: RuleEvaluationType,
+    ) -> Type[Rule]:
+        """Decorator function."""
+        if func.__doc__ is None and description is None:
+            raise AttributeError("Rule must define `description` or `func.__doc__`.")
+
+        # Get description parameter, otherwise use the docstring
+        rule_description = description or (
+            func.__doc__.split("\n")[0] if func.__doc__ else None
+        )
+
+        def wrapped_func(self: Rule, *args: Any, **kwargs: Any) -> RuleViolation | None:
+            """Wrap func to add `self`."""
+            return func(*args, **kwargs)
+
+        # Get default parameters from the rule definition
+        default_config = {
+            key: val.default
+            for key, val in inspect.signature(func).parameters.items()
+            if val.default != inspect.Parameter.empty
+        }
+
+        # Create the rule class inheriting from Rule
+        rule_class = type(
+            func.__name__,
+            (Rule,),
+            {
+                "description": rule_description,
+                "severity": severity,
+                "model_filters": model_filters or frozenset(),
+                "default_config": default_config,
+                "evaluate": wrapped_func,
+                # Save provided evaluate function
+                "_orig_evaluate": func,
+                # Forward origin of the decorated function
+                "__qualname__": func.__qualname__,  # https://peps.python.org/pep-3155/
+                "__module__": func.__module__,
+            },
+        )
+
+        return rule_class
+
+    if __func is not None:
+        # The syntax @rule is used
+        return decorator_rule(__func)
+    else:
+        # The syntax @rule(...) is used
+        return decorator_rule
