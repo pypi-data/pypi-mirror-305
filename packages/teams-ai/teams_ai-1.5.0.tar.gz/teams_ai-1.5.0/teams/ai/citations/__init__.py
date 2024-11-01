e2d0a9afee96947748d6e628d5e9fbb4284da441
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-01 07:05:59 +0000

    Add package teams-ai/1.5.0/teams_ai-1.5.0.tar.gz

---
+++ packages/teams-ai/teams_ai-1.5.0.tar.gz/teams_ai-1.5.0/teams/ai/citations/__init__.py
@@ -0,0 +1,14 @@
+"""
+Copyright (c) Microsoft Corporation. All rights reserved.
+Licensed under the MIT License.
+"""
+
+from .citations import (
+    AIEntity,
+    Appearance,
+    ClientCitation,
+    Pattern,
+    SensitivityUsageInfo,
+)
+
+__all__ = ["ClientCitation", "Appearance", "SensitivityUsageInfo", "Pattern", "AIEntity"]
