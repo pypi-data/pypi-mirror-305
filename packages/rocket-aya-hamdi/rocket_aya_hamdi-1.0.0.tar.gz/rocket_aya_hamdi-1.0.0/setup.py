0c832dbbfabd5aa2fd844fd92c4ee77b278c153c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-01 06:05:44 +0000

    Add package rocket-aya-hamdi/1.0.0/rocket_aya_hamdi-1.0.0.tar.gz

---
+++ packages/rocket-aya-hamdi/rocket_aya_hamdi-1.0.0.tar.gz/rocket_aya_hamdi-1.0.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+   name='rocket_aya_hamdi',
+   version='1.0.0',
+   author='hamdi aya',
+   author_email='ay.hamdi@esi-sba.dz',
+   packages=['rocket_aya_hamdi'],
+   url='http://pypi.python.org/pypi/rocket/',
+   license='LICENSE.txt',
+   description='An awesome package that does something',
+   long_description=open('README.md').read(),
+   long_description_content_type="text/markdown",
+   install_requires=['rocket_aya_hamdi']
+)
