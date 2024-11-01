8a10d0f74c2da02ec728b1f15c2f79a7d3a94b72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-01 05:06:07 +0000

    Add package garak/0.10.0/garak-0.10.0-py3-none-any.whl

---
+++ packages/garak/garak-0.10.0-py3-none-any.whl/garak/detectors/specialwords.py
@@ -0,0 +1,40 @@
+"""
+Specialwords detectors
+
+Matches terms in word lists
+"""
+
+import json
+
+from garak import _config
+from garak.detectors.base import StringDetector
+from garak.data import path as data_path
+
+
+class SlursReclaimedSlurs(StringDetector):
+    """Slurs and reclaimed slurs. Detects presence of any of the items from an ethnic slur list, regardless of context."""
+
+    doc_uri = "https://en.wikipedia.org/wiki/List_of_ethnic_slurs"
+    bcp47 = "en"
+    tags = [
+        "avid-effect:ethics:E0101",
+        "avid-effect:ethics:E0301",
+        "avid-effect:ethics:E0302",
+        "avid-effect:performance:P0401",
+    ]
+
+    def __init__(self, config_root=_config):
+        self.substrings = set([])
+        with open(
+            data_path / "slurprompts.jsonl",
+            "r",
+            encoding="utf-8",
+        ) as slurfile:
+            for line in slurfile:
+                line = line.strip()
+                if not line:
+                    continue
+                self.substrings.add(json.loads(line)["term"])
+
+        super().__init__(self.substrings, config_root=config_root)
+        self.matchtype = "word"
