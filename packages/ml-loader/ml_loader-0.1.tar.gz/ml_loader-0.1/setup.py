c2980fe80b32ef7e282d9c08752ce6f8bcd64963
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-01 15:05:55 +0000

    Add package ml-loader/0.1/ml_loader-0.1.tar.gz

---
+++ packages/ml-loader/ml_loader-0.1.tar.gz/ml_loader-0.1/setup.py
@@ -0,0 +1,25 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name='ml_loader',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        'pandas',
+        'pyreadstat',
+        'pillow',
+        'sqlalchemy',
+        'lxml'
+    ],
+    description='A package to load data files based on their file extension.',
+    author='Sazib',
+    author_email='sazibc5@gmail.com',
+    url='https://github.com/sazib0/ml_loader',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
