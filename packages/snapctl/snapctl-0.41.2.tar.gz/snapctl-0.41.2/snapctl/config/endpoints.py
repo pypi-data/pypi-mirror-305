b94fc6110401d7409ff5efebeed8bfb1d13a800d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-01 08:06:07 +0000

    Add package snapctl/0.41.2/snapctl-0.41.2.tar.gz

---
+++ packages/snapctl/snapctl-0.41.2.tar.gz/snapctl-0.41.2/snapctl/config/endpoints.py
@@ -0,0 +1,9 @@
+"""
+This file contains the endpoints for the Snapser API.
+"""
+END_POINTS = {
+    'DEV': 'https://gateway.dev.snapser.io/snapser',
+    'DEV_TWO': 'https://gateway.dev.snapser.io/devtwo',
+    'PLAYTEST': 'https://gateway.dev.snapser.io/playtest',
+    'PROD': 'https://gateway.snapser.com/snapser'
+}
