85ab8736395436c8c906dbe80a7a66f7e92466e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-01 14:05:33 +0000

    Add package chaturbate-poller/0.13.12/chaturbate_poller-0.13.12.tar.gz

---
+++ packages/chaturbate-poller/chaturbate_poller-0.13.12.tar.gz/chaturbate_poller-0.13.12/tests/test_config_manager.py
@@ -0,0 +1,10 @@
+from chaturbate_poller.config_manager import ConfigManager
+
+
+class TestConfigManager:
+    """Tests for the config module."""
+
+    def test_get_with_default(self) -> None:
+        """Test the get method with a default value."""
+        config_manager = ConfigManager()
+        assert config_manager.get("NON_EXISTENT_KEY", "default_value") == "default_value"
