248026fdc92dab1b555b96c9e5c0b11d7eb53696
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-01 16:05:01 +0000

    Add package JoelAntony-LibraryManager-BPS-Doha/2.0.0/JoelAntony_LibraryManager_BPS_Doha-2.0.0-py3-none-any.whl

---
+++ packages/JoelAntony-LibraryManager-BPS-Doha/JoelAntony_LibraryManager_BPS_Doha-2.0.0-py3-none-any.whl/library_management_system/__init__.py
@@ -0,0 +1,8 @@
+
+from .main import Library, LibraryGUI
+
+# Package version
+__version__ = "0.1.0"
+
+# Optionally define what gets imported when using 'from library_management_system import *'
+__all__ = ['Library', 'LibraryGUI']
