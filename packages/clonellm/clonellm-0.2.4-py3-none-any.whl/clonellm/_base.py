fbce07c9708ccafe818b86172aae3469e4d0a52e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-01 03:17:24 +0000

    Add package clonellm/0.2.4/clonellm-0.2.4-py3-none-any.whl

---
+++ packages/clonellm/clonellm-0.2.4-py3-none-any.whl/clonellm/_base.py
@@ -0,0 +1,24 @@
+from abc import ABCMeta
+from typing import Any, Optional
+
+from litellm import get_api_key, get_llm_provider
+
+__all__ = ("LiteLLMMixin",)
+
+
+class LiteLLMMixin(metaclass=ABCMeta):
+    def __init__(self, model: str, api_key: Optional[str] = None, **kwargs: Any) -> None:
+        self.model = model
+        self.api_key = api_key
+        self._litellm_kwargs: dict[str, Any] = kwargs
+
+    @property
+    def _llm_provider(self) -> str:
+        return get_llm_provider(model=self.model)[1]  # type: ignore[no-any-return]
+
+    @property
+    def _api_key(self):
+        if self.api_key:
+            return self.api_key
+        else:
+            return get_api_key(llm_provider=self._llm_provider, dynamic_api_key=self.api_key)
