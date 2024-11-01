ba3e9c5c5c86d7a169f6c6ee9641c79653dc1d4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-01 08:05:52 +0000

    Add package pulse-cli/1.0.1/pulse_cli-1.0.1.tar.gz

---
+++ packages/pulse-cli/pulse_cli-1.0.1.tar.gz/pulse_cli-1.0.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pulse-cli',
+    version='1.0.1',
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+    ],
+    entry_points={
+        'console_scripts': [
+            'pulse=pulse_cli.cli:main',
+        ],
+    },
+    author='Petrobench',
+    author_email='support@petrobench.com',
+    description='A cross-platform CLI tool for managing the Pulse service',
+    url='https://your-repository-url.com',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: POSIX :: Linux',
+        'License :: OSI Approved :: MIT License',
+    ],
+    python_requires='>=3.6',
+)
