0095a92650b689f864382d5737ef948cf6a4be8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-01 06:06:16 +0000

    Add package wxbtool/0.1.3/wxbtool-0.1.3.tar.gz

---
+++ packages/wxbtool/wxbtool-0.1.3.tar.gz/wxbtool-0.1.3/setup.py
@@ -0,0 +1,52 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="wxbtool",
+    version="0.1.3",
+    author="Mingli Yuan",
+    author_email="mingli.yuan@gmail.com",
+    description="A toolkit for WeatherBench based on PyTorch",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/mountain/wxbtool",
+    project_urls={
+        "Documentation": "https://packaging.python.org/tutorials/distributing-packages/",
+        "Source": "https://github.com/mountain/wxbtool",
+        "Tracker": "https://github.com/mountain/wxbtool/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=setuptools.find_packages(),
+    python_requires=">=3.9",
+    install_requires=[
+        "torch",
+        "numpy",
+        "netcdf4",
+        "toolz",
+        "dask",
+        "xarray",
+        "opencv-python",
+        "arrow",
+        "python-decouple",
+        "leibniz",
+        "flask",
+        "msgpack",
+        "msgpack-numpy",
+        "requests",
+        "gunicorn",
+        "arghandler",
+    ],
+    test_suite="pytest",
+    tests_require=["pytest"],
+    entry_points={
+        "console_scripts": [
+            "wxb = wxbtool.wxb:main",
+        ]
+    },
+)
