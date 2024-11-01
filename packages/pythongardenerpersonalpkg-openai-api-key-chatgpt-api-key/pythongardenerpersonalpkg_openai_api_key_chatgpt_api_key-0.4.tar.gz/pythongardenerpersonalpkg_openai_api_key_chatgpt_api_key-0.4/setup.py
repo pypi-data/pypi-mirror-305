440095f65444fd5b7f4c60c46ace4f77dbdc4f88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-01 03:18:13 +0000

    Add package pythongardenerpersonalpkg-openai-api-key-chatgpt-api-key/0.4/pythongardenerpersonalpkg_openai_api_key_chatgpt_api_key-0.4.tar.gz

---
+++ packages/pythongardenerpersonalpkg-openai-api-key-chatgpt-api-key/pythongardenerpersonalpkg_openai_api_key_chatgpt_api_key-0.4.tar.gz/pythongardenerpersonalpkg_openai_api_key_chatgpt_api_key-0.4/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pythongardenerpersonalpkg-openai-api-key-chatgpt-api-key',
+    version='0.4',
+    packages=find_packages(),
+    description='A simple example package with openai api key',
+    author='Your Name',
+    author_email='your.email@example.com',
+    url='https://github.com/yourusername/pythongardenerpersonalpkg',  # Optional
+    install_requires=[],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
