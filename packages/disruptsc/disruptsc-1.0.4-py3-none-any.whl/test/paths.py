48bdc4ff37d908d0235d8ff6f92f9a964d444369
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-01 04:05:42 +0000

    Add package disruptsc/1.0.4/disruptsc-1.0.4-py3-none-any.whl

---
+++ packages/disruptsc/disruptsc-1.0.4-py3-none-any.whl/test/paths.py
@@ -0,0 +1,21 @@
+import pathlib
+import sys
+#logger = logging.getLogger(__name__)
+
+ROOT_FOLDER = pathlib.Path(__file__).parent.parent
+PARAMETER_FOLDER = ROOT_FOLDER / "parameter"
+OUTPUT_FOLDER = ROOT_FOLDER / "output"
+INPUT_FOLDER = ROOT_FOLDER / "input"
+TMP_FOLDER = ROOT_FOLDER / "tmp"
+
+sys.path.insert(1, str(ROOT_FOLDER))
+sys.path.insert(1, str(ROOT_FOLDER / "code" / "model"))
+sys.path.insert(1, str(ROOT_FOLDER / "code" / "parameters"))
+sys.path.insert(1, str(ROOT_FOLDER / "code" / "simualations"))
+# if __file__ == "__main__":
+#     print(ROOT_FOLDER)
+
+# code_directory = Path(os.path.abspath(__file__)).parent
+# project_directory = code_directory.parent
+# working_directory = Path(os.getcwd())
+# working_directory_parent = working_directory.parent
