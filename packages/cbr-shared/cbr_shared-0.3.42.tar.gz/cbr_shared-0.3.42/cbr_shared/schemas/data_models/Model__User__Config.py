91e32b4155f348927ed7e533d1250c1867bd65c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-01 03:17:06 +0000

    Add package cbr-shared/0.3.42/cbr_shared-0.3.42.tar.gz

---
+++ packages/cbr-shared/cbr_shared-0.3.42.tar.gz/cbr_shared-0.3.42/cbr_shared/schemas/data_models/Model__User__Config.py
@@ -0,0 +1,17 @@
+from dataclasses import dataclass
+
+from osbot_utils.base_classes.Type_Safe import Type_Safe
+from osbot_utils.helpers.Random_Guid    import Random_Guid
+from osbot_utils.utils.Misc             import random_text
+
+
+@dataclass
+class Model__User__Config(Type_Safe):
+    user_id    : Random_Guid
+    user_name  : str
+
+    @staticmethod
+    def random_user():
+        user_id   = Random_Guid()
+        user_name = random_text("an-random-user", lowercase=True)
+        return Model__User__Config(user_id=user_id, user_name=user_name)
