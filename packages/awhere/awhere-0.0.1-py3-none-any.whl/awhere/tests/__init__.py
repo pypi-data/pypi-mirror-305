2a21a6f59ba1103f193a54ff46b2218556dcf510
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-01 06:06:20 +0000

    Add package awhere/0.0.1/awhere-0.0.1-py3-none-any.whl

---
+++ packages/awhere/awhere-0.0.1-py3-none-any.whl/awhere/tests/__init__.py
@@ -0,0 +1,4 @@
+# Copyright 2024 Weavers @ Eternal Loom. All rights reserved.
+#
+# Use of this software is governed by the license that can be
+# found in LICENSE file in the source repository.
---
+++ packages/awhere/awhere-0.0.1-py3-none-any.whl/awhere/tests/test_mod.py
@@ -0,0 +1,17 @@
+"""
+Copyright 2024 Weavers @ Eternal Loom. All rights reserved.
+Use of this software is governed by the license that can be
+found in LICENSE file in the source repository.
+"""
+
+# Copyright 2023 Weavers @ Eternal Loom. All rights reserved.
+#
+# Use of this software is governed by the license that can be
+# found in LICENSE file in the source repository.
+
+
+def test_module_import():
+    """Test module import"""
+    import awhere
+
+    assert awhere is not None
