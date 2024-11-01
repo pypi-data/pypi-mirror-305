a9cf7942b1dcb102d10071a882df772a9ec5717c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-01 15:05:34 +0000

    Add package modern-di/0.3.0/modern_di-0.3.0-py3-none-any.whl

---
+++ packages/modern-di/modern_di-0.3.0-py3-none-any.whl/modern_di/providers/__init__.py
@@ -0,0 +1,15 @@
+from modern_di.providers.base import AbstractProvider, BaseCreatorProvider
+from modern_di.providers.context_adapter import ContextAdapter
+from modern_di.providers.factory import Factory
+from modern_di.providers.resource import Resource
+from modern_di.providers.singleton import Singleton
+
+
+__all__ = [
+    "AbstractProvider",
+    "BaseCreatorProvider",
+    "ContextAdapter",
+    "Factory",
+    "Singleton",
+    "Resource",
+]
