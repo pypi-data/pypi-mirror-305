b1cad4c9aec1d8b96bc64c9795886d5167761875
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-01 03:18:21 +0000

    Add package lydata/0.1.1/lydata-0.1.1-py3-none-any.whl

---
+++ packages/lydata/lydata-0.1.1-py3-none-any.whl/lydata/__init__.py
@@ -0,0 +1,33 @@
+"""Library for handling lymphatic involvement data."""
+
+import logging
+
+import lydata._version as _version
+from lydata.accessor import C, Q
+from lydata.loader import (
+    available_datasets,
+    join_datasets,
+    load_datasets,
+)
+from lydata.utils import enhance
+from lydata.validator import validate_datasets
+
+__author__ = "Roman Ludwig"
+__email__ = "roman.ludwig@usz.ch"
+__uri__ = "https://github.com/rmnldwg/lydata"
+__version__ = _version.__version__
+
+__all__ = [
+    "accessor",
+    "Q",
+    "C",
+    "available_datasets",
+    "join_datasets",
+    "load_datasets",
+    "validate_datasets",
+    "enhance",
+]
+
+logger = logging.getLogger(__name__)
+logger.addHandler(logging.NullHandler())
+logger.setLevel(logging.WARNING)
