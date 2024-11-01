3fa65cfdf023d78f69ea553031a3378bdd783c41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-01 15:06:13 +0000

    Add package unsloth-zoo/2024.11.0/unsloth_zoo-2024.11.0.tar.gz

---
+++ packages/unsloth-zoo/unsloth_zoo-2024.11.0.tar.gz/unsloth_zoo-2024.11.0/unsloth_zoo/__init__.py
@@ -0,0 +1,7 @@
+# Copyright (C) 2024-present the Unsloth AI team. All rights reserved.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+__version__ = "2024.11.0"
