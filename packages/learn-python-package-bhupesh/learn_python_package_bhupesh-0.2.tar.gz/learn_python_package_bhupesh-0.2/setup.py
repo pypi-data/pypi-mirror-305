4e69bc2d4a567f79aaf12b2968018ef14e73579e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-02 04:07:14 +0000

    Add package learn-python-package-bhupesh/0.2/learn_python_package_bhupesh-0.2.tar.gz

---
+++ packages/learn-python-package-bhupesh/learn_python_package_bhupesh-0.2.tar.gz/learn_python_package_bhupesh-0.2/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='learn_python_package_bhupesh',
+    version='0.2',
+    packages=find_packages(),
+    install_requires=[],  # List any dependencies here
+)
