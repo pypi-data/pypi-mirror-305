f07b878ba1b35560f23045162fa1fe98e5a80924
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-02 04:07:16 +0000

    Add package tree-ring-analyzer/0.1.1/tree_ring_analyzer-0.1.1.tar.gz

---
+++ packages/tree-ring-analyzer/tree_ring_analyzer-0.1.1.tar.gz/tree_ring_analyzer-0.1.1/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+
+# Function to read the requirements.txt file
+def read_requirements():
+    with open("requirements.txt") as f:
+        return f.read().splitlines()
+
+
+# Function to read the README.md file
+def read_readme():
+    with open("README.md", "r", encoding="utf-8") as f:
+        return f.read()
+
+
+setup(
+    name="tree-ring-analyzer",
+    version="0.1.1",
+    description="A package for tree ring detection in images",
+    long_description=read_readme(),
+    long_description_content_type="text/markdown",
+    author="Tony Meissner",
+    author_email="tonymeissner70@@gmail.com",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    install_requires=read_requirements(),
+    entry_points={
+        "console_scripts": [
+            "tree-ring-analyzer=main:main",
+        ],
+    },
+    python_requires=">=3.7",
+)
