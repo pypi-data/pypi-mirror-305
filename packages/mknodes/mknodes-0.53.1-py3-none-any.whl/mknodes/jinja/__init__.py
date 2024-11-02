f9a7f5091c800d0217f03eb5ac40945501c055ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-02 04:07:19 +0000

    Add package mknodes/0.53.1/mknodes-0.53.1-py3-none-any.whl

---
+++ packages/mknodes/mknodes-0.53.1-py3-none-any.whl/mknodes/jinja/__init__.py
@@ -0,0 +1,38 @@
+"""MkNodes jinja environment and helpers."""
+
+from __future__ import annotations
+
+import functools
+
+import jinjarope
+import tomli_w
+from jinjarope import inspectfilters, yamltools
+
+from mknodes import paths
+from mknodes.utils import classhelpers, inspecthelpers
+
+
+def setup_env(env: jinjarope.Environment):
+    """Used as extension point for the jinjarope environment.
+
+    Arguments:
+        env: The jinjarope environment to extend
+    """
+    node_klasses = get_nodes()
+    env.globals |= dict(mk=node_klasses, _mk=node_klasses)
+    env.globals |= {"inspecthelpers": inspecthelpers, "classhelpers": classhelpers}
+    path = paths.RESOURCES / "filters.toml"
+    env.load_jinja_file(path)
+    env.filters |= node_klasses
+    env.filters |= {
+        "dump_yaml": yamltools.dump_yaml,
+        "dump_toml": tomli_w.dumps,
+        "load_yaml": yamltools.load_yaml,
+    }
+
+
+@functools.cache
+def get_nodes():
+    import mknodes as mk
+
+    return {k.__name__: k for k in inspectfilters.list_subclasses(mk.MkNode)}
