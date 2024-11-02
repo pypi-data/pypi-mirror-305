3d9cb9fc351d40ce17ad89ee0be0bb87b52d44f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-02 03:09:18 +0000

    Add package pandapos-api/1.0.2/pandapos-api-1.0.2.tar.gz

---
+++ packages/pandapos-api/pandapos-api-1.0.2.tar.gz/pandapos-api-1.0.2/panda_pos_api/models/Ticket.py
@@ -0,0 +1,85 @@
+
+
+
+
+
+from typing import List, Optional
+from pydantic import BaseModel
+
+
+class OrderState(BaseModel):
+    stateName: Optional[str]
+    state: Optional[str]
+    stateValue: Optional[str]
+
+class OrderTag(BaseModel):
+    tagName: Optional[str]
+    tag: Optional[str]
+    price: float
+    rate: Optional[float]
+    quantity: float
+    note: Optional[str]
+    
+
+class Order(BaseModel):
+    id: int
+    uid: str
+    name: Optional[str]
+    menuItemName: Optional[str]
+    quantity: float
+    enforceQuantity: bool
+    price: float
+    portion: Optional[str]
+    calculatePrice: bool
+    decreaseInventory: bool
+    increaseInventory: bool
+    states: Optional[list[OrderState]]
+    tags: Optional[list[OrderTag]]
+
+
+class TicketEntity(BaseModel):
+    entityType: Optional[str]
+    name: Optional[str]
+
+
+class TicketState(BaseModel):
+    stateName: Optional[str]
+    state: Optional[str]
+    stateValue: Optional[str]
+    quantityExp: Optional[str]
+
+class TicketTag(BaseModel):
+    tagName: Optional[str]
+    tag: Optional[str]
+
+class TicketCalculation(BaseModel):
+    name: Optional[str]
+    amount: float
+
+class Ticket(BaseModel):
+    
+    id: Optional[int]
+    uid: Optional[str]
+    type: Optional[str]
+    date: Optional[str]
+    department: Optional[str]
+    terminal: Optional[str]
+    user: Optional[str]
+    number: Optional[str]
+    note: Optional[str]
+    orders: Optional[list[Order]]
+    entities: Optional[list[TicketEntity]]
+    tags: Optional[list[TicketTag]]
+    calculations: Optional[list[TicketCalculation]]
+
+
+class EntityCustomDataInputType(BaseModel):
+    name: str
+    value: str
+
+
+class Entity(BaseModel):
+    entityType: str
+    name: str
+    customData: Optional[List[EntityCustomDataInputType]]
+    
