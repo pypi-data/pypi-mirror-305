940f4a7a9a429e62b5846d097cff7760c2e5a0c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-02 04:06:22 +0000

    Add package pg-httpserver/1.1.8/pg_httpserver-1.1.8-py3-none-any.whl

---
+++ packages/pg-httpserver/pg_httpserver-1.1.8-py3-none-any.whl/pg_httpserver/__init__.py
@@ -0,0 +1,5 @@
+from pg_httpserver.fapi import run, app, CODE_VERSION
+from pg_httpserver.define import ENV_HANDLER_DIR, httpserver_init, \
+    ENV_NEEDS_BODY_MIDDLEWARE
+from pg_httpserver.manager import GameConfigManager, GamePropertyManager
+VERSION = "1.1.8"
