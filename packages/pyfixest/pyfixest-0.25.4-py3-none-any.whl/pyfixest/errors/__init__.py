fc9420ec8d5bab327a07c04ad9197c36ead79b6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-02 03:10:40 +0000

    Add package pyfixest/0.25.4/pyfixest-0.25.4-py3-none-any.whl

---
+++ packages/pyfixest/pyfixest-0.25.4-py3-none-any.whl/pyfixest/errors/__init__.py
@@ -0,0 +1,78 @@
+class FixedEffectInteractionError(Exception):  # noqa: D101
+    pass
+
+
+class CovariateInteractionError(Exception):  # noqa: D101
+    pass
+
+
+class DuplicateKeyError(Exception):  # noqa: D101
+    pass
+
+
+class EndogVarsAsCovarsError(Exception):  # noqa: D101
+    pass
+
+
+class InstrumentsAsCovarsError(Exception):  # noqa: D101
+    pass
+
+
+class UnderDeterminedIVError(Exception):  # noqa: D101
+    pass
+
+
+class UnsupportedMultipleEstimationSyntax(Exception):  # noqa: D101
+    pass
+
+
+class VcovTypeNotSupportedError(Exception):  # noqa: D101
+    pass
+
+
+class NanInClusterVarError(Exception):  # noqa: D101
+    pass
+
+
+class DepvarIsNotNumericError(Exception):  # noqa: D101
+    pass
+
+
+class NotImplementedError(Exception):  # noqa: D101
+    pass
+
+
+class NonConvergenceError(Exception):  # noqa: D101
+    pass
+
+
+class MatrixNotFullRankError(Exception):  # noqa: D101
+    pass
+
+
+class EmptyDesignMatrixError(Exception):  # noqa: D101
+    pass
+
+
+class FeatureDeprecationError(Exception):  # noqa: D101
+    pass
+
+
+__all__ = [
+    "FixedEffectInteractionError",
+    "CovariateInteractionError",
+    "DuplicateKeyError",
+    "EndogVarsAsCovarsError",
+    "InstrumentsAsCovarsError",
+    "UnderDeterminedIVError",
+    "UnsupportedMultipleEstimationSyntax",
+    "VcovTypeNotSupportedError",
+    "MultiEstNotSupportedError",
+    "NanInClusterVarError",
+    "DepvarIsNotNumericError",
+    "NotImplementedError",
+    "NonConvergenceError",
+    "MatrixNotFullRankError",
+    "EmptyDesignMatrixError",
+    "FeatureDeprecationError",
+]
