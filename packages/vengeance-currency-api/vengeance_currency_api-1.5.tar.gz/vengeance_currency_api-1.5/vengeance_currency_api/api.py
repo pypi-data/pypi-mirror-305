9e0e5e3503903706a6c65325c68878b94870de02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-02 03:09:53 +0000

    Add package vengeance-currency-api/1.5/vengeance_currency_api-1.5.tar.gz

---
+++ packages/vengeance-currency-api/vengeance_currency_api-1.5.tar.gz/vengeance_currency_api-1.5/vengeance_currency_api/api.py
@@ -0,0 +1,34 @@
+from flask import Flask, jsonify
+import json
+import os
+
+class CurrencyAPI:
+    def __init__(self):
+        self.app = Flask(__name__)
+        self.load_currency_data()
+        self.setup_routes()
+
+    def load_currency_data(self):
+        # Use the directory of the running script
+        current_dir = os.path.dirname(os.path.abspath(__file__))
+        json_path = os.path.join(current_dir, '..', 'currencies.json')  # Adjust path
+        
+        with open(json_path, 'r', encoding='utf-8') as file:
+            self.currency_data = json.load(file)
+        self.currency_dict = {item["Country"]: {"symbol": item["Symbol"], "code": item["Code"]} for item in self.currency_data}
+
+    def setup_routes(self):
+        @self.app.route('/currency/<country>', methods=['GET'])
+        def get_currency(country):
+            currency = self.currency_dict.get(country)
+            if currency:
+                return jsonify({country: currency}), 200
+            else:
+                return jsonify({"error": "Country not found"}), 404
+
+        @self.app.route('/currencies', methods=['GET'])
+        def get_all_currencies():
+            return jsonify(self.currency_dict), 200
+
+    def run(self, **kwargs):
+        self.app.run(**kwargs)
