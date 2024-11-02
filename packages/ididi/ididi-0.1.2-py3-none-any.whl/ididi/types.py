fadc3025318d8f3bb4377c4125cda4505f71e65f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-02 04:06:29 +0000

    Add package ididi/0.1.2/ididi-0.1.2-py3-none-any.whl

---
+++ packages/ididi/ididi-0.1.2-py3-none-any.whl/ididi/types.py
@@ -0,0 +1,20 @@
+import typing as ty
+from dataclasses import dataclass
+
+type T_Factory[I, **P] = ty.Callable[P, I] | type[I]
+type TDecor[T_Factory] = ty.Callable[[T_Factory], T_Factory]
+
+
+class INodeConfig(ty.TypedDict, total=False):
+    """
+    Only used for type hinting, typing.Unpack[INodeConfig]
+    For actual config, use NodeConfig
+    """
+
+    reuse: bool
+    # lazy: bool 
+
+
+@dataclass(kw_only=True, frozen=True, slots=True, unsafe_hash=True)
+class NodeConfig:
+    reuse: bool = True
