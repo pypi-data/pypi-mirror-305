a21e79717a562ea0bb913cbf3fab5dfe7502db62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-02 03:10:02 +0000

    Add package km-plugin-feishu/1.1/km_plugin_feishu-1.1.tar.gz

---
+++ packages/km-plugin-feishu/km_plugin_feishu-1.1.tar.gz/km_plugin_feishu-1.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(name='km-plugin-feishu',
+      version='1.1',
+      install_requires=[
+          'km-sdk',
+          'requests',
+      ],
+      dependency_links=[
+          'https://pipy.cloudladder.net.cn/repository/km/simple',
+      ],
+      entry_points={
+          "knowledge_management_plugin": [
+              'FEISHU = km_plugin_feishu.feishu_api:FeiShuApi'
+          ]
+      },
+      packages=find_packages(),
+      description='知识管理系统插件-飞书系统对接',
+      author='KnowledgeManagement',
+      )
