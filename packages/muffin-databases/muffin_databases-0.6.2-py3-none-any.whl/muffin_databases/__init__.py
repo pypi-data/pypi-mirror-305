82eb8bcab8e8ef5dcbdf5fc39b06fea0b587602d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-02 03:10:24 +0000

    Add package muffin-databases/0.6.2/muffin_databases-0.6.2-py3-none-any.whl

---
+++ packages/muffin-databases/muffin_databases-0.6.2-py3-none-any.whl/muffin_databases/__init__.py
@@ -0,0 +1,51 @@
+"""Support databases for Muffin framework."""
+
+from typing import Any, ClassVar
+
+from databases import Database
+from muffin.plugins import BasePlugin
+
+
+class Plugin(BasePlugin):
+
+    """Manage databases connections for Muffin."""
+
+    name = "databases"
+    defaults: ClassVar = {
+        "params": {},
+        "url": "sqlite:///:memory:",
+    }
+
+    def __init__(self, *args, **kwargs):
+        """Initialize a database."""
+        self.__database__ = None
+        super(Plugin, self).__init__(*args, **kwargs)
+
+    def setup(self, *args, **options):
+        """Initialize a database."""
+        super(Plugin, self).setup(*args, **options)
+
+        self.__database__ = Database(self.cfg.url, **self.cfg.params)
+
+    @property
+    def database(self):
+        """The database property."""
+        if self.__database__ is None:
+            raise RuntimeError("Plugin is not initialized.")
+
+        return self.__database__
+
+    def __getattr__(self, name: str) -> Any:
+        """Proxy attributes to self database."""
+        if name == "middleware":
+            return object.__getattribute__(self, name)
+
+        return getattr(self.database, name)
+
+    async def startup(self):
+        """Disconnect the database."""
+        await self.database.connect()
+
+    async def shutdown(self):
+        """Disconnect the database."""
+        await self.database.disconnect()
