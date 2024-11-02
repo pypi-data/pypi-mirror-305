a0d1473967d89dc0745f8edc14b8086c560beb2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-02 03:10:39 +0000

    Add package JomansHelper/0.1/jomanshelper-0.1.tar.gz

---
+++ packages/JomansHelper/jomanshelper-0.1.tar.gz/jomanshelper-0.1/setup.py
@@ -0,0 +1,13 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name="JomansHelper",
+    version="0.1",
+    packages=find_packages(),  
+    description="Jomans Helper",
+    author="Dein Name",
+    author_email="deinname@example.com",
+#    url="https://github.com/username/my_package",  
+    install_requires=[],
+)
