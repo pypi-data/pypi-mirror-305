949603fad32364cf913a4d30ba5e44ccbb3e27a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-02 03:10:21 +0000

    Add package bihc/0.0.8.10/bihc-0.0.8.10.tar.gz

---
+++ packages/bihc/bihc-0.0.8.10.tar.gz/bihc-0.0.8.10/bihc/__init__.py
@@ -0,0 +1,11 @@
+from . import power
+from . import plot
+from . import impedance
+from . import beam
+
+
+from .beam import Beam
+from .impedance import Impedance
+
+from .plot import progressbar
+
---
+++ packages/bihc/bihc-0.0.8.10.tar.gz/bihc-0.0.8.10/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup
+
+####################################
+# Add long description from README #
+####################################
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+
+#########
+# Setup #
+#########
+
+setup(
+    name="bihc",
+    version="0.0.8.10",
+    description="BIHC: Beam-Induced Heating Computation package",
+    author="Elena de la Fuente, Francesco Giordano, Leonardo Sito",
+    author_email="benoit.salvant@cern.ch", 
+    packages=['bihc'],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/LeonardoSito/BIHC",
+    project_urls={"Bug Tracker": "https://github.com/LeonardoSito/BIHC/issues"},
+    install_requires = [
+                    'numpy',
+                    'matplotlib',
+                    'scipy',
+                       ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+
+)
