a7393ed90080a966873f01d7dc7af0c7c6d9e827
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-02 03:09:23 +0000

    Add package blue-brain-token-fetch/3.0.2/blue_brain_token_fetch-3.0.2.tar.gz

---
+++ packages/blue-brain-token-fetch/blue_brain_token_fetch-3.0.2.tar.gz/blue_brain_token_fetch-3.0.2/blue_brain_token_fetch/__init__.py
@@ -0,0 +1,10 @@
+"""
+Version corresponding to the git version tag
+"""
+from pkg_resources import get_distribution, DistributionNotFound
+
+try:
+    __version__ = get_distribution(__name__).version
+except DistributionNotFound:
+    # package is not installed
+    pass
