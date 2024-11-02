fb775ba43c2bd25d7d3b07249fdf1c89f5a48920
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-02 04:07:19 +0000

    Add package mknodes/0.53.1/mknodes-0.53.1.tar.gz

---
+++ packages/mknodes/mknodes-0.53.1.tar.gz/mknodes-0.53.1/mknodes/info/yamlfile.py
@@ -0,0 +1,71 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from jinjarope import serializefilters, yamltools
+import upath
+
+from mknodes.info import configfile
+from mknodes.utils import log
+
+
+if TYPE_CHECKING:
+    import os
+
+
+logger = log.get_logger(__name__)
+
+
+class YamlFile(configfile.ConfigFile):
+    def __init__(
+        self,
+        path: str | os.PathLike[str] | None = None,
+        mode: yamltools.LoaderStr = "unsafe",
+        resolve_inherit_tag: bool = False,
+    ):
+        super().__init__(path)
+        if resolve_inherit_tag:
+            self.resolve_inherit_tag(mode)
+
+    def resolve_inherit_tag(
+        self,
+        mode: yamltools.LoaderStr = "unsafe",
+    ):
+        """Resolve INHERIT key-value pair for this YAML file.
+
+        If this YAML file contains a key-value pair like "INHERIT: path_to_config.yml",
+        this method will resolve that tag by using the config at given path as the
+        "parent config".
+
+        Also supports a list of files for INHERIT.
+
+        Arguments:
+            mode: The Yaml loader type
+        """
+        if not self.path:
+            msg = "Config file needs file path (INHERIT path is relative to file path)"
+            raise ValueError(msg)
+        abspath = upath.UPath(self.path).absolute()
+        if "INHERIT" not in self._data:
+            return
+        file_path = self._data.pop("INHERIT")
+        file_paths = [file_path] if isinstance(file_path, str) else file_path
+        for path in file_paths:
+            parent_cfg = abspath.parent / path
+            logger.debug("Loading inherited configuration file: %s", parent_cfg)
+            text = parent_cfg.read_text("utf-8")
+            parent = yamltools.load_yaml(text, mode)
+            self._data = serializefilters.merge(parent, self._data)
+
+    @classmethod
+    def _dump(cls, data: dict) -> str:
+        return yamltools.dump_yaml(data)
+
+    @classmethod
+    def _load(cls, data: str, mode: yamltools.LoaderStr = "unsafe") -> dict | list:
+        return yamltools.load_yaml(data, mode)
+
+
+if __name__ == "__main__":
+    info = YamlFile(".pre-commit-config.yaml")
+    print(info)
