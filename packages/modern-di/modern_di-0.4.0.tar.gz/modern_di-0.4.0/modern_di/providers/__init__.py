0e3c3dbec9e30b4f98d6bb6be5d446c94f29142f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-02 03:09:57 +0000

    Add package modern-di/0.4.0/modern_di-0.4.0.tar.gz

---
+++ packages/modern-di/modern_di-0.4.0.tar.gz/modern_di-0.4.0/modern_di/providers/__init__.py
@@ -0,0 +1,16 @@
+from modern_di.providers.dict import Dict
+from modern_di.providers.factory import Factory
+from modern_di.providers.list import List
+from modern_di.providers.resource import Resource
+from modern_di.providers.selector import Selector
+from modern_di.providers.singleton import Singleton
+
+
+__all__ = [
+    "Factory",
+    "Dict",
+    "List",
+    "Selector",
+    "Singleton",
+    "Resource",
+]
